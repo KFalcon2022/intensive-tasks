42ca931f98e862ebad17f5e0dea69cb342e4629c
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-11-19 01:11:11 +0300

    commit1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,15 +5,20 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         int age = 0;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (age % 10 == 1 && age != 11 && age != 12 && age != 13 && age != 14) {
+            return "Вам " + age + " год.";
+        } else if ((age % 10 == 2 || age % 10 == 3 || age % 10 == 4) && (age != 11 && age != 12 && age != 13 && age != 14)) {
+            return "Вам " + age + " года.";
+        } else {
+            return "Вам " + age + " лет.";
+        }
     }
 }
