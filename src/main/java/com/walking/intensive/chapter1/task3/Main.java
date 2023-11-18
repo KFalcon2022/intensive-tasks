cd17919ee3d36e1921b7c9e0dcff05e953a75f07
BigTzar <realbigtzar@gmail.com>
2023-11-19 01:19:44 +0300

    Решение задачи 3 раздела 1

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,12 +6,17 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+        for (int i=1;i<3000;i++)
+            System.out.println("" + i + " год "+(isLeap(i)?"високосный":"невисокосный"));
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+//        год, номер которого кратен 400, — високосный;
+//        другие года, номера которых кратны 100, — невисокосные;
+//        другие года, номер которых кратен 4, — високосные;
+//        остальные года — невисокосные.
+        if (year%400==0) {return true;}
+        if (year%100==0) {return false;}
+        return year % 4 == 0;
     }
 }
