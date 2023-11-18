202817e6c3627cc6b73a917d3f18e383d41ec598
Константин Синяков <k.siniakov@yandex.ru>
2023-11-19 01:20:58 +0300

    Интенсив. Решение задачи 3 из раздела 1

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,12 +6,21 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        for (int i = 1889; i < 2024; i++) {
+            System.out.println("Год " + i + " " + (isLeap(i) ? "високосный" : "не вискосный"));
+        }
 
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
+        if (year % 100 == 0) {
+            return false;
+        }
 
-        return false; // Заглушка. При реализации - удалить
+        if (year % 4 == 0) {
+            return true;
+        }
+
+        return false;
     }
 }
