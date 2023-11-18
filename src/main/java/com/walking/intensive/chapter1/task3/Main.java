bf05d814b8afe64da56a523fb3a6f95ac2618ccb
Константин Синяков <k.siniakov@yandex.ru>
2023-11-19 01:26:06 +0300

    Интенсив. Решение задачи 3 из раздела 1. Исправления

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -13,14 +13,14 @@ public class Main {
     }
 
     static boolean isLeap(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+
         if (year % 100 == 0) {
             return false;
         }
 
-        if (year % 4 == 0) {
-            return true;
-        }
-
-        return false;
+        return year % 4 == 0;
     }
 }
