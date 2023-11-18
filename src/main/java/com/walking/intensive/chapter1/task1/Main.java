64813df0ceec5c0bd6e2f98c4544d01e97c2b3e9
ааа <@d.d>
2023-11-18 16:11:21 +0400

    Tasks 1-5

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,7 +1,19 @@
 package com.walking.intensive.chapter1.task1;
 
+/**
+ * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
+ */
 public class Main {
     public static void main(String[] args) {
+//        Для собственных проверок можете делать любые изменения в этом методе
+        int age = 0;
 
+        System.out.println(getAgeString(age));
+    }
+
+    static String getAgeString(int age) {
+//        Место для вашего кода
+
+        return null; // Заглушка. При реализации - удалить
     }
 }
--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -1,7 +1,33 @@
 package com.walking.intensive.chapter1.task4;
 
+/**
+ * Условие: <a href="https://geometry-math.ru/homework/Java-parameter.html">ссылка</a>
+ */
 public class Main {
     public static void main(String[] args) {
+//        Для собственных проверок можете делать любые изменения в этом методе
+        double a = 0;
+        double b = 0;
+        double c = 0;
 
+        System.out.println(solveQuadraticEquation(a, b, c));
+
+    }
+
+    /**
+     * При формировании строки, корни уравнения должны указываться по возрастанию.
+     * <p>
+     * Примеры результирующей строки:
+     * <p>
+     * Количество решений: 2. Корни: -4;4
+     * <p>
+     * Количество решений: 1. Корень: 0
+     * <p>
+     * Количество решений: 0.
+     */
+    static String solveQuadraticEquation(double a, double b, double c) {
+        //        Место для вашего кода
+
+        return null; // Заглушка. При реализации - удалить
     }
 }
