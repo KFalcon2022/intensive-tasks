b92d05009c399ebf2b2f82749054d608b1e29c28
i5t4b3r <i5t4b3r@gmail.com>
2023-11-19 02:01:19 +0300

    end task: chapter1.task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,17 +1,57 @@
 package com.walking.intensive.chapter1.task2;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество этажей в здании: ");
+        int floorAmount = scanner.nextInt();
+
+        System.out.print("Введите количество подъездов в здании: ");
+        int entranceAmount = scanner.nextInt();
+
+        System.out.print("Введите нужную квартиру: ");
+        int flatNumber = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int apartmentsEntranceCount = floorAmount * 4;
+        int apartmentsCount = apartmentsEntranceCount * entranceAmount;
+        int entrancesCount = (flatNumber - 1) / apartmentsEntranceCount + 1;
+        final int apartmentsQuantityFloor = 4;
+        String message;
 
-        return null; // Заглушка. При реализации - удалить
+
+        if (flatNumber > apartmentsCount) {
+            return message = "err";
+        }
+
+        int floorCount;
+
+        if (flatNumber % apartmentsQuantityFloor == 0) {
+            floorCount = (flatNumber / apartmentsQuantityFloor) - floorAmount * (entrancesCount - 1);
+        } else {
+            floorCount = (flatNumber / apartmentsQuantityFloor + 1) - floorAmount * (entrancesCount - 1);
+        }
+
+        String location = switch (flatNumber % apartmentsQuantityFloor) {
+            case (0) -> "справа от лифта, вправо";
+            case (1) -> "слева от лифта, влево";
+            case (2) -> "слева от лифта, вправо";
+            case (3) -> "справа от лифта, влево";
+            default -> "";
+        };
+
+        return message = entrancesCount + " подъезд, " + floorCount + " этаж, " + location;
     }
 }
+
