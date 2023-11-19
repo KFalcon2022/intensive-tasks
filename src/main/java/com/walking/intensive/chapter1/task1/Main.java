644e3df754673a00a2ae0c222709fad6445b5cf8
njmist <njmist@gmail.com>
2023-11-19 14:12:26 +0300

    Программа выводит в консоль "Вам n лет"

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,23 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 56;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 //        Место для вашего кода
+        int remainder = age % 100;
 
-        return null; // Заглушка. При реализации - удалить
+        if (age < 0 || age > 127) {
+            return "Это врядли";
+        } else if (remainder == 1 || remainder == 21) {
+            return "Вам " + age + " год";
+        } else if (remainder >= 2 && remainder < 5 || remainder > 21 && remainder <= 24) {
+            return "Вам " + age + " года";
+        } else {
+            return "Вам " + age + " лет";
+        }
     }
 }
