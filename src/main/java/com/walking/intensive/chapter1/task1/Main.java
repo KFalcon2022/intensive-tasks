15d1861f95dc37f645886b725a55d199c32c81f2
cranium1983 <shagen.arustamov@gmail.com>
2023-11-19 18:58:43 +0300

    возвращаю int age = 0; в master-ветке

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 5;
+        int age = 0;
 
         System.out.println(getAgeString(age));
     }
