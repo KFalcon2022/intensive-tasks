9b36774b62a93641b9c136106c9a274eda909ace
ganka-xn <jxn@inbox.ru>
2023-11-19 21:04:26 +0300

    task3

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -3,14 +3,34 @@ package com.walking.intensive.chapter1.task3;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-leap-year.html">ссылка</a>
  */
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        for (int i = 1598; i < 2025; i++)
+        {
+            String leap;
 
+            if (isLeap(i))
+                leap = "високосный";
+            else
+                leap = "обычный";
+
+            System.out.println(i + " - " + leap + " год");
+        }
     }
 
-    static boolean isLeap(int year) {
+    static boolean isLeap(int year)
+    {
         //        Место для вашего кода
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0 && (year / 100) % 4 != 0)
+                return false;
+            else
+                return true;
+        }
 
         return false; // Заглушка. При реализации - удалить
     }
