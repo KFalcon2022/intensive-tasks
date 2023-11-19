e4eeff58347521c8789dbec3af58882ea2f23ce5
ganka-xn <jxn@inbox.ru>
2023-11-19 19:42:23 +0300

    task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -3,13 +3,20 @@ package com.walking.intensive.chapter1.task2;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+        for (int i = 1; i < 52; i++)
+        {
+            System.out.println(getFlatLocation(4, 3, i));
+        }
+        System.out.println();
     }
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
+    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber)
+    {
         //        Место для вашего кода
 
         return null; // Заглушка. При реализации - удалить
