642776ab63b7efaed0d4cdeb1262c2d7d0320fa6
mayalange <@maya_lange>
2023-11-19 14:16:30 +0300

    added solution for task1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,22 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 67;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        if (5 <= age && age <= 20) {
+            return "Вам " + age + " лет";
+        } else if (age % 10 == 1) {
+            return "Вам " + age + " год";
+        } else if (age % 10 >= 2 && age % 10 <= 4) {
+            return "Вам " + age + " года";
+        } else {
+            return "Вам " + age + " лет";
+        }
 
-        return null; // Заглушка. При реализации - удалить
     }
 }
+
