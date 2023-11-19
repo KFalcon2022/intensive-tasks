7296f9157560f40864f549093158a53c9b68e1c3
ETSphere <Elem174@proton.me>
2023-11-19 18:26:57 +0500

    task 1 - done?

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,21 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = -4;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age > 0) {
+            if (age == 1) {
+                return "Вам 1 год";
+            } else if (age >= 2 & age < 5) {
+                return "Вам " + age + " года";
+            }
+        } else if (age < 0) {
+            return "Число лет не может быть отрицательным, проверьте ваши данные";
+        }
+        return "Вам " + age + " лет";
     }
 }
