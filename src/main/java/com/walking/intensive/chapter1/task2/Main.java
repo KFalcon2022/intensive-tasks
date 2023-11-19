e29d4e039c76f9b26fb137dc64f2d535b156b58b
EkkoXIII <astaroth644@gmail.com>
2023-11-19 12:16:33 +0300

    Реализовал поиск расположения квартиры

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,17 +1,63 @@
 package com.walking.intensive.chapter1.task2;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество подъездов: ");
+        int entrancesQuantity = scanner.nextInt();
 
-    }
+        System.out.print("Введите количество этажей: ");
+        int floorsQuantity = scanner.nextInt();
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        System.out.print("Введите номер квартиры: ");
+        int apartNumber = scanner.nextInt();
 
-        return null; // Заглушка. При реализации - удалить
+        scanner.close();
+
+        int roomsPerFloor = 4;
+
+        if ((apartNumber < 1) || (apartNumber > (roomsPerFloor * floorsQuantity * entrancesQuantity))) {
+            System.out.println("Такой квартиры в доме нет");
+            return;
+        }
+
+        String apartLoc = null;
+        switch (apartNumber % roomsPerFloor) {
+            case 0:
+                apartLoc = "Справа от лифта, вправо";
+                break;
+            case 1:
+                apartLoc = "Слева от лифта, влево";
+                break;
+            case 2:
+                apartLoc = "Слева от лифта, вправо";
+                break;
+            case 3:
+                apartLoc = "Справа от лифта, влево";
+                break;
+        }
+
+        int entranceNum;
+
+        if (apartNumber % (roomsPerFloor * floorsQuantity) == 0) {
+            entranceNum = apartNumber / (roomsPerFloor * floorsQuantity);
+        } else {
+            entranceNum = apartNumber / (roomsPerFloor * floorsQuantity) + 1;
+        }
+
+        int floorNum;
+
+        if (apartNumber % roomsPerFloor == 0) {
+            floorNum = apartNumber / roomsPerFloor - (entranceNum - 1) * floorsQuantity;
+        } else {
+            floorNum = apartNumber / roomsPerFloor - (entranceNum - 1) * floorsQuantity + 1;
+        }
+
+        System.out.println(apartNumber + " кв - " + entranceNum + " подъезд, " + floorNum + " этаж, " + apartLoc);
     }
 }
