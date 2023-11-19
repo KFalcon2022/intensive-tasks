8e4a30eb1feea039445e75c6d5fc81d0392dc9a8
ETSphere <Elem174@proton.me>
2023-11-19 21:32:14 +0500

    Високосные года работают информацию как определять високосный ли год взята с интернета. if / else оптимизаровала IDEa

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,12 +6,18 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        int testYear = 2023;
+        boolean isTestYearLeap = isLeap(testYear);
 
+        if (isTestYearLeap) {
+            System.out.printf("Год %d - это високосный год.", testYear);
+        } else {
+            System.out.printf("Год %d - это не високосный год.", testYear);
+        }
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
+
 }
