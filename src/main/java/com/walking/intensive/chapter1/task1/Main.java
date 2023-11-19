dbe739a81791c0cb6147ec6cbe519b0cac1614b5
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-11-19 15:14:15 +0300

    commit1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -3,22 +3,26 @@ package com.walking.intensive.chapter1.task1;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
-public class Main {
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-        int age = 0;
+        int age = 32;  //Ответ 32 года, все верно.
 
         System.out.println(getAgeString(age));
-    }
-
-    static String getAgeString(int age) {
-
-        if (age % 10 == 1 && age != 11 && age != 12 && age != 13 && age != 14) {
-            return "Вам " + age + " год.";
-        } else if ((age % 10 == 2 || age % 10 == 3 || age % 10 == 4) && (age != 11 && age != 12 && age != 13 && age != 14)) {
-            return "Вам " + age + " года.";
-        } else {
-            return "Вам " + age + " лет.";
         }
-    }
-}
+
+static String getAgeString(int age) {
+
+        String result;
+
+        if (age <= 0) {
+        result = "Неверный возраст.";
+        } else if (age % 10 == 1 && age != 11 && age != 12 && age != 13 && age != 14) {
+        result = "Вам " + age + " год.";
+        } else if ((age % 10 == 2 || age % 10 == 3 || age % 10 == 4) && (age != 11 && age != 12 && age != 13 && age != 14)) {
+        result = "Вам " + age + " года.";
+        } else {
+        result = "Вам " + age + " лет.";
+        }
+        return result;
+        }
+        }
