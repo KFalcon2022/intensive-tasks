229afef70bc0076def362ac053fc2d2b9fd844b3
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-11-19 15:00:08 +0300

    commit1

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,40 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
 
+        System.out.println(getFlatLocation(2, 1, 2));
     }
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
+        String result;
+        int flatsPerFloor = 4;
+        int entrance = 1 + (flatNumber / (floorAmount * flatsPerFloor));
+        int floor = 1 + flatNumber / flatsPerFloor;
+
+        if ((flatNumber % (floorAmount * flatsPerFloor)) == 0) {       // Этот и следующие 2 блока if не обязательны, но я не справился с выводом формул, :(
+            entrance -= 1;                                             // Буду рад если выложите разбор этой задачи в будущем
+        }
+
+        if ((flatNumber % flatsPerFloor) == 0) {
+            floor -= 1;
+        }
+
+        if (floor > floorAmount) {
+            floor = floor - floorAmount * (entrance - 1);
+        }
+
+        if (flatNumber <= 0 || entranceAmount < entrance) {
+            result = "Такой квартиры в доме нет";
+        } else if (flatNumber % flatsPerFloor == 1) {
+            result = entrance + " подъезд, " + floor + " этаж, слева от лифта, влево";
+        } else if (flatNumber % flatsPerFloor == 2) {
+            result = entrance + " подъезд, " + floor + " этаж, слева от лифта, вправо";
+        } else if (flatNumber % flatsPerFloor == 3) {
+            result = entrance + " подъезд, " + floor + " этаж, справа от лифта, влево";
+        } else {
+            result = entrance + " подъезд, " + floor + " этаж, справа от лифта, вправо";
+        }
+        return result;
     }
 }
