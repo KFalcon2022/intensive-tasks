87002c9fa73b56542749cc289bf2408ccb8a2fd5
andu <panaserv.real@gmail.com>
2023-11-19 21:41:25 +0700

    task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,38 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
 
-        System.out.println(getAgeString(age));
+        for (int i = 0; i < 15; i++) {
+            int age = (int) Math.round(Math.random() * 130);
+            int lastNumber = getLostNumber(age);
+            System.out.printf("Вам %d %s\n", age, getAgeString(lastNumber));
+        }
+    }
+
+
+    static String getExceptionsHundred(int age) {
+        String resultString;
+        if (age >= 105 && age <= 120) {
+            resultString = "лет";
+        } else resultString = getAgeString(getLostNumber(age));
+        return resultString;
+    }
+
+    static int getLostNumber(int age) {
+        age %= 10;
+        return age;
     }
 
     static String getAgeString(int age) {
 //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        String ageString;
+        if (age > 1 && age < 5) {
+            ageString = "года";
+        } else if (((age >= 5) && (age <= 9)) || (age == 0)) {
+            ageString = "лет";
+        } else {
+            ageString = "год";
+        }
+        return ageString; // Заглушка. При реализации - удалить
     }
 }
