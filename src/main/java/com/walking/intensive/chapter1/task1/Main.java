89ed0827814251507db55c69a308d29a653dd05d
EkkoXIII <astaroth644@gmail.com>
2023-11-19 11:08:50 +0300

    Реализовал вывод возраста пользователя через метода getAgeString

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,51 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите свой возраст: ");
+        int age = scanner.nextInt();
+
+        scanner.close();
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        switch (age % 100) {
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20: {
+                return "Вам " + age + " лет";
+            }
+            default: {
+                switch (age % 10) {
+                    case 1:
+                        return "Вам " + age + " год";
+                    case 2:
+                    case 3:
+                    case 4: {
+                        return "Вам " + age + " года";
+                    }
+                    default: {
+                        return "Вам " + age + " лет";
+                    }
+                }
+            }
+        }
     }
 }
