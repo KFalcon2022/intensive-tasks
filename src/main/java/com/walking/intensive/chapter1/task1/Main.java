b4b2093e16d1b6039956fdfa7b64ad2549f9e79b
EkkoXIII <astaroth644@gmail.com>
2023-11-19 11:08:50 +0300

    Учел комментарии к решению.

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,42 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите свой возраст: ");
+        int age = scanner.nextInt();
+
+        scanner.close();
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        if (age < 0) {
+            return "Вы ввели неправильный возраст. Он должен быть больше нуля";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        if (((age % 100) > 10) && ((age % 100) < 20)) {
+            return "Вам " + age + " лет";
+        } else {
+            switch (age % 10) {
+                case 1:
+                    return "Вам " + age + " год";
+                case 2:
+                case 3:
+                case 4: {
+                    return "Вам " + age + " года";
+                }
+                default: {
+                    return "Вам " + age + " лет";
+                }
+            }
+        }
     }
 }
