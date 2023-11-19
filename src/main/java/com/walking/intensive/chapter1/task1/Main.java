d6b2f4935145af33553a2e8811f386bdea0cde00
TitLukich <Nef-92@yandex.ru>
2023-11-19 10:30:47 +0300

    Решение первой задачи первого занятия

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,30 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        scanner.close();
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        String result = "Вам " + age;
+        if ((age % 100 > 10 && age % 100 < 20) || (age % 10 == 0) || (age % 10 > 4)) {
+            result += " лет";
+        } else if (age % 10 == 1) {
+            result += " год";
+        } else {
+            result += " года";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        return result; // Заглушка. При реализации - удалить
     }
 }
