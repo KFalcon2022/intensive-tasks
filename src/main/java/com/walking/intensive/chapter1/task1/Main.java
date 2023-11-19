cf7da6b8634d6dfce83d6f17a2e1bfa050bd79b2
Greem4 <i@greem4.ru>
2023-11-19 13:36:15 +0300

    Chapter 1, Task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,28 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+     if (age % 10 == 1 && age % 100 != 11) {
+         return String.format("Вам %d год", age);
+     }
+     if (age % 10 >= 2 && age % 10 <= 4) {
+         return String.format("Вам %d года", age);
+     }
+     return String.format("Вам %d лет", age);
     }
 }
