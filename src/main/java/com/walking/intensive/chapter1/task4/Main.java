9d5404b84b662c5ee898bc927f5f577f2d45e6fa
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-11-19 20:22:47 +0300

    first realization task4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -2,6 +2,7 @@ package com.walking.intensive.chapter1.task4;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-parameter.html">ссылка</a>
+ * https://www.youtube.com/watch?v=ON9P6JnY4sU
  */
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,36 @@ public class Main {
         double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
+
+        a = 0;
+        b = 0;
+        c = 5;
+
+        System.out.println(solveQuadraticEquation(a, b, c));
+
+        a = 1;
+        b = 5;
+        c = 6;
+
+        System.out.println("        a = 1;\n" +
+                "        b = 5;\n" +
+                "        c = 6;" + solveQuadraticEquation(a, b, c));
+
+        a = 1;
+        b = -5;
+        c = 6;
+
+        System.out.println("        a = 1;\n" +
+                "        b = -5;\n" +
+                "        c = 6;" + solveQuadraticEquation(a, b, c));
+
+        a = 1;
+        b = -5;
+        c = -6;
+
+        System.out.println("        a = 1;\n" +
+                "        b = -5;\n" +
+                "        c = -6;" + solveQuadraticEquation(a, b, c));
 
     }
 
@@ -24,10 +55,39 @@ public class Main {
      * Количество решений: 1. Корень: 0
      * <p>
      * Количество решений: 0.
+     *
+     * a -
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if(a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество при a, b и с равных нулю.";
+        } else if(a == 0 && b == 0 && c == 5) {
+            return "Количество решений: 0.";
+        }
+
+        double discriminant = b * b - 4 * a * c;
+        int count = 0;
+        String result = "Количество решений: ";
+        double x1 = 0;
+        double x2 = 0;
+
+        if (discriminant == 0) {
+            count = 1;
+        } else if (discriminant > 0) {
+            count = 2;
+        } else {
+            result += (count + ".");
+        }
+
+        if (count >= 1) {
+            x1 = (-1 * b - Math.sqrt(discriminant) / 2 * a);
+            result += "1. Корень: " + (int)x1;
+        } else {
+            x2 = (-1 * b + Math.sqrt(discriminant) / 2 * a);
+            result = ";" + (int)x2;
+        }
+
+        return result;
     }
 }
