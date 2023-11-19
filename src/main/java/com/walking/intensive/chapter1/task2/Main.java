2dd66e72fe007728a6a4cf6c056ca5f8409d641d
lambo064 <tasha_lambo@mail.ru>
2023-11-19 17:25:16 +0300

    task 1

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,49 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        int floorAmount = 10;
+        int entranceAmount = 2;
+        int flatNumber = 45;
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber > (floorAmount * entranceAmount * 4)) {
+            return "такой квартиры в доме нет";
+        }
+
+        int flatFloor;
+        int flatEachEntrance = floorAmount * 4;
+        int flatEntrance;
+        String roomSideFromElevator;
+        String roomSide;
+
+        flatEntrance = countFloor(flatNumber, flatEachEntrance);
+
+        int restFlats = flatNumber - (flatEntrance - 1) * 4 * floorAmount;
+
+        flatFloor = countFloor(restFlats, 4);
+
+        int roomSideCount = restFlats - (flatFloor - 1) * 4;
+
+        if (roomSideCount == 1 || roomSideCount == 2)  {
+            roomSideFromElevator = "слева";
+        }
+        else roomSideFromElevator = "справа";
+
+        if (roomSideCount % 2 == 0 ) {
+            roomSide = "вправо";
+        }
+        else  roomSide = "влево";
+
+        return flatEntrance + " подъезд, " + flatFloor + " этаж, " + roomSideFromElevator + " от лифта, " + roomSide;
+    }
+
+    static int countFloor (int flatNumber, int flatEachEntrance ) {
+        if (flatNumber % flatEachEntrance > 0 ) {
+            return flatNumber / flatEachEntrance + 1;
+        }
+        else return flatNumber / flatEachEntrance;
     }
 }
