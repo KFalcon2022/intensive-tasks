6cc20ac2770e1fa889c61ed29cdcca8ac815f870
dmitryNet <d.evstafiev@crystals.ru>
2023-11-20 01:27:09 +0300

    task2 - solution

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,45 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        for (int i = 1; i <= 32; i++) {
+            System.out.println(getFlatLocation(3, 4, i));
+        }
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber <= 0 || flatNumber > floorAmount * entranceAmount * 4) {
+             return "Такой квартиры не существует в этом доме";
+        }
+
+        //квартира на этаже
+        int flatOnFloor = flatNumber % 4;
+        if (flatOnFloor == 0) {
+            flatOnFloor = 4;
+        }
+
+        //выбор направления
+        String direction = switch (flatOnFloor) {
+            case (1) -> "слева от лифта, влево";
+            case (2) -> "слева от лифта, вправо";
+            case (3) -> "справа от лифта, влево";
+            case (4) -> "справа от лифта, вправо";
+            default -> "";
+        };
+
+        //Подъезд
+        int entranceOfFlat = (flatNumber / (floorAmount * 4));
+        if (flatNumber % (floorAmount * 4) > 0) {
+            entranceOfFlat++;
+        }
+
+        //Этаж, где квартира
+        int floorOfFlat = (flatNumber - 1) / 4 % floorAmount + 1;
+
+        String format = "%s кв. – %s подъезд, %s этаж, %s";
+        return format.formatted(flatNumber, entranceOfFlat, floorOfFlat, direction);
+
     }
 }
