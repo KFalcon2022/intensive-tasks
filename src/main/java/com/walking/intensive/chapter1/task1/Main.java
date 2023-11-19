019a20ae51081a3018680e0056aab318fa0af03c
Jroslav Trofimov <Jroslav1985@gmail.com>
2023-11-19 15:14:41 +0500

    commit Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,15 +5,16 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 38;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+    	if (age % 10 >= 1 && age % 10 <= 4) {
+			return "Вам " + age + " года.";
+		} else {
+			return "Вам " + age + " лет.";
+		}
     }
 }
