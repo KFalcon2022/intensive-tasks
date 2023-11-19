5d2fb5aed98b00c5cf08a0aa9a643f7be55d08b2
Milyukov Sergey <serrguk@gmail.om>
2023-11-19 20:10:09 +0700

    Chapter 1, task 1 complete

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -4,16 +4,26 @@ package com.walking.intensive.chapter1.task1;
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
-    public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+    public static final String[] WORD_CASE = {"год", "года", "лет"};
 
-        System.out.println(getAgeString(age));
+    public static void main(String[] args) {
+
+        int ageOutOfBounds = 1;
+
+        System.out.println(getAgeString(ageOutOfBounds));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        String stringAge = String.valueOf(age);
+        int lastDigit = Integer.parseInt(stringAge.substring(stringAge.length() - 1));
+        if (age < 0 || age > 127) {
+            return "Ошибка, введите корректный возраст!";
+        } else if (age == 1 || age > 20 && lastDigit == 1) {
+            return String.format("Вам %d %s", age, WORD_CASE[0]);
+        } else if (age > 20 && lastDigit > 0 && lastDigit < 5) {
+            return String.format("Вам %d %s", age, WORD_CASE[1]);
+        } else {
+            return String.format("Вам %d %s", age, WORD_CASE[2]);
+        }
     }
 }
