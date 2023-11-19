706bdde942b7a92d96dc05ef9d12a00d3eabff23
Greem4 <i@greem4.ru>
2023-11-19 15:11:19 +0300

    chapter 1, task 2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,29 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        String location = getFlatLocation(1, 1, 4);
+        System.out.println(location);
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int flatsPerFloor = 4;
+        int totalFlats = floorAmount * entranceAmount * flatsPerFloor;
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber < 1 || flatNumber > totalFlats) {
+            return "Такой квартиры нет";
+        }
+
+        int flatIndex = flatNumber - 1;
+        int flatsPerEntrance = floorAmount * flatsPerFloor;
+
+        int entrance = flatIndex / flatsPerEntrance + 1;
+        int floor = (flatIndex % flatsPerEntrance) / flatsPerFloor + 1;
+        String[] positions = {
+                "слева от лифта, влево", "слева от лифта, вправо",
+                "справа от лифта, влево", "справа от лифта, вправо"};
+        String positionDescription = positions[flatIndex % flatsPerFloor];
+
+        return String.format("%d кв - %d подъезд, %d этаж, %s",flatNumber, entrance, floor, positionDescription);
     }
 }
