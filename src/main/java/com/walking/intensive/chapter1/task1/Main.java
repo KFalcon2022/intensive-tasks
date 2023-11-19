59131c190b8203427f050a5666befa1bbf5ed2e4
Kuba52rus <Kuba52rus@gmail.com>
2023-11-19 20:28:06 +0300

    First task from chapter 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,26 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
-        System.out.println(getAgeString(age));
+        for (int i = -2; i < 130; i++) {
+            int age = i;
+            System.out.println(getAgeString(age));
+        }
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        String years;
+        if (age < 0) {
+            years = " что неверно. Возраст не может быть отрицательным";
+        } else if (age % 100 > 4 && age % 100 < 21 || age == 11) {
+            years = " лет";
+        } else if (age % 10 == 1) {
+            years = " год";
+        } else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) {
+            years = " года";
+        } else
+            years = " лет";
+
+        return "Вам " + age + years;
     }
 }
