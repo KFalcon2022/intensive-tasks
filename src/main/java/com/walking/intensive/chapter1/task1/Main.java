f1f4770e908822b687b28f2ce06358433953060c
Jroslav Trofimov <Jroslav1985@gmail.com>
2023-11-19 20:17:03 +0500

    Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -4,17 +4,19 @@ package com.walking.intensive.chapter1.task1;
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
-    public static void main(String[] args) {
-        int age = 38;
+	public static void main(String[] args) {
+		int age = 21;
 
-        System.out.println(getAgeString(age));
-    }
+		System.out.println(getAgeString(age));
+	}
 
-    static String getAgeString(int age) {
-    	if (age % 10 >= 1 && age % 10 <= 4) {
+	static String getAgeString(int age) {
+		if (age % 10 >= 2 && age % 10 <= 4) {
 			return "Вам " + age + " года.";
+		} else if (age % 10 == 1) {
+			return "Вам " + age + " год.";
 		} else {
 			return "Вам " + age + " лет.";
 		}
-    }
+	}
 }
