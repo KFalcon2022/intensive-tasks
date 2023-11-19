18510a0b02c472315d519d0f65b3b6353ca6c4a0
Константин Синяков <k.siniakov@yandex.ru>
2023-11-19 13:00:50 +0300

    Интенсив. Решение задачи 4 из раздела 1.

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -5,13 +5,14 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
-
-        System.out.println(solveQuadraticEquation(a, b, c));
-
+        System.out.println(solveQuadraticEquation(0, 0, 0));
+        System.out.println(solveQuadraticEquation(0, 0, 5));
+        System.out.println(solveQuadraticEquation(0, 2, 4));
+        System.out.println(solveQuadraticEquation(0, 2, -4));
+        System.out.println(solveQuadraticEquation(3, 7, -6));
+        System.out.println(solveQuadraticEquation(-1, 7, 8));
+        System.out.println(solveQuadraticEquation(4, 4, 1));
+        System.out.println(solveQuadraticEquation(2, 1, 1));
     }
 
     /**
@@ -26,8 +27,36 @@ public class Main {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    return "Количество решений: Infinity.";
+                } else {
+                    return "Количество решений: 0.";
+                }
+            } else {
+                return "Количество решений: 1. Корень:" + (-c) / b;
+            }
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double discriminant = getDiscriminant(a, b, c);
+
+        if (discriminant > 0) {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+            if (x1 < x2) {
+                return "Количество решений: 2. Корни: " + x1 + ";" + x2;
+            }
+
+            return "Количество решений: 2. Корни: " + x2 + ";" + x1;
+        } else if (discriminant == 0) {
+            return "Количество решений: 1. Корень:" + (-b) / (2 * a);
+        }
+        return "Количество решений: 0.";
+    }
+
+    static double getDiscriminant(double a, double b, double c) {
+        return b * b - 4 * a * c;
     }
 }
