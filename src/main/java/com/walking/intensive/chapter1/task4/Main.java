803ff4f1ef1d2d32b123accd3a757c50a6f521ce
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-11-19 17:59:10 +0300

    commit4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -5,29 +5,49 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+
+        double a = 1;
+        double b = 2;
+        double c = 3;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
     }
 
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b == 0) {
+            return "Нет решений.";
+        }
+
+        if (!isQuadratic(a)) {
+            double x = -c / b;
+            return "Количество решений: 1, корень: " + x;
+        }
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (isQuadratic(a)) {
+            if (discriminant > 0) {
+
+                double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                return "Количество решений: 2. Корни: " + x1 + ", " + x2;
+
+            } else if (discriminant == 0) {
+
+                double x = (-b / (2 * a));
+                return "Количество решений: 1. Корень: " + x;
+
+            } else {
+                return ("Количество решений: 0.");
+            }
+        } else {
+            return "Уравнение не квадратное.";
+        }
+    }
+
+    static boolean isQuadratic(double a) {
+        return (a != 0);
     }
 }
