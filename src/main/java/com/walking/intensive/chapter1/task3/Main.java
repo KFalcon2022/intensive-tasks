73684af18bd9b7ffbd859cad93aba1a7ea26c017
Константин Синяков <k.siniakov@yandex.ru>
2023-11-19 13:23:05 +0300

    Интенсив. Решение задачи 3 из раздела 1. Добавил валидацию отрицательных значений

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,8 +6,15 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        for (int i = 1889; i < 2024; i++) {
-            System.out.println("Год " + i + " " + (isLeap(i) ? "високосный" : "не вискосный"));
+        for (int i = -3; i < 2024; i++) {
+            if (i > 0) {
+                System.out.println("Год " + i + " " + (isLeap(i) ? "високосный" : "не вискосный"));
+            } else {
+                System.out.println("Неверное число");
+                if (i == 0) {
+                    i = 1894;
+                }
+            }
         }
 
     }
