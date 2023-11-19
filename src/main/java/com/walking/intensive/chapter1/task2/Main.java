a2ffd5db53d9d29835cbdd4fe878e9264680e592
TitLukich <Nef-92@yandex.ru>
2023-11-19 20:20:08 +0300

    Задача 2 занятие 1

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,17 +1,40 @@
 package com.walking.intensive.chapter1.task2;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество этажей в доме: ");
+        int floorAmount = scanner.nextInt();
+        System.out.print("Введите количество подъездов в доме: ");
+        int entranceAmount = scanner.nextInt();
+        System.out.print("Введите номер квартиры: ");
+        int flatNumber = scanner.nextInt();
+        scanner.close();
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
-
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        String flatLocation;
+        int flatsOnFloor = 4;
+        if ((floorAmount * entranceAmount * flatsOnFloor) < flatNumber || flatNumber < 1) {
+            return "Такой квартиры в доме нет";
+        } else {
+            int entrance = (int) Math.ceil((double) flatNumber / flatsOnFloor / floorAmount);
+            int floor = (int) Math.ceil((double) flatNumber / flatsOnFloor) - (entrance - 1) * floorAmount;
+            if (flatNumber % 4 == 0) {
+                flatLocation = "справа от лифта, вправо";
+            } else if (flatNumber % 3 == 0) {
+                flatLocation = "справа от лифта, влево";
+            } else if (flatNumber % 2 == 0) {
+                flatLocation = "слева от лифта, вправо";
+            } else {
+                flatLocation = "слева от лифта, влево";
+            }
+            return flatNumber + " кв - " + entrance + " подъезд, " + floor + " этаж, " + flatLocation;
+        }
     }
 }
