658437973f72384a3de712e78163110cf3495d25
Galina Magazinova <gmagazinova@gmail.com>
2023-11-19 19:48:11 +0300

    fix task1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,16 +5,18 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-        int age = 128;
+        int age = 3;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
         String message;
-        if (age <= 0 || age > 127) {
+        if (age < 0 || age > 127) {
             message = "Неверно введен возраст";
-        } else if (age < 5) {
+        } else if (age == 1 || (age > 20 && (age % 10 == 1))) {
+            message = "Вам " + age + " год";
+        } else if (age != 0 && age < 5 || (age > 20 && (age % 10 > 1 && age % 10 < 5))) {
             message = "Вам " + age + " года";
         } else {
             message = "Вам " + age + " лет";
