d60faa623a781a2c9b853b8dafde9a2dc6164aa7
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-11-19 21:22:48 +0300

    requested fixes

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -15,38 +15,38 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+//        int age = 0;
+//
+//        System.out.println(getAgeString(age));
 
-        System.out.println(getAgeString(age));
-
-
-        for(int i = 0; i <= 127; i++) {
+        for (int i = 0; i <= 127; i++) {
             System.out.println(getAgeString(i));
         }
     }
 
     static String getAgeString(int age) {
-        String youStr = "Вам ";
-        String years = " лет.";
-        String yearsAnother = " года.";
-        String singleYear = " год.";
-
-        if(age < 0) {
-            return "Ты еще не родилися.";
-        } else if(age > 127) {
+        if (age < 0) {
+            return "Ты еще не родился.";
+        } else if (age > 127) {
             return "Поздравляю! Ты новый долгожитель!";
-        } else if(age == 0) {
-            return "Поздравляю! Ты родился.";
-        } else if(age == 1) {
-            return youStr + age + singleYear;
-        } else if(age < 5 ) {
-            return youStr + age + yearsAnother;
-        } else if(age <= 20 || (age % 10 >= 5 || age % 10 == 0)) {
-            return youStr + age + years;
-        } else if (age % 10 == 1) {
-            return youStr + age + singleYear;
         }
 
-        return youStr + age + yearsAnother;
+        String you = "Вам ";
+
+        if(age == 0) {
+            return you + age + " лет.";
+        } else if (age == 1) {
+            return you + age + " год.";
+        } else if(age < 5 ) {
+            return you + age + " года.";
+        } else if(age <= 20 || (age > 104 && age < 121)) {
+            return you + age + " лет.";
+        } else if (age % 10 == 1) {
+            return you + age + " год.";
+        } else if (age % 10 >= 5 || age % 10 == 0) {
+            return you + age + " лет.";
+        }
+
+        return you + age + " года.";
     }
 }
