8f2fd43643014f424fdd9df0e605a3c9d86437ea
Greem4 <i@greem4.ru>
2023-11-19 21:13:34 +0300

    chapter 1, task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -5,12 +5,12 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
 
-        System.out.println(solveQuadraticEquation(a, b, c));
+        System.out.println(solveQuadraticEquation(4, -5, 6));
+        System.out.println(solveQuadraticEquation(2, -7, 8));
+        System.out.println(solveQuadraticEquation(0, 0, 0));
+        System.out.println(solveQuadraticEquation(0, 0, 5));
+        System.out.println(solveQuadraticEquation(1, -3, 2));
 
     }
 
@@ -26,8 +26,42 @@ public class Main {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    return "Количество решений: бесконечное количество";
 
-        return null; // Заглушка. При реализации - удалить
+                } else {
+                    return "Количество решений: 0";
+                }
+
+            } else {
+                double x = -c / b;
+                return "Количество решений: 1. Корень: " + x;
+            }
+
+        } else {
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0) {
+                double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                if (x1 < x2) {
+                    return "Количество решений: 2. Корни: " + x1 + " ; " + x2;
+                }else {
+                    return "Количество решений: 2. Корни: " + x2 + " ; " + x1;
+                }
+
+            } else if (discriminant == 0) {
+                double x = -b / (2 * a);
+                return "Количество решений: 1. Корень: " + x;
+
+            }else {
+
+                return "Количество решений: 0";
+
+            }
+        }
     }
 }
