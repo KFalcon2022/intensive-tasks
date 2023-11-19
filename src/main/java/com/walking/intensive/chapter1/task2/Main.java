94cd6874d34a88c62ebfa595b9c8084ee1609802
tvadim1 <tvadim1@yandex.ru>
2023-11-19 23:31:13 +0600

    Initial commit

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,51 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        int floorAmount = 5;
+        int entranceAmount = 3;
+        int flatNumber = 41;
 
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
+
+        /*for (flatNumber = 1; flatNumber <= floorAmount * 4 * entranceAmount; flatNumber++) {
+            System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
+        }*/
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        String flatString = null;
+        int indexOfFlat;
+        int indexOfEntrance;        //номер подъезда, если от нуля
+        int indexOfFloorAbsolute;   //"абсолютный" номер этажа, если бы не было подъездов
+        int indexOfFloorReal;       //номер этажа
+        int flatAmountInOneEntrance = 4 * floorAmount;
+        int totalFlatAmount = flatAmountInOneEntrance * entranceAmount;
 
-        return null; // Заглушка. При реализации - удалить
+        if ((flatNumber <= 0) || (flatNumber > totalFlatAmount)) {
+            return "Не существует такой квартиры";
+        }
+
+        indexOfEntrance = (flatNumber - 1) / flatAmountInOneEntrance;
+
+        indexOfFloorAbsolute = ((flatNumber - 1) / 4) + 1;
+        indexOfFloorReal = (indexOfFloorAbsolute % floorAmount == 0) ? floorAmount : indexOfFloorAbsolute % floorAmount;
+
+        indexOfFlat = flatNumber % 4;
+        if (indexOfFlat == 1) {
+            flatString = "слева от лифта, влево";
+        } else if (indexOfFlat == 2) {
+            flatString = "слева от лифта, вправо";
+        } else if (indexOfFlat == 3) {
+            flatString = "справа от лифта, влево";
+        } else if (indexOfFlat == 0) {
+            flatString = "справа от лифта, вправо";
+        }
+
+        return "%s кв - %s подъезд, %s этаж, %s".formatted(
+                flatNumber,
+                Integer.toString(++indexOfEntrance),
+                Integer.toString(indexOfFloorReal),
+                flatString);
+
     }
 }
