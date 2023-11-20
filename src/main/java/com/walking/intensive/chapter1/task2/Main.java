68ff40a52b83f4f9b62bb2d5496c803d1cda487c
Константин Синяков <k.siniakov@yandex.ru>
2023-11-21 00:08:00 +0300

    Решение задачи 2 из раздела 1. Поправил алгоритм с учетом замечаний

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -4,34 +4,34 @@ package com.walking.intensive.chapter1.task2;
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Main {
-    final static int FLAT_COUNT_AT_FLOOR = 4;
+
 
     public static void main(String[] args) {
-        int FLOOR_AMOUNT = 4;
-        int ENTRANCE_AMOUNT = 2;
-        int maxFlatNumber = FLOOR_AMOUNT * ENTRANCE_AMOUNT * FLAT_COUNT_AT_FLOOR;
+        int floorAmount = 4;
+        int entranceAmount = 2;
+        int flatFloorCount = 4;
+        int maxFlatNumber = floorAmount * entranceAmount * flatFloorCount;
+
         for (int i = 0; i <= maxFlatNumber; i++) {
-            System.out.println(getFlatLocation(FLOOR_AMOUNT, ENTRANCE_AMOUNT, i));
+            System.out.println(getFlatLocation(floorAmount, entranceAmount, i, flatFloorCount));
         }
     }
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        if (floorAmount * FLAT_COUNT_AT_FLOOR * entranceAmount >= flatNumber && flatNumber > 0) {
-            int flatAmountAtEntrance = floorAmount * FLAT_COUNT_AT_FLOOR;
-            for (int entranceNumber = 1; entranceNumber < entranceAmount + 1; entranceNumber++) {
-                for (int floorNumber = 1; floorNumber < floorAmount + 1; floorNumber++) {
-                    int currentMaxFLatNumberAtFloor = (entranceNumber - 1) * flatAmountAtEntrance + floorNumber * FLAT_COUNT_AT_FLOOR;
+    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber, int flatFloorCount) {
 
-                    if (currentMaxFLatNumberAtFloor >= flatNumber)
-                        return flatNumber + " кв – " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + getFlatPosition(flatNumber);
-                }
-            }
+        if (floorAmount * flatFloorCount * entranceAmount >= flatNumber && flatNumber > 0) {
+            int flatAmountAtEntrance = floorAmount * flatFloorCount;
+
+            int entranceNumber = (flatNumber - 1) / flatAmountAtEntrance + 1;
+            int floorNumber = ((flatNumber - 1) % flatAmountAtEntrance) / flatFloorCount + 1;
+
+            return flatNumber + " кв – " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + getFlatPosition(flatNumber, flatFloorCount);
         }
         return "Такой квартиры в доме нет";
     }
 
-    static String getFlatPosition(int flatNumber) {
-        return switch (flatNumber % FLAT_COUNT_AT_FLOOR) {
+    static String getFlatPosition(int flatNumber, int flatFloorCount) {
+        return switch (flatNumber % flatFloorCount) {
             case 0 -> "вправо от лифта, вправо";
             case 1 -> "слева от лифта, влево";
             case 2 -> "слева от лифта, вправо";
