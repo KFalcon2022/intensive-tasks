d9f961fc82338a1daa8a30d552e3e799f9a37e85
egor.djun <xedshotg@gmail.com>
2023-11-20 14:34:14 +0700

    hi

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,37 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите ваш возраст: ");
+        int age = scanner.nextInt();
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        if(age < 0){
+            return "Ошибка! Возраст не может быть отрецательным!";
+        }
+        if(age > 127){
+            return "Ого! Новый мировой рекорд!";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+
+        if ((age >= 5) && (age <= 20)) {
+            return "Вам " + age + " лет";
+        } else if (age % 10 == 1) {
+            return "Вам " + age + " год";
+        } else if ((age % 10 > 1) && (age % 10 <= 4)) {
+            return "Вам " + age + " года";
+        } else {
+            return "Вам " + age + " лет";
+        }
     }
 }
