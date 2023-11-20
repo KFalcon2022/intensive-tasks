2716c64b88cf4d516eca41d06f3453c6db4b8252
lambo064 <tasha_lambo@mail.ru>
2023-11-20 13:24:52 +0300

    task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -5,29 +5,50 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
+
+        double a = 3;
+        double b = 2;
         double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
     }
 
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b != 0) {
+            return ("Количество решений: 1. Корень:" + -c / b);
+        }
+
+        if (a == 0 && b == 0 && c == 0) {
+            return ("Количество решений: бесконечно");
+        }
+
+        if (a == 0 && b == 0 && c != 0) {
+            return ("Количество решений: 0");
+        }
+
+        double d = b * b - 4 * a * c;
+
+        double x1 = (-b - Math.sqrt(d)) / (2 * a);
+
+        double x2 = (-b + Math.sqrt(d)) / (2 * a);
+
+        if (d == 0)
+
+            return ("Количество решений: 1. Корень:" + x1);
+
+        else if (d < 0)
+
+            return ("Количество решений: 0");
+
+        else if (x1 < x2)
+
+            return ("Количество решений: 2. Корни:" + x1 + " ," + x2);
+
+        else
+
+            return ("Количество решений: 2. Корни:" + x2 + " ," + x1);
+
     }
 }
