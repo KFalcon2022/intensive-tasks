ec28ac4543ce4bd96f66d58bd579e54af661ad58
Reilan <reilan86@gmail.com>
2023-11-20 10:20:47 +0300

    Вариант решения задачи 1-1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,38 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 121;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (age > 127) {
+            return "Давай не будем обманывать, сладкий пирожочек, напиши сколько тебе лет на самом деле.";
+        }
+
+        else {
+
+            String ageString = Integer.toString(age);
+            char caseMarker = ageString.charAt(ageString.length() - 1);
+            String resultString;
+
+            switch (caseMarker) {
+                case '1':
+                    resultString = "Вам " + ageString + " год.";
+                    break;
+                case '2':
+                case '3':
+                case '4':
+                    resultString = "Вам " + ageString + " года.";
+                    break;
+                default:
+                    resultString = "Вам " + ageString + " лет.";
+                    break;
+            }
+
+            return resultString;
+        }
     }
 }
