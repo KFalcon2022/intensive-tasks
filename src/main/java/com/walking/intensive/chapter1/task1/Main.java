e638a13575b63a06241da12181acc74f328b9a6d
KrisLu23 <krisbondarev@yandex.ru>
2023-11-20 17:12:55 +0300

    Task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,9 +6,12 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 114;
-
-        System.out.println(getAgeString(age));
+//        int age = 114;
+//
+//        System.out.println(getAgeString(age));
+        for (int i = 0; i < 127; i++) {
+            System.out.println(getAgeString(i));
+        }
 
     }
 
@@ -16,18 +19,13 @@ public class Main {
         if (age < 0) {
             return "Введен неверный возраст.";
         }
-        if (age % 10 == 1 && age != 11) {
+        if (age % 10 == 1 && age % 100 != 11) {
             return "Вам " + age + " год.";
         }
 
         if (1 < age % 10 && age % 10 < 5 && age % 100 != 12 && age % 100 != 13 && age % 100 != 14) {
-
-            if (1 < age % 10 && age % 10 < 5 && age != 12 && age != 13 && age != 14) {
-
-                return "Вам " + age + " года.";
-            }
-
-
-        } return "Вам " + age + " лет.";
+            return "Вам " + age + " года.";
+        }
+        return "Вам " + age + " лет.";
     }
 }
