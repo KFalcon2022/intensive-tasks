45b0815bac11464fab9ee48fcbddc995eb1fe4f4
Oleg <kovalyovop@yandex.ru>
2023-11-21 02:00:12 +0300

    Task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,45 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        String test1 = getFlatLocation(5, 2, 21);
+        String test2 = getFlatLocation(5, 2, 41);
+        System.out.println(test1);
+        System.out.println();
+        System.out.println(test2);
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        final int totalFlatFloor = 4;
+        int totalFlatsEntrance = floorAmount * totalFlatFloor; // итого квартир в подъезде
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber < 1 || flatNumber > (entranceAmount * totalFlatsEntrance)) {
+            return "Квартиры с таким номером в доме нет";
+        }
+
+        int entranceNumber;
+
+        if (flatNumber % totalFlatsEntrance == 0) {
+            entranceNumber = flatNumber / totalFlatsEntrance;
+        } else {
+            entranceNumber = flatNumber / totalFlatsEntrance + 1;
+        }
+
+        int floorNumber;
+
+        if (flatNumber % totalFlatFloor == 0) {
+            floorNumber = (flatNumber / totalFlatFloor) - floorAmount * (entranceNumber - 1);
+        } else {
+            floorNumber = (flatNumber / totalFlatFloor + 1) - floorAmount * (entranceNumber - 1);
+        }
+
+        String flatLocation = switch (flatNumber % totalFlatFloor) { // расположение относительно лифта
+            case (0) -> "справа от лифта, вправо";
+            case (1) -> "слева от лифта, влево";
+            case (2) -> "слева от лифта, вправо";
+            case (3) -> "справа от лифта, влево";
+            default -> "";
+        };
+
+        return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + flatLocation;
     }
 }
