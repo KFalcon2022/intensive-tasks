bfb7901a9c6b437fad0c7216984669b62194036a
Alexey <lolex1@mail.ru>
2023-11-20 19:46:53 +0300

    test2

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -13,9 +13,6 @@ public class Main {
     }
 
     static boolean isLeap(int year) {
-        if ((year % 4 == 0 && year % 100 != 0) | year % 400 == 0){
-            return true;
-        }
-        return false;
+        return (year % 4 == 0 && year % 100 != 0) | year % 400 == 0;
     }
 }
