fce178602c7824c164bc749245352bfb87b75957
njmist <njmist@gmail.com>
2023-11-21 00:36:19 +0300

    Очищение этой ветки от всего кода

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,23 +6,12 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int year = 2100;
-        if (isLeap(year)) {
-            System.out.println("По григорианскому календарю " + year + " год является високосным");
-        } else {
-            System.out.println("По григорианскому календарю " + year + " год является невисокосным");
-        }
+
     }
 
     static boolean isLeap(int year) {
         //        Место для вашего кода
-        if (year % 100 == 0 && year % 400 != 0) {
-            return false;
-        } else if (year % 400 == 0 || year % 4 == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
+        return false; // Заглушка. При реализации - удалить
+    }
 }
