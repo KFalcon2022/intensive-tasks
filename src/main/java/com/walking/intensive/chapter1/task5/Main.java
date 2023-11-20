cd8298d7d70ea303c7c90f5dbb1a57d0cb9cbe4e
bergen <bergy@yandex.ru>
2023-11-20 18:52:29 +0300

    Task5

--- src/main/java/com/walking/intensive/chapter1/task5/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Main.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-triangle.html">ссылка</a>
  */
@@ -11,8 +13,9 @@ public class Main {
 
     static double getAreaByHeron(double a, double b, double c) {
         //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        double p = (a + b + c) / 2;
+        double s = Math.sqrt(p*(p - a) * (p-b) * (p - c));
+        return s;
     }
 
     /**
@@ -20,17 +23,27 @@ public class Main {
      */
     static double[] getHeights(double a, double b, double c) {
         //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double p = (a + b + c) / 2;
+        double s = Math.sqrt(p*(p - a) * (p-b) * (p - c));
+        double[] heights = new double[3];
+        heights[0] = (2 * s) / a;
+        heights[1] = (2 * s) / b;
+        heights[2] = (2 * s) / c;
+        return Arrays.stream(heights).sorted().toArray();
     }
 
     /**
      * Располагайте медианы по возрастанию.
      */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double p = (a + b + c) / 2;
+        double s = Math.sqrt(p*(p - a) * (p-b) * (p - c));
+        double[] medians = new double[3];
+        medians[0] = Math.sqrt(2 * a * a + 2 * b * b - c * c ) / 2;
+        medians[1] = Math.sqrt(2 * c * c + 2 * a * a - b * b ) / 2;
+        medians[2] = Math.sqrt(2 * c * c + 2 * b * b - a * a ) / 2;
+        return Arrays.stream(medians).sorted().toArray();
+//        return null; // Заглушка. При реализации - удалить
     }
 
     /**
@@ -38,8 +51,13 @@ public class Main {
      */
     static double[] getBisectors(double a, double b, double c) {
         //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double p = (a + b + c) / 2;
+        double s = Math.sqrt(p*(p - a) * (p-b) * (p - c));
+        double[] bisectors = new double[3];
+        bisectors[0] = (Math.sqrt(a*b*(a+b+c)*(a+b-c))) / a+b;
+        bisectors[1] = (Math.sqrt(a*c*(a+b+c)*(a-b+c))) / a+c;
+        bisectors[2] = (Math.sqrt(b*c*(a+b+c)*(b+c-a))) / b+c;
+        return Arrays.stream(bisectors).sorted().toArray();
     }
 
     /**
@@ -47,20 +65,23 @@ public class Main {
      */
     static double[] getAngles(double a, double b, double c) {
         //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double[] angles = new double[3];
+        angles[0] = Math.acos((a*a + b*b - c*c) / 2 * a * b);
+        angles[1] = Math.acos((c*c + a*a - b*b) / 2 * c * a);
+        angles[2] = Math.acos((b*b + c*c - a*a) / 2 * b * c);
+        return Arrays.stream(angles).sorted().toArray();
     }
 
     static double getInscribedCircleRadius(double a, double b, double c) {
         //        Место для вашего кода
 
-        return 0; // Заглушка. При реализации - удалить
+        return (a*b*c)/2;
     }
 
     static double getCircumradius(double a, double b, double c) {
         //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        double p = (a + b + c) / 2;
+        return (a*b*c)/(4 * Math.sqrt(p*(p-a)*(p-b)*(p-c)));
     }
 
     static double getAreaAdvanced(double a, double b, double c) {
@@ -68,4 +89,14 @@ public class Main {
 
         return 0; // Заглушка. При реализации - удалить
     }
+
+    static boolean isDegenerate(double a, double b, double c){
+        if ((a > (b + c)) && (b > (a + c)) && (c > (a + b))) {
+            System.out.println("Треугольник не должен быть вырожденным и должен существовать.");
+            return false;
+        } else {
+            return true;
+        }
+    };
+
 }
