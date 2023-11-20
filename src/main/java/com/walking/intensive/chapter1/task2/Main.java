82ac49bf748d8a3d9c2288bf2f06b7bc26857f4b
Galina Magazinova <gmagazinova@gmail.com>
2023-11-20 13:46:39 +0300

    task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,38 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        int flatNumber = 71;
+        int floorAmount = 9;
+        int entranceAmount = 3;
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        String flatLocation;
+        int flatsPerFloor = 4;
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber > flatsPerFloor * floorAmount * entranceAmount) {
+            flatLocation = "Такой квартиры нет в доме";
+        } else {
+            int flatsPerEntrance = flatsPerFloor * floorAmount;
+            int entranceNumber = (int) Math.ceil((double) flatNumber / flatsPerEntrance);
+            int floorNumber = (int) Math.ceil((double) (flatNumber - (entranceNumber - 1) * flatsPerEntrance) / flatsPerFloor);
+            int positionOnFloor = flatNumber % flatsPerFloor;
+
+            String positionMessage;
+            if (positionOnFloor == 0) {
+                positionMessage = "справа от лифта, вправо";
+            } else if (positionOnFloor == 1) {
+                positionMessage = "слева от лифта, влево";
+            } else if (positionOnFloor == 2) {
+                positionMessage = "слева от лифта, вправо";
+            } else {
+                positionMessage = "справа от лифта, влево";
+            }
+
+            flatLocation = flatNumber + " кв = " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + positionMessage;
+
+        }
+        return flatLocation;
     }
 }
