96c1c0b3b543a285bd6234e4fdf635791b789923
Lunularis <s.n.volynkina@yandex.ru>
2023-11-20 09:04:31 +0700

    task2 (1)

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -7,11 +7,40 @@ public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
 
+        System.out.println(getFlatLocation(5,4,4));
+
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        String answer;
+        int flatAmount = 4;
+
+        if (flatAmount*floorAmount*entranceAmount < flatNumber){
+            answer = "Такой квартиры нет в доме";
+        }else {
+
+            int entrance = flatNumber / (floorAmount * flatAmount);
+            answer = "" + (entrance + 1) + " подъезд, ";
+
+            int flatNumberEntrance = flatNumber - entrance * floorAmount * flatAmount;
+            int floor = flatNumberEntrance / flatAmount;
+            answer += (floor + 1) + " этаж, ";
+
+            int flatLocation = flatNumberEntrance - floor*flatAmount;
+
+            if (flatLocation == 1 | flatLocation == 2){
+                answer += "слева от лифта, ";
+            }else {
+                answer += "справа от лифта, ";
+            }
+
+            if ((flatLocation % 2) == 1){
+                answer += "влево";
+            }else {
+                answer += "вправо";
+            }
+        }
+        return answer;
     }
 }
