dd1eeb859b90dfe28e625b031998ace1e732f449
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-11-20 19:26:44 +0300

    fix

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -17,13 +17,11 @@ public class Main {
     }
 
     static boolean isLeap(int year) {
-        if(year % 400 == 0) {
-            return true;
-        } else if(year % 100 == 0) {
-            return false;
-        } else if(year % 4 == 0) {
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             return true;
         }
+
         return false;
     }
 }
