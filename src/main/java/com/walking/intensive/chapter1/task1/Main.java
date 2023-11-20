439ff6387beccd8417be6da963927527213f12e0
njmist <njmist@gmail.com>
2023-11-21 00:16:22 +0300

    Пустой мастер

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,26 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 100;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 //        Место для вашего кода
+        int remainder = age % 100 / 10; //Понял, где тупанул
 
-        return null; // Заглушка. При реализации - удалить
+        if (age < 0 || age > 127) {
+            return "Это врядли";
+        } else if (remainder == 1) {
+            return "Вам " + age + " год";
+        } else if (age % 10 == 1){
+            return "Вам " + age + " год";
+        }
+        else if (age % 10 >= 2 && age % 10 < 5) {
+            return "Вам " + age + " года";
+        } else {
+            return "Вам " + age + " лет";
+        }
     }
 }
