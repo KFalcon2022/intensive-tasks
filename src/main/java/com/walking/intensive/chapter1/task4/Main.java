887d165565dbc736a19658df0713a22164f3eecc
Lunularis <s.n.volynkina@yandex.ru>
2023-11-21 00:19:14 +0700

    task4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -6,8 +6,8 @@ package com.walking.intensive.chapter1.task4;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 1;
-        double b = -4;
+        double a = 0;
+        double b = 0;
         double c = -5;
 
         System.out.println(solveQuadraticEquation(a, b, c));
@@ -31,27 +31,34 @@ public class Main {
         byte answerCount = 0;
         double x1,x2;
 
-        double d;
-        d = Math.pow(b, 2) - 4 * a * c;
-
-        if(d==0){
+        if (a==0 & b!=0 ){
+            //это линейное уравнение
             answerCount = 1;
-            x1 = -1*b/(2*a);
+            x1 = -1*c/b;
             answer = "Корень: "+x1;
-        } else if (d>0) {
-            answerCount = 2;
-            answer = "Корни: ";
-            x1 = (-1*b+Math.sqrt(d))/(2*a);
-            x2 = (-1*b-Math.sqrt(d))/(2*a);
+        } else{
 
-            if (x1<x2){
-                answer += x1 + ";" + x2;
-            }else {
-                answer += x2 + ";" + x1;
+            double d;
+            d = Math.pow(b, 2) - 4 * a * c;
+
+            if(d==0 & a!=0){
+                answerCount = 1;
+                x1 = -1*b/(2*a);
+                answer = "Корень: "+x1;
+            } else if (d>0) {
+                answerCount = 2;
+                answer = "Корни: ";
+                x1 = (-1*b+Math.sqrt(d))/(2*a);
+                x2 = (-1*b-Math.sqrt(d))/(2*a);
+
+                if (x1<x2){
+                    answer += x1 + ";" + x2;
+                }else {
+                    answer += x2 + ";" + x1;
+                }
             }
         }
-
-        answer = "Количество решений: "+answerCount + ". "+answer;
+        answer = "Количество решений: "+answerCount + ". " + answer;
         return answer;
     }
 }
