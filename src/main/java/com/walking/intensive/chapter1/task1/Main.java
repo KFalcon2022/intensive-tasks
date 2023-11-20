2a6a77d98f796e826de1b9b65a027a009bb43afa
SkofieldAE <alexandrevd2107@gmail.com>
2023-11-21 00:18:32 +0300

    update

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,30 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+
+        Scanner years = new Scanner(System.in);
+
+        System.out.println("Введите ваш возраст от 1 до 127");
+        int age = years.nextInt();
+        years.close();
 
         System.out.println(getAgeString(age));
     }
-
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age <= 0 || age > 127) {
+            return "Вы не попали в диапозон";
+        }
+        else if (age % 10 == 1 && age % 100 != 11) {
+            return "Вам " + age + " год";
+        } else if (age % 10 > 1 && age % 10 < 5 && (age % 100 <= 9 || age % 100 > 19)) {
+            return "Вам " + age + " года";
+        }
+        return "Вам " + age + " лет";
     }
 }
