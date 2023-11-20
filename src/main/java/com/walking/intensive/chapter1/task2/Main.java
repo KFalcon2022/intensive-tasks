02c649ef5d29c3510a70bb243ebb9af22c320584
stogov_so <stogov_sergei@mail.ru>
2023-11-20 21:03:05 +0500

    Chapter1_task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,53 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        int floorAmount = 5;
+        int entranceAmount = 8;
+        int flatNumber = 96;
 
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int apartmentsOnFloor = 4;
 
-        return null; // Заглушка. При реализации - удалить
+        if (floorAmount <= 0 || entranceAmount <= 0 || flatNumber <= 0) {
+            return "Количество этажей, подъездов и номер квартиры должны быть положительными. " +
+                    "Проверьте корректность введенных значений";
+        } else if (floorAmount * entranceAmount * apartmentsOnFloor < flatNumber) {
+            return "Квартира № " + flatNumber + " в данном здание не существует.";
+        }
+
+        String flatLocation;
+
+        if (flatNumber % apartmentsOnFloor == 1) {
+            flatLocation = "слева от лифта, влево";
+        } else if (flatNumber % apartmentsOnFloor == 2) {
+            flatLocation = "слева от лифта, вправо";
+        } else if (flatNumber % apartmentsOnFloor == 3) {
+            flatLocation = "справа от лифта, влево";
+        } else {
+            flatLocation = "справа от лифта, вправо";
+        }
+
+        int currentEntrance = 1;
+
+        for (int i = 1; i <= entranceAmount; i++) {
+            if (i * floorAmount * apartmentsOnFloor < flatNumber) {
+                currentEntrance++;
+            }
+        }
+
+        int apartmentsInEntrance = apartmentsOnFloor * floorAmount;
+        int currentFloor;
+        int numberApartmentsInCurrentEntrance = flatNumber - (currentEntrance - 1) * apartmentsInEntrance;
+
+        if (numberApartmentsInCurrentEntrance % apartmentsOnFloor == 0) {
+            currentFloor = numberApartmentsInCurrentEntrance / apartmentsOnFloor;
+        } else {
+            currentFloor = numberApartmentsInCurrentEntrance / apartmentsOnFloor + 1;
+        }
+
+        return flatNumber + " кв – " + currentEntrance + " подъезд, " + currentFloor + " этаж, " + flatLocation;
     }
 }
