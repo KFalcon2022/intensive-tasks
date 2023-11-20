f58be82cd9e1e4e64a358a22ff676278fca712eb
sofajedi <kirill.ello@gmail.com>
2023-11-20 18:07:34 +0300

    Решение задачи 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,29 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
+        int age = 1001;
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        int lastOneNumber = age % 10;
+        int lastTwoNumbers = age % 100;
+    //    if (age < 0 || age > 127) {
+    //        return "Поздравляю, вы вне существующих рамок ждём ваше свидетельство о рождении на почту sobaka@sobaka.ru";
+    //    }
+        if (age == 0) {
+            return "Вам " + age + " лет";
+        }
+        if (lastTwoNumbers > 9 & lastTwoNumbers < 20) {
+            return "Вам " + age + " лет";
+        }
+        if (lastOneNumber > 1 & lastOneNumber < 5) {
+            return "Вам " + age + " годa";
+        }
+        if (lastOneNumber == 1) {
+            return "Вам " + age + " год";
+        } else {
+            return "Вам " + age + " лет";
+        }
     }
 }
