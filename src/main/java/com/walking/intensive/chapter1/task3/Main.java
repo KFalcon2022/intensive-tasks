a6b0d348a36524befd939bfcce7a702267adf0e5
bergen <bergy@yandex.ru>
2023-11-20 16:52:19 +0300

    Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,12 +6,16 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+//        System.out.println(isLeap(1900));
+//        System.out.println(isLeap(1979));
+//        System.out.println(isLeap(1980));
     }
 
     static boolean isLeap(int year) {
+        if (year % 4 != 0) return false;
+        return year % 100 != 0;
         //        Место для вашего кода
 
-        return false; // Заглушка. При реализации - удалить
+//        return false; // Заглушка. При реализации - удалить
     }
 }
