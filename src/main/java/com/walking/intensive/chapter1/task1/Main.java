e46e2a80a1134edda7f4de2ad60a4e5682eb283c
Артём <artyomlychkoff@gmail.com>
2023-11-20 21:43:53 +0300

    task1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,18 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 96;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        String suffix;
+         switch (age % 10) {
+            case 1: suffix = " год"; break;
+            case 2: suffix = " года"; break;
+            default: suffix = " лет"; break;
+        }
+        return age + suffix;
     }
 }
