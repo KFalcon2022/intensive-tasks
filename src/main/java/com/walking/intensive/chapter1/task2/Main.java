5dfe18f7ae94ba78d3c55ce9bf8ed4727c7a2ed1
bergen <bergy@yandex.ru>
2023-11-20 16:41:53 +0300

    Task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -6,12 +6,36 @@ package com.walking.intensive.chapter1.task2;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        int floors = Integer.parseInt(args[0]);
+        int entrances = Integer.parseInt(args[1]);
+        int flatNumber = Integer.parseInt(args[1]);
+
+        System.out.println(getFlatLocation(floors, entrances, flatNumber));
+
+//        System.out.println(getFlatLocation(5,5, 1));
+//        System.out.println(getFlatLocation(5,5, 2));
+//        System.out.println(getFlatLocation(5,5, 3));
+//        System.out.println(getFlatLocation(5,5, 4));
+
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         //        Место для вашего кода
+        if (flatNumber > floorAmount * entranceAmount * 4 || flatNumber < 0) return "Нет такой квартиры";
 
-        return null; // Заглушка. При реализации - удалить
+        int flatsInEntrance = floorAmount * 4;
+        int entranceNumber = flatNumber/flatsInEntrance + 1;
+        int floorNumber = ((flatNumber - 1) % flatsInEntrance) / 4 + 1;
+        int flatPosition = flatNumber % 4;
+
+        if (flatPosition == 1)
+            return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж , слева от лифта, влево";
+        if (flatPosition == 2)
+            return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж , слева от лифта, вправо";
+        if (flatPosition == 3)
+            return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж , справа от лифта, влево";
+        return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж , справа от лифта, вправо";
+//        return null; // Заглушка. При реализации - удалить
     }
 }
