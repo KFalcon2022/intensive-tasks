574cd8765cce9c5a489b9fc97fac42bdb17f6ad7
tvadim1 <tvadim1@yandex.ru>
2023-11-20 17:53:55 +0600

    Initial Commit

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -26,8 +26,43 @@ public class Main {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        int solutionNumber = 0;
+        String solution = null;
 
-        return null; // Заглушка. При реализации - удалить
+        if ((a == 0) && (b != 0)) {             //проверяем простые решения
+            solutionNumber = 1;
+            double x = -c / b;
+            solution = "Корень: " + x;
+        } else if ((b == 0) && (a != 0) && (c < 0)) {
+            solutionNumber = 2;
+            double x1 = Math.sqrt(c);
+            double x2 = -x1;
+            solution = "Корни: " + x1 + ";" + x2;
+        } else if ((b == 0) && (a != 0) && (c == 0)) {
+            solutionNumber = 1;
+            double x = 0;
+            solution = "Корень: " + x;
+        } else if ((b == 0) && (a == 0) && (c != 0)) {
+            solution = "Ненулевой коэфициент \"c\" оказался равен 0. Логическая ошибка. Проверьте ввод.";
+        } else if ((b == 0) && (a == 0) && (c == 0)) {
+            solution = "0 = 0";
+        } else {
+            double determinant = Math.pow(b, 2) - 4 * a * c;
+            if (determinant > 0) {
+                solutionNumber = 2;
+                double x1 = (-b + Math.sqrt(determinant)) / 2 / a;
+                double x2 = (-b - Math.sqrt(determinant)) / 2 / a;
+                //solution = "Корни: %f;%f".formatted(x1,x2); //оказывается приводит к float
+                solution = "Корни: " + x1 + ";" + x2;
+            } else if (determinant == 0) {
+                solutionNumber = 1;
+                double x = -b / 2 / a;
+                solution = "Корень: " + x;
+            } else {
+                solution = "";
+            }
+        }
+
+        return "Количество решений: %d. %s".formatted(solutionNumber, solution);
     }
 }
