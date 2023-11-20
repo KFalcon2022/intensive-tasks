ae6f6ccde30c83593eb8ff550909ce371c2fc26e
tvadim1 <tvadim1@yandex.ru>
2023-11-20 16:01:40 +0600

    Commit 2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -17,8 +17,36 @@ public class Main {
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int entranceIndex;        //номер подъезда, если от нуля
+        int floorAbsoluteIndex;   //"абсолютный" номер этажа, если бы не было подъездов
+        int floorRealIndex;       //номер этажа
+        int oneEntranceFlatAmount = 4 * floorAmount;
+        int totalFlatAmount = oneEntranceFlatAmount * entranceAmount;
 
-        return null; // Заглушка. При реализации - удалить
+        if ((flatNumber <= 0) || (flatNumber > totalFlatAmount)) {
+            return "Не существует такой квартиры";
+        }
+
+        entranceIndex = (flatNumber - 1) / oneEntranceFlatAmount;
+
+        floorAbsoluteIndex = ((flatNumber - 1) / 4) + 1;
+        floorRealIndex = (floorAbsoluteIndex % floorAmount == 0) ? floorAmount : floorAbsoluteIndex % floorAmount;
+
+        String flatString = null;
+        int indexOfFlat = flatNumber % 4;
+
+        flatString = switch (indexOfFlat) {
+            case (1) -> "слева от лифта, влево";
+            case (2) -> "слева от лифта, вправо";
+            case (3) -> "справа от лифта, влево";
+            default -> "справа от лифта, вправо";
+        };
+
+        return "%s кв - %s подъезд, %s этаж, %s".formatted(
+                flatNumber,
+                Integer.toString(++entranceIndex),
+                Integer.toString(floorRealIndex),
+                flatString);
+
     }
 }
