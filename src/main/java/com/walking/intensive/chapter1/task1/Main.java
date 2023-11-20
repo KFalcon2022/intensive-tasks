43521eca8adf78928832fa6f40ac8ccfda305c91
Reilan <reilan86@gmail.com>
2023-11-20 21:58:54 +0300

    Вариант решения задачи 1-1 переделано на остаток от деления

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,38 +6,30 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 121;
+        int age = 31;
 
-        System.out.println(getAgeString(age));
+        System.out.println("Вам " + age + getAgeString(age));
     }
 
     static String getAgeString(int age) {
+        String resultString;
 
-        if (age > 127||age <0) {
+        if (age > 127 || age < 0) {
             return "Давай не будем обманывать, сладкий пирожочек, напиши сколько тебе лет на самом деле.";
         }
 
-        else {
-
-            String ageString = Integer.toString(age);
-            char caseMarker = ageString.charAt(ageString.length() - 1);
-            String resultString;
-
-            switch (caseMarker) {
-                case '1':
-                    resultString = "Вам " + ageString + " год.";
-                    break;
-                case '2':
-                case '3':
-                case '4':
-                    resultString = "Вам " + ageString + " года.";
-                    break;
-                default:
-                    resultString = "Вам " + ageString + " лет.";
-                    break;
-            }
-
-            return resultString;
+        if (age % 10 == 1 && age % 100 != 11) {
+            resultString = " год.";
         }
+
+        else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+            resultString = " года.";
+        }
+
+        else {
+            resultString = " лет.";
+        }
+        return resultString;
     }
+
 }
