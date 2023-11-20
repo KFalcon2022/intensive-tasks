b8d56f57c7841aaf56bbf81a5af64cda9d20a45c
ganka-xn <jxn@inbox.ru>
2023-11-20 15:29:39 +0300

    task2

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,28 +5,31 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-//        int age = 0;
 
-        for (int i = -15; i < 225; i++)
-        {
+        for (int i = -15; i < 225; i++) {
             System.out.println(getAgeString(i));
         }
 
-//        System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-        if (age < 0) return "Некорректно введен возраст";
+
+        if (age < 0) {
+            return "Некорректно введен возраст";
+        }
 
         int remainder = age % 10;
         String result;
 
-        if (((age % 100) > 10 && (age % 100) < 20) || (remainder == 0 || remainder > 4))
+        if (((age % 100) > 10 && (age % 100) < 20) || (remainder == 0 || remainder > 4)) {
             result = " лет";
-        else if (remainder == 1) result = " год";
-        else result = " года";
+        }
+        else if (remainder == 1) {
+            result = " год";
+        }
+        else {
+            result = " года";
+        }
 
         return "Вам " + age + result; // Заглушка. При реализации - удалить
     }
--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -3,31 +3,31 @@ package com.walking.intensive.chapter1.task2;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
-public class Main
-{
-    public static void main(String[] args)
-    {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        for (int i = 1; i < 52; i++)
-        {
-            System.out.println(getFlatLocation(4, 3, i));
+public class Main {
+    public static void main(String[] args) {
+
+        int floorAmount = 4;
+        int entranceAmount = 3;
+
+        for (int i = 1; i < 52; i++) {
+            System.out.println(getFlatLocation(floorAmount, entranceAmount, i));
         }
-        System.out.println();
+
     }
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber)
-    {
+    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         //        Место для вашего кода
+        int flatsOnFloor = 4;
         int flatsInEntrance = floorAmount * 4; // квартир в подъезде
-        if (flatNumber > flatsInEntrance * entranceAmount)
-        {
+
+        if (flatNumber > flatsInEntrance * entranceAmount) {
             return "Кв. " + flatNumber + " - такой квартиры в доме нет!";
         }
 
-        int location = flatNumber % 4; // место на этаже
+        int location = flatNumber % flatsOnFloor; // место на этаже
 
         int entrance = ((flatNumber - 1) / flatsInEntrance) + 1; // подъезд
-        int floor = ((flatNumber - 1) % flatsInEntrance) / 4 + 1; // этаж
+        int floor = ((flatNumber - 1) % flatsInEntrance) / flatsOnFloor + 1; // этаж
 
         StringBuilder sb = new StringBuilder();
         sb.append(flatNumber)
@@ -36,8 +36,8 @@ public class Main
                 .append(" подъезд, ")
                 .append(floor)
                 .append(" этаж, ");
-        switch (location)
-        {
+
+        switch (location) {
             case 1 -> sb.append("слева от лифта, влево");
             case 2 -> sb.append("слева от лифта, вправо");
             case 3 -> sb.append("справа от лифта, влево");
