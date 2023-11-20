ff011694dc5dde70e75c8d9269e30a65c39c5359
lambo064 <tasha_lambo@mail.ru>
2023-11-20 21:43:32 +0300

    task 1 исправила

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,22 +5,22 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 105;
-
+        int age =102 ;
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 
-        if ((age > 10 && age < 20) || (age >= 100 && age <= 120)) {
+        if ((age >= 10 && age <= 20) || (age >= 110 && age <= 120)) {
             return ("Вам " + age + " лет");
-        } else {
+        }
+        else if (age == 1) return ("Вам " + age + " год");
+        else {
             int lastAgeInt = getLastAgeInt(age);
 
             if (lastAgeInt > 1 && lastAgeInt < 5) {
                 return ("Вам " + age + " года");
-            } else if (lastAgeInt == 1 || lastAgeInt == 0 || (lastAgeInt >= 5 && lastAgeInt <= 9)) {
+            } else if (lastAgeInt == 0 || (lastAgeInt >= 5 && lastAgeInt <= 9)) {
                 return ("Вам " + age + " лет");
             } else return ("Вам " + age + " год");
         }
@@ -28,7 +28,7 @@ public class Main {
 
     static int getLastAgeInt(int age) {
         if (age > 100) {
-            return (age % 100);
+            return (age % 100 % 10);
         } else if (age > 10) {
             return (age % 10);
         } else return age;
