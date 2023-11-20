cac549e56db866cfb7c45f252b8263d653381c7e
Reilan <reilan86@gmail.com>
2023-11-20 14:12:40 +0300

    Вариант решения задачи 1-2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -6,35 +6,42 @@ package com.walking.intensive.chapter1.task2;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int floorAmount = 30;
-        int entranceAmount = 9;
-        for (int i = 1; i < floorAmount * entranceAmount * 4 + 3; i++) {
-            System.out.println(getFlatLocation(floorAmount, entranceAmount, i));
-        }
-        //System.out.println(getFlatLocation(floorAmount, entranceAmount, 36));
+        System.out.println(getFlatLocation(6, 10, 239));
+
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
-        int maxFlats = floorAmount * entranceAmount * 4;
-        int flatsInEntrance = floorAmount * 4;
-        if (flatNumber > maxFlats) {
-            return "Квартиры в доме нет";
-        }
-        int orderOfFlatInEntrance = flatNumber % flatsInEntrance;
-        //ищем номер подъезда делением номера квартиры на количество квартир в подъезде + 1
-        int entranceNumber = flatNumber / flatsInEntrance + (orderOfFlatInEntrance == 0 ? 0 : 1);
 
-        int orderOfFlatAtFloor = orderOfFlatInEntrance % 4;
-        //ищем номер этажа (порядковый номер квартиры в подъезде делим на кол-во квартир на этаже)
-        int floorNumber = (orderOfFlatInEntrance==0?floorAmount:orderOfFlatInEntrance/4) + (orderOfFlatAtFloor == 0 ? 0 : 1);
-        String endOfReturnalText = switch (orderOfFlatAtFloor) {
-            case 1 -> "слева от лифта, влево";
-            case 2 -> "слева от лифта, вправо";
-            case 3 -> "справа от лифта, влево";
-            case 0 -> "справа от лифта, вправо";
-            default -> "";
-        };
-        return String.format("%d кв – %d подъезд, %d этаж, %s", flatNumber, entranceNumber, floorNumber, endOfReturnalText);
+        int flatOnFloor = 4;
+        String resultString="";
+
+        if (flatNumber > (floorAmount * entranceAmount * 4) || flatNumber <= 0) {
+            return "Такой квартиры в доме нет.";
+        }
+
+        else {
+
+            int flatEnterance = (flatNumber - 1) / (floorAmount * flatOnFloor) + 1;
+            int flatFloor = ((flatNumber - 1) / flatOnFloor) % floorAmount + 1;
+            int flatCoordinates = (flatNumber - 1) % flatOnFloor + 1;
+
+            switch (flatCoordinates) {
+                case 1:
+                    resultString = flatNumber + " кв – " + flatEnterance + " подъезд, " + flatFloor + " этаж, слева от лифта, влево";
+                    break;
+                case 2:
+                    resultString = flatNumber + " кв – " + flatEnterance + " подъезд, " + flatFloor + " этаж, слева от лифта, вправо";
+                    break;
+                case 3:
+                    resultString = flatNumber + " кв – " + flatEnterance + " подъезд, " + flatFloor + " этаж,справа от лифта, влево";
+                    break;
+                case 4:
+                    resultString = flatNumber + " кв – " + flatEnterance + " подъезд, " + flatFloor + " этаж, справа от лифта, вправо";
+                    break;
+            }
+
+            return resultString;
+
+        }
     }
 }
