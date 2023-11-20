50f53b14d87048ef5b11fcdbe32c0172f4f5b1d4
stogov_so <stogov_sergei@mail.ru>
2023-11-20 11:45:58 +0500

    Chapter1_task1.

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,22 +6,23 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 111;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 
-        if (age % 10 == 1 && age != 11) {
-            return "Вам " + age + " год.";
-        } else if (age % 10 > 1 && age % 10 < 5) {
-            if (age < 12 || age > 14) {
-                return "Вам " + age + " года.";
-            }
-            return "Вам " + age + " лет.";
-        } else if (age <= 0 || age > 127) {
+        if (age <= 0 || age > 127) {
             return "Возраст введен некоректно. Проверьте введенное значение.";
+        } else if (age % 100 < 5 || age % 100 > 20) {
+            if (age % 10 == 1) {
+                return "Вам " + age + " год.";
+            } else if (age % 10 > 1 && age % 10 < 5) {
+                return "Вам " + age + " года.";
+            } else {
+                return "Вам " + age + " лет.";
+            }
         } else {
             return "Вам " + age + " лет.";
         }
