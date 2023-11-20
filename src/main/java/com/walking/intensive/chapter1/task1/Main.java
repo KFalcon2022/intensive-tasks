e9e2097d382e5d6e2f6d9c6a75d434011456ae17
Oleg <kovalyovop@yandex.ru>
2023-11-20 22:51:30 +0300

    Внесены правки, согласно полученным замечаниям:
    -Оформить согласно требованиям
    -Сигнатуру вернуть. Метод должен принимать параметр int и возвращать строку.
    -Сканер в задаче не требовался

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,36 +1,28 @@
 package com.walking.intensive.chapter1.task1;
-
-import java.util.Scanner;
-
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-        getAge();
+        int age = 127;
+
+        System.out.println(getAgeString(age));
     }
 
-    static void getAge() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число от 1 до 127 включительно:");
-        int year = scanner.nextInt();
-
-        scanner.close();
-
-        if (year < 1 || year > 127) {
-            System.out.println("Введенное число за пределами заданного периода.\n" +
-                    "Запустите программу снова и введите число от 1 до 127 включительно.");
-            return;
+    static String getAgeString(int age) {
+        if (age < 1 || age > 127) {
+            return "Введенное число за пределами заданного периода.\n" +
+                    "Запустите программу снова и введите число от 1 до 127 включительно.";
         }
 
-        if ((year >= 11 && year <= 14) || (year >= 111 && year <= 114)) {
-            System.out.println("Вам " + year + " лет");
-        } else if (year % 10 == 1) {
-            System.out.println("Вам " + year + " год");
-        } else if (year % 10 == 2 || year % 10 == 3 || year % 10 == 4) {
-            System.out.println("Вам " + year + " года");
+        if ((age >= 11 && age <= 14) || (age >= 111 && age <= 114)) {
+            return "Вам " + age + " лет";
+        } else if (age % 10 == 1) {
+            return "Вам " + age + " год";
+        } else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) {
+            return "Вам " + age + " года";
         } else {
-            System.out.println("Вам " + year + " лет");
+            return "Вам " + age + " лет";
         }
     }
 }
