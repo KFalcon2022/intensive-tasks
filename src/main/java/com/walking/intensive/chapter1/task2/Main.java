cb82708a553c838c8498fc350226ac5538a6c6a6
dimapros <dima.ru.98@mail.ru>
2023-11-20 22:38:59 +1000

    Задание 3.

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,17 +1,65 @@
 package com.walking.intensive.chapter1.task2;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
- */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Введите количество этажей: ");
+        int floorAmount = sc.nextInt();
+        if (floorAmount <= 0) {
+            System.out.println("Количество этажей не может быть меньше 1.");
+            return;
+        }
+
+        System.out.println("Введите количество подъездов: ");
+        int entranceAmount = sc.nextInt();
+        if (entranceAmount <= 0) {
+            System.out.println("Количество подъездов не может быть меньше 1.");
+            return;
+        }
+
+        System.out.println("Введите номер квартиры: ");
+        int flatNumber = sc.nextInt();
+        if (flatNumber <= 0) {
+            System.out.println("Номер квартиры должен быть равен хотя бы 1.");
+            return;
+        }
+
+        sc.close();
+
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int flatCounter = 0;
+        String location = "";
 
-        return null; // Заглушка. При реализации - удалить
+        for (int i = 1; i <= entranceAmount; i++) {
+            for (int j = 1; j <= floorAmount; j++) {
+                for (int k = 1; k <= 4; k++) {
+                    flatCounter++;
+                    if (flatCounter == flatNumber) {
+                        location = flatNumber + " кв - " + i + " подъезд, " + j + " этаж, ";
+                        switch (k) {
+                            case (1):
+                                location = location + "слева от лифта, влево.";
+                                return location;
+                            case (2):
+                                location = location + "слева от лифта, вправо.";
+                                return location;
+                            case (3):
+                                location = location + "справа от лифта, влево.";
+                                return location;
+                            case (4):
+                                location = location + "справа от лифта, вправо.";
+                                return location;
+                        }
+                    }
+                }
+            }
+        }
+        return "В доме с таким количеством этажей и подъездов не может быть квартиры с номером " + flatNumber + ".";
     }
 }
