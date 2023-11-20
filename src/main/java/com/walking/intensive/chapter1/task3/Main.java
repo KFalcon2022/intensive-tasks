9bd745881f482ad53351d9b7b1250368963c326d
Oleg <kovalyovop@yandex.ru>
2023-11-21 00:12:57 +0300

    Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -5,13 +5,14 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        boolean isLeapYear = isLeap(2024);
+        String typeYear = (isLeapYear) ? "Введенный год является високосным" : "Введенный год не является високосным";
+        System.out.println(typeYear);
 
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        final int dividerLeapYear = 4;
+        return year % dividerLeapYear == 0;
     }
 }
