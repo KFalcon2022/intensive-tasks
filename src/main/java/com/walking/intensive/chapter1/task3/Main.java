e48336e9419cd1d3ad317a3443c9ed91d68ffc23
user <tasiqq@gmail.com>
2023-11-20 17:30:34 +0300

    Chapter#1, Task#3: - изменен метод isLeap.

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 
-        int years[] = {2023, 2020, 2000, 1996, 1900, 1800, 1600, 1200, 444, 500, 400, 0};
+        int years[] = {2023, 2020, 2000, 1996, 1900, 1800, 1600, 1200, 444, 500, 400, 0, -44, -100, -400};
 
         for (int year : years
         ) {
@@ -15,12 +15,7 @@ public class Main {
     }
 
     static boolean isLeap(int year) {
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0) {
-            return false;
-        } else if (year % 4 == 0) {
-            return true;
-        } else return false;
+
+        return (year % 400 == 0 || (year % 100 != 0 & year % 4 == 0));
     }
 }
