9e0dfc8c74193a41fd88bc1ed8f4d50ef9add906
bergen <bergy@yandex.ru>
2023-11-20 17:44:07 +0300

    Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -11,7 +11,11 @@ public class Main {
         double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
-
+//        System.out.println(solveQuadraticEquation(0, 0, 5));
+//        System.out.println(solveQuadraticEquation(1, 5, 4));
+//        System.out.println(solveQuadraticEquation(1, -1, -90));
+//        System.out.println(solveQuadraticEquation(1, 12, 36));
+//        System.out.println(solveQuadraticEquation(3, 4, 10));
     }
 
     /**
@@ -27,7 +31,20 @@ public class Main {
      */
     static String solveQuadraticEquation(double a, double b, double c) {
         //        Место для вашего кода
+        if (a == b) return "Бесконечность не предел!";
+        double d = (b * b) - (4 * a * c);
+        if (d < 0) return "Количество решений: 0.";
+        if (d == 0) {
+            double x = (b * -1) / ( 2 * a );
+            return "Количество решений: 1. Корень: " + x;
+        }
 
+        if (d > 0) {
+            double x1 = ((b * -1) + (Math.sqrt(d)) ) / ( 2 * a );
+            double x2 = ((b * -1) - (Math.sqrt(d)) ) / ( 2 * a );
+            if (x1 < x2) return "Количество решений: 2. Корни:" + x1 + ";"+ x2;
+            if (x1 > x2) return "Количество решений: 2. Корни:" + x2 + ";"+ x1;
+        }
         return null; // Заглушка. При реализации - удалить
     }
 }
