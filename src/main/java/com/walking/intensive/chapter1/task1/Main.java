a1f3610385afdd00a3043e2aba7783a97b39f016
KrisLu23 <krisbondarev@yandex.ru>
2023-11-20 17:13:55 +0300

    Task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,12 +6,10 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-//        int age = 114;
-//
-//        System.out.println(getAgeString(age));
-        for (int i = 0; i < 127; i++) {
-            System.out.println(getAgeString(i));
-        }
+        int age = 114;
+
+        System.out.println(getAgeString(age));
+
 
     }
 
