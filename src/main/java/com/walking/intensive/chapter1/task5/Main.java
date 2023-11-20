3c9c1fcc17e161f2539e14db2aadefd054089bd7
ganka-xn <jxn@inbox.ru>
2023-11-20 19:56:05 +0300

    task5

--- src/main/java/com/walking/intensive/chapter1/task5/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Main.java
@@ -1,5 +1,8 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.util.Arrays;
+import java.util.TreeSet;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-triangle.html">ссылка</a>
  */
@@ -19,53 +22,92 @@ public class Main {
      * Располагайте высоты по возрастанию.
      */
     static double[] getHeights(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        TreeSet<Double> heights = new TreeSet<>();
+        heights.add(2 * getAreaByHeron(a, b, c)/ a);
+        heights.add(2 * getAreaByHeron(a, b, c)/ b);
+        heights.add(2 * getAreaByHeron(a, b, c)/ c);
+
+        return heights.stream().mapToDouble(Double::doubleValue).toArray();
     }
 
     /**
      * Располагайте медианы по возрастанию.
      */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
+        TreeSet<Double> medians = new TreeSet<>();
+        medians.add(Math.sqrt(2 * (a * a + c * c) - b * b) / 2);
+        medians.add(Math.sqrt(2 * (a * a + b * b) - c * c) / 2);
+        medians.add(Math.sqrt(2 * (b * b + c * c) - a * a) / 2);
 
-        return null; // Заглушка. При реализации - удалить
+        return medians.stream().mapToDouble(Double::doubleValue).toArray();
+
     }
 
     /**
      * Располагайте биссектрисы по возрастанию.
      */
     static double[] getBisectors(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        TreeSet<Double> bisectors = new TreeSet<>();
+        bisectors.add(Math.sqrt(a * b * (a + b + c) * (a + b - c)) / (a + b));
+        bisectors.add(Math.sqrt(a * c * (a + b + c) * (a + c - b)) / (a + c));
+        bisectors.add(Math.sqrt(b * c * (a + b + c) * (b + c - a)) / (b + c));
+
+        return bisectors.stream().mapToDouble(Double::doubleValue).toArray();
     }
 
     /**
      * Располагайте углы по возрастанию.
      */
     static double[] getAngles(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        TreeSet<Double> angles = new TreeSet<>();
+        angles.add(radiansToDegrees(Math.acos((a * a + b * b - c * c) / (2 * a * b))));
+        angles.add(radiansToDegrees(Math.acos((a * a + c * c - b * b) / (2 * a * c))));
+        angles.add(radiansToDegrees(Math.acos((b * b + c * c - a * a) / (2 * b * c))));
+
+        return angles.stream().mapToDouble(Double::doubleValue).toArray();
     }
 
     static double getInscribedCircleRadius(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return 0; // Заглушка. При реализации - удалить
+        return getAreaByHeron(a, b, c) / getSemiPerimeter(a, b, c);
     }
 
     static double getCircumradius(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return 0; // Заглушка. При реализации - удалить
+        return a * b * c / (4 * getAreaByHeron(a, b, c));
     }
 
     static double getAreaAdvanced(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return 0; // Заглушка. При реализации - удалить
+        double cosAlpha = (b * b + c * c - a * a) / (2 * b * c);
+        double sinAlpha = Math.sqrt(1 - cosAlpha * cosAlpha);
+
+        return b * c * sinAlpha / 2;
+    }
+
+    static boolean isTriangleExists(double a, double b, double c) {
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return false;
+        }
+
+        if (a + b > c && b + c > a && a + c > b) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static double getSemiPerimeter(double a, double b, double c) {
+
+        return (a + b + c) / 2;
+    }
+
+    static double radiansToDegrees(double radians) {
+
+            return (radians * 180) / Math.PI;
     }
 }
