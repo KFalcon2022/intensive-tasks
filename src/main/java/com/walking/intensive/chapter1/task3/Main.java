5b3f2202035085f8fffab332da5aa042ddea4eed
Alexey <lolex1@mail.ru>
2023-11-20 19:45:56 +0300

    test2

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -5,13 +5,17 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        if (isLeap(2013)){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        if ((year % 4 == 0 && year % 100 != 0) | year % 400 == 0){
+            return true;
+        }
+        return false;
     }
 }
