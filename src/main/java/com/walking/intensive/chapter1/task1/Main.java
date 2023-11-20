fef07270a8b3c24b9bc48a098315b75808bca4f1
dimapros <dima.ru.98@mail.ru>
2023-11-20 19:41:34 +1000

    Задание 1.

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,41 @@
 package com.walking.intensive.chapter1.task1;
 
-/**
- * РЈСЃР»РѕРІРёРµ: <a href="https://geometry-math.ru/homework/Java-age.html">СЃСЃС‹Р»РєР°</a>
- */
 public class Main {
     public static void main(String[] args) {
-//        Р”Р»СЏ СЃРѕР±СЃС‚РІРµРЅРЅС‹С… РїСЂРѕРІРµСЂРѕРє РјРѕР¶РµС‚Рµ РґРµР»Р°С‚СЊ Р»СЋР±С‹Рµ РёР·РјРµРЅРµРЅРёСЏ РІ СЌС‚РѕРј РјРµС‚РѕРґРµ
-        int age = 0;
+        int age = 100;
+        int age1 = -5;
+        int age2 = 0;
+        int age3 = 2;
+        int age4 = 6;
+        int age5 = 10;
+        int age6 = 37;
+        int age7 = 111;
+        int age8 = 130;
 
         System.out.println(getAgeString(age));
+        System.out.println(getAgeString(age1));
+        System.out.println(getAgeString(age2));
+        System.out.println(getAgeString(age3));
+        System.out.println(getAgeString(age4));
+        System.out.println(getAgeString(age5));
+        System.out.println(getAgeString(age6));
+        System.out.println(getAgeString(age7));
+        System.out.println(getAgeString(age8));
     }
 
     static String getAgeString(int age) {
-//        РњРµСЃС‚Рѕ РґР»СЏ РІР°С€РµРіРѕ РєРѕРґР°
-
-        return null; // Р—Р°РіР»СѓС€РєР°. РџСЂРё СЂРµР°Р»РёР·Р°С†РёРё - СѓРґР°Р»РёС‚СЊ
+        if (age < 0) {
+            return "Введено отрицательное число. Введите число, равное не менее 0.";
+        } else if (age > 127) {
+            return "Превышен рекорд долгожительности, равного 127 лет.";
+        } else if (age % 10 == 0 || (age <= 20 && age >= 11) || (age <= 120 && age >= 111)) {
+            return "Вам " + age + " лет.";
+        } else if (age % 10 <= 4 && age % 10 >= 2) {
+            return "Вам " + age + " года.";
+        } else if (age % 10 <= 9 && age % 10 >= 5) {
+            return "Вам " + age + " лет.";
+        } else {
+            return "Вам " + age + " год.";
+        }
     }
 }
