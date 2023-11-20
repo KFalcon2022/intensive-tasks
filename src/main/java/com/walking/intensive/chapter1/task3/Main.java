947b953c2598f9582791fef77fafd56388a07c95
k2336351 <k2336351@yandex.ru>
2023-11-20 17:18:03 +0300

    Solution task3

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,12 +6,33 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(isLeap(1975));
+        System.out.println(isLeap(1724));
+        System.out.println(isLeap(2000));
+        System.out.println(isLeap(1300));
 
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
+        boolean isLeap;
 
-        return false; // Заглушка. При реализации - удалить
+        int remainderOfDivisionOnFour = year % 4;
+        int remainderOfDivisionOnOneHundred = year % 100;
+
+        // Если год не кратен 4, то он точно невисокосный
+        if (remainderOfDivisionOnFour != 0) {
+            isLeap = false;
+        } else {
+            // Если год кратен 4, но не кратен 100, он високосный
+            if (remainderOfDivisionOnOneHundred != 0) {
+                isLeap = true;
+            } else {
+                // Если результат деления года на 100 кратен 4, то год високосный,
+                // а если не кратен - невисокосный
+                isLeap = (year / 100) % 4 == 0;
+            }
+        }
+
+        return isLeap;
     }
 }
