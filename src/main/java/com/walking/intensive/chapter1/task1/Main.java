89c573baa905f30a3db1b11bc5a8c25b9e53c09c
cranium1983 <shagen.arustamov@gmail.com>
2023-11-20 10:01:15 +0300

    Решение task1. Доработка по замечанию

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 41;
+        int age = 124;
 
         System.out.println(getAgeString(age));
     }
@@ -18,10 +18,10 @@ public class Main {
         }
 
         short year = (short) (age % 10);
+        short century = (short) (age % 100);
 
         String yearStr;
-
-        if (age == 11 || age == 12 || age == 13 || age == 14) {
+        if (century == 11 || century == 12 || century == 13 || century == 14) {
             yearStr = "лет";
         } else if (year == 1) {
             yearStr = "год";
