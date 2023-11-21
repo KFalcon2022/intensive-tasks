5c3bff271e63a8a3d4f556ae92c8f1b21fdaec94
MariaMasha91 <poleznosti@bk.ru>
2023-11-21 16:40:10 +0300

    Решение первой задачи через if-else

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -7,13 +7,26 @@ public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         int age = 0;
-
+        if (age < 0) {
+            System.out.println("Вы еще не родились:)");
+        }
+        else if (age % 100 == 11 | age % 100 == 12 | age % 100 == 13 | age % 100 == 14) {
+            System.out.println("Вам " + age + " лет");
+        }
+        else if (age % 10 == 1) {
+            System.out.println("Вам " + age + " год");
+        }
+        else if (age % 10 == 2 | age % 10 == 3 | age % 10 == 4) {
+            System.out.println("Вам " + age + " года");
+        }
+        else {
+            System.out.println("Вам " + age + " лет");
+        }
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+//      Место для вашего кода
+        return null;
     }
 }
