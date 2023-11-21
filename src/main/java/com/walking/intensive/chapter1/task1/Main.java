90b7816f4f7a6feac2f7d851dbefa23606b9dece
esysolina <evelina.ova@gmail.com>
2023-11-22 00:03:53 +0500

    solution of first task

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,37 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.io.*;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
-    public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+    public static void main(String[] args) throws IOException {
 
-        System.out.println(getAgeString(age));
+        System.out.println("Ведите ваш возраст: ");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String youAge = reader.readLine();
+        int nAge = Integer.parseInt(youAge);
+
+        System.out.println("Вам " + youAge + " " + getAgeString(nAge));
+
     }
 
-    static String getAgeString(int age) {
-//        Место для вашего кода
+    static String getAgeString(int number) {
+        int ageLastNumber = number % 10;
+        boolean isExclusion = (number % 100 >= 11) && (number % 100 <= 14);
+        String old = "";
 
-        return null; // Заглушка. При реализации - удалить
+        if (ageLastNumber == 1)
+            old = "год";
+        else if (ageLastNumber == 0 || ageLastNumber >= 5 && ageLastNumber <= 9)
+            old = "лет";
+        else if (ageLastNumber >= 2 && ageLastNumber <= 4)
+            old = "года";
+        if (isExclusion)
+            old = "лет";
+
+        return old;
     }
 }
