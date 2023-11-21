8657647b5772e1dec863e4c951c45c837a4b0022
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-11-21 21:31:48 +0300

    commit7

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -3,15 +3,15 @@ package com.walking.intensive.chapter1.task3;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-leap-year.html">ссылка</a>
  */
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
 
         isLeap(100);
     }
 
-    static boolean isLeap(int year) {
+    static boolean isLeap(int year){
 
-        return (year % 4 == 0 && year % 100 != 0) ? true : false;
+        return year % 4 == 0 && year % 100 != 0;
 
     }
 }
