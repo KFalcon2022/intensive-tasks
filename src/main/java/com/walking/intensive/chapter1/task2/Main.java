d7f561e54e168eee515f27dd0493e15a3c604c11
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-11-21 21:26:10 +0300

    commit6

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -6,21 +6,21 @@ package com.walking.intensive.chapter1.task2;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(getFlatLocation(2, 1, 2));
+        System.out.println(getFlatLocation(2, 2, 1));
     }
 
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        String result;
-        int flatsPerFloor = 4;
-        int entrance = 1 + (flatNumber / (floorAmount * flatsPerFloor));
-        int floor = 1 + flatNumber / flatsPerFloor;
 
-        if ((flatNumber % (floorAmount * flatsPerFloor)) == 0) {       // Этот и следующие 2 блока if не обязательны, но я не справился с выводом формул, :(
-            entrance -= 1;                                             // Буду рад если выложите разбор этой задачи в будущем
+        final int FLATS_PER_FLOOR = 4;
+        int entrance = 1 + (flatNumber / (floorAmount * FLATS_PER_FLOOR));
+        int floor = 1 + flatNumber / FLATS_PER_FLOOR;
+
+        if ((flatNumber % (floorAmount * FLATS_PER_FLOOR)) == 0) {
+            entrance -= 1;
         }
 
-        if ((flatNumber % flatsPerFloor) == 0) {
+        if ((flatNumber % FLATS_PER_FLOOR) == 0) {
             floor -= 1;
         }
 
@@ -28,17 +28,21 @@ public class Main {
             floor = floor - floorAmount * (entrance - 1);
         }
 
+
         if (flatNumber <= 0 || entranceAmount < entrance) {
-            result = "Такой квартиры в доме нет";
-        } else if (flatNumber % flatsPerFloor == 1) {
-            result = entrance + " подъезд, " + floor + " этаж, слева от лифта, влево";
-        } else if (flatNumber % flatsPerFloor == 2) {
-            result = entrance + " подъезд, " + floor + " этаж, слева от лифта, вправо";
-        } else if (flatNumber % flatsPerFloor == 3) {
-            result = entrance + " подъезд, " + floor + " этаж, справа от лифта, влево";
-        } else {
-            result = entrance + " подъезд, " + floor + " этаж, справа от лифта, вправо";
+            return "Такой квартиры в доме нет";
         }
-        return result;
+
+        int position = flatNumber % FLATS_PER_FLOOR;
+
+        switch (position) {
+            case (1):
+                return entrance + " подъезд, " + floor + " этаж, слева от лифта, влево";
+            case (2):
+                return entrance + " подъезд, " + floor + " этаж, слева от лифта, вправо";
+            case (3):
+                return entrance + " подъезд, " + floor + " этаж, справа от лифта, влево";
+        }
+        return entrance + " подъезд, " + floor + " этаж, справа от лифта, вправо";
     }
 }
