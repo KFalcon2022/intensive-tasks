99b471bd3137cf0f7fe78958af1e1dd5d58a4c2c
Reilan <reilan86@gmail.com>
2023-11-21 15:39:32 +0300

    Chapter 1 task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -8,27 +8,24 @@ public class Main {
 //        Для собственных проверок можете делать любые изменения в этом методе
         int age = 31;
 
-        System.out.println("Вам " + age + getAgeString(age));
+        System.out.println(getAgeString(age));;
     }
 
     static String getAgeString(int age) {
-        String resultString;
-
         if (age > 127 || age < 0) {
             return "Давай не будем обманывать, сладкий пирожочек, напиши сколько тебе лет на самом деле.";
         }
 
+        String resultString = String.format("Вам %d ", age);
+
         if (age % 10 == 1 && age % 100 != 11) {
-            resultString = " год.";
+            resultString += "год.";
+        } else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+            resultString += "года.";
+        } else {
+            resultString += "лет.";
         }
 
-        else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
-            resultString = " года.";
-        }
-
-        else {
-            resultString = " лет.";
-        }
         return resultString;
     }
 
