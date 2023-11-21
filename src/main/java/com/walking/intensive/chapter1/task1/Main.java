7e6a8ceb0ff01c3e0b0b87526d3a94e327af93b3
lambo064 <tasha_lambo@mail.ru>
2023-11-21 16:09:43 +0300

    исправила

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-        int age =102 ;
+        int age = 24;
         System.out.println(getAgeString(age));
     }
 
@@ -13,24 +13,26 @@ public class Main {
 
         if ((age >= 10 && age <= 20) || (age >= 110 && age <= 120)) {
             return ("Вам " + age + " лет");
-        }
-        else if (age == 1) return ("Вам " + age + " год");
-        else {
+        } else if (age == 1) {
+            return ("Вам " + age + " год");
+        } else {
             int lastAgeInt = getLastAgeInt(age);
 
             if (lastAgeInt > 1 && lastAgeInt < 5) {
                 return ("Вам " + age + " года");
             } else if (lastAgeInt == 0 || (lastAgeInt >= 5 && lastAgeInt <= 9)) {
                 return ("Вам " + age + " лет");
-            } else return ("Вам " + age + " год");
+            } else {
+                return ("Вам " + age + " год");
+            }
         }
     }
 
     static int getLastAgeInt(int age) {
-        if (age > 100) {
-            return (age % 100 % 10);
-        } else if (age > 10) {
+        if (age > 10) {
             return (age % 10);
-        } else return age;
+        } else {
+            return age;
+        }
     }
 }
