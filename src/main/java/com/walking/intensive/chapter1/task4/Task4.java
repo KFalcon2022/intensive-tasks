6eef673504d926a1b38713c8a710cb2f3038a091
EkkoXIII <astaroth644@gmail.com>
2023-11-21 21:01:46 +0300

    Решение задачи 4 из главы 1

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -1,17 +1,15 @@
-package com.walking.intensive.chapter1.task4;
+package src.main.java.com.walking.intensive.chapter1.task4;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-parameter.html">ссылка</a>
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
         double b = 0;
         double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
-
     }
 
     /**
@@ -26,8 +24,28 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if ((a==0)&&(b==0)){
+            return "Выжение не явлется уравненением!";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant == 0) {
+            double root = (0 - b) / (2 * a);
+            return "Количество решений: 1. Корень: " + root;
+        }
+
+        if (discriminant > 0) {
+            double rootOne = (((0 - b) + Math.sqrt(discriminant))) / (2 * a);
+            double rootTwo = (((0 - b) - Math.sqrt(discriminant))) / (2 * a);
+
+            if (rootOne < rootTwo) {
+                return "Количество решений: 2. Корни: " + rootOne + ";" + rootTwo;
+            } else {
+                return "Количество решений: 2. Корни: " + rootTwo + ";" + rootOne;
+            }
+        }
+
+        return "Количество решений: 0.";
     }
 }
