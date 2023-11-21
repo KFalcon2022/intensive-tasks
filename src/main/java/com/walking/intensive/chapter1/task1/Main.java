bbefa4e35588c22532fc9739c6a65c5580dcf66c
Ramaw <lohnez09@yandex.ru>
2023-11-21 11:46:45 +0300

    Chapter 1, Task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,17 +5,15 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-        int age = 124;
+        int age = 115;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        if (age % 10 == 1 && age != 11) {
+        if (age % 10 == 1 && age % 100 != 11) {
             return "Вам " + age + " год";
-        } else if (age == 2 || age == 3 || age == 4) {
-            return "Вам " + age + " года";
-        } else if ((age > 21) && (age % 10 == 2 || age % 10 == 3 || age % 10 == 4)) {
+        } else if ((age % 10 == 2 && age % 100 != 12) || (age % 10 == 3 && age % 100 != 13) || (age % 10 == 4 && age % 100 != 14)) {
             return "Вам " + age + " года";
         } else if(age <= 127 ){
             return "Вам " + age + " лет";
