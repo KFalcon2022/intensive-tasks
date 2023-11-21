c9afdcd91c0c48388af968d7f8bef12d95a1ad2e
sofajedi <kirill.ello@gmail.com>
2023-11-21 21:51:45 +0300

    Решение задачи 2 блока 1

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,36 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
 
+        int floorAmount = 9;
+        int entranceAmount = 4;
+        int flatNumber = 1;
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int totalFlats = floorAmount * entranceAmount * 4;
+        int flatFloor = (flatNumber - 1) / 4 + 1;
+        int flatEntrance = (flatFloor - 1) / floorAmount + 1;
+        int flatFloorDivided = flatFloor % floorAmount;
+        int dividedByFlatsOnFloor = flatNumber % 4;
+        if (flatFloorDivided == 0) {
+            flatFloorDivided = flatFloorDivided + floorAmount;
+        }
+        if (flatNumber <= 0 || flatNumber > totalFlats) {
+            return "Нет такой квартиры";
+        }
+        switch (dividedByFlatsOnFloor) {
+            case 0:
+                return flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloorDivided + " Этаж" + " справа от лифта, вправо";
+            case 1:
+                return flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloorDivided + " Этаж" + " слева от лифта, влево";
+            case 2:
+                return flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloorDivided + " Этаж" + " слева от лифта, вправо";
+            case 3:
+                return flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloorDivided + " Этаж" + " справа от лифта, влево";
+        }
+        return "Ошибка";
     }
 }
