44ed749b58dc7d3a8534221c09fafc74df8c3e83
DearCuba <rovbo85@gmail.com>
2023-11-21 11:14:14 +0500

    Внесены изменения в соответствии с замечаниями рецензента, а именно: - Проведена работа над оформлением, удалены лишние пустые строки и двоеточие; - Удалены лишние строки кода являющиеся рудиментарными;

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,38 +1,32 @@
 package com.walking.intensive.chapter1.task1;
+
 import java.util.Scanner;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
-    public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Веедите ваш возраст: ");
+    int age = scanner.nextInt();
+    scanner.close();
+    System.out.println(getAgeString(age));
+  }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Веедите ваш возраст: ");
-        int age = scanner.nextInt();
-        scanner.close();
-
-        System.out.println(getAgeString(age));
-
+  static String getAgeString(int age) {
+    int ageLastNum = age % 10;
+    String year = "";
+    if (ageLastNum == 1) {
+      year = " год";
+    } else if (ageLastNum == 0 || ageLastNum >= 5) {
+      year = " лет";
+    } else if (ageLastNum >= 2) {
+      year = " года";
     }
-
-    static String getAgeString(int age) {
-
-        int ageLastNum = age % 10;
-        String year= "";
-
-        if (ageLastNum == 1) {
-            year = " год";
-        } else if (ageLastNum == 0 || ageLastNum >= 5 && ageLastNum <= 9) {
-            year = " лет";
-        } else if (ageLastNum >= 2 && ageLastNum <= 4) {
-            year= " года";
-        }
-        if (age % 100 >= 11 && age % 100 <= 14) {
-            year= " лет";
-        }
-
-        return "Вам : " + age + year;
+    if (age % 100 >= 11 && age % 100 <= 14) {
+      year = " лет";
     }
+    return "Вам " + age + year;
+  }
 }
