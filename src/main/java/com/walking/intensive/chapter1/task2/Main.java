30b3c45ab92b602384f9a3776a92df9121d535d8
DearCuba <rovbo85@gmail.com>
2023-11-21 20:03:18 +0500

    Закончил задание 2 в главе 1.

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -4,37 +4,57 @@ package com.walking.intensive.chapter1.task2;
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Main {
-    public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int floorAmount = 30;
-        int entranceAmount = 9;
-        for (int i = 1; i < floorAmount * entranceAmount * 4 + 3; i++) {
-            System.out.println(getFlatLocation(floorAmount, entranceAmount, i));
-        }
-        //System.out.println(getFlatLocation(floorAmount, entranceAmount, 36));
-    }
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
-        int maxFlats = floorAmount * entranceAmount * 4;
-        int flatsInEntrance = floorAmount * 4;
-        if (flatNumber > maxFlats) {
-            return "Квартиры в доме нет";
-        }
-        int orderOfFlatInEntrance = flatNumber % flatsInEntrance;
-        //ищем номер подъезда делением номера квартиры на количество квартир в подъезде + 1
-        int entranceNumber = flatNumber / flatsInEntrance + (orderOfFlatInEntrance == 0 ? 0 : 1);
-
-        int orderOfFlatAtFloor = orderOfFlatInEntrance % 4;
-        //ищем номер этажа (порядковый номер квартиры в подъезде делим на кол-во квартир на этаже)
-        int floorNumber = (orderOfFlatInEntrance==0?floorAmount:orderOfFlatInEntrance/4) + (orderOfFlatAtFloor == 0 ? 0 : 1);
-        String endOfReturnalText = switch (orderOfFlatAtFloor) {
-            case 1 -> "слева от лифта, влево";
-            case 2 -> "слева от лифта, вправо";
-            case 3 -> "справа от лифта, влево";
-            case 0 -> "справа от лифта, вправо";
-            default -> "";
-        };
-        return String.format("%d кв – %d подъезд, %d этаж, %s", flatNumber, entranceNumber, floorNumber, endOfReturnalText);
+  public static void main(String[] args) {
+    int floorAmount = 5;
+    int entranceAmount = 3;
+    int flatPerFloor = 4;
+    for (int flatNum = 0; flatNum <= 61; flatNum++) {
+      getFlatLocation(floorAmount, entranceAmount, flatNum, flatPerFloor);
     }
+  }
+
+  static int increment(int x) {
+    return x + 1;
+  }
+
+  static int decrement(int x) {
+    return x - 1;
+  }
+
+  static int findEntranceNum(int flatNum, int maxFlatPerEntrance) {
+    return increment(decrement(flatNum) / maxFlatPerEntrance);
+  }
+
+  static int findFloorNum(int flatNum, int maxFlatPerEntrance, int flatPerFloor) {
+    return increment((decrement(flatNum) % maxFlatPerEntrance) / flatPerFloor);
+  }
+
+  static int findFlatLocation(int flatNum, int maxFlatPerEntrance, int flatPerFloor, int floorNum,
+      int entranceNum) {
+    return flatNum - flatPerFloor * decrement(floorNum) - maxFlatPerEntrance * decrement(
+        entranceNum);
+  }
+
+  static void getFlatLocation(int floorAmount, int entranceAmount, int flatNum, int flatPerFloor) {
+    int maxFlatAmount = floorAmount * entranceAmount * flatPerFloor;
+    if (flatNum > 0 && flatNum <= maxFlatAmount) {
+      int maxFlatPerEntrance = maxFlatAmount / entranceAmount;
+      int entranceNum = findEntranceNum(flatNum, maxFlatPerEntrance);
+      int floorNum = findFloorNum(flatNum, maxFlatPerEntrance, flatPerFloor);
+      int flatLocation = findFlatLocation(flatNum, maxFlatPerEntrance, flatPerFloor, floorNum,
+          entranceNum);
+      String location = switch (flatLocation) {
+        case 1 -> " слева от лифта, влево";
+        case 2 -> " слева от лифта, вправо";
+        case 3 -> " справа от лифта, влево";
+        case 4 -> " справа от лифта, вправо";
+        default -> "ошибка в определении местонахождения квартиры";
+      };
+      System.out.println(
+          flatNum + " кв – " + entranceNum + " подъезд, " + floorNum + " этаж," + location);
+    } else {
+      System.out.println("Такой квартиры в доме нет");
+    }
+  }
 }
