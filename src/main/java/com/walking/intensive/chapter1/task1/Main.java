b53d8d94a4a9a2a458c4ee8ee1f1eef14e348eca
apjam <apjam@yandex.ru>
2023-11-21 22:21:04 +0600

    task1 is done

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,15 +5,31 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 124;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        // 1 год, 2, 3, 4 года, 5 - 20, 21 год, 22 - 24 года, 25 - 30 лет,
+        if (age < 0 || age > 127) {
+            return "Wrong age";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        int lastNum = getLastNum(age);
+        if (lastNum == 1) {
+            return "Вам " + age + " год";
+        } else if (lastNum >= 2 && lastNum <= 4) {
+            return "Вам " + age + " года";
+        } else {
+            return "Вам " + age + " лет";
+        }
+    }
+
+    static int getLastNum(int age) {
+        if (age < 10) {
+            age += 10;
+        }
+        return age % 10;
     }
 }
