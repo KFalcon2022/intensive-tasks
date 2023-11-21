c5e9bd18f82dc377ea1a97babd54906d898a73e8
Pavlo <pavlokutsyn@gmail.com>
2023-11-22 00:49:56 +0100

    task1_20231121_PK

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,15 +5,22 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 103;
+        System.out.println("Вам " + age + " " + getAgeString(age));
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age % 100 >= 11 && age % 100 <= 14) {
+            return "лет";
+        } else {
+            if (age % 10 == 1) {
+                return "год";
+            } else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) {
+                return "года";
+            }
+            return "лет";
+        }
     }
 }
