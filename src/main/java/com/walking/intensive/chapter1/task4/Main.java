3dac883e57981b85b99928ae3377ff27ddcb9224
Reilan <reilan86@gmail.com>
2023-11-21 20:03:24 +0300

    Chapter 1 task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -24,27 +24,29 @@ public class Main {
         double squareRoot1, squareRoot2;
         if (a == 0 && b == 0 && c == 0) {
             return "Количество решений: бесконечное множество.";
-        } else if (a == 0 && b == 0) {
+        }
+        if (a == 0 && b == 0) {
             return "Количество решений: 0.";
-        } else if (a == 0) {
+        }
+        if (a == 0) {
             squareRoot1 = -c / b;
-            return "Количество решений: 1. Корень: " + squareRoot1;
+            return "Количество решений: 1. Корень: " + ((squareRoot1 == -0.0) ? "0.0" : squareRoot1);
+        }
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
+            return "Количество решений: 0.";
+        }
+        if (discriminant == 0) {
+            squareRoot1 = -b / (2 * a);
+            return "Количество решений: 1. Корень: " + ((squareRoot1 == -0.0) ? "0.0" : squareRoot1);
+        }
+        squareRoot1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        squareRoot2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        if (squareRoot1 > squareRoot2) {
+            return "Количество решений: 2. Корень: " + squareRoot2 + ", " + squareRoot1;
         } else {
-            double discriminant = b * b - 4 * a * c;
-            if (discriminant < 0) {
-                return "Количество решений: 0.";
-            } else if (discriminant == 0) {
-                squareRoot1 = -b / (2 * a);
-                return "Количество решений: 1. Корень: " + squareRoot1;
-            } else {
-                squareRoot1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-                squareRoot2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-                if (squareRoot1 > squareRoot2) {
-                    return "Количество решений: 2. Корень: " + squareRoot2 + ", " + squareRoot1;
-                } else {
-                    return "Количество решений: 2. Корень: " + squareRoot1 + ", " + squareRoot2;
-                }
-            }
+            return "Количество решений: 2. Корень: " + squareRoot1 + ", " + squareRoot2;
         }
     }
 }
+
