0b0c6e04e7cb7309c32fc72709c479efbd44642f
Makoto-2005 <makoto2005@yandex.ru>
2023-11-21 23:54:49 +0300

    rename parameter and delete else

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -10,24 +10,26 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Рекорд долгожительства на данный момент составляет 127 лет. Введите Ваш возраст: ");
-        int yourAge = in.nextInt();
+        int age = in.nextInt();
 
         in.close();
-        System.out.println(getAgeString(yourAge));
+        System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int yourAge) {
 
         if (yourAge < 0 || yourAge > 127) {
             return "Проверьте правильность введённых данных либо обратитесь в Книгу Рекордов Гинесса.";
-        } if (yourAge % 100 >= 11 && yourAge % 100 <= 19) {
-            return "Вам " + yourAge + " лет";
-        } if (yourAge % 10 >= 2 && yourAge % 10 <= 4) {
-            return "Вам " + yourAge + " года";
-        } if (yourAge % 10 == 1) {
-            return "Вам " + yourAge + " год";
-        } else {
+        }
+        if (yourAge % 100 >= 11 && yourAge % 100 <= 19) {
             return "Вам " + yourAge + " лет";
         }
+        if (yourAge % 10 >= 2 && yourAge % 10 <= 4) {
+            return "Вам " + yourAge + " года";
+        }
+        if (yourAge % 10 == 1) {
+            return "Вам " + yourAge + " год";
+        }
+        return "Вам " + yourAge + " лет";
     }
 }
