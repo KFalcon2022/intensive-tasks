227627f39050576c3c35336bed83223766709144
Reilan <reilan86@gmail.com>
2023-11-21 16:11:58 +0300

    Chapter 1 task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -1,17 +1,12 @@
 package com.walking.intensive.chapter1.task4;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-parameter.html">ссылка</a>
- */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        // Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
         double b = 0;
         double c = 0;
-
         System.out.println(solveQuadraticEquation(a, b, c));
-
     }
 
     /**
@@ -26,46 +21,30 @@ public class Main {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
-        double discriminant = Math.pow(b, 2) - 4 * a * c;
-        double squareRoot1 = 0, squareRoot2 = 0;
-
-        if (Double.isNaN(discriminant)) {
-            discriminant = 0;
-        }
-
-        if (discriminant < 0) {
+        double squareRoot1, squareRoot2;
+        if (a == 0 && b == 0 && c == 0) {
+            return "Количество решений: бесконечное множество.";
+        } else if (a == 0 && b == 0) {
             return "Количество решений: 0.";
-        }
-
-        if (discriminant == 0) {
-            squareRoot1 = -b / (2 * a);
-        }
-
-        if (Double.isNaN(squareRoot1)) {
-            squareRoot1 = 0;
+        } else if (a == 0) {
+            squareRoot1 = -c / b;
             return "Количество решений: 1. Корень: " + squareRoot1;
-        }
-
-        if (discriminant > 0) {
-            squareRoot1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            squareRoot2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-        }
-
-        if (Double.isNaN(squareRoot1)) {
-            squareRoot1 = 0;
-        }
-
-        if (Double.isNaN(squareRoot2)) {
-            squareRoot2 = 0;
-        }
-
-        if (squareRoot1 > squareRoot2) {
-            return "Количество решений: 2. Корень: " + squareRoot2 + ", " + squareRoot1;
-        }
-
-        else {
-            return "Количество решений: 2. Корень: " + squareRoot1 + ", " + squareRoot2;
+        } else {
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant < 0) {
+                return "Количество решений: 0.";
+            } else if (discriminant == 0) {
+                squareRoot1 = -b / (2 * a);
+                return "Количество решений: 1. Корень: " + squareRoot1;
+            } else {
+                squareRoot1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                squareRoot2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                if (squareRoot1 > squareRoot2) {
+                    return "Количество решений: 2. Корень: " + squareRoot2 + ", " + squareRoot1;
+                } else {
+                    return "Количество решений: 2. Корень: " + squareRoot1 + ", " + squareRoot2;
+                }
+            }
         }
     }
 }
