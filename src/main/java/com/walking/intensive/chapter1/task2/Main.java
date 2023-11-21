b4c7f451f0e0633009db241504c9aa74070fe73f
NightwolfCB <nightwolf63@rambler.ru>
2023-11-21 07:33:05 +0300

    Revert "task2 initial commit"
    This reverts commit b795b64006a1d3c9972327e9d10ff446ff2120b1.

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,46 +1,17 @@
 package com.walking.intensive.chapter1.task2;
 
-import java.util.Scanner;
-
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-        int floorAmount = 9;
-        int entranceAmount = 6;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Номер вашей квартиры:");
-        int flatNumber = input.nextInt();
-        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
+//        Для собственных проверок можете делать любые изменения в этом методе
+
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        String flatSide = "";
-        int entranceNumber = 0;
-        int floorNumber = 0;
-        int flatAmount = floorAmount * 4;
+        //        Место для вашего кода
 
-        if (flatNumber > flatAmount) {
-            entranceNumber = 1 + (flatNumber - 1) / flatAmount;
-            floorNumber = 1 + ((flatNumber - 1) - (entranceNumber - 1) * flatAmount) / 4;
-        } else if (flatNumber < flatAmount) {
-            entranceNumber = 1;
-            floorNumber = 1 + (flatNumber - 1) / 4;
-        } else if (flatNumber > flatAmount * entranceAmount) {
-            System.out.println("Такой квартиры не существует!");
-        }
-
-        if ((flatNumber % 4) == 1) {
-            flatSide = "слева от лифта, влево";
-        } else if ((flatNumber % 4) == 2) {
-            flatSide = "слева от лифта, вправо";
-        } else if ((flatNumber % 4) == 3) {
-            flatSide = "справа от лифта, влево";
-        } else if ((flatNumber % 4) == 0) {
-            flatSide = "справа от лифта, вправо";
-        }
-
-        return entranceNumber + " подъезд, " + floorNumber + " этаж, " + flatSide;
+        return null; // Заглушка. При реализации - удалить
     }
 }
