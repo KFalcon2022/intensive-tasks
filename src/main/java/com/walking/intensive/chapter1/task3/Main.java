7e50364817dfb66f14ed106e3f8b7e7cfaa2d595
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-21 14:08:04 +0300

    Решение Chapter1 Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -1,17 +1,28 @@
 package com.walking.intensive.chapter1.task3;
 
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-leap-year.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+        int year = 4;
+        System.out.println(isLeap(year));
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
+        if (year % 400 == 0) {
+            return true;
 
-        return false; // Заглушка. При реализации - удалить
+        } else {
+            if (year % 100 == 0) {
+                return false;
+            } else if (year % 4 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
