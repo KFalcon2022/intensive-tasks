7d5aed1894744ad7965ddd6b7706cf10a1dee14a
Артём <artyomlychkoff@gmail.com>
2023-11-21 13:02:45 +0300

    chapter1 task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,32 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        int flatNumber = 9;
+
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        String answer = "";
+        String afterElevatorRight = "справа от лифта, ";
+        String afterElevatorLeft = "слева от лифта, ";
+        String finalTurnLeft = "влево";
+        String finalTurnRight = "вправо";
 
-        return null; // Заглушка. При реализации - удалить
+        if (floorAmount * 4 * entranceAmount > flatNumber) {
+            if (flatNumber > 4) {
+                int floorCount = 1;
+                int entranceCount = 0;
+                while (flatNumber > 4) {
+                    flatNumber -= 4;
+                    floorCount += 1;
+                }
+
+        } else {
+//                String prefix = "1 подъезд, 1 этаж";
+//                String suffix = flatNumber % 2 == 0 ? finalTurnLeft : finalTurnRight;
+            }
+    } else { answer = "Такой квартиры нет"; }
+        return answer;
     }
 }
