eafbdf25efaaac12986f371ae6484f6e32e23519
Alexey <lolex1@mail.ru>
2023-11-21 20:16:26 +0300

    test2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,39 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        int floorAmount = 3;
+        int entranceAmount = 5;
+        int flatNumber = 19;
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        String result = "Такой квартиры нет";
+        if (!isFlatTrue(floorAmount, entranceAmount, flatNumber)){
+            return result;
+        }
+        String[] flatLocationOnFloor = {"справа от лифта, вправо", "слева от лифта, влево", "слева от лифта, вправо",
+                "справа от лифта, влево"};
+        result = flatNumber + " кв – "
+                + getEntrancePosition(floorAmount, flatNumber) + " подъезд, "
+                + getFloorPosition(floorAmount, flatNumber) + " этаж, "
+                + flatLocationOnFloor[getFlatPosition(flatNumber)];
+        return result;
+    }
 
-        return null; // Заглушка. При реализации - удалить
+    static int getFlatPosition(int flatNumber){
+        return flatNumber % 4;
+    }
+
+    static int getFloorPosition(int floorAmount, int flatNumber) {
+        return (flatNumber - 1) % (floorAmount * 4) / 4 + 1;
+    }
+
+    static int getEntrancePosition(int floorAmount, int flatNumber){
+        return (flatNumber - 1) / (floorAmount * 4) + 1;
+    }
+
+    static boolean isFlatTrue(int floorAmount, int entranceAmount, int flatNumber){
+        return ((floorAmount * 4 * entranceAmount) >= flatNumber) && flatNumber > 0;
     }
 }
