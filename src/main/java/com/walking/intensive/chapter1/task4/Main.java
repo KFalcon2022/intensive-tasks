6ef53b3df515b55bc6d2e40d7c5403ded823e197
Sergey G. <gsv.1996@mail.ru>
2023-11-22 00:41:19 +0400

    добавил скобок в блоки if и else

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -28,27 +28,27 @@ public class Main {
             //        Место для вашего кода
             String numberOfSolutions = "Количество решений: ";
             double firstRoot, secondRoot;
-            if (a == 0 && b == 0 && c == 0) return numberOfSolutions + "бесконечно.";
-            else if (a == 0 && b == 0) return numberOfSolutions + 1 + ". Корень: " + c + ".";
-            else if (a == 0 && c == 0 || b == 0 && c == 0) return numberOfSolutions + 1 + ". Корень: " + 0 + ".";
+            if (a == 0 && b == 0 && c == 0) {return numberOfSolutions + "бесконечно.";}
+            else if (a == 0 && b == 0) {return numberOfSolutions + 1 + ". Корень: " + c + ".";}
+            else if (a == 0 && c == 0 || b == 0 && c == 0) {return numberOfSolutions + 1 + ". Корень: " + 0 + ".";}
 
             else if (a == 0) {
                 return numberOfSolutions + 1 + ". Корень: " + (-c / b) + ".";
             } else if (b == 0) {
-                if (c < 0 && a < 0 || c > 0 && a > 0) return numberOfSolutions + 0 + ".";
+                if (c < 0 && a < 0 || c > 0 && a > 0) {return numberOfSolutions + 0 + ".";}
                 firstRoot = -Math.sqrt(c / a);
                 secondRoot = Math.sqrt(c / a);
                 return numberOfSolutions + 2 + ". Корни: " + firstRoot + ";" + secondRoot + ".";
             } else if (c == 0) {
                 firstRoot = 0;
                 secondRoot = -b / a;
-                if (secondRoot > 0) return numberOfSolutions + 2 + ". Корни: " + firstRoot + ";" + secondRoot + ".";
-                else return numberOfSolutions + 1 + ". Корень: " + firstRoot + ".";
+                if (secondRoot > 0) {return numberOfSolutions + 2 + ". Корни: " + firstRoot + ";" + secondRoot + ".";}
+                else {return numberOfSolutions + 1 + ". Корень: " + firstRoot + ".";}
 
             } else {
                 double discriminant = b * b - 4 * a * c;
-                if (discriminant < 0) return numberOfSolutions + 0 + ".";
-                else if (discriminant == 0) return numberOfSolutions + 1 + ". Корень: " + (-b / (2 * a)) + ".";
+                if (discriminant < 0) {return numberOfSolutions + 0 + ".";}
+                else if (discriminant == 0) {return numberOfSolutions + 1 + ". Корень: " + (-b / (2 * a)) + ".";}
                 else {
                     firstRoot = -b + Math.sqrt(discriminant) / 2 * a;
                     secondRoot = -b - Math.sqrt(discriminant) / 2 * a;
