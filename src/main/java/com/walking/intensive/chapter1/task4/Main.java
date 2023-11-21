a2d0666339e1f7f252b77e53f30e1672f5b94fb3
dimapros <dima.ru.98@mail.ru>
2023-11-21 21:33:39 +1000

    Задание 4.

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -1,33 +1,57 @@
 package com.walking.intensive.chapter1.task4;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-parameter.html">ссылка</a>
- */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите число a: ");
+        double a = sc.nextInt();
+
+        System.out.println("Введите число b: ");
+        double b = sc.nextInt();
+
+        System.out.println("Введите число c: ");
+        double c = sc.nextInt();
+
+        sc.close();
 
         System.out.println(solveQuadraticEquation(a, b, c));
-
     }
 
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        double discriminant = b * b - 4 * a * c;
+        int amountSolutions = 0;
 
-        return null; // Заглушка. При реализации - удалить
+        if (discriminant > 0) {
+            int x1 = (int)((-b + Math.sqrt(discriminant)) / (2 * a));
+            int x2 = (int)((-b - Math.sqrt(discriminant)) / (2 * a));
+
+            amountSolutions++;
+            if (a == 1 && (x1 + x2 == -b && x1 * x2 == c)) {             // Проверяю, есть ли решения по теореме Виетта.
+                amountSolutions++;
+            }
+
+            String answer = "Количество решений: " + amountSolutions + ". " + "Корни: ";
+
+            if (x1 < x2) {
+                answer+= x1 + ";" + x2;
+            }
+            else if (x2 < x1) {
+                answer += x2 + ";" + x1;
+            }
+            return answer;
+
+        } else if (discriminant == 0) {
+            int x = (int) (-b / 2 * a);
+            amountSolutions++;
+            if (a == 1 && (x + x == -b && x * x == c)) {
+                amountSolutions++;
+            }
+            return "Количество решений: " + amountSolutions + ". " + "Корень: " + x;
+        } else {
+            return "Количество решений: " + amountSolutions;
+        }
     }
 }
