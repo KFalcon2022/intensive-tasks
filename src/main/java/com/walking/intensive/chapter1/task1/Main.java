7add881010c1f43f0137b284e734ade0dda78ebd
Makoto-2005 <makoto2005@yandex.ru>
2023-11-21 17:14:16 +0300

    problem: 'Age'

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,32 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
 
-        System.out.println(getAgeString(age));
+        Scanner in = new Scanner(System.in);
+        System.out.println("Рекорд долгожительства на данный момент составляет 127 лет. Введите Ваш возраст: ");
+        int yourAge = in.nextInt();
+
+        in.close();
+        System.out.println(getAgeString(yourAge));
     }
 
-    static String getAgeString(int age) {
-//        Место для вашего кода
+    static String getAgeString(int yourAge) {
 
-        return null; // Заглушка. При реализации - удалить
+        if (yourAge <= 0 || yourAge > 127) {
+            return "Проверьте правильность введённых данных либо обратитесь в Книгу Рекордов Гинесса.";
+        } else if (yourAge % 100 >= 11 && yourAge % 100 <= 19) {
+            return "Вам " + yourAge + " лет";
+        } else if (yourAge % 10 >= 2 && yourAge % 10 <= 4) {
+            return "Вам " + yourAge + " годa";
+        } else if (yourAge % 10 == 1) {
+            return "Вам " + yourAge + " год";
+        } else
+            return "Вам " + yourAge + " лет";
     }
 }
