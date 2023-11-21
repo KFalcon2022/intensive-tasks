d485d0c096b68862f65badbd1c14f496385a1e6a
egor.djun <xedshotg@gmail.com>
2023-11-21 12:36:36 +0700

    hi 3 task

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -1,28 +1,42 @@
 package com.walking.intensive.chapter1.task3;
 
+import java.util.Random;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-leap-year.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
 
-        int year = 2023;
+        Random random = new Random();
+        int year = random.nextInt(1582, 2024);
 
-        if (isLeap(year)) {
-            System.out.println(year + " год - високостный!");
-        } else {
-            System.out.println(year + " год - невисокостный!");
-        }
+        System.out.println(isLeap(year) ? year + " год невискокостный!" : year + " год вискокостный!");
     }
 
     static boolean isLeap(int year) {
 
-        if (year % 400 == 0) {          //  високостный
-            return true;
-        } else if (year % 100 == 0) {   //  невисокостный
-            return false;
-        } else {
-            return year % 4 == 0;       //  високостный
-        }
+        return year % 100 == 0 || year % 4 != 0;
     }
+
 }
+
+
+
+
+
+   /* static boolean isLeap(int year) {
+
+        if (year % 400 == 0) {      //  високостный
+
+            return true;
+        }
+        if (year % 100 == 0) {   //  невисокостный
+            return false;
+        }
+        if (year % 4 == 0) {
+            return true;       //  високостный
+        }
+        return false;
+    }
+}*/
