9a067cf1d850a3932f60e99e5baaef0385684a3e
JacobAndrs <officaman@gmail.com>
2023-11-21 19:06:58 +0300

    Проверьте на работоспособность, знаю что надо бы переменных добавить, сделать более читабельным, если успею до субботы решить остальные, то наведу красоту

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,17 +1,55 @@
 package com.walking.intensive.chapter1.task2;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
- */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        Scanner inFloor = new Scanner(System.in);
+        System.out.print("Введите кол-во этажей: ");
+        int floor = inFloor.nextInt();
+
+        Scanner inEntrance = new Scanner(System.in);
+        System.out.print("Введите кол-во подьездов: ");
+        int entrance = inEntrance.nextInt();
+
+        Scanner inApartment = new Scanner(System.in);
+        System.out.print("Введите номер квартиры: ");
+        int flat = inApartment.nextInt();
+
+        System.out.println(getFlatLocation(floor, entrance, flat));
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        //flatNumber - квартира.   entranceAmount - подьезд. floorAmount - этажи
+        // Узнеаем есть ли вообще такая квартива в доме?/
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber > (floorAmount * 4 * entranceAmount) || flatNumber < 0) {
+            return "Такой квартиры нет";
+        }
+
+        //  Узнаем номер подьезда/
+        int entranceAnswer = (flatNumber - 1) / (floorAmount * 4) + 1;
+
+        //  Узнаем этаж*/
+        int florAnswer = floorAmount - ((floorAmount * 4 * entranceAnswer - flatNumber) / 4);
+
+        //  Направление движения/
+        int placeNom = flatNumber - ((floorAmount * 4) * (entranceAnswer - 1) + ((florAnswer - 1) * 4));
+
+        String place = switch (placeNom) {
+            default -> null;
+            case 1 -> "слева от лифта, влево";
+            case 2 -> "слева от лифта, вправо";
+            case 3 -> "справа от лифта, влево";
+            case 4 -> "справа от лифта, вправо";
+        };
+        return flatNumber + " кв - " + entranceAnswer + "подьезд," + florAnswer + " этаж," + place;
+
     }
 }
+
+
+
+
