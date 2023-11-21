69b30ccd3a1c5f75730e4fcbdd83bfee16b017c7
Greem4 <i@greem4.ru>
2023-11-21 12:31:04 +0300

    chapter 1, task4 update 2

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -26,42 +26,32 @@ public class Main {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        if (a == 0) {
-            if (b == 0) {
-                if (c == 0) {
-                    return "Количество решений: бесконечное количество";
-
-                } else {
-                    return "Количество решений: 0";
-                }
-
-            } else {
-                double x = -c / b;
-                return "Количество решений: 1. Корень: " + x;
-            }
-
-        } else {
-
-            double discriminant = b * b - 4 * a * c;
-
-            if (discriminant > 0) {
-                double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-                double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-                if (x1 < x2) {
-                    return "Количество решений: 2. Корни: " + x1 + " ; " + x2;
-                }else {
-                    return "Количество решений: 2. Корни: " + x2 + " ; " + x1;
-                }
-
-            } else if (discriminant == 0) {
-                double x = -b / (2 * a);
-                return "Количество решений: 1. Корень: " + x;
-
-            }else {
-
-                return "Количество решений: 0";
-
-            }
+        if (a == 0 && b == 0 && c == 0) {
+            return "Количество решений: бесконечное количество";
         }
+
+        if (a == 0 & b == 0) {
+            return "Количество решений: 0";
+        }
+
+        if (a == 0) {
+            double x = -c / b;
+            return "Количество решений: 1. Корень: " + x;
+        }
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            return "Количество решений: 2. Корни: " + Math.min(x1, x2) + " ; " + Math.max(x1, x2);
+        }
+
+        if (discriminant == 0) {
+            double x = -b / (2 * a);
+            return "Количество решений: 1. Корень: " + x;
+        }
+
+        return "Количество решений: 0";
     }
 }
