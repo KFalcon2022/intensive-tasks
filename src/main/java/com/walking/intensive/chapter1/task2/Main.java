0558a0638257d198064dfd108a13837098945ae2
ETSphere <elem174@proton.me>
2023-11-21 16:31:56 +0500

    Дом с квартирами.

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,40 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        System.out.println(getFlatLocation(3, 1, 25));
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int maxFlatFloor = 4;
+        if (flatNumber <= 0 || flatNumber > floorAmount * maxFlatFloor) {
+            System.out.printf("Сожалеем в %d подезде, на этаже %d, нет квартиры %d, искомая ", entranceAmount, floorAmount, flatNumber);
+        }
+        int entrance = (flatNumber - 1) / (floorAmount * maxFlatFloor) + 1;
+        int floor = ((flatNumber - 1) % (floorAmount * maxFlatFloor)) / maxFlatFloor + 1;
 
-        return null; // Заглушка. При реализации - удалить
+        String position = getString(flatNumber);
+
+        return String.format("квартира %d расположена в %d подезде, на %d этаже , %s", flatNumber, entrance, floor, position);
+    }
+
+    private static String getString(int flatNumber) {
+        String position = null;
+        String floorPosition1 = "слева от лифта, влево";
+        String floorPosition2 = "слева от лифта, вправо";
+        String floorPosition3 = "справа от лифта, влево";
+        String floorPosition4 = "справа от лифта, вправо";
+
+        if (flatNumber % 4 == 1) {
+            position = floorPosition1;
+        } else if (flatNumber % 4 == 2) {
+            position = floorPosition2;
+        } else if (flatNumber % 4 == 3) {
+            position = floorPosition3;
+        } else if (flatNumber % 4 == 0) {
+            position = floorPosition4;
+        }
+        return position;
     }
 }
