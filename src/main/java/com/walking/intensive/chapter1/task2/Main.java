95542f2c0e1ae3e9669bc3cfb2c176c708bdc0a7
Galina Magazinova <gmagazinova@gmail.com>
2023-11-21 11:58:12 +0300

    fix task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,17 +5,18 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-        int flatNumber = 71;
+        int flatNumber = 73;
         int floorAmount = 9;
         int entranceAmount = 3;
+
         System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         String flatLocation;
-        int flatsPerFloor = 4;
+        final int flatsPerFloor = 4;
 
-        if (flatNumber > flatsPerFloor * floorAmount * entranceAmount) {
+        if (flatNumber > flatsPerFloor * floorAmount * entranceAmount || flatNumber <= 0) {
             flatLocation = "Такой квартиры нет в доме";
         } else {
             int flatsPerEntrance = flatsPerFloor * floorAmount;
@@ -23,20 +24,16 @@ public class Main {
             int floorNumber = (int) Math.ceil((double) (flatNumber - (entranceNumber - 1) * flatsPerEntrance) / flatsPerFloor);
             int positionOnFloor = flatNumber % flatsPerFloor;
 
-            String positionMessage;
-            if (positionOnFloor == 0) {
-                positionMessage = "справа от лифта, вправо";
-            } else if (positionOnFloor == 1) {
-                positionMessage = "слева от лифта, влево";
-            } else if (positionOnFloor == 2) {
-                positionMessage = "слева от лифта, вправо";
-            } else {
-                positionMessage = "справа от лифта, влево";
-            }
+            String positionMessage = switch (positionOnFloor) {
+                case 0 -> "справа от лифта, вправо";
+                case 1 -> "слева от лифта, влево";
+                case 2 -> "слева от лифта, вправо";
+                default -> "справа от лифта, влево";
+            };
 
             flatLocation = flatNumber + " кв = " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + positionMessage;
-
         }
+
         return flatLocation;
     }
 }
