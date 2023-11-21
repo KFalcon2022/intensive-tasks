6bd2b0bfd339305b555ca6a4d860d6a576cb4ed2
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-11-21 22:02:26 +0300

    commit9

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -16,35 +16,30 @@ public class Main {
 
     static String solveQuadraticEquation(double a, double b, double c) {
 
-        if (a == 0 && b == 0) {
-            return "Нет решений.";
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное число решений.";
         }
 
-        if (!isQuadratic(a)) {
-            double x = -c / b;
-            return "Количество решений: 1, корень: " + x;
+        if (a == 0 && b == 0) {
+            return "Нет решений.";
         }
 
         double discriminant = b * b - 4 * a * c;
 
         if (isQuadratic(a)) {
             if (discriminant > 0) {
-
                 double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
                 double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
                 return "Количество решений: 2. Корни: " + x1 + ", " + x2;
-
             } else if (discriminant == 0) {
-
                 double x = (-b / (2 * a));
-                return "Количество решений: 1. Корень: " + x;
 
-            } else {
-                return ("Количество решений: 0.");
+                return "Количество решений: 1. Корень: " + x;
             }
-        } else {
-            return "Уравнение не квадратное.";
+             return ("Количество решений: 0.");
         }
+
+         return "Уравнение линейное. Число решений: 1, корень: " + (-c / b);
     }
 
     static boolean isQuadratic(double a) {
