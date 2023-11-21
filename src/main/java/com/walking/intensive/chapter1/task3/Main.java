6be6edfd815e420a582de644c85311a7779a369c
Vbliznev <7492008@mail.ru>
2023-11-21 15:39:00 +0300

    Merge pull request #87 from KFalcon2022/revert
    Revert

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,12 +6,15 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+        isLeap(1900);
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
+        boolean result = false;
+        if ((year % 4 == 0 & year % 100 != 0) | year % 400 == 0) {
+            result = true;
+        }
 
-        return false; // Заглушка. При реализации - удалить
+        return result; // Заглушка. При реализации - удалить
     }
 }
