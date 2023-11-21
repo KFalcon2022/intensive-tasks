edf394a136179d3f8a227edc19f17e23ca756f08
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-21 13:37:05 +0300

    Решение Chapter1 Task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,37 +5,36 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-/*
-floorAmoout - количество этажей, entranceAmount -колич подъездов,flatNumber - номер кв
-кол-во кв - 4 * floorAmoout * entranceAmount
- */
-
+        System.out.println(getFlatLocation(3, 2, 16));
     }
+
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        final int FLATonFLOOR = 4;
-    final int totalFLAT = FLATonFLOOR * entranceAmount * floorAmount;
-    if (totalFLAT < flatNumber || flatNumber < 1){
-        return "Такой квартиры нет в данном доме";
-    }
-else {int nomberPodezd = flatNumber / (floorAmount * FLATonFLOOR)+1; // Номер подъезда
-        int nomberFlour = flatNumber % (floorAmount * FLATonFLOOR) / FLATonFLOOR + 1; //номер этажа
-        switch (flatt%4) {//направление
-            case 1-> {
-                System.out.println(flatt+ " кв: " +nomberPodezd +" подъезд, "+ nomberFlour + " этаж, слева от лифта, влево");
+        final int FLATonFLOOR = 4; // константа кол-во кв на этаже
+        final int totalFLAT = FLATonFLOOR * entranceAmount * floorAmount; // константа кол-во квартир всего в доме
+
+        if (totalFLAT < flatNumber || flatNumber < 1) {
+            return "Такой квартиры нет в данном доме";
+
+        } else {
+            int nomberPodezd = flatNumber / (floorAmount * FLATonFLOOR) + 1; // Номер подъезда
+            int nomberFlour = flatNumber % (floorAmount * FLATonFLOOR) / FLATonFLOOR + 1; //номер этажа
+            switch (flatNumber % 4) {//направление
+                case 1 -> {
+                    return flatNumber + " кв: " + nomberPodezd + " подъезд, " + nomberFlour + " этаж, слева от лифта, влево";
+                }
+                case 2 -> {
+                    return flatNumber + " кв: " + nomberPodezd + " подъезд, " + nomberFlour + " этаж, слева от лифта, вправо";
+                }
+                case 3 -> {
+                    return flatNumber + " кв: " + nomberPodezd + " подъезд, " + nomberFlour + " этаж, справа от лифта, влево";
+                }
+                default -> {
+                    return flatNumber + " кв: " + nomberPodezd + " подъезд, " + nomberFlour + " этаж, справа от лифта, вправо";
+                }
             }
-            case 2-> {
-                System.out.println(flatt+ " кв: " +nomberPodezd +" подъезд, "+ nomberFlour + " этаж, слева от лифта, вправо");
-            }
-            case 3->{
-                System.out.println(flatt+ " кв: " +nomberPodezd +" подъезд, "+ nomberFlour + " этаж, справа от лифта, влево");
-            }
-            default -> {
-                System.out.println(flatt+ " кв: " +nomberPodezd +" подъезд, "+ nomberFlour + " этаж, справа от лифта, вправо");
-            }
+
+
         }
-
-
-    }
 
     }
 }
