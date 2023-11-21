9ea231dbcfa228076131937bb0e07b25dbda594f
k2336351 <k2336351@yandex.ru>
2023-11-21 14:25:30 +0300

    Changes for task1 chapter 1 (comments deleted and code formatted)

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -13,51 +13,32 @@ public class Main {
 
     static String getAgeString(int age) {
 
-        final int MAX_AGE = 127; // Максимальный возраст человека на данный момент вынесем в константу
+        final int MAX_AGE = 127;
 
         if (age >= 0 && age <= MAX_AGE) {
 
-            /*
-            Если заданный возраст находится в пределах от 0 до 127 лет включительно,
-            то заводим две целочисленные переменные:
-            */
-            int remainderOfDivisionOnTen = age % 10;   // отстаток от деления возраста на 10
-            int remainderOfDivisionOnOneHundred = age % 100;  // и остаток от деления возраста на 100
+            int remainderOfDivisionOnTen = age % 10;
+            int remainderOfDivisionOnOneHundred = age % 100;
 
-            /*
-            И задаём следующие условия:
-
-            - если остаток от деления на 10 равен 1, а остаток от деления на 100 не равен 11,
-            то возвращаемая строка будет примерно такого вида : "Вам n год."
-            */
             if (remainderOfDivisionOnTen == 1 && remainderOfDivisionOnOneHundred != 11) {
                 return "Вам " + age + " год.";
 
-              /*
-              - если остаток от деления на 10 находится в диапозоне от 2 до 4 включительно,
-              а остаток от деления на сто  не равен ни 12, ни 13, ни 14,
-              то пример возвращаемой строки будет: "Вам n года."
-              */
             } else if ((remainderOfDivisionOnTen >= 2 && remainderOfDivisionOnTen <= 4) &&
-                       (remainderOfDivisionOnOneHundred < 12 || remainderOfDivisionOnOneHundred > 14)) {
+                    (remainderOfDivisionOnOneHundred < 12 || remainderOfDivisionOnOneHundred > 14)) {
                 return "Вам " + age + " года.";
+
+            } else {
+                return "Вам " + age + " лет.";
             }
 
-            // - во всех остальных случаях возвращаемая строка будет: "Вам n лет."
-            else
-                return "Вам " + age + " лет.";
-
-        /*
-        Если указанный возраст превышает отметку в 127 лет или имеет отрицательное значение,
-        то будут возвращаться соответствующие информационные сообщения
-        */
         } else if (age > MAX_AGE) {
             return "Рекорд долгожительства на данный момент составляет 127 лет. " +
                     "Пожалуйста, введите свой реальный возраст." +
                     "\nЕсли Вы старше " + MAX_AGE +
                     " лет, то свяжитесь со службой поддержки для вывода корректного результата.";
 
-        } else
+        } else {
             return "Отрицательного возраста не бывает. Пожалуйста, введите корректные данные.";
+        }
     }
 }
