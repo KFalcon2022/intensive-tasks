528302d1808ceb4093ce350fd006ff9e547a4344
mirchello <mrumilov@gmail.com>
2023-11-21 12:08:37 +0300

    часть 1 задача 1

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,15 +6,12 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        System.out.println(isLeap(2000));
+
     }
 
     static boolean isLeap(int year) {
         //        Место для вашего кода
-        boolean result = false;
-        if (year%4 == 0 && (year%100!=0 || year%400==0) ){
-            result = true;
-        }
-        return result; // Заглушка. При реализации - удалить
+
+        return false; // Заглушка. При реализации - удалить
     }
 }
