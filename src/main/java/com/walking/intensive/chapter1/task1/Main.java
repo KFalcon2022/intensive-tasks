31f628560d57ddd66cf1ea9f39928fc63b55545a
dorogao5 <145500950+dorogao5@users.noreply.github.com>
2023-11-22 00:47:49 +0300

    correction of the first task

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -7,16 +7,27 @@ public class Main {
     public static void main(String[] args) {
 
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 123;
+        int age = 121;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        String printAge;
-        if (age > 10 & age < 15) printAge = "Вам " + age + " лет.";
-        else if (age%10 > 0 & age%10 < 5 ) printAge = "Вам " + age + " года.";
-        else printAge = "Вам " + age + " лет.";
-        return printAge;
+        if (age < 0 & age > 127){
+            return "error";
+        }
+        else if (age%100 > 10 & age%100 < 15) {
+            return "Вам " + age + " лет.";
+        }
+        else if (age%10 > 1 & age%10 < 5 ){
+            return "Вам " + age + " года.";
+        }
+        else if (age %10 == 1){
+            return "Вам " +age+" год.";
+        }
+        else {
+            return "Вам " + age + " лет.";
+        }
+
     }
 }
