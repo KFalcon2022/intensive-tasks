c5691b4a2ad6b613c4fbf6e9c4fd462631b5261c
TitLukich <Nef-92@yandex.ru>
2023-11-21 18:57:36 +0300

    Chapter 1, Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -1,33 +1,47 @@
 package com.walking.intensive.chapter1.task4;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-parameter.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
     }
-
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0 && b == 0 && c == 0) {
+            return "бесконечное множество решений";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b == 0) {
+            return "некорректное уравнение";
+        }
+
+        if (a == 0) {
+            return "уравнение не квадратное, x=" + (-c / b);
+        }
+
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+
+        if (discriminant > 0) {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            return "Количество решений: 2. Корни: " + Math.min(x1, x2) + "; " + Math.max(x1, x2);
+        }
+
+        if (discriminant == 0) {
+            double x = (-b) / (2 * a);
+            return "Количество решений: 1. Корень: " + x;
+        } else {
+            return "Количество решений: 0.";
+        }
     }
 }
