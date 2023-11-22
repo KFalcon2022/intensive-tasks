c31903e0e307b43a49c25de13ac19ad458edf0a0
k2336351 <k2336351@yandex.ru>
2023-11-22 23:25:40 +0300

    Solution Task 2 Chapter 1 (удалены лишние данные, изменены названия переменных, отформатирован код)

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -15,86 +15,54 @@ public class Main {
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
 
-        final int FLAT_AMOUNT_ON_THE_FLOOR = 4; // у нас есть фиксированное количество квартир на этаже
+        final int FLOOR_FLAT_AMOUNT = 4;
+        int flatAmount = FLOOR_FLAT_AMOUNT * floorAmount * entranceAmount;
+        String flatLocation;
 
-        // Общее количество квартир в доме
-        int flatAmount = FLAT_AMOUNT_ON_THE_FLOOR * floorAmount * entranceAmount;
-
-        // Задаём условия проверки корректности введённого номера квартиры
         if (flatNumber <= 0 || flatNumber > flatAmount) {
-            return "Квартиры с указанным номером нет. Проверьте введённые данные.";
-
+            flatLocation = "Квартиры с указанным номером нет. Проверьте введённые данные.";
         } else {
-            // Заводим переменную, в которую положим значение номера подъезда
             int entranceNumber;
+            int entranceFlatAmount = FLOOR_FLAT_AMOUNT * floorAmount;
+            int divisionEntranceNumber = flatNumber / entranceFlatAmount;
 
-            /*
-            Для определения номера подъезда необходимо разделить номер квартиры на количество квартир в подъезде,
-            также нам необходимо знать о наличии остатка после такого деления, чтобы корректно определить
-            номер подъезда
-            */
-            int flatAmountInTheEntrance = FLAT_AMOUNT_ON_THE_FLOOR * floorAmount;
-            int divisionForEntranceNumber = flatNumber / flatAmountInTheEntrance;
-            int remainderOfDivisionForEntranceNumber = flatNumber % flatAmountInTheEntrance;
-
-            // Если результат такого деления целое число, то этот результат и есть номер подъезда
-            if (remainderOfDivisionForEntranceNumber == 0) {
-                entranceNumber = divisionForEntranceNumber;
-                /*
-                Если в результате деления мы получаем остаток,
-                то результат деления нужно инкрементировать, чтобы получить номер подъезда
-                */
+            if (flatNumber % entranceFlatAmount == 0) {
+                entranceNumber = divisionEntranceNumber;
             } else {
-                entranceNumber = ++divisionForEntranceNumber;
+                entranceNumber = ++divisionEntranceNumber;
             }
 
-            // Далее заводим переменную для этажа, на котором находится квартира
             int floorNumber;
+            int divisionFloorNumber =
+                    (flatNumber - (entranceNumber - 1) * entranceFlatAmount) / FLOOR_FLAT_AMOUNT;
 
-            /*
-            Чтобы определить номер этажа нам нужно из номера квартиры
-            вычесть общее количество квартир в предыдущих подъездах: (entranceNumber - 1) * flatAmountInTheEntrance
-            и полученную разность разделить на количество квартир на этаже.
-            Также нам необходим остаток от деления номера квартиры на количество квартир на этаже.
-             */
-            int divisionForFloorNumber =
-                    (flatNumber - (entranceNumber - 1) * flatAmountInTheEntrance) / FLAT_AMOUNT_ON_THE_FLOOR;
-            int remainderOfDivisionOnFlatAmountOnTheFloor = flatNumber % FLAT_AMOUNT_ON_THE_FLOOR;
-
-            if (remainderOfDivisionOnFlatAmountOnTheFloor == 0) {
-                floorNumber = divisionForFloorNumber;
+            if (flatNumber % FLOOR_FLAT_AMOUNT == 0) {
+                floorNumber = divisionFloorNumber;
             } else {
-                floorNumber = ++divisionForFloorNumber;
+                floorNumber = ++divisionFloorNumber;
             }
 
-            /*
-            Теперь заведём строку, которая будет описывать расположение квартиры на этаже относительно лифта,
-            значение котрой будет зависеть от остатка от деления номера квартиры на количество квартир на этаже
-             _________________
-            |                 |
-            | 2             3 |
-            |                 |
-            | 1    _____    4 |
-            |______|_|_|______|
-            */
-            String placeOnTheFloor;
-            switch (remainderOfDivisionOnFlatAmountOnTheFloor) {
+            String floorPlace;
+            int modFloorFlatAmount = flatNumber % FLOOR_FLAT_AMOUNT;
+
+            switch (modFloorFlatAmount) {
                 case 1:
-                    placeOnTheFloor = "первая квартира слева от лифта.";
+                    floorPlace = "первая квартира слева от лифта.";
                     break;
                 case 2:
-                    placeOnTheFloor = "вторая квартира слева от лифта.";
+                    floorPlace = "вторая квартира слева от лифта.";
                     break;
                 case 3:
-                    placeOnTheFloor = "вторая квартира справа от лифта.";
+                    floorPlace = "вторая квартира справа от лифта.";
                     break;
                 default:
-                    placeOnTheFloor = "первая квартира справа от лифта.";
+                    floorPlace = "первая квартира справа от лифта.";
             }
-            return "Номер квартиры : " + flatNumber +
-                    "\nНомер подъезда: " + entranceNumber +
-                    "\nЭтаж: " + floorNumber +
-                    "\nРасположение на этаже: " + placeOnTheFloor;
+            flatLocation = String.format("Номер квартиры : %d" +
+                    "\nНомер подъезда: %d" +
+                    "\nЭтаж: %d" +
+                    "\nРасположение на этаже: %s", flatNumber, entranceNumber, floorNumber, floorPlace);
         }
+        return flatLocation;
     }
 }
