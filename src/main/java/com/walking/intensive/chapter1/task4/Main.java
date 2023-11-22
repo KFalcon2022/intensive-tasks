1f1329411c28a854de0361621b09b36b87bccdce
tvadim1 <tvadim1@yandex.ru>
2023-11-22 18:08:54 +0600

    замена всех if-else на if-return при котором дальнейший код не выполняется

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -6,8 +6,8 @@ package com.walking.intensive.chapter1.task4;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 2;
-        double b = 0;
+        double a = 1;
+        double b = 5;
         double c = 2;
 
         System.out.println(solveQuadraticEquation(a, b, c));
@@ -29,29 +29,23 @@ public class Main {
         String solution = "Количество решений: ";
 
         if ((a == 0) && (b != 0)) {
-            double x = -c / b;
-            return solution + "1. Корень: " + x;
-        } else if ((b == 0) && (a == 0) && (c != 0)) {
-            return solution + "0. Решений не существует (в действительных числах)";
-        } else if ((b == 0) && (a == 0) && (c == 0)) {
-            return solution + "бесконечное множество решений";
-        } else {
-            double determinant = Math.pow(b, 2) - 4 * a * c;
-            if (determinant > 0) {
-                double x1 = (-b + Math.sqrt(determinant)) / 2 / a;
-                double x2 = (-b - Math.sqrt(determinant)) / 2 / a;
-                if (x1 > x2) {
-                    double temp = x1;
-                    x1 = x2;
-                    x2 = temp;
-                }
-                return solution + "2. Корни: " + x1 + ";" + x2;
-            } else if (determinant == 0) {
-                double x = -b / 2 / a;
-                return solution + "1. Корень: " + x;
-            } else {
-                return solution + "0. Решений не существует (в действительных числах)";
-            }
+            return solution + "1. Корень: " + (-c / b);
         }
+        if ((b == 0) && (a == 0) && (c != 0)) {
+            return solution + "0. Решений не существует (в действительных числах)";
+        }
+        if ((b == 0) && (a == 0) && (c == 0)) {
+            return solution + "бесконечное множество решений";
+        }
+        double determinant = Math.pow(b, 2) - 4 * a * c;
+        if (determinant > 0) {
+            double x1 = (-b + Math.sqrt(determinant)) / 2 / a;
+            double x2 = (-b - Math.sqrt(determinant)) / 2 / a;
+            return (x1 > x2) ? x2 + ";" + x1 : x1 + ";" + x2;
+        }
+        if (determinant == 0) {
+            return solution + "1. Корень: " + (-b / 2 / a);
+        }
+        return solution + "0. Решений не существует (в действительных числах)";
     }
 }
