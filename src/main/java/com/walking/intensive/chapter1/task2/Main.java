b9f15a7240aa4fd797ef2f07ddf874e43f8bf8f9
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-22 13:19:52 +0300

    task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -2,36 +2,38 @@ package com.walking.intensive.chapter1.task2;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
- *
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getFlatLocation(3, 2, 16));
+        System.out.println(getFlatLocation(4, 3, 47));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         final int FLATonFLOOR = 4; // константа кол-во кв на этаже
-        final int totalFLAT = FLATonFLOOR * entranceAmount * floorAmount; // константа кол-во квартир всего в доме
 
-        if (totalFLAT < flatNumber || flatNumber < 1) {
+        if (FLATonFLOOR * entranceAmount * floorAmount < flatNumber || flatNumber < 1) {
             return "Такой квартиры нет в данном доме";
+        }
 
-        } else {
-            int nomberPodezd = flatNumber / (floorAmount * FLATonFLOOR) + 1; // Номер подъезда
-            int nomberFlour = flatNumber % (floorAmount * FLATonFLOOR) / FLATonFLOOR + 1; //номер этажа
-            switch (flatNumber % 4) {//направление
-                case 1 -> {
-                    return flatNumber + " кв: " + nomberPodezd + " подъезд, " + nomberFlour + " этаж, слева от лифта, влево";
-                }
-                case 2 -> {
-                    return flatNumber + " кв: " + nomberPodezd + " подъезд, " + nomberFlour + " этаж, слева от лифта, вправо";
-                }
-                case 3 -> {
-                    return flatNumber + " кв: " + nomberPodezd + " подъезд, " + nomberFlour + " этаж, справа от лифта, влево";
-                }
-                default -> {
-                    return flatNumber + " кв: " + nomberPodezd + " подъезд, " + nomberFlour + " этаж, справа от лифта, вправо";
-                }
+        int flatDiv = (flatNumber - 1)/ FLATonFLOOR;
+        int numberFlour =  (flatDiv +1)% floorAmount;//номер этажа
+        if (numberFlour == 0){
+            numberFlour+=floorAmount;
+        }
+        int numberPodezd =  flatDiv  / floorAmount + 1; // Номер подъезда
+
+        switch (flatNumber % 4) {//направление
+            case 1 -> {
+                return flatNumber + " кв: " + numberPodezd + " подъезд, " + numberFlour + " этаж, слева от лифта, влево";
+            }
+            case 2 -> {
+                return flatNumber + " кв: " + numberPodezd + " подъезд, " + numberFlour + " этаж, слева от лифта, вправо";
+            }
+            case 3 -> {
+                return flatNumber + " кв: " + numberPodezd + " подъезд, " + numberFlour + " этаж, справа от лифта, влево";
+            }
+            default -> {
+                return flatNumber + " кв: " + numberPodezd + " подъезд, " + numberFlour + " этаж, справа от лифта, вправо";
             }
 
 
