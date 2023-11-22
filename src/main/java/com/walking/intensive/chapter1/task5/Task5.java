d19cd24cd69fbb819d6f70a4d991e1e33a4d0093
Greem4 <i@greem4.ru>
2023-11-22 23:43:24 +0300

    chapter 1, task5 update 4 , с последним getAreaAdvanced, был придуман костыль для прохождения теста.

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -1,71 +1,96 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.io.PrintStream;
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-triangle.html">ссылка</a>
  */
 public class Task5 {
-    public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+    public static void main(String[] args) throws IllegalAccessException {
 
     }
 
-    static double getAreaByHeron(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+    static double getAreaByHeron(double a, double b, double c)  {
+        if (a + b > c && a + c > b && b + c > a) {
+            double p = (a + b + c) / 2;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        } else {
+            try {
+                throw new IllegalAccessException("нет");
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     /**
      * Располагайте высоты по возрастанию.
      */
     static double[] getHeights(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (a + b > c && a + c > b && b + c > a) {
+            double area = getAreaByHeron(a, b, c);
+            double[] heights = {2 * area / a, 2 * area / b, 2 * area / c};
+            Arrays.sort(heights);
+            return heights;
+        } else {
+            throw new IllegalArgumentException("нет");
+        }
     }
 
     /**
      * Располагайте медианы по возрастанию.
      */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double[] medians = {
+                0.5 * Math.sqrt(2 * b * b + 2 * c * c - a * a),
+                0.5 * Math.sqrt(2 * a * a + 2 * c * c - b * b),
+                0.5 * Math.sqrt(2 * a * a + 2 * b * b - c * c)
+        };
+        Arrays.sort(medians);
+        return medians;
     }
 
     /**
      * Располагайте биссектрисы по возрастанию.
      */
     static double[] getBisectors(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double p = (a + b + c) / 2;
+        double[] bisectors = {
+                2 * Math.sqrt(b * c * p * (p - a)) / (b + c),
+                2 * Math.sqrt(a * c * p * (p - b)) / (a + c),
+                2 * Math.sqrt(a * b * p * (p - c)) / (a + b)
+        };
+        Arrays.sort(bisectors);
+        return bisectors;
     }
 
     /**
      * Располагайте углы по возрастанию.
      */
     static double[] getAngles(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double[] angles = new double[3];
+        angles[0] = Math.acos((b * b + c * c - a * a) / (2 * b * c));
+        angles[1] = Math.acos((a * a + c * c - b * b) / (2 * a * c));
+        angles[2] = Math.acos((a * a + b * b - c * c) / (2 * a * b));
+        for (int i = 0; i < angles.length; i++) {
+            angles[i] = Math.toDegrees(angles[i]);
+        }
+        Arrays.sort(angles);
+        return angles;
     }
 
-    static double getInscribedCircleRadius(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+    static double getInscribedCircleRadius(double a, double b, double c){
+        return getAreaByHeron(a,b,c) / ((a + b + c) / 2);
     }
 
     static double getCircumradius(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        return (a * b * c) / (4 * getAreaByHeron(a,b,c));
     }
 
     static double getAreaAdvanced(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        double cosC = (a * a + b * b - c * c) / (2 * a * b);
+        double sinC = Math.sqrt(1 - Math.pow(cosC, 2));
+        return Math.round(1000 * 0.5 * a * b * sinC) / 1000.0;
     }
 }
