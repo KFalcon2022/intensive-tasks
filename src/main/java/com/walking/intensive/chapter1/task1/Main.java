8494713aba5c8d1e75383825d5790aa4fa7b5822
smmotor <smmotor777@gmail.com>
2023-11-22 18:29:06 +0600

    task1[*]

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -3,6 +3,7 @@ package com.walking.intensive.chapter1.task1;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
+
 import java.util.Scanner;
 
 
@@ -15,19 +16,16 @@ public class Main {
     }
 
     static String getAgeString(int age) {
-        if (age > 0 && age < 130) {
-            if ((age % 10 == 0 || age % 10 >= 5)  || (age % 100 >= 11 && age % 100 <= 14)) {
+        if (age < 0 || age > 127) {
+            return "Введен некорректный возраст";
+        } else {
+            if ((age % 10 == 0 || age % 10 >= 5) || (age % 100 >= 11 && age % 100 <= 14)) {
                 return String.format("Вам %d лет", age);
-            }
-            else if (age % 10 == 1) {
+            } else if (age % 10 == 1) {
                 return String.format("Вам %d год", age);
-            }
-            else {
+            } else {
                 return String.format("Вам %d года", age);
             }
-        }
-        else {
-            return "Введен некорректный возраст";
         }
     }
 }
