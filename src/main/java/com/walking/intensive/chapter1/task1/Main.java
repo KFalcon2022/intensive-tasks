4e7840dc722a5ebe67f39674caaee89f53f60d89
”Anna <annazalta@gmail.com>
2023-11-22 19:31:53 +0300

    task 1 post review fixes

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,9 +5,6 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        //int age = 0;
-
         for (int age=0 ; age<=127; age++) {
             System.out.println(getAgeString(age));
         }
@@ -16,25 +13,16 @@ public class Main {
     static String getAgeString(int age) {
         String ageMessage = "Вам ";
 
-        if (age == 0){
-            //"Я родился! (с) Лунтик"
-            ageMessage = "Вы - Лунтик";
-        }
-        else if (age % 10 == 0 || age % 10 > 4 || age % 100 > 10 && age % 100 < 15){
-            // если возраст заканчивается на 0
-            // если возраст заканчивается на 5-9
-            // если возраст заканчивается на 11-14
+         if (age % 10 == 0 || age % 10 > 4 || age % 100 > 10 && age % 100 < 15){
             ageMessage = ageMessage + age + " лет";
         }
         else if (age % 10 == 1){
-            //если возраст заканчивается на 1
             ageMessage = ageMessage + age + " год";
         }
         else if (age % 10 > 1){
-            //если возраст заканчивается на 2-4
-            ageMessage = ageMessage + age + " годa";
+            ageMessage = ageMessage + age + " года";
         }
 
-        return ageMessage; // Возвращаем ответ
+        return ageMessage;
     }
 }
