55058f9dc2c568136fb8a9cfe21baf304a362cab
kriss.gord <kriss.gord@gmail.com>
2023-11-22 16:12:25 +0500

    chapter 1 task 1 test

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,12 +6,33 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 
-        int age = 15;
-
-        System.out.println(getAgeString(age));
+        for (int i = -1; i < 128; i++) {
+            System.out.println(getAgeString(i));
+        }
     }
 
     static String getAgeString(int age) {
-        return age + " is your age";
+        int module = age % 100;
+        if (age > 127 || age < 0) {
+            return "Incorrect age!";
+        } else if (module >= 11 && module <= 20) {
+            return "Вам " + age + " лет";
+        } else {
+            module = age % 10;
+            switch (module) {
+                case 1 -> {
+                    return "Вам " + age + " год";
+                }
+                case 2, 3, 4 -> {
+                    return "Вам " + age + " года";
+                }
+                case 5, 6, 7, 8, 9, 0 -> {
+                    return "Вам " + age + " лет";
+                }
+                default -> {
+                    return "Error!";
+                }
+            }
+        }
     }
 }
