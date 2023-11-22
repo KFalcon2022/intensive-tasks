d72ba641141caad89c4355b94bb1ad6aef59156b
Sergey Milyukov <serrguk@gmail.com>
2023-11-22 11:24:51 +0700

    Chapter 1, task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -6,9 +6,9 @@ package com.walking.intensive.chapter1.task4;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        double a = 1;
+        double b = 9;
+        double c = 2;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -26,8 +26,41 @@ public class Main {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0) {
+            return "На 0 делить нельзя!";
+        }
+
+        double discriminant = getDiscriminant(a, b, c);
+
+        if (discriminant < 0) {
+            return "Количество решений: 0.";
+        } else if (discriminant == 0) {
+            return "Количество решений: 1. Корень: " + getOneRoot(a, b);
+        } else {
+            double[] roots = getTwoRoots(a, b, c);
+            return String.format("Количество решений: 2. Корни: %s",
+                    roots[0] > roots[1] ? roots[0] + "; " + roots[1] : roots[1] + "; " + roots[0]);
+        }
+    }
+
+    static double getOneRoot(double a, double b) {
+        return -b / (2 * a);
+    }
+
+    static double[] getTwoRoots(double a, double b, double c) {
+        double root1 = (-b + Math.sqrt(getDiscriminant(a, b, c)) / (2 * a));
+        double root2 = (-b - Math.sqrt(getDiscriminant(a, b, c)) / (2 * a));
+
+        double[] roots = new double[2];
+
+        roots[0] = Math.round(root1 * 100) / 100.0;
+        roots[1] = Math.round(root2 * 100) / 100.0;
+
+        return roots;
+    }
+
+    static double getDiscriminant(double a, double b, double c) {
+        return Math.pow(b, 2) - 4 * a * c;
     }
 }
