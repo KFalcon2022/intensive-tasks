ce9f89265c1df1cdbf15323fbf7cbdb1bdc34f6d
Alexey <lolex1@mail.ru>
2023-11-22 22:31:29 +0300

    Chapter 1 Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -10,24 +10,17 @@ public class Main {
     }
 
     static String getAgeString(int age) {
-        String result = "Некорректный возраст";
         int remainderNum10 = age % 10;
         int remainderNum100 = age % 100;
-        if (!YearsNoMore127AndNonNegative(age)) {
-            return result;
+        if (age < 0 || age > 127) {
+            return "Некорректный возраст";
         }
         if ((remainderNum10 == 1 | remainderNum100 == 1) && age != 11 && age != 111) {
-            result = ": Вам " + age + " год";
+            return  "Вам " + age + " год";
         } else if ((remainderNum10 > 1 && remainderNum10 < 5) | (remainderNum100 > 1 && remainderNum100 < 5)
                 && (age < 5 | age > 20 && age < 110 | age > 120) ) {
-            result = ": Вам " + age + " года";
-        } else {
-            result = ": Вам " + age + " лет";
+            return "Вам " + age + " года";
         }
-        return result;
-    }
-
-    static boolean YearsNoMore127AndNonNegative(int age) {
-        return age >= 0 && age < 128;
+        return "Вам " + age + " лет";
     }
 }
