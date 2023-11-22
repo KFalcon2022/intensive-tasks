26cf4775ab604c019dfd3b00c8c3bbe1d56a0867
Oleg <kovalyovop@yandex.ru>
2023-11-22 19:13:11 +0300

    Task4 дополнил: обработка линейного уравнения, если a = 0; убрал else if после return; перенес discriminant к условию, где он используется; правильный вывод корней, если их 2, сначала минимальный.

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -6,28 +6,40 @@ package com.walking.intensive.chapter1.task4;
 public class Main {
     public static void main(String[] args) {
         double a = 1;
-        double b = 10;
+        double b = 5;
         double c = 1;
 
         System.out.println(solveQuadraticEquation(a, b, c));
     }
 
     static String solveQuadraticEquation(double a, double b, double c) {
-        double discriminant = b * b - 4 * a * c;
+
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное количество решений";
+        }
+
+        if (a == 0 && b == 0) {
+            return "Нет решений";
+        }
 
         if (a == 0) {
-            return "Старший коэффициент не должен быть равен 0, уравнение не квадратное";
+            double x1 = -c / b;
+            return "Количество решений: 1. Корень: " + x1;
         }
+
+        double discriminant = b * b - 4 * a * c;
 
         if (discriminant < 0) {
             return "Количество решений: 0.";
-        } else if (discriminant == 0) {
+        }
+
+        if (discriminant == 0) {
             double x1 = -b / (a * a);
             return "Количество решений: 1. Корень: " + x1;
         } else {
             double x1 = (-b + Math.sqrt(discriminant)) / (a * a);
             double x2 = (-b - Math.sqrt(discriminant)) / (a * a);
-            return "Количество решений: 2. Корни: " + x1 + ", " + x2;
+            return "Количество решений: 2. Корни: " + Math.min(x1, x2) + ", " + Math.max(x1, x2);
         }
     }
 }
