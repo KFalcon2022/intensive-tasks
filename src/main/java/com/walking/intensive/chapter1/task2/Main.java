9df78a55ba6efceef808e3a06f1eed3fb0592426
ichigovizor <ichigovizor@gmail.com>
2023-11-22 18:07:31 +0600

    Решение задачи Дом

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -6,12 +6,36 @@ package com.walking.intensive.chapter1.task2;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+        System.out.println(getFlatLocation(4, 3, 45));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int flatQuantity = floorAmount*entranceAmount*4;
+        int position = flatNumber % 4;
+        String positionStr = "empty";
+
+        switch (position) {
+            case (0):
+                positionStr = "справа от лифта, вправо";
+                break;
+            case (1):
+                positionStr = "слева от лифта, влево";
+                break;
+            case (2):
+                positionStr = "слева от лифта, вправо";
+                break;
+            case (3):
+                positionStr = "справа от лифта, влево";
+                break;
+        }
+
+        if (flatNumber>flatQuantity|flatNumber==0) {
+            return "Квартиры не существует";
+        }
+        else {
+            return flatNumber + " кв - " + ((flatNumber-1)/(floorAmount*4)+1) + " подъезд, " + ((((flatNumber-(((flatNumber-1)/(floorAmount*4)))*(floorAmount*4))-1)/4)+1) + " этаж, " + positionStr;
+        }
+
     }
 }
