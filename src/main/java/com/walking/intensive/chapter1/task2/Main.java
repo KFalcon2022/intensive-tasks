b34f4339b46d75e962e5de05ec42f6247f522041
Dmitry <milordoff@yandex.ru>
2023-11-22 23:37:53 +0300

    task 2 solution

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,17 +1,41 @@
 package com.walking.intensive.chapter1.task2;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
- */
+
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
 
+        System.out.println(getFlatLocation(3, 4, 8));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int flatLocation = flatNumber  % 4;
+        int flatTotal = floorAmount * entranceAmount * 4;
+        int flatPerEntrance = flatTotal / entranceAmount;
+        int flatEntrance = (flatNumber - 1) / flatPerEntrance + 1;
+        int flatFloor = (flatNumber - 1) % 12 / 4 + 1;
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber > flatTotal) {
+            return ("Квартиры с номером " + flatNumber + " нет в данном доме");
+        } else if (flatNumber <= 0) {
+            return ("Номер квартиры не может быть равен или меньше 0");
+        } else {
+            switch (flatLocation) {
+                case 1 -> {
+                    return flatEntrance + " подъезд, " + flatFloor + " этаж, слева от лифта, влево";
+                }
+                case 2 -> {
+                    return flatEntrance + " подъезд, " + flatFloor + " этаж, слева от лифта, вправо";
+                }
+                case 3 -> {
+                    return flatEntrance + " подъезд, " + flatFloor + " этаж, справа от лифта, влево";
+                }
+                case 0 -> {
+                    return flatEntrance + " подъезд, " + flatFloor + " этаж, справа от лифта, вправо";
+                }
+                default -> {
+                    return "Oops, something wrong";
+                }
+            }
+        }
     }
 }
