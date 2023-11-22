fcbdbea98647e9aef87f0df12ce62e43c9d358cb
”Anna <annazalta@gmail.com>
2023-11-22 22:38:08 +0300

    task 3 post review fixes

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,21 +6,15 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 
-        int year = 2024;
-        if (isLeap(year)){
+        int year = 1584;
+        if (isLeap(year)) {
             System.out.println("Год " + year + " високосный");
-        }
-        else {
+        } else {
             System.out.println("Год " + year + " не високосный");
         }
     }
 
     static boolean isLeap(int year) {
-        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return (year % 400 == 0 || year % 4 == 0 && year % 100 != 0);
     }
 }
