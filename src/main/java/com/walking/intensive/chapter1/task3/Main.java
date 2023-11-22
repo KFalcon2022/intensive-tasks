d9b143c8a79e3586fe84bba7ae47e8e390205a01
KrisLu23 <krisbondarev@yandex.ru>
2023-11-22 19:30:06 +0300

    Task 3

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -5,15 +5,13 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        System.out.println(isLeap(2024));
+
+        System.out.println(isLeap(2023));
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            return true;
-        }
-        return false;// Заглушка. При реализации - удалить
+
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+
     }
 }
