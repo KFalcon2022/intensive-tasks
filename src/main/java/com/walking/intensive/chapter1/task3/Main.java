69cdf75464281006978618aca57b794c383069e8
DearCuba <rovbo85@gmail.com>
2023-11-22 13:13:38 +0500

    Выполнил задание 3 в главе 1.

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -1,17 +1,22 @@
 package com.walking.intensive.chapter1.task3;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-leap-year.html">ссылка</a>
  */
 public class Main {
-    public static void main(String[] args) {
+
+  public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Введите интересующий вас год: ");
+    int year = scanner.nextInt();
+    scanner.close();
+    System.out.println(isLeap(year));
+  }
 
-    }
-
-    static boolean isLeap(int year) {
-        //        Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
-    }
+  static boolean isLeap(int year) {
+    return year % 400 == 0 | year % 4 == 0 ^ year % 100 == 0;
+  }
 }
