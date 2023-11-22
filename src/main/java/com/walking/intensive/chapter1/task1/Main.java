a6e8f98fd4f7fed9b01c868d9e33d158b8ee6b6f
U_M1CVT <akachamina@alfabank.ru>
2023-11-22 17:42:12 +0300

    Исправление

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,27 +5,15 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
+//        Для собственных проверок можете делать любые изменения в этом методе
         int age = 0;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        if (age > 127 || age < 0) {
-            return ("Возраст введен некорректно");
-        }
-        int i = age % 100;
-        if (i >= 11 && i <= 14) {
-            return ("Вам" + age + "лет");
-        }
-        int a = i % 10;
-        if (a == 1) {
-            return ("Вам" + age + "год");
-        } else if (a == 2 || a == 3 || a == 4) {
-            return ("Вам" + age + "года");
-        } else {
-            return ("Вам" + age + "лет");
-        }
+//        Место для вашего кода
 
+        return null; // Заглушка. При реализации - удалить
     }
 }
