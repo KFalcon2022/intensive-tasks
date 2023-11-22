028f558293e1c0f58232b7de1ad658e95c2ebf19
ichigovizor <ichigovizor@gmail.com>
2023-11-22 12:53:30 +0600

    Решение задачи Возраст

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,14 +6,34 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 97;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int ostatok = age % 10;
+
+        if (age > 127) {
+            return "Превышен порог возраста";
+        }
+
+        else if ( ostatok == 1 && age != 11 && age != 111){
+            return "Вам " + age + " год";
+        }
+
+        else if ((age > 10 && age < 15) | (age > 110 && age < 115)) {
+            return "Вам " + age + " лет";
+        }
+
+        else if (ostatok > 1 && ostatok < 5) {
+            return "Вам " + age + " года";
+        }
+
+        else {
+            return "Вам " + age + " лет";
+        }
+
     }
 }
