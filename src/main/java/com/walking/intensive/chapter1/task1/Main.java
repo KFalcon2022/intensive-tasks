3046123af750b01d4e0720bda93d339505c90057
njmist <njmist@gmail.com>
2023-11-22 20:58:50 +0300

    Исправленное решение 1 задачи

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 100;
+        int age = 101;
 
         System.out.println(getAgeString(age));
     }
@@ -17,15 +17,14 @@ public class Main {
 
         if (age < 0 || age > 127) {
             return "Это врядли";
-        } else if (remainder == 1) {
-            return "Вам " + age + " год";
-        } else if (age % 10 == 1){
-            return "Вам " + age + " год";
-        }
-        else if (age % 10 >= 2 && age % 10 < 5) {
-            return "Вам " + age + " года";
-        } else {
+        } else if (age > 9 && age < 20 || age > 109 && age < 120) { // Более элегантного решения не придумал
             return "Вам " + age + " лет";
+        } else if (remainder == 1 || age % 10 == 1) { // А тут понял, что у меня в двух условиях один результат и объединил в одну строчку
+            return "Вам " + age + " год";
+        } else if (age % 10 >= 2 && age % 10 < 5) {
+            return "Вам " + age + " года";
         }
+
+        return "Вам " + age + " лет"; // Я понял, если не выполнились предыдущие if условия, то метод вернет эту строчку
     }
 }
