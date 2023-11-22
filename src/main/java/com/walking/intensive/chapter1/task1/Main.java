dcab120065516ceb35f6e1cd5db5eba017b888c9
kriss.gord <kriss.gord@gmail.com>
2023-11-22 13:31:44 +0500

    test-1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,7 +6,8 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 15;
+
 
         System.out.println(getAgeString(age));
     }
