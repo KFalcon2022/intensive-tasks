49985352fe220236df3f91956c3cd291624600d7
SenyaDiamond <SenyaDiamond@gmail.com>
2023-11-22 15:33:16 +0300

    Решение задачи 1.2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,35 +1,39 @@
 package com.walking.intensive.chapter1.task2;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество этажей: ");
+        int floorAmount = scanner.nextInt();
+        System.out.print("Введите количество парадных: ");
+        int entranceAmount = scanner.nextInt();
+        for (int i = 0; i <= (floorAmount * entranceAmount * 4); i++) {
+            System.out.println(getFlatLocation(floorAmount, entranceAmount, i));
+        }
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         String flatLocation = "";
 
-        //Проверки входных значений
         long totalFlats = 4L * floorAmount * entranceAmount;
         if (flatNumber > totalFlats || flatNumber <= 0) {
-            flatLocation += "Квартиры " + flatNumber + " в доме нет";
-            return flatLocation;
+            return "Квартиры " + flatNumber + " в доме нет";
         }
         if (floorAmount <= 0 || entranceAmount <= 0) {
-            flatLocation += "Параметры дома заданы неверно";
-            return flatLocation;
+            return "Параметры дома заданы неверно";
         }
 
-        long entranceNumber = (flatNumber / (totalFlats / entranceAmount)) + 1;
-        //Обработка случая, когда искомая квартира крайняя в парадной.
-        if (flatNumber % (totalFlats / entranceAmount) == 0) {
-            entranceNumber--;
-        }
-        long floorNumber = ((flatNumber - (((entranceNumber - 1) * 4L * floorAmount) + 1)) / 4) + 1;
+        long entranceNumber = entranceAmount - ((totalFlats - flatNumber) / (4L * floorAmount));
+        long lastFlatInEntrance = entranceNumber * 4L * floorAmount;
+        long floorNumber = floorAmount - ((lastFlatInEntrance - flatNumber) / 4);
         flatLocation += flatNumber + " кв – " + entranceNumber + " подъезд, " + floorNumber + " этаж, ";
-        if (flatNumber > (4L * floorAmount * (entranceNumber - 1)) + (4L * (floorNumber - 1)) + 2) {
+        if ((flatNumber % 4 == 0) || ((flatNumber + 1) % 4 == 0)) {
             flatLocation += "справа от лифта, ";
         } else {
             flatLocation += "слева от лифта, ";
