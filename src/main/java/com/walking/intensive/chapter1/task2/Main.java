873ae81a7b7e67b78757d0f35d3bb9b4749b108c
sofajedi <kirill.ello@gmail.com>
2023-11-22 21:09:11 +0300

    Chapter 1 Task 2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -8,32 +8,36 @@ public class Main {
 
         int floorAmount = 9;
         int entranceAmount = 4;
-        int flatNumber = 1;
+        int flatNumber = 139;
         System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
 
-        int totalFlats = floorAmount * entranceAmount * 4;
-        int flatFloor = (flatNumber - 1) / 4 + 1;
-        int flatEntrance = (flatFloor - 1) / floorAmount + 1;
-        int flatFloorDivided = flatFloor % floorAmount;
-        int dividedByFlatsOnFloor = flatNumber % 4;
-        if (flatFloorDivided == 0) {
-            flatFloorDivided = flatFloorDivided + floorAmount;
+        int totalFlatFloor = (flatNumber - 1) / 4 + 1;
+        int flatEntrance = (totalFlatFloor - 1) / floorAmount + 1;
+        int CurrentFlatFloor = totalFlatFloor % floorAmount;
+
+        if (CurrentFlatFloor == 0) {
+
+            CurrentFlatFloor = CurrentFlatFloor + floorAmount;
+
         }
-        if (flatNumber <= 0 || flatNumber > totalFlats) {
+
+        if (flatNumber <= 0 || flatNumber > floorAmount * entranceAmount * 4) {
+
             return "Нет такой квартиры";
+
         }
-        switch (dividedByFlatsOnFloor) {
+        switch (flatNumber % 4) {
             case 0:
-                return flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloorDivided + " Этаж" + " справа от лифта, вправо";
+                return flatNumber + " кв - " + flatEntrance + " подъезд, " + CurrentFlatFloor + " Этаж" + " справа от лифта, вправо";
             case 1:
-                return flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloorDivided + " Этаж" + " слева от лифта, влево";
+                return flatNumber + " кв - " + flatEntrance + " подъезд, " + CurrentFlatFloor + " Этаж" + " слева от лифта, влево";
             case 2:
-                return flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloorDivided + " Этаж" + " слева от лифта, вправо";
+                return flatNumber + " кв - " + flatEntrance + " подъезд, " + CurrentFlatFloor + " Этаж" + " слева от лифта, вправо";
             case 3:
-                return flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloorDivided + " Этаж" + " справа от лифта, влево";
+                return flatNumber + " кв - " + flatEntrance + " подъезд, " + CurrentFlatFloor + " Этаж" + " справа от лифта, влево";
         }
         return "Ошибка";
     }
