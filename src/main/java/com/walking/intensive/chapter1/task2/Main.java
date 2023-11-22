35d079ba5c77a06bfc73033b95d3b5cc8a90ee74
ETSphere <elem174@proton.me>
2023-11-22 16:36:03 +0500

    Переделано согласно правкам Zawadskiy

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -6,39 +6,31 @@ package com.walking.intensive.chapter1.task2;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(getFlatLocation(3, 1, 19));
+        System.out.println(getFlatLocation(3, 3, 19));
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        int maxFlatFloor = 4;
-        if (flatNumber <= 0 || flatNumber > floorAmount * maxFlatFloor) {
-            System.out.printf("Сожалеем в %d подезде, на этаже %d, нет квартиры %d, искомая ", entranceAmount, floorAmount, flatNumber);
+
+        int entrance = (flatNumber - 1) / (floorAmount * 4) + 1;
+        int floor = ((flatNumber - 1) % (floorAmount * 4)) / 4 + 1;
+        int searchFlat = flatNumber % 4;
+
+        if (flatNumber <= 0 || flatNumber > floorAmount * entranceAmount * 4) {
+            return "Такой квартиры в доме нет";
         }
-        int entrance = (flatNumber - 1) / (floorAmount * maxFlatFloor) + 1;
-        int floor = ((flatNumber - 1) % (floorAmount * maxFlatFloor)) / maxFlatFloor + 1;
 
-        String position = getString(flatNumber);
-
-        return String.format("квартира %d расположена в %d подезде, на %d этаже , %s", flatNumber, entrance, floor, position);
-    }
-
-    private static String getString(int flatNumber) {
-        String position = null;
-        String floorPosition1 = "слева от лифта, влево";
-        String floorPosition2 = "слева от лифта, вправо";
-        String floorPosition3 = "справа от лифта, влево";
-        String floorPosition4 = "справа от лифта, вправо";
-
-        if (flatNumber % 4 == 1) {
-            position = floorPosition1;
-        } else if (flatNumber % 4 == 2) {
-            position = floorPosition2;
-        } else if (flatNumber % 4 == 3) {
-            position = floorPosition3;
-        } else if (flatNumber % 4 == 0) {
-            position = floorPosition4;
+        switch (searchFlat) {
+            case 0:
+                return flatNumber + " кв - " + entrance + " подъезд, " + floor + " этаж, слева от лифта, влево";
+            case 1:
+                return flatNumber + " кв - " + entrance + " подъезд, " + floor + " этаж, слева от лифта, вправо";
+            case 2:
+                return flatNumber + " кв - " + entrance + " подъезд, " + floor + " этаж, справа от лифта, влево";
+            case 3:
+                return flatNumber + " кв - " + entrance + " подъезд, " + floor + " этаж, справа от лифта, вправо";
         }
-        return position;
+        return "Ошибка";
     }
 }
+
