d012a17dc793c69349fb0f9302df60e053498a00
Magpier <garson.ao@gmail.com>
2023-11-22 23:38:33 +0300

    Chapte1 Task 4 ver 2

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -5,29 +5,42 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
-
-        System.out.println(solveQuadraticEquation(a, b, c));
+        System.out.println(solveQuadraticEquation(2, 10, 1));
+        System.out.println(solveQuadraticEquation(25, 10, 1));
+        System.out.println(solveQuadraticEquation(50, 10, 1));
+        System.out.println(solveQuadraticEquation(0, 10, 1));
+        System.out.println(solveQuadraticEquation(0, 0, 1));
+        System.out.println(solveQuadraticEquation(50, 0, 0));
+        System.out.println(solveQuadraticEquation(0, 0, 0));
 
     }
 
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
-    static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+    static String solveQuadraticEquation(double a, double b, double c) {
+        double x1;
+        double x2;
+        double discriminant = b * b - 4 * a * c;
+        if (a == 0 && b != 0) {
+            x1 = -c / b;
+            return "Количество решений 1. Корень: " + x1;
+        }
+        if ((a == 0 && b == 0) || discriminant < 0) {
+            return "Количество решений: 0.";
+        }
+        if (b == 0 && c == 0) {
+            return "Количество решений 1. Корень: " + 0;
+        }
+        if (discriminant == 0) {
+            x1 = -b / (2 * a);
+            return "Количество решений 1. Корень: " + x1;
+        }
+        if (discriminant > 0) {
+            x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            return "Количество решений 2. Корни: " + x1 + ";" + x2;
+        }
+        return "Количество решений: 0.";
+
+
     }
 }
