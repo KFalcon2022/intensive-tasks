b065fbd7f5f5ecb6d4db6dd84b70fe1fd1ddbe20
dorogao5 <145500950+dorogao5@users.noreply.github.com>
2023-11-22 23:44:40 +0300

    chapter 1 task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -13,18 +13,18 @@ public class Main {
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
 
         int FLAT_MAX_NUMBER = floorAmount * entranceAmount * 4;
-        int entranceNumber =  (flatNumber-1) / (FLAT_MAX_NUMBER/ entranceAmount)+1;
-        int floorNumber = (((flatNumber-1) % (floorAmount*4)+1)-1) / 4 +1;
-        int place = ((flatNumber-1) % (floorAmount*4)+1) - 4*(floorNumber-1);
-        String placeFlat = switch (place){
-            case 1 ->  "слева от лифта, влево";
-            case 2 ->  "слева от лифта, вправо";
-            case 3 ->  "справа от лифта, влево";
-            case 4 ->  "справа от лифта, вправо";
-            default ->  null;
-            };
-        String result = String.format("%d кв - %d подъезд, %d этаж, %s", flatNumber, entranceNumber, floorNumber, placeFlat );
+        int entranceNumber = (flatNumber - 1) / (FLAT_MAX_NUMBER / entranceAmount) + 1;
+        int floorNumber = (((flatNumber - 1) % (floorAmount * 4) + 1) - 1) / 4 + 1;
+        int place = ((flatNumber - 1) % (floorAmount * 4) + 1) - 4 * (floorNumber - 1);
+        String placeFlat = switch (place) {
+            case 1 -> "слева от лифта, влево";
+            case 2 -> "слева от лифта, вправо";
+            case 3 -> "справа от лифта, влево";
+            case 4 -> "справа от лифта, вправо";
+            default -> null;
+        };
+        String result = String.format("%d кв - %d подъезд, %d этаж, %s", flatNumber, entranceNumber, floorNumber, placeFlat);
 
-            return result;
-        }
+        return result;
     }
+}
