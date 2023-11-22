be80bf82a4ee33d35d821cde64cdbf10ac54660b
dimapros <dima.ru.98@mail.ru>
2023-11-22 20:04:46 +1000

    Сhapter1.Task2.

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,27 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Введите количество этажей: ");
         int floorAmount = sc.nextInt();
-        if (floorAmount <= 0) {
-            System.out.println("Количество этажей не может быть меньше 1.");
-            return;
-        }
 
         System.out.println("Введите количество подъездов: ");
         int entranceAmount = sc.nextInt();
-        if (entranceAmount <= 0) {
-            System.out.println("Количество подъездов не может быть меньше 1.");
-            return;
-        }
 
         System.out.println("Введите номер квартиры: ");
         int flatNumber = sc.nextInt();
-        if (flatNumber <= 0) {
-            System.out.println("Номер квартиры должен быть равен хотя бы 1.");
-            return;
-        }
 
         sc.close();
 
@@ -33,33 +20,36 @@ public class Main {
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        int flatCounter = 0;
+        int amountFlatsOnFloor = 4;
+        int amountFlats = floorAmount * entranceAmount * amountFlatsOnFloor;
         String location = "";
 
-        for (int i = 1; i <= entranceAmount; i++) {
-            for (int j = 1; j <= floorAmount; j++) {
-                for (int k = 1; k <= 4; k++) {
-                    flatCounter++;
-                    if (flatCounter == flatNumber) {
-                        location = flatNumber + " кв - " + i + " подъезд, " + j + " этаж, ";
-                        switch (k) {
-                            case (1):
-                                location = location + "слева от лифта, влево.";
-                                return location;
-                            case (2):
-                                location = location + "слева от лифта, вправо.";
-                                return location;
-                            case (3):
-                                location = location + "справа от лифта, влево.";
-                                return location;
-                            case (4):
-                                location = location + "справа от лифта, вправо.";
-                                return location;
-                        }
-                    }
-                }
-            }
+        if (flatNumber > amountFlats || flatNumber <= 0) return "Квартиры с таким номером в доме не существует.";
+
+        if (floorAmount <= 0) return "Количество этажей не может быть меньше 1.";
+
+        if (entranceAmount <= 0) return "Количество подъездов не может быть меньше 1.";
+
+        int searchableEntrance = (int) Math.ceil(flatNumber / (floorAmount * (double)amountFlatsOnFloor));  // Находим необходимый подъезд.
+        int searchableFloor = (int) (Math.ceil(flatNumber / (double)amountFlatsOnFloor) % floorAmount);     // Находим необходимый этаж.
+
+        location = flatNumber + " кв - " + searchableEntrance + " подъезд, " + searchableFloor + " этаж, ";
+
+        switch (flatNumber % amountFlatsOnFloor) {
+            case (1):
+                location+= "слева от лифта, влево.";
+                return location;
+            case (2):
+                location += "слева от лифта, вправо.";
+                return location;
+            case (3):
+                location += "справа от лифта, влево.";
+                return location;
+            case (0):
+                location += "справа от лифта, вправо.";
+                return location;
+            default:
+                return "Нахождение квартиры на этаже неизвестно.";
         }
-        return "В доме с таким количеством этажей и подъездов не может быть квартиры с номером " + flatNumber + ".";
     }
 }
