1470af514cb0d9f8090896927f67fa4ed6e9a124
dimapros <dima.ru.98@mail.ru>
2023-11-22 22:00:07 +1000

    Сhapter1.Task3.

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -18,7 +18,6 @@ public class Main {
     }
 
     public static boolean isLeapYear(int year) {
-        if (year > 0 && (year % 4 == 0 || year % 100 == 0 & year % 400 == 0)) return true;
-        else return false;
+        return year > 0 && (year % 4 == 0 || year % 100 == 0 & year % 400 == 0);
     }
 }
