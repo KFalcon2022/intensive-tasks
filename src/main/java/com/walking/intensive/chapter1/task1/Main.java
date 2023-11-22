f88b3514387188bf349a75959ac0073230f34e9d
RVishez <oliohcmpn@gmail.com>
2023-11-22 16:08:00 +0100

    1st task completed

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,14 +6,26 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
-        System.out.println(getAgeString(age));
+        int[] ages = new int[128];
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = i;
+            System.out.println(getAgeString(i));
+        }
+//        System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 //        Место для вашего кода
+        String ageWord;
 
-        return null; // Заглушка. При реализации - удалить
+        if (((age + 100) % 100 >= 11 && (age + 100) % 100 <= 19)) {
+            ageWord = "лет";
+        } else if (((age + 10) % 10) == 1) {
+            ageWord = "год";
+        } else if ((age + 10) % 10 > 1 && (age + 10) % 10 < 5) {
+            ageWord = "года";
+        } else ageWord = "лет";
+
+        return ("Твой возраст: " + age + " " + ageWord); // Заглушка. При реализации - удалить
     }
 }
