d384e334c36e1b2e258fdd7d1bec76b5abc3b224
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-22 15:10:29 +0300

    task4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -5,30 +5,30 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        // a × x2 + b × x + c = 0 .
-        double a = 0;
-        double b = 0;
-        double c = 0;
 
-        System.out.println(solveQuadraticEquation(a, b, c));
-
+//        System.out.println(solveQuadraticEquation(0, 0, 3));
+//        System.out.println(solveQuadraticEquation(0, 2, 3));
+//        System.out.println(solveQuadraticEquation(2, 5, -3));
+//        System.out.println(solveQuadraticEquation(10, 5, 2));
+//        System.out.println(solveQuadraticEquation(3, -10, 3));
     }
 
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
+        if ((a == 0 & b == 0) || (a == 0)) {
+            return "Действительных корней нет";
+        }
 
+        double D = Math.pow(b, 2) - 4 * a * c;
 
-        return null; // Заглушка. При реализации - удалить
+        if (D > 0) {
+            double xOne = ((-b) - Math.sqrt(D)) / (2 * a);
+            double xTwo = ((-b) + Math.sqrt(D)) / (2 * a);
+            return xOne > xTwo ? String.format("Количество решений: 2. Корни: %.3f; %.3f", xOne, xTwo) : String.format("Количество решений: 2. Корни: %.3f; %.3f", xTwo, xOne);
+        } else if (D == 0) {
+            double xOne = (-b) / (2 * a);
+            return String.format("Количество решений: 1. Корень %.3f", xOne);
+        } else {
+            return "Действительных корней нет";
+        }
     }
 }
