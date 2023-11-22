57a3cec23d0b3b6c6658e945b514cb2a8e63d6f2
Sergey Milyukov <serrguk@gmail.com>
2023-11-22 11:35:54 +0700

    Chapter 1, task 2, class House removed

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -1,21 +1,46 @@
 package com.walking.intensive.chapter1.task2;
 
-import java.util.Scanner;
-
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Main {
     public static void main(String[] args) {
 
-        House house = new House(2, 3);
+    int floorAmount = 2;
+    int entranceAmount = 3;
+    int flatNumber = 23;
 
-        Scanner scanner = new Scanner(System.in);
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
+}
 
-        System.out.print("Введите номер искомой квартиры: ");
-        int flatNumber = scanner.nextInt();
-        scanner.close();
-        System.out.println(house.getFlatLocation(flatNumber));
-        System.out.println(house.getFlatAmount());
+    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
+
+        int flatCountInFloor = 4;
+        int flatCountInHouse = flatCountInFloor * floorAmount * entranceAmount;
+        int flatCountInEntrance = flatCountInFloor * floorAmount;
+
+        if (flatNumber > flatCountInHouse || flatNumber <= 0) {
+            return "Такой квартиры в этом доме нет";
+        }
+
+        int entrance = (flatNumber - 1) / flatCountInEntrance + 1;
+        int floor = ((flatNumber - 1) % flatCountInEntrance) / flatCountInFloor + 1;
+
+        String direction;
+        String sideFromElevator;
+
+        if (flatNumber % 2 == 0) {
+            direction = "вправо";
+        } else {
+            direction = "влево";
+        }
+
+        if (flatNumber % 2 != 0 && (flatNumber + 1) % 4 == 0 || flatNumber % 4 == 0) {
+            sideFromElevator = "справа от лифта";
+        } else {
+            sideFromElevator = "слева от лифта";
+        }
+        return String.format("%d подъезд, %d этаж, %s, %s",
+                entrance, floor, sideFromElevator, direction);
     }
 }
