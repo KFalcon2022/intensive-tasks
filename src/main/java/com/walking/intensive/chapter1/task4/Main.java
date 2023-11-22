041bc091019cbeecd7066d69f015ced216d21087
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-22 15:24:22 +0300

    кое-что исправил

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -6,16 +6,19 @@ package com.walking.intensive.chapter1.task4;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(solveQuadraticEquation(4, 0, 7));
-        System.out.println(solveQuadraticEquation(0, 2, 3));
-        System.out.println(solveQuadraticEquation(2, 5, -3));
-        System.out.println(solveQuadraticEquation(10, 5, 2));
-        System.out.println(solveQuadraticEquation(3, -10, 3));
+//        System.out.println(solveQuadraticEquation(4, 0, 7));
+//        System.out.println(solveQuadraticEquation(0, 2, 3));
+//        System.out.println(solveQuadraticEquation(2, 5, -3));
+//        System.out.println(solveQuadraticEquation(10, 5, 2));
+//        System.out.println(solveQuadraticEquation(3, -10, 0));
     }
 
     static String solveQuadraticEquation(double a, double b, double c) {
-        if (a == 0) {
+        if (a == 0 & b == 0) {
             return "Действительных корней нет";
+        } else if (a == 0 & b != 0) {
+            double xOne = -c / b;
+            return String.format("Количество решений: 1. Корень %.3f", xOne);
         }
 
         double D = Math.pow(b, 2) - 4 * a * c;
