15d9a59fe7b489d6e555d9a84560757ec864f44d
Stas Shadrin <shadrin060388@gmail.com>
2023-11-22 13:16:07 +0300

    Решение 3 задачи 1 главы

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,12 +6,12 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+        int year = 1988;
+        System.out.println(isLeap(year));
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
 
-        return false; // Заглушка. При реализации - удалить
+        return  (year % 4 == 0) || ((year % 100 == 0) && (year % 400 == 0));
     }
 }
