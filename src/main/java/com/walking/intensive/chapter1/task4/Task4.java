4e3d11fa3723bbd78d15af445506e6d06c4a90b5
Pavlo <pavlokutsyn@gmail.com>
2023-11-22 15:06:39 +0100

    Chapter1/task4/QuadraticEquation_PK

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -5,10 +5,10 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+
+        double a = -4;
+        double b = 8;
+        double c = 12;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -26,8 +26,26 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0) {
+            if (b == 0) {
+                return c == 0 ? "An infinite number of solutions." : "no solutions";
+            } else {
+                double x = -c / b;
+                return "Number of solutions: 1. Root: " + x;
+            }
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double d = b * b - 4 * a * c;
+        if (d > 0) {
+            double x1 = (-b + Math.sqrt(d)) / (2 * a);
+            double x2 = (-b - Math.sqrt(d)) / (2 * a);
+            return "Number of solutions: 2. Roots: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
+        } else if (d == 0) {
+            double x = -b / (2 * a);
+            return "Number of solutions: 1. Root: " + x;
+        } else {
+            return "Number of solutions: 0.";
+
+        }
     }
 }
