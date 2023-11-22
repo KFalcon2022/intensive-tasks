c16edc8219f09b89f2309da7c8f62aafd6277e73
Oleg <kovalyovop@yandex.ru>
2023-11-23 01:30:21 +0300

    Task4 поправил формулы теперь добавили тесты, тесты прошел

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -5,9 +5,9 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-        double a = 6;
-        double b = 100;
-        double c = 10;
+        double a = 1;
+        double b = 0;
+        double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
     }
@@ -15,15 +15,15 @@ public class Task4 {
     static String solveQuadraticEquation(double a, double b, double c) {
 
         if (a == 0 && b == 0 && c == 0) {
-            return "Бесконечное количество решений";
+            return "Решений бесконечно";
         }
 
         if (a == 0 && b == 0) {
-            return "Нет решений";
+            return "Количество решений: 0.";
         }
 
         if (a == 0) {
-            double x1 = -c / b;
+            int x1 = (int) (c / b);
             return "Количество решений: 1. Корень: " + x1;
         }
 
@@ -33,12 +33,12 @@ public class Task4 {
         if (discriminant < 0) {
             result = "Количество решений: 0.";
         } else if (discriminant == 0) {
-            double x1 = -b / (a * a);
+            int x1 = (int) (-b / (2 * a));
             result = "Количество решений: 1. Корень: " + x1;
         } else {
-            double x1 = (-b + Math.sqrt(discriminant)) / (a * a);
-            double x2 = (-b - Math.sqrt(discriminant)) / (a * a);
-            result = "Количество решений: 2. Корни: " + Math.min(x1, x2) + ", " + Math.max(x1, x2);
+            int x1 = (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
+            int x2 = (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
+            result = "Количество решений: 2. Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
         }
         return result;
     }
