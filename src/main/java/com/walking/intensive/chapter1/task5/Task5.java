1431b25215a3358aeee804244b8b4c14bbbfa5d9
Reilan <reilan86@gmail.com>
2023-11-23 02:25:47 +0300

    Chapter 1 task 5

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -8,9 +8,9 @@ import java.util.Arrays;
 public class Task5 {
     public static void main(String[] args) {
 
-        double a = 11;
-        double b = 21;
-        double c = 24;
+        double a = 12;
+        double b = 13;
+        double c = 5;
 
         System.out.println("1. Площадь треугольника по формуле Герона " + areaByHeron(a, b, c));
         System.out.println("2. Высоты треугольника по возрастанию " + Arrays.toString(getHeights(a, b, c)));
@@ -19,7 +19,7 @@ public class Task5 {
         System.out.println("5. Углы треугольника по возрастанию " + Arrays.toString(getAngles(a, b, c)));
         System.out.println("6. Длина радиуса вписанной в треугольник окружности " + getInscribedCircleRadius(a, b, c));
         System.out.println("7. Длина радиуса описаной вокруг треугольника окружности " + getCircumradius(a, b, c));
-        System.out.println("8. Нахождения cos угла С между сторонами a и b, затем преобразование его в sin с последующим расчетом площади через него " + getAreaAdvanced(a, b, c));
+        System.out.println("8. Нахождения cos углов A, B, С  с последующим преобразование их в sin и расчетом площади через эти углы" + Arrays.toString(getAreaAdvanced(a, b, c)));
     }
 
     static double areaByHeron(double a, double b, double c) {
@@ -41,11 +41,13 @@ public class Task5 {
      * Располагайте высоты по возрастанию.
      */
     static double[] getHeights(double a, double b, double c) {
+
         double areaHeron = areaByHeron(a, b, c);
         double[] heightsArray = new double[3];
         heightsArray[0] = (2 * areaHeron) / a;
         heightsArray[1] = (2 * areaHeron) / b;
         heightsArray[2] = (2 * areaHeron) / c;
+
         Arrays.sort(heightsArray);
         return heightsArray;
     }
@@ -55,10 +57,12 @@ public class Task5 {
      * Располагайте медианы по возрастанию.
      */
     static double[] getMedians(double a, double b, double c) {
+
         double[] mediansArray = new double[3];
         mediansArray[0] = (Math.sqrt(2 * (Math.pow(b, 2) + Math.pow(c, 2)) - Math.pow(a, 2))) / 2;
         mediansArray[1] = (Math.sqrt(2 * (Math.pow(a, 2) + Math.pow(c, 2)) - Math.pow(b, 2))) / 2;
         mediansArray[2] = (Math.sqrt(2 * (Math.pow(a, 2) + Math.pow(b, 2)) - Math.pow(c, 2))) / 2;
+
         Arrays.sort(mediansArray);
         return mediansArray;
     }
@@ -67,8 +71,9 @@ public class Task5 {
      * Располагайте биссектрисы по возрастанию.
      */
     static double[] getBisectors(double a, double b, double c) {
+
         double[] bisectorsArray = new double[3];
-        double halfPerimeter = (a + b + c) / 2;
+        double halfPerimeter = (a + b + c) / 2;;
         bisectorsArray[0] = 2 * Math.sqrt(b * c * (halfPerimeter - a) / (b + c));
         bisectorsArray[1] = 2 * Math.sqrt(a * c * (halfPerimeter - b) / (a + c));
         bisectorsArray[2] = 2 * Math.sqrt(a * b * (halfPerimeter - c) / (a + b));
@@ -82,17 +87,22 @@ public class Task5 {
      * Располагайте углы по возрастанию.
      */
     static double[] getAngles(double a, double b, double c) {
+
         double[] anglesArray = new double[3];
         anglesArray[0] = Math.toDegrees(Math.acos(((Math.pow(b, 2) + Math.pow(c, 2)) - Math.pow(a, 2)) / (2 * b * c)));
         anglesArray[1] = Math.toDegrees(Math.acos(((Math.pow(a, 2) + Math.pow(c, 2)) - Math.pow(b, 2)) / (2 * a * c)));
         anglesArray[2] = Math.toDegrees(Math.acos(((Math.pow(a, 2) + Math.pow(b, 2)) - Math.pow(c, 2)) / (2 * a * b)));
+
         Arrays.sort(anglesArray);
         return anglesArray;
+
     }
 
     static double getInscribedCircleRadius(double a, double b, double c) {
+
         double areaHeron = areaByHeron(a, b, c);
         double inscribedRadius = 0;
+
         if (a + b > c && a + c > b && b + c > a) {
             inscribedRadius = (2 * areaHeron) / (a + b + c);
         }
@@ -100,8 +110,10 @@ public class Task5 {
     }
 
     static double getCircumradius(double a, double b, double c) {
+
         double areaHeron = areaByHeron(a, b, c);
         double circumRadius = 0;
+
         if (a + b > c && a + c > b && b + c > a) {
             circumRadius = (a * b * c) / (4 * areaHeron);
         }
@@ -109,10 +121,24 @@ public class Task5 {
     }
 
 
-    static double getAreaAdvanced(double a, double b, double c) {
-        double cosAngle = ((Math.pow(a, 2) + Math.pow(b, 2)) - Math.pow(c, 2)) / (2 * b * c);
-        double sinAngle = Math.sqrt(1 - Math.pow(cosAngle, 2));
-        double result = ((a * b) / 2) * sinAngle;
-        return result;
+    static double[] getAreaAdvanced(double a, double b, double c) {
+
+        double cosAngleA = (Math.pow(b, 2) + Math.pow(c, 2) - Math.pow(a, 2)) / (2 * b * c);
+        double sinAngleA = Math.sqrt(1 - Math.pow(cosAngleA, 2));
+
+        double cosAngleB = (Math.pow(a, 2) + Math.pow(c, 2) - Math.pow(b, 2)) / (2 * a * c);
+        double sinAngleB = Math.sqrt(1 - Math.pow(cosAngleB, 2));
+
+        double cosAngleC = (Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b);
+        double sinAngleC = Math.sqrt(1 - Math.pow(cosAngleC, 2));
+
+        double[] areaAdvancedArray = new double[3];
+        areaAdvancedArray[0] = ((b * c) / 2) * sinAngleA;
+        areaAdvancedArray[1] = ((a * c) / 2) * sinAngleB;
+        areaAdvancedArray[2] = ((a * b) / 2) * sinAngleC;
+
+        return areaAdvancedArray;
+
+
     }
 }
