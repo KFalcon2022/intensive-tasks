c143985513c530791882fe1eb734a740d94037f1
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-23 09:44:01 +0300

    Task 1.4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -6,17 +6,19 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 
-//        System.out.println(solveQuadraticEquation(4, 0, 7));
-//        System.out.println(solveQuadraticEquation(0, 2, 3));
-//        System.out.println(solveQuadraticEquation(2, 5, -3));
-//        System.out.println(solveQuadraticEquation(10, 5, 2));
-//        System.out.println(solveQuadraticEquation(3, -10, 0));
+        System.out.println(solveQuadraticEquation(0, 0, 0));
+        System.out.println(solveQuadraticEquation(0, 0, 5));
+        System.out.println(solveQuadraticEquation(0, 1, 2));
+        System.out.println(solveQuadraticEquation(1, 0, 0));
+        System.out.println(solveQuadraticEquation(1, 0, 1));
+        System.out.println(solveQuadraticEquation(1, 0, -1));
     }
 
     static String solveQuadraticEquation(double a, double b, double c) {
         if (a == 0 & b == 0) {
             return "Действительных корней нет";
-        } else if (a == 0 & b != 0) {
+        }
+        if (a == 0 & b != 0) {
             double xOne = -c / b;
             return String.format("Количество решений: 1. Корень %.3f", xOne);
         }
@@ -26,9 +28,12 @@ public class Task4 {
         if (D > 0) {
             double xOne = ((-b) - Math.sqrt(D)) / (2 * a);
             double xTwo = ((-b) + Math.sqrt(D)) / (2 * a);
-            return xOne > xTwo ? String.format("Количество решений: 2. Корни: %.3f; %.3f", xOne, xTwo) : String.format("Количество решений: 2. Корни: %.3f; %.3f", xTwo, xOne);
+            return xOne > xTwo ? String.format("Количество решений: 2. Корни: %.3f; %.3f", xTwo, xOne) : String.format("Количество решений: 2. Корни: %.3f; %.3f", xOne, xTwo);
         } else if (D == 0) {
             double xOne = (-b) / (2 * a);
+            if (xOne == -0) {
+                return String.format("Количество решений: 1. Корень %.3f", -xOne);
+            }
             return String.format("Количество решений: 1. Корень %.3f", xOne);
         } else {
             return "Действительных корней нет";
