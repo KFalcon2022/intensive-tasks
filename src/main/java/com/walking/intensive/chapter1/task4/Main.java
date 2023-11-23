5621f177702a34a099214dcbd63e6f47aee3f70a
mrumilov <mrumilov@fw-t.ru>
2023-11-23 12:56:08 +0300

    часть 1 задача 4 Правки к PR: Добавлена проверка граничных состояний, рефакторинг

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -28,25 +28,27 @@ public class Main {
     static String solveQuadraticEquation(double a, double b, double c) {
         //        Место для вашего кода
         String resultCount = "Количество решений: %d.";
+        String result;
 
-        if (a==0 && b==0 && c==0){
-            return "Количество решений: бесконечно";
+        if (a == 0 && b == 0 && c == 0) {
+            result = "Количество решений: бесконечно";
+        } else if (a == 0 && b == 0 && c != 0) {
+            result = "Количество решений: решений нет";
+        } else if (a == 0 && b != 0 && c != 0) {
+            result = String.format(resultCount, 1) + String.format(" Корень: %,.2f", c * (-1) / b);
+        } else {
+            double discr = Math.pow(b, 2) - 4 * a * c;
+
+            if (discr == 0) {
+                result = String.format(resultCount, 1) + String.format(" Корень: %,.2f", (-1) * b / (2 * a));
+            } else if (discr > 0) {
+                result = String.format(resultCount, 2) + String.format(" Корни: %,.2f;%,.2f", ((-1) * b + java.lang.Math.sqrt(discr)) / (2 * a),
+                        ((-1) * b - Math.sqrt(discr)) / (2 * a));
+            } else {
+                result = String.format(resultCount, 0);
+            }
         }
 
-        if (a==0 && b!=0 && c!=0){
-            return String.format(resultCount, 1) + String.format(" Корень: %,.2f", c*(-1)/b);
-        }
-        double discr = b*b - 4*a*c;
-
-        if (discr ==0){
-            return String.format(resultCount, 1) + String.format(" Корень: %,.2f", (-1)*b/(2*a));
-        }
-        else if (discr > 0){
-            return String.format(resultCount, 2) + String.format(" Корни: %,.2f;%,.2f", ((-1)*b+java.lang.Math.sqrt(discr))/(2*a),
-                    ((-1)*b-java.lang.Math.sqrt(discr))/(2*a));
-        }
-        else {
-            return String.format(resultCount, 0);
-        }
+        return result;
     }
 }
