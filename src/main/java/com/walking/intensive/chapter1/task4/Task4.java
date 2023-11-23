d9238bcfd8f7d8cd42215f5cff5b76018b3ab719
SenyaDiamond <SenyaDiamond@gmail.com>
2023-11-23 18:55:46 +0300

    Chapter 1 Task 4 v. 1.3

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -27,25 +27,29 @@ public class Task4 {
      */
     static String solveQuadraticEquation(double a, double b, double c) {
 
+        String result = "";
         if (a == 0) {
-            return "Уравнение не является квадратным";
+            result =  "Решений бесконечно";
         } else if (b == 0 && c == 0) {
-            return "Количество решений: 1. Корень: 0";
+            result = "Количество решений: 1. Корень: 0";
         } else if (b == 0) {
-            return c / a > 0 ? "Количество решений: 0" : "Количество решений: 2. Корни: -" + Math.sqrt(-c / a) + ";" + Math.sqrt(-c / a);
+            result = c / a > 0 ? "Количество решений: 0." : "Количество решений: 2. Корни: -" + Math.sqrt(-c / a) + ";" + Math.sqrt(-c / a);
         } else if (c == 0) {
-            return (-b / a) < 0 ? "Количество решений: 2. Корни: " + (-b / a) + ";0" : "Количество решений: 2. Корни: 0;" + (-b / a);
+            result = (-b / a) < 0 ? "Количество решений: 2. Корни: " + (-b / a) + ";0" : "Количество решений: 2. Корни: 0;" + (-b / a);
         }
-
+        if (!result.isEmpty()) {
+            return result;
+        }
         double discriminant = ((b * b) - 4 * a * c);
         if (discriminant < 0) {
-            return "Количество решений: 0";
+            result = "Количество решений: 0.";
         } else if (discriminant == 0) {
-            return "Количество решений: 1. Корень: " + (-b / (2 * a));
+            result = "Количество решений: 1. Корень: " + (-b / (2 * a));
         } else {
             double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
             double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            return x1 < x2 ? "Количество решений: 2. Корни: " + x1 + ";" + x2 : "Количество решений: 2. Корни: " + x2 + ";" + x1;
+            result = x1 < x2 ? "Количество решений: 2. Корни: " + x1 + ";" + x2 : "Количество решений: 2. Корни: " + x2 + ";" + x1;
         }
+        return result;
     }
 }
