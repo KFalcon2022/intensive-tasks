a9f9a8c446201f911edde4019e490a74ec6b19f6
dimapros <dima.ru.98@mail.ru>
2023-11-23 22:40:20 +1000

    Chapter1.Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -1,33 +1,62 @@
 package com.walking.intensive.chapter1.task4;
 
-/**
- * РЈСЃР»РѕРІРёРµ: <a href="https://geometry-math.ru/homework/Java-parameter.html">СЃСЃС‹Р»РєР°</a>
- */
-public class Main {
+import java.util.Scanner;
+
+public class Task4 {
     public static void main(String[] args) {
-//        Р”Р»СЏ СЃРѕР±СЃС‚РІРµРЅРЅС‹С… РїСЂРѕРІРµСЂРѕРє РјРѕР¶РµС‚Рµ РґРµР»Р°С‚СЊ Р»СЋР±С‹Рµ РёР·РјРµРЅРµРЅРёСЏ РІ СЌС‚РѕРј РјРµС‚РѕРґРµ
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите число a: ");
+        double a = sc.nextInt();
+
+        System.out.println("Введите число b: ");
+        double b = sc.nextInt();
+
+        System.out.println("Введите число c: ");
+        double c = sc.nextInt();
+
+        sc.close();
 
         System.out.println(solveQuadraticEquation(a, b, c));
-
     }
 
-    /**
-     * РџСЂРё С„РѕСЂРјРёСЂРѕРІР°РЅРёРё СЃС‚СЂРѕРєРё, РєРѕСЂРЅРё СѓСЂР°РІРЅРµРЅРёСЏ РґРѕР»Р¶РЅС‹ СѓРєР°Р·С‹РІР°С‚СЊСЃСЏ РїРѕ РІРѕР·СЂР°СЃС‚Р°РЅРёСЋ.
-     * <p>
-     * РџСЂРёРјРµСЂС‹ СЂРµР·СѓР»СЊС‚РёСЂСѓСЋС‰РµР№ СЃС‚СЂРѕРєРё:
-     * <p>
-     * РљРѕР»РёС‡РµСЃС‚РІРѕ СЂРµС€РµРЅРёР№: 2. РљРѕСЂРЅРё: -4;4
-     * <p>
-     * РљРѕР»РёС‡РµСЃС‚РІРѕ СЂРµС€РµРЅРёР№: 1. РљРѕСЂРµРЅСЊ: 0
-     * <p>
-     * РљРѕР»РёС‡РµСЃС‚РІРѕ СЂРµС€РµРЅРёР№: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        РњРµСЃС‚Рѕ РґР»СЏ РІР°С€РµРіРѕ РєРѕРґР°
+        double discriminant = b * b - 4 * a * c;
 
-        return null; // Р—Р°РіР»СѓС€РєР°. РџСЂРё СЂРµР°Р»РёР·Р°С†РёРё - СѓРґР°Р»РёС‚СЊ
+        if (a == 0 && b == 0 && c == 0) {
+            return "Решений бесконечно";
+        }
+
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+
+        if (a == 0 && b != 0) {
+            int x = (int) (-c / b);
+            return "Количество решений: 1." + " Корень: " + x; // Не могу понять, почему по тестам в этом случае будет ответ 2,
+        }                                                      // ведь при переносе 2 в правую сторону уравнения (bx + c = 0 - это bx = -c) она станет с минусом.
+                                                               // Не знаю, в чем я туплю.
+        if (discriminant > 0) {
+            int x1 = (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
+            int x2 = (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
+
+            String answer = "Количество решений: 2." + " Корни: ";
+
+            if (x1 < x2) {
+                answer += x1 + ";" + x2;
+            } else if (x2 < x1) {
+                answer += x2 + ";" + x1;
+            }
+
+            return answer;
+
+        } else if (discriminant == 0) {
+            int x = (int) (-b / 2 * a);
+
+            return "Количество решений: 1. " + "Корень: " + x;
+
+        } else {
+            return "Количество решений: 0.";
+        }
     }
 }
