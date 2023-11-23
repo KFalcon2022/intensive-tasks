5b07fe3f177f79c94295787cb76fd3c366cfbb2d
MariaMasha91 <poleznosti@bk.ru>
2023-11-23 23:32:23 +0300

    Первая задача, корректировки по комментариям

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -3,17 +3,41 @@ package com.walking.intensive.chapter1.task1;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
-public class Task1 {
+ppublic class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
-        System.out.println(getAgeString(age));
+        int age = 35;
+        if (age < 0) {
+            System.out.println("Вы еще не родились:)");
+        } else if (age > 128) {
+            System.out.println("Столько не живут:)");
+        } else {
+            getAgeString(age);
+        }
     }
-
-    static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+    static void getAgeString(int a) {
+        int remOfDivBy100 = a % 100;
+        int remOfDivBy10 = a % 10;
+        switch (remOfDivBy100) {
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+                System.out.println("Вам " + a + " лет");
+                break;
+            default:
+                switch (remOfDivBy10) {
+                    case 1:
+                        System.out.println("Вам " + a + " год");
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        System.out.println("Вам " + a + " года");
+                        break;
+                    default:
+                        System.out.println("Вам " + a + " лет");
+                }
+        }
     }
 }
