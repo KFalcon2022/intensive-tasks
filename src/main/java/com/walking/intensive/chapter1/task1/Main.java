bd22b082acb73ee6b05aad8cb6f855eb68c90755
U_M1CVT <akachamina@alfabank.ru>
2023-11-23 14:21:08 +0300

    Исправления по замечаниям

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -14,18 +14,19 @@ public class Main {
         if (age > 127 || age < 0) {
             return ("Возраст введен некорректно");
         }
+
         int i = age % 100;
         if (i >= 11 && i <= 14) {
-            return ("Вам" + age + "лет");
+            return ("Вам " + age + " лет");
         }
         int a = i % 10;
-        if (a == 1) {
-            return ("Вам" + age + "год");
-        } else if (a == 2 || a == 3 || a == 4) {
-            return ("Вам" + age + "года");
-        } else {
-            return ("Вам" + age + "лет");
-        }
 
+        if (a == 1) {
+            return ("Вам " + age + " год");
+        } if (a >= 2 && a <= 4) {
+            return ("Вам " + age + " года");
+        } else {
+            return ("Вам " + age + " лет");
+        }
     }
 }
