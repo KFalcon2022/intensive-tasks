39a84bae3e5f852c2de37901df3331603858e9e5
Dmitry <milordoff@yandex.ru>
2023-11-23 23:32:20 +0300

    task 3 solution

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -5,12 +5,12 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println(isLeap(2000));
-        System.out.println(isLeap(4));
-        System.out.println(isLeap(100));
-        System.out.println(isLeap(200));
-        System.out.println(isLeap(1999));
-        System.out.println(isLeap(400));
+        System.out.println(isLeap(2000));       //true
+        System.out.println(isLeap(4));          //true
+        System.out.println(isLeap(100));        //false
+        System.out.println(isLeap(200));        //false
+        System.out.println(isLeap(1999));       //false
+        System.out.println(isLeap(400));        //true
     }
 
     static boolean isLeap(int year) {
