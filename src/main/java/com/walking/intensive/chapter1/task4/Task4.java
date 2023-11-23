efbf6ea9e1c9bfd372e94667ea0bee367783f7aa
SenyaDiamond <SenyaDiamond@gmail.com>
2023-11-23 08:07:04 +0300

    Chapter 1 Task 4 v. 1.2

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,8 +26,26 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0) {
+            return "Уравнение не является квадратным";
+        } else if (b == 0 && c == 0) {
+            return "Количество решений: 1. Корень: 0";
+        } else if (b == 0) {
+            return c / a > 0 ? "Количество решений: 0" : "Количество решений: 2. Корни: -" + Math.sqrt(-c / a) + ";" + Math.sqrt(-c / a);
+        } else if (c == 0) {
+            return (-b / a) < 0 ? "Количество решений: 2. Корни: " + (-b / a) + ";0" : "Количество решений: 2. Корни: 0;" + (-b / a);
+        }
+
+        double discriminant = ((b * b) - 4 * a * c);
+        if (discriminant < 0) {
+            return "Количество решений: 0";
+        } else if (discriminant == 0) {
+            return "Количество решений: 1. Корень: " + (-b / (2 * a));
+        } else {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            return x1 < x2 ? "Количество решений: 2. Корни: " + x1 + ";" + x2 : "Количество решений: 2. Корни: " + x2 + ";" + x1;
+        }
     }
 }
