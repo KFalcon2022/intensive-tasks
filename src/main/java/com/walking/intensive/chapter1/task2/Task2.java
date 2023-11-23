31f411699e041fa4b8681d177814fcd70249d3ec
Vbliznev <7492008@mail.ru>
2023-11-23 13:40:51 +0300

    Merge pull request #87 from KFalcon2022/revert
    Revert

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        System.out.println(getFlatLocation(4, 3, 5));
+        System.out.println(getFlatLocation(10, 3, -1));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
@@ -18,10 +18,14 @@ public class Task2 {
 
         flatAmount = 4 * floorAmount * entranceAmount;
         if (flatNumber > flatAmount || flatNumber <= 0) {
-            return "Такой квартиры нет";
+            return "Такой квартиры не существует";
+
         }
         entranceNumber = (int) (Math.ceil((double) flatNumber / 4 / floorAmount));
         floorNumber = ((int) (Math.ceil((double) flatNumber / 4))) % floorAmount;
+        if (floorNumber==0){
+            floorNumber = floorAmount;
+        }
         System.out.println(flatNumber % 4);
         switch (flatNumber % 4) {
             case (1):
