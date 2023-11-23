675b4b20886536fd7ba724526647100b7188c6b2
Alphyra <ageyeva.sveta@gmail.com>
2023-11-23 16:07:57 +0300

    Chapter 1 Task 3 V.3

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -14,6 +14,6 @@ public class Main {
     }
 
     static boolean isLeap(int year) {
-        return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? true : false;
+        return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
     }
 }
