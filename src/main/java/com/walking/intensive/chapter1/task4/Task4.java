06c1d3a164d9f779d7b57d2d9602b828fd705698
egor.djun <xedshotg@gmail.com>
2023-11-23 11:48:49 +0700

    Подправил вывод, добавил логики (если а == 0)

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -7,8 +7,8 @@ public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 0;
-        double c = 5;
+        double b = 1;
+        double c = 2;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -28,21 +28,24 @@ public class Task4 {
     static String solveQuadraticEquation(double a, double b, double c) {
         //не полные квадратные уровнения
         if (a == 0 && b == 0 && c == 0) {
-            return "Ошибка!";
+            return "Решений бесконечно";
+        }
+        if(a == 0 && b !=0 && c !=0){
+           return "Количество решений: 1. Корень: " + c;
         }
         if (b == 0 && c != 0) {
             double x = (-c) / a;
             if (x > 0) {
-                double x1 = Math.sqrt((-c) / a);
-                double x2 = -Math.sqrt((-c) / a);
-                return "Количество решений: 2. Корни " + x1 + "; " + x2;
+                double x1 = -Math.sqrt((-c) / a);
+                double x2 = Math.sqrt((-c) / a);
+                return "Количество решений: 2. Корни " + x1 + ";" + x2;
             }
             if (x < 0){
                 return "Количество решений: 0.";
             }
         }
         if (b != 0 && c == 0){
-            return "Количество решений: 2. Корни " + 0 + "; " + ((-b)/a);
+            return "Количество решений: 2. Корни " + 0 + ";" + ((-b)/a);
         }
         if (b == 0 && c == 0){
             return "Количество решений: 1. Корень 0";
@@ -56,7 +59,7 @@ public class Task4 {
         if (d > 0) {
             double x1 = (-b - Math.sqrt(d)) / (2 * a);
             double x2 = (-b + Math.sqrt(d)) / (2 * a);
-            return "Количество решений: 2. Корни " + x1 + "; " + x2;
+            return "Количество решений: 2. Корни " + x1 + ";" + x2;
         }
         return "Количество решений: 1. Корень " + ((-b) / (2 * a));
     }
