7e888e20c5e02a608f75dc04c411863c0619a8d6
mirchello <mrumilov@gmail.com>
2023-11-23 10:38:13 +0300

    часть 1 задача 2 Правки к PR: изменен расчет граничных условий (расчет этажа и поворота от лифта)

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task2;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        System.out.println(getFlatLocation(10, 3, 41));
+        System.out.println(getFlatLocation(10, 3, 32));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
@@ -21,9 +21,9 @@ public class Main {
             return result;
         }
 
-        int entranceNumber = flatNumber / entranceCount;
-        int floorNumber = (flatNumber % entranceCount) / floorCount;
-        int leftrightTyrn = flatNumber % floorCount;
+        int entranceNumber = (flatNumber-1) / entranceCount;
+        int floorNumber = ((flatNumber-1) % entranceCount) / floorCount;
+        int leftrightTyrn = flatNumber % floorCount == 0 ? floorCount : flatNumber % floorCount;
 
         result = "%d кв - %d подъезд, %d этаж, %s от лифта, %s";
         String turnFromLift = leftrightTyrn <= 2 ? "слева" : "справа";
