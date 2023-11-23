af608ad05959f6a6c1fe3839971a9a8a00448cb4
Dmitry <milordoff@yandex.ru>
2023-11-23 22:34:57 +0300

    task 1 solution (Corrections)

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-        int age = 114;
+        int age = 1;
 
         System.out.println(getAgeString(age));
     }
@@ -14,20 +14,17 @@ public class Main {
         int remainder10 = age % 10;
         int remainder100 = age % 100;
 
-        if (age >= 0 & age < 128) {
-            if (remainder100 == 11 || remainder100 == 12 || remainder100 == 13 || remainder100 == 14) {
-                return ("Вам " + age + " лет");
-            } else {
-                if (remainder10 == 1) {
-                    return ("Вам " + age + " год");
-                } else if (remainder10 == 2 || remainder10 == 3 || remainder10 == 4) {
-                    return ("Вам " + age + " года");
-                } else {
-                    return ("Вам " + age + " лет");
-                }
-            }
+        if (age < 0 || age > 127) {
+            return "Неверно указан возраст";
+        } else if (remainder100 == 11 || remainder100 == 12 || remainder100 == 13 || remainder100 == 14) {
+            return "Вам " + age + " лет";
+        } else if (remainder10 == 1 && remainder100 != 11) {
+            return "Вам " + age + " год";
+        } else if (remainder10 == 2 & remainder100 != 12 || remainder10 == 3 & remainder100 != 13 ||
+                   remainder10 == 4 & remainder100 != 14) {
+            return "Вам " + age + " года";
         } else {
-            return ("Неверно указан возраст");
+            return "Вам " + age + " лет";
         }
     }
 }
