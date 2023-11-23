fd2525743811594054afe0a899492429247ca2a0
Akarumai <yzavgoro124@gmail.com>
2023-11-23 11:32:12 +0300

    task1_version1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,14 +6,25 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 22;
 
         System.out.println(getAgeString(age));
+        if (age > 127) System.out.println("У Вас новый рекорд долгожительства!(или Вы не человек)");
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        String yourAge = "Вам ";
+
+        if (age < 0) {
+            return "Ваше летоисчисление еще не началось!";
+        } else if (age % 10 == 0 || age % 100 > 10 && age % 100 < 15 || age % 10 > 4) {
+            yourAge = yourAge + age + " лет!";
+        } else if (age % 10 > 1) {
+            yourAge = yourAge + age + " года!";
+        } else {
+            yourAge = yourAge + age + " год!";
+        }
+        return yourAge;
     }
 }
