ef2012ea0eae59dc633a51c7e291d7d9f41860be
sauza7 <sauza777@yandex.ru>
2023-11-23 19:28:00 +0300

    Chapter 1 Task2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -1,17 +1,40 @@
 package com.walking.intensive.chapter1.task2;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
- */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        System.out.println(getFlatLocation(10,2,41));
     }
-
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int entranceNumber = 0;
+        String flatPosition, flatDirection;
+
+        int entranceCapacity = floorAmount * 4;
+        int flatAmount = entranceCapacity * entranceAmount;
+
+        if ( flatNumber < 1 || flatNumber > flatAmount || floorAmount <1 || entranceAmount < 1)  {
+            return "Такой квартиры не существует";
+        }
+
+        for (int i = 1 ; i <= entranceAmount ; i++) {
+            if (flatNumber <= (entranceCapacity * i)) {
+                entranceNumber = i;
+                break;
+            }
+        }
+
+        int flatNumberInEntrance = flatNumber - (entranceNumber-1) * entranceCapacity;
+        int floorNumber = flatNumberInEntrance % 4 == 0 ? flatNumberInEntrance / 4 : (flatNumberInEntrance / 4) + 1;
+
+        if (flatNumberInEntrance % 4 == 0 || flatNumberInEntrance % 4 == 3) {
+            flatPosition = "справа";
+            flatDirection = "вправо";
+        } else {
+            flatPosition = "слева";
+            flatDirection = "влево";
+        }
+
+        return String.format ("%d кв - %d подъезд, %d этаж, %s от лифта, %s",
+                flatNumber, entranceNumber, floorNumber , flatPosition, flatDirection);
     }
 }
