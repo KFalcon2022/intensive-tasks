55e30aaad8cb79a4b9eaf94e1c8168ba09d6196a
Stas <shadrin060388@gmail.com>
2023-11-23 11:43:57 +0300

    Update2 Chapter 1 Task 3

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -6,12 +6,12 @@ package com.walking.intensive.chapter1.task3;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int year = 1700;
+        int year = 1600;
         System.out.println(isLeap(year));
     }
 
     static boolean isLeap(int year) {
 
-        return  ((year % 4 == 0) || (year % 100 == 0)) && (year % 400 == 0);
+        return  ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
 }
