2164461a0d70ce8eea396547cd2d398a03da91ba
Boris <aksenov.boris8595@gmeil.com>
2023-11-24 01:31:20 +0500

    Решение 1 задачи, тесты пройдены.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,14 +6,26 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 112;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        String getAgeStr = "";
+        if (age%100 == 11 || age%100 == 12 || age%100 == 13 || age%100 == 14) {
+            getAgeStr = " лет";
+        } else {
+            if (age % 10 == 1) {
+                getAgeStr = " год";
+            } else {
+                if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) {
+                    getAgeStr = " года";
+                } else {
+                    getAgeStr = " лет";
+                }
+            }
+        }
+        return "Вам " + age + getAgeStr;
     }
 }
