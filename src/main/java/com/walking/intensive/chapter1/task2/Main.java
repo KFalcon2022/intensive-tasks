dda70c17444fdca0fd050541bf3e94c89ac7e14b
k2336351 <k2336351@yandex.ru>
2023-11-24 00:42:14 +0300

    Solution Chapter 1  Task 2 (добавлены условия проверки на валидность)

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -6,60 +6,63 @@ package com.walking.intensive.chapter1.task2;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int floorAmount = 2;
-        int entranceAmount = 3;
+        int floorAmount = -2;
+        int entranceAmount = -3;
         int flatNumber = 11;
 
         System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-
         final int FLOOR_FLAT_AMOUNT = 4;
-        int flatAmount = FLOOR_FLAT_AMOUNT * floorAmount * entranceAmount;
         String flatLocation;
 
-        if (flatNumber <= 0 || flatNumber > flatAmount) {
-            flatLocation = "Квартиры с указанным номером нет. Проверьте введённые данные.";
+        if (floorAmount <= 0 || entranceAmount <= 0 || flatNumber <= 0) {
+            flatLocation = "Введены некорректные данные.";
         } else {
-            int entranceNumber;
-            int entranceFlatAmount = FLOOR_FLAT_AMOUNT * floorAmount;
-            int divisionEntranceNumber = flatNumber / entranceFlatAmount;
-
-            if (flatNumber % entranceFlatAmount == 0) {
-                entranceNumber = divisionEntranceNumber;
+            int flatAmount = FLOOR_FLAT_AMOUNT * floorAmount * entranceAmount;
+            if (flatNumber > flatAmount) {
+                flatLocation = "Квартиры с указанным номером нет. Проверьте введённые данные.";
             } else {
-                entranceNumber = ++divisionEntranceNumber;
+                int entranceNumber;
+                int entranceFlatAmount = FLOOR_FLAT_AMOUNT * floorAmount;
+                int divisionEntranceNumber = flatNumber / entranceFlatAmount;
+
+                if (flatNumber % entranceFlatAmount == 0) {
+                    entranceNumber = divisionEntranceNumber;
+                } else {
+                    entranceNumber = ++divisionEntranceNumber;
+                }
+
+                int floorNumber;
+                int divisionFloorNumber =
+                        (flatNumber - (entranceNumber - 1) * entranceFlatAmount) / FLOOR_FLAT_AMOUNT;
+
+                if (flatNumber % FLOOR_FLAT_AMOUNT == 0) {
+                    floorNumber = divisionFloorNumber;
+                } else {
+                    floorNumber = ++divisionFloorNumber;
+                }
+
+                String floorPlace;
+                int modFloorFlatAmount = flatNumber % FLOOR_FLAT_AMOUNT;
+
+                switch (modFloorFlatAmount) {
+                    case 1:
+                        floorPlace = "слева от лифта, влево";
+                        break;
+                    case 2:
+                        floorPlace = "слева от лифта, вправо";
+                        break;
+                    case 3:
+                        floorPlace = "справа от лифта, влево";
+                        break;
+                    default:
+                        floorPlace = "справа от лифта, вправо";
+                }
+                flatLocation = String.format("%d кв - %d подъезд, %d этаж, %s",
+                        flatNumber, entranceNumber, floorNumber, floorPlace);
             }
-
-            int floorNumber;
-            int divisionFloorNumber =
-                    (flatNumber - (entranceNumber - 1) * entranceFlatAmount) / FLOOR_FLAT_AMOUNT;
-
-            if (flatNumber % FLOOR_FLAT_AMOUNT == 0) {
-                floorNumber = divisionFloorNumber;
-            } else {
-                floorNumber = ++divisionFloorNumber;
-            }
-
-            String floorPlace;
-            int modFloorFlatAmount = flatNumber % FLOOR_FLAT_AMOUNT;
-
-            switch (modFloorFlatAmount) {
-                case 1:
-                    floorPlace = "слева от лифта, влево";
-                    break;
-                case 2:
-                    floorPlace = "слева от лифта, вправо";
-                    break;
-                case 3:
-                    floorPlace = "справа от лифта, влево";
-                    break;
-                default:
-                    floorPlace = "справа от лифта, вправо";
-            }
-            flatLocation = String.format("%d кв - %d подъезд, %d этаж, %s",
-                    flatNumber, entranceNumber, floorNumber, floorPlace);
         }
         return flatLocation;
     }
