5ea4dc466063077b3e6313609a83438f6db22865
Vbliznev <7492008@mail.ru>
2023-11-23 14:41:51 +0300

    Merge pull request #87 from KFalcon2022/revert
    Revert

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,39 +6,14 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 111;
+        int age = 0;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        String year = "";
-        String result = "";
+//        Место для вашего кода
 
-        int lastNumberFromAge = age % 10;
-
-        if (lastNumberFromAge == 1) {
-            year = "год";
-        } else {
-            if (lastNumberFromAge == 2 || lastNumberFromAge == 3 || lastNumberFromAge == 4) {
-                year = "года";
-            } else {
-                year = "лет";
-
-            }
-        }
-            switch (age) {
-                case 11-> year = "лет";
-                case 12 -> year = "лет";
-                case 13 -> year = "лет";
-                case 14 -> year = "лет";
-                case 111 -> year = "лет";
-                case 112 -> year = "лет";
-                case 113 -> year = "лет";
-                case 114 -> year = "лет";
-            }
-
-        result = ("Вам " + age + " " + year);
-        return result;
+        return null; // Заглушка. При реализации - удалить
     }
 }
--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -6,42 +6,12 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        System.out.println(getFlatLocation(10, 3, -1));
+
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        int flatAmount = 0;
-        int entranceNumber = 0; //номер подъезда
-        int floorNumber = 0; //номер этажа
-        String result = "";
-        String direction = "";
+        //        Место для вашего кода
 
-        flatAmount = 4 * floorAmount * entranceAmount;
-        if (flatNumber > flatAmount || flatNumber <= 0) {
-            return "Такой квартиры не существует";
-
-        }
-        entranceNumber = (int) (Math.ceil((double) flatNumber / 4 / floorAmount));
-        floorNumber = ((int) (Math.ceil((double) flatNumber / 4))) % floorAmount;
-        if (floorNumber==0){
-            floorNumber = floorAmount;
-        }
-        System.out.println(flatNumber % 4);
-        switch (flatNumber % 4) {
-            case (1):
-                direction = "слева от лифта, влево";
-                break;
-            case (2):
-                direction = "слева от лифта, вправо";
-                break;
-            case (3):
-                direction = "справа от лифта, влево";
-                break;
-            case (0):
-                direction = "справа от лифта, вправо";
-                break;
-        }
-        result = flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + direction;
-        return result; // Заглушка. При реализации - удалить
+        return null; // Заглушка. При реализации - удалить
     }
 }
