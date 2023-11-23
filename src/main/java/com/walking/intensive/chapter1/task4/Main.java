5adb7c07c04211d883929dbc59512d78187b28ab
Lunularis <s.n.volynkina@yandex.ru>
2023-11-23 09:00:54 +0700

    Chapter 1 Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -5,10 +5,10 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         double a = 0;
         double b = 0;
-        double c = 0;
+        double c = -5;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -26,8 +26,39 @@ public class Main {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        String answer = "";
+        byte answerCount = 0;
+        double x1, x2;
+
+        if (a == 0 & b != 0) {
+            //это линейное уравнение
+            answerCount = 1;
+            x1 = -1 * c / b;
+            answer = "Корень: " + x1;
+        } else {
+
+            double d;
+            d = Math.pow(b, 2) - 4 * a * c;
+
+            if (d == 0 & a != 0) {
+                answerCount = 1;
+                x1 = -1 * b / (2 * a);
+                answer = "Корень: " + x1;
+            } else if (d > 0) {
+                answerCount = 2;
+                answer = "Корни: ";
+                x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+                x2 = (-1 * b - Math.sqrt(d)) / (2 * a);
+
+                if (x1 < x2) {
+                    answer += x1 + ";" + x2;
+                } else {
+                    answer += x2 + ";" + x1;
+                }
+            }
+        }
+        answer = "Количество решений: " + answerCount + ". " + answer;
+        return answer;
     }
 }
