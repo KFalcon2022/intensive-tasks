f17347f955b13f4eecb570977eb507bc0b8a6b08
BogdanKaliuzhniy <kaliuzhnb@gmail.com>
2023-11-23 20:40:40 +0300

    Task_3_LeapYear

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -1,17 +1,22 @@
 package com.walking.intensive.chapter1.task3;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-leap-year.html">ссылка</a>
  */
 public class Task3 {
     public static void main(String[] args) {
-        //Для собственных проверок можете делать любые изменения в этом методе
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
 
+        System.out.println(isLeap(year) ? year + " год високосный" : year + "год не високосный");
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        if (year % 400 == 0 && year % 100 == 0) {
+            return true;
+        } else return year % 100 != 0 && year % 4 == 0;
     }
+
 }
