93825334010a0310a5786f19638d7d1aa6b63d99
njmist <njmist@gmail.com>
2023-11-23 22:26:50 +0300

    Решение 3 задачи, проверил на тесте

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -5,13 +5,20 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-        //Для собственных проверок можете делать любые изменения в этом методе
-
+//        Для собственных проверок можете делать любые изменения в этом методе
+        int year = 2004;
+        if (isLeap(year)) {
+            System.out.println("По григорианскому календарю " + year + " год является високосным");
+        } else {
+            System.out.println("По григорианскому календарю " + year + " год является невисокосным");
+        }
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
+        //        Место для вашего кода
 
-        return false; // Заглушка. При реализации - удалить
+        return (year % 400 == 0 || year % 4 == 0 && year % 100 != 0); // Теперь я лучше понимаю
+
     }
+
 }
