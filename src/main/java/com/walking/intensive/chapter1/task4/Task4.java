de3d36d6b946f7fc1f4a40c7c74d54b309ec318b
Alexander <143022318+vonabirgea@users.noreply.github.com>
2023-11-23 17:34:20 +0000

    Chapter 1 Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -6,9 +6,9 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
+        double a = 1;
         double b = 0;
-        double c = 0;
+        double c = -1;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -26,8 +26,33 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0 && b == 0 && c == 0) {
+            return "Решений бесконечно";
+        }
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+        if (a == 0) {
+            double root = -c / b;
+            return String.format("Количество решений: 1. Корень: %.0f", root);
+        }
+        double descriminant = b * b - 4 * a * c;
+        if (descriminant > 0) {
+            double root1 = (-b  + Math.sqrt(descriminant)) / (2 * a);
+            double root2 = (-b  - Math.sqrt(descriminant)) / (2 * a);
 
-        return null; // Заглушка. При реализации - удалить
+            if (root2 < root1) {
+                double temp = root2;
+                root2 = root1;
+                root1 = temp;
+            }
+
+            return String.format("Количество решений: 2. Корни: %.0f; %.0f", root1, root2);
+        }
+        if (descriminant - 0 < 0.00001) {
+            double root = -b / (2 * a);
+            return String.format("Количество решений: 1. Корень: %.0f", root);
+        }
+        return "Что-то пошло не так";
     }
 }
