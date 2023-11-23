be9c282cd8e4a5c0100185b744932d0adbd62626
Stas <shadrin060388@gmail.com>
2023-11-23 18:09:09 +0300

    Chapter 1 Task 2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,13 +5,40 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        System.out.println(getFlatLocation(10, 3, 81));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int apartmentsOnFloor = 4;
+        int apartmentsInEntrance = floorAmount * apartmentsOnFloor; //количество квартир в подъезде
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber < 1 || flatNumber > apartmentsInEntrance * entranceAmount){
+            return "Такой квартиры не существует";
+        }
+
+        int entranceNumber; // номер подъезда
+
+        if (flatNumber % apartmentsInEntrance == 0) {
+            entranceNumber = flatNumber / apartmentsInEntrance;
+        } else {
+            entranceNumber = flatNumber / apartmentsInEntrance + 1;
+        }
+
+        int floorNumber; // номер этажа
+
+        if (flatNumber % apartmentsOnFloor == 0) {
+            floorNumber = (flatNumber / apartmentsOnFloor) - floorAmount * (entranceNumber - 1);
+        } else {
+            floorNumber = (flatNumber / apartmentsOnFloor + 1) - floorAmount * (entranceNumber - 1);
+        }
+
+        String flatLocation = switch (flatNumber % apartmentsOnFloor) { // расположение относительно лифта
+            case (1) -> "слева от лифта, влево";
+            case (2) -> "слева от лифта, вправо";
+            case (3) -> "справа от лифта, влево";
+            case (0) -> "справа от лифта, вправо";
+            default -> "";
+        };
+        return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + flatLocation;
     }
 }
