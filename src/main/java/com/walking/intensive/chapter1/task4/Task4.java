1d16c131f5b52902d04d90d0710d683c3f09667d
njmist <njmist@gmail.com>
2023-11-23 22:09:56 +0300

    решение 4 задачи, тест 3 не проходит, но 0 * x + 1 * x + 2 = 0 это x = - 2

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -6,9 +6,9 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 1;
-        double b = -1;
-        double c = 0;
+        double a = 0;
+        double b = 1;
+        double c = 2;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -26,23 +26,34 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
+
         double d = b * b - 4 * a * c;
-        if (a == 0 && b != 0) {
-            return "Это линейное уравнение";
-        } else if (a == 0) {
-            return "Это не уравнение";
-        } else if (d < 0) {
-            return "Количество решений: 0.";
-        } else if (d == 0) {
-            double x = -b / (2 * a);
-            return "Количество решений: 1. Корень: " + x;
-        } else {
-            double x1 = (-b + Math.sqrt(d)) / (2 * a), x2 = (-b - Math.sqrt(d)) / (2 * a);
-            if (x1 < x2) {
-                return "Количество решений: 2. Корни: " + x1 + "; " + x2 + ".";
-            } else {
-                return "Количество решений: 2. Корни: " + x2 + "; " + x1 + ".";
-            }
+
+        if (a == 0 && b == 0 && c == 0) {
+            return "Решений бесконечно";
         }
+
+        if (a == 0 && b == 0 || d < 0) {
+            return "Количество решений: 0.";
+        }
+
+        if (a == 0) {
+            int x = (int) Math.round(-c);
+            return "Количество решений: 1. Корень: " + x;
+        }
+
+        if (d == 0) {
+            int x = (int) Math.round(-b / (2 * a));
+            return "Количество решений: 1. Корень: " + x;
+        }
+
+        int x1 = (int) Math.round((-b + Math.sqrt(d)) / (2 * a)), x2 = (int) Math.round((-b - Math.sqrt(d)) / (2 * a));
+
+        if (x1 < x2) {
+            return "Количество решений: 2. Корни: " + x1 + ";" + x2;
+        }
+
+        return "Количество решений: 2. Корни: " + x2 + ";" + x1;
+
     }
 }
