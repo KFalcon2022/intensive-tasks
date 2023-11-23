574a4d3aacecf70157a0e532fdeb3af4990abceb
Vbliznev <7492008@mail.ru>
2023-11-23 14:29:29 +0300

    Merge pull request #87 from KFalcon2022/revert
    Revert

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,44 +1,17 @@
-package com.walking.intensive.chapter1.task1;
+package com.walking.intensive.chapter1.task2;
 
 /**
- * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
+ * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
-public class Task1 {
+public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 111;
 
-        System.out.println(getAgeString(age));
     }
 
-    static String getAgeString(int age) {
-        String year = "";
-        String result = "";
+    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
+        //        Место для вашего кода
 
-        int lastNumberFromAge = age % 10;
-
-        if (lastNumberFromAge == 1) {
-            year = "год";
-        } else {
-            if (lastNumberFromAge == 2 || lastNumberFromAge == 3 || lastNumberFromAge == 4) {
-                year = "года";
-            } else {
-                year = "лет";
-
-            }
-        }
-        switch (age) {
-            case 11 -> year = "лет";
-            case 12 -> year = "лет";
-            case 13 -> year = "лет";
-            case 14 -> year = "лет";
-            case 111 -> year = "лет";
-            case 112 -> year = "лет";
-            case 113 -> year = "лет";
-            case 114 -> year = "лет";
-        }
-
-        result = ("Вам " + age + " " + year);
-        return result;
+        return null; // Заглушка. При реализации - удалить
     }
 }
