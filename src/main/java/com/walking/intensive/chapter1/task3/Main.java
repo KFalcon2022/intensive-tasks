10d9dd4dba73d2725fdf088248ba95daaead175f
apjam <apjam@yandex.ru>
2023-11-23 23:04:18 +0600

    task3 is done

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -5,13 +5,19 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(isLeap(2100));
 
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        if (year % 4 == 0) {
+            if (year % 400 != 0 && year % 100 == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 }
