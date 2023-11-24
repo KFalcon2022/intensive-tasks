51d185cb06f5121c76fb283105819544f2e14590
NJmist <njmist@gmail.com>
2023-11-24 16:33:44 +0300

    4 задача исправление + поправил переменную дискриминанта, сделал понятное название и инициализировал ближе к применению

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -27,13 +27,12 @@ public class Task4 {
      */
     static String solveQuadraticEquation(double a, double b, double c) {
 
-        double d = b * b - 4 * a * c;
-
         if (a == 0 && b == 0 && c == 0) {
             return "Решений бесконечно";
         }
+        double discriminant = b * b - 4 * a * c;
 
-        if (a == 0 && b == 0 || d < 0) {
+        if (a == 0 && b == 0 || discriminant < 0) {
             return "Количество решений: 0.";
         }
 
@@ -43,13 +42,13 @@ public class Task4 {
             return "Количество решений: 1. Корень: " + x;
         }
 
-        if (d == 0) {
+        if (discriminant == 0) {
             int x = (int) Math.round(-b / (2 * a));
 
             return "Количество решений: 1. Корень: " + x;
         }
 
-        int x1 = (int) Math.round((-b + Math.sqrt(d)) / (2 * a)), x2 = (int) Math.round((-b - Math.sqrt(d)) / (2 * a));
+        int x1 = (int) Math.round((-b + Math.sqrt(discriminant)) / (2 * a)), x2 = (int) Math.round((-b - Math.sqrt(discriminant)) / (2 * a));
 
         if (x1 < x2) {
             return "Количество решений: 2. Корни: " + x1 + ";" + x2;
