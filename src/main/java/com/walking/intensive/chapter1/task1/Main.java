92feec929484aef66c903e32c86137d71e696b95
bergen <bergy@yandex.ru>
2023-11-24 16:15:41 +0300

    remove solution of task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,17 +6,10 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
-        System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 //        Место для вашего кода
-        if (age < 1) return "Вы не родились";
-        if (age < 2) return "Вам 1 год";
-        if (age < 5 ) return "Вам" + age + "года";
-        return "Вам" + age + "лет";
-//        return null; // Заглушка. При реализации - удалить
+        return null; // Заглушка. При реализации - удалить
     }
 }
