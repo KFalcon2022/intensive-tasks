431eee49d68a571df0c77e3cba44682d53e025ea
mirchello <mrumilov@gmail.com>
2023-11-24 14:10:31 +0300

    часть 1 задача 4 Правки к PR: зменен текст выводов; изменен порядок выводимых корней

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -7,8 +7,8 @@ public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 1;
-        double b = 4;
-        double c = 4;
+        double b = 0;
+        double c = -1;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -31,19 +31,23 @@ public class Task4 {
         String result;
 
         if (a == 0 && b == 0 && c == 0) {
-            result = "Количество решений: бесконечно";
+            result = "Решений бесконечно";
         } else if (a == 0 && b == 0 && c != 0) {
-            result = "Количество решений: решений нет";
-        } else if (a == 0 && b != 0 && c != 0) {
-            result = String.format(resultCount, 1) + String.format(" Корень: %,.2f", c * (-1) / b);
+            result = "Количество решений: 0.";
+        } else if (a != 0 && b == 0 && c == 0){
+            result = "Количество решений: 1. Корень: 0";
+        }
+        else if (a == 0 && b != 0 && c != 0) {
+            result = String.format(resultCount, 1) + String.format(" Корень: %d", Math.round(c * (-1) / b));
         } else {
             double discr = Math.pow(b, 2) - 4 * a * c;
 
             if (discr == 0) {
-                result = String.format(resultCount, 1) + String.format(" Корень: %,.2f", (-1) * b / (2 * a));
+                result = String.format(resultCount, 1) + String.format(" Корень: %d", Math.round((-1) * b / (2 * a)));
             } else if (discr > 0) {
-                result = String.format(resultCount, 2) + String.format(" Корни: %,.2f;%,.2f", ((-1) * b + java.lang.Math.sqrt(discr)) / (2 * a),
-                        ((-1) * b - Math.sqrt(discr)) / (2 * a));
+                result = String.format(resultCount, 2) + String.format(" Корни: %d;%d",
+                        Math.round(((-1) * b - Math.sqrt(discr)) / (2 * a)),
+                        Math.round(((-1) * b + java.lang.Math.sqrt(discr)) / (2 * a)));
             } else {
                 result = String.format(resultCount, 0);
             }
