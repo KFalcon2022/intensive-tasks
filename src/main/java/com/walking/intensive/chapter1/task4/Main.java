45e69e8fd1deeca9be9d1fa92e9be3cf12154fdc
apjam <apjam@yandex.ru>
2023-11-25 00:22:07 +0600

    task4 is done2

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -6,9 +6,9 @@ package com.walking.intensive.chapter1.task4;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 5;
-        double b = 25;
-        double c = 5;
+        double a = 0;
+        double b = 1;
+        double c = 2;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -27,25 +27,22 @@ public class Main {
      */
     static String solveQuadraticEquation(double a, double b, double c) {
 //        a × x2 + b × x + c = 0 .
-        if (Double.compare(a, 0.0) == 0) {
-            return "Аргумент \"a\" не может быть равен 0";
-        }
         double D = Math.pow(b, 2) - (4.0 * a * c);
-        double x1, x2;
-
-        String textCount;
         if (D < 0) {
-            textCount = "Количество решений: 0.";
-        } else {
-            x1 = (-b + Math.sqrt(D)) / (2.0 * a);
-            if (Double.compare(D, 0.0) == 0) {
-                textCount = "Количество решений: 1. Корень: " + x1;
-            } else {
-                x2 = (-b - Math.sqrt(D)) / (2.0 * a);
-                textCount = "Количество решений: 2. Корни: " + x1 + ";" + x2;
-            }
+            return "Количество решений: 0.";
         }
 
-        return textCount;
+        double x1, x2;
+        if (Double.compare(a, 0.0) == 0) {
+            x1 = -c / b;
+            return "Количество решений: 1. Корень: " + x1;
+        }
+        x1 = (-b + Math.sqrt(D)) / (2.0 * a);
+        if (Double.compare(D, 0.0) == 0) {
+            return "Количество решений: 1. Корень: " + x1;
+        } else {
+            x2 = (-b - Math.sqrt(D)) / (2.0 * a);
+            return "Количество решений: 2. Корни: " + x1 + ";" + x2;
+        }
     }
 }
