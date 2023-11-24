174aa7ead23be2914e322fa46f92f71ec87b3149
Galina Magazinova <gmagazinova@gmail.com>
2023-11-24 09:03:13 +0300

    task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -5,13 +5,20 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-        //Для собственных проверок можете делать любые изменения в этом методе
+        int year = 2023;
 
+        if (isLeap(year)) {
+            System.out.println(year + " год является високосным ");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else return year % 4 == 0;
     }
 }
