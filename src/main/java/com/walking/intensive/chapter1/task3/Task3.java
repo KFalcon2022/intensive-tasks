0dc060cf4e2b14bb7ed4a2a6e375105a6464f691
MariaMasha91 <poleznosti@bk.ru>
2023-11-24 23:01:38 +0300

    Первая задача, корректировки по комментариям

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -5,13 +5,14 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-        //Для собственных проверок можете делать любые изменения в этом методе
-
+        int year = 400;
+        isLeap(year);
     }
-
-    static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+    static void isLeap(int year) {
+        if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
+            System.out.println("Год " + year + " високосный");
+        } else {
+            System.out.println("Год " + year + " невисокосный");
+        }
     }
 }
