20f99fd5c0307ce2bdc33bf258522c9b7d38c4f5
dorogao5 <145500950+dorogao5@users.noreply.github.com>
2023-11-24 20:49:15 +0300

    chapter 1 Task5

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -20,19 +20,19 @@ public class Main {
         if (a == 0 && b == 0 && c == 0) {
             return "Бесконечное множество решений";
         }
-        if (a == 0 && b == 0 && c!=0 || b == 0 && c > 0){
+        if (a == 0 && b == 0 && c != 0 || b == 0 && c > 0) {
             return "invalid numbers";
         }
-        if (b == 0 && c < 0){
-            double x1 = Math.sqrt(-c/a);
-            double x2 = -Math.sqrt(-c/a);
+        if (b == 0 && c < 0) {
+            double x1 = Math.sqrt(-c / a);
+            double x2 = -Math.sqrt(-c / a);
             return "Количество решений: 2. Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
         }
-        if(b == 0 && c == 0){
+        if (b == 0 && c == 0) {
             return "Количество решений: 1. Корень: 0";
         }
-        if (a == 0){
-            return String.format("Количество решений: 1. Корень %f", -c/b);
+        if (a == 0) {
+            return String.format("Количество решений: 1. Корень %f", -c / b);
         }
         double diskr = b * b - 4 * a * c;
         double x1 = ((-b + Math.sqrt(diskr)) / 2 * a);
