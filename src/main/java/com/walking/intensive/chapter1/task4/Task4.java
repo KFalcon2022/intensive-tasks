7c6f6d99772da4b6b1e2f7a532e1330b18b8bb36
Rust <madishkin2@gmail.com>
2023-11-24 11:43:38 +0300

    Chapter 1 Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -7,8 +7,8 @@ public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 0;
-        double c = 0;
+        double b = 1;
+        double c = 2;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -27,7 +27,42 @@ public class Task4 {
      */
     static String solveQuadraticEquation(double a, double b, double c) {
         //        Место для вашего кода
+        String message = "Решений бесконечно";
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 & b == 0 & c == 0) {
+            return message;
+        } else if (a == 0 & b == 0) {
+            //double x = -c / b;
+            message = "Количество решений: 0.";
+            return message;
+        } else if (a == 0) {
+            double x = -c / b;
+            message = String.format("Количество решений: 1. Корень: %.0f", x);
+            return message;
+        } else {
+            double D = (b * b) - (4 * a * c); //дискриминант квадратного уравнения
+
+            if (D < 0) {
+                message = "Количество решений: 0.";
+            }
+            if (D == 0) {
+                double x = -b / 2 * a;
+                if (x == -0.0){
+                    x = Math.abs(x);
+                }
+                message = String.format("Количество решений: 1. Корень: %.0f", x);
+            }
+            if (D > 0) {
+                double x1 = (-b + Math.sqrt(D)) / (2 * a);
+                double x2 = (-b - Math.sqrt(D)) / (2 * a);
+                if (x2 > x1) {
+                    message = String.format("Количество решений: 2. Корни: %.0f;%.0f" , x1 , x2);
+                } else {
+                    message = String.format("Количество решений: 2. Корни: %.0f;%.0f" , x2 , x1);
+                }
+            }
+        }
+
+        return message;
     }
 }
