94968e9aa858815772d9cbed7c5dc9eb513181b5
Captain-Greg <antonovgeorgij12@gmail.com>
2023-11-24 22:39:36 +0300

    Chapter 1 Task 2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,13 +5,35 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        System.out.println(getFlatLocation(4, 4, 44));
     }
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+    static String outcome = "";
 
-        return null; // Заглушка. При реализации - удалить
+    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
+
+        int flatAmount = floorAmount * entranceAmount * 4;
+        int flatsInEntrance = floorAmount * 4;
+        int positionOnFloor = flatNumber % 4;
+
+        if (0 < flatNumber && flatAmount >= flatNumber) {
+            outcome += (flatNumber + " кв - ");
+        } else {
+            return outcome = "Несуществующая квартира";
+        }
+
+        outcome += (((flatNumber - 1) / flatsInEntrance + 1) + " подъезд, " +
+                (((flatNumber - 1) % flatsInEntrance) / 4 + 1) + " этаж, ");
+        switch (positionOnFloor) {
+            case (1):
+                return outcome += ("слева от лифта, влево");
+            case (2):
+                return outcome += ("слева от лифта, вправо");
+            case (3):
+                return outcome += ("справа от лифта, влево");
+            case (0):
+                return outcome += ("справа от лифта, вправо");
+        }
+         return "Ошибка";
     }
 }
