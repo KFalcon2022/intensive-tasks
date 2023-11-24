f057362a783be8ed4f02104219f19c94aff3fa20
Rust <madishkin2@gmail.com>
2023-11-24 09:10:25 +0300

    Chapter 1 Task 3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -6,12 +6,15 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
         //Для собственных проверок можете делать любые изменения в этом методе
-
+        System.out.println("Is it leap year? : " + isLeap(1903));
     }
 
     static boolean isLeap(int year) {
         //Место для вашего кода
+        if (year % 4 == 0) {
+            return year % 100 != 0 || year % 400 == 0;
+        }
 
-        return false; // Заглушка. При реализации - удалить
+        return false;
     }
 }
