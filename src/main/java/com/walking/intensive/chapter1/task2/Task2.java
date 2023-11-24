250f0836cbcd95c895b5f19bb4f19a2ee3bd9663
Pavlo <pavlokutsyn@gmail.com>
2023-11-24 15:06:42 +0100

    Chapter1 task2 "Подтянул тесты на ПК на ветку 2 задачи, отладил код следуя рекомемндациям ревьювера"

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -2,48 +2,38 @@ package com.walking.intensive.chapter1.task2;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
-
  */
 public class Task2 {
     public static void main(String[] args) {
-        int flatNumber = 13;
-        int entranceAmount = 7;
-        int floorAmount = 5;
+        int floorAmount = 10;
+        int entranceAmount = 3;
+        int flatNumber = 88;
 
-        System.out.println(getFlatLocation(flatNumber, entranceAmount, floorAmount));
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
-    static String getFlatLocation(int flatNumber, int entranceAmount, int floorAmount) {
+    static String getFlatLocation( int floorAmount, int entranceAmount, int flatNumber) {
         int flatsPerFloor = 4;
-        int flatsPerFrontEntrance = floorAmount * flatsPerFloor;
+        int totalFlatsPerEntrance = flatsPerFloor * floorAmount;
+        int totalFlats = totalFlatsPerEntrance * entranceAmount;
 
-        if (flatNumber < 1 || flatNumber > entranceAmount * flatsPerFrontEntrance) {
-            return "Квартири не существует";
+        if (flatNumber < 1 || flatNumber > totalFlats) {
+            return "Такой квартиры не существует";
         }
 
-        int entrance = (flatNumber - 1) / flatsPerFrontEntrance + 1;
-        int floor = ((flatNumber - 1) % flatsPerFrontEntrance) / flatsPerFloor + 1;
-        int flatPosition = (flatNumber - 1) % flatsPerFloor;
-        String position;
+        int entrance = (flatNumber - 1) / totalFlatsPerEntrance + 1;
+        int floorWithinEntrance = (flatNumber - 1) % totalFlatsPerEntrance;
+        int floor = floorWithinEntrance / flatsPerFloor + 1;
+        int flatPositionWithinFloor = floorWithinEntrance % flatsPerFloor;
 
-        switch (flatPosition) {
-            case 0:
-                position = "слева от лифта, влево";
-                break;
-            case 1:
-                position = "слева от лифта, вправо";
-                break;
-            case 2:
-                position = "справа от лифта, влево";
-                break;
-            case 3:
-                position = "справа от лифта, вправо";
-                break;
-            default:
-                position = "Квартири не существует";
-                break;
-        }
+        String position = switch (flatPositionWithinFloor) {
+            case 0 -> "слева от лифта, влево";
+            case 1 -> "слева от лифта, вправо";
+            case 2 -> "справа от лифта, влево";
+            case 3 -> "справа от лифта, вправо";
+            default -> throw new IllegalStateException("Unexpected value: " + flatPositionWithinFloor);
+        };
 
-        return "Подъезд: " + entrance + ", этаж: " + floor + ", квартира: " + flatNumber + " " + position;
+        return flatNumber + " кв - " + entrance + " подъезд, " + floor + " этаж, " + position;
     }
 }
