646ad97a48e9d0c7e0d4f21a32a8793c7acad5ad
Mi6290 <zaharovmisha@yandex.ru>
2023-11-24 19:27:20 +0300

    Resolve task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -9,54 +9,21 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 111;
+
+        int age = 5;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-
-        if (age <= 0) {
-            System.out.println("Вы еще не родились");
-        } else if (age >= 1 && age <= 10) {
-            if (age == 1) {
-                System.out.println("Вам " + age + " год");
-            } else if (age >= 2 && age <= 4) {
-                System.out.println("Вам " + age + " года");
-            } else if (age >= 5 && age <= 10) {
-                System.out.println("Вам " + age + " лет");
-            }
-        } else if (age >= 11 && age <= 19) {
+        if (((age >= 5 && age <= 20) || (age >= 105 && age <= 120)) || ((age % 10) >= 5 && (age % 10) <= 9) || ((age % 100) >= 5 && (age % 100) <= 9)) {
             System.out.println("Вам " + age + " лет");
-        } else if (age >= 20 && age <= 99) {
-            if ((age % 10) == 1) {
-                System.out.println("Вам " + age + " год");
-            } else if ((age % 10) >= 2 && (age % 10) <= 4) {
-                System.out.println("Вам " + age + " года");
-            } else if ((age % 10) == 0 || (age % 10) >= 5 || (age % 10) <= 9) {
-                System.out.println("Вам " + age + " лет");
-            }
-        } else if (age >= 100 && age <= 110) {
-            if ((age % 10) == 1) {
-                System.out.println("Вам " + age + " год");
-            } else if ((age % 10) >= 2 && (age % 10) <= 4) {
-                System.out.println("Вам " + age + " года");
-            } else if ((age % 10) == 0 || (age % 10) >= 5 || (age % 10) <= 9) {
-                System.out.println("Вам " + age + " лет");
-            }
-        } else if (age >= 111 && age <= 119) {
-            System.out.println("Вам " + age + " лет");
-        } else if (age >= 120 && age <= 127) {
-            if ((age % 10) == 1) {
-                System.out.println("Вам " + age + " год");
-            } else if ((age % 10) >= 2 && (age % 10) <= 4) {
-                System.out.println("Вам " + age + " года");
-            } else if ((age % 10) == 0 || (age % 10) >= 5 || (age % 10) <= 9) {
-                System.out.println("Вам " + age + " лет");
-            }
-        } else {
-            System.out.println("Вы новый долгожитель");
+        } else if (age == 1 || (age % 10) == 1 || (age % 100) == 1) {
+            System.out.println("Вам " + age + " год");
+        } else if (((age % 10) >= 2 && (age % 10) <= 4) || ((age % 100) >= 2 && (age % 100) <= 4)) {
+            System.out.println("Вам " + age + " года");
         }
         return null;
+
     }
 }
