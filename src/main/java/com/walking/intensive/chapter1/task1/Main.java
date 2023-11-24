1b9da5d887699d28b15780e38ba868ba0f779e57
Sardinka26 <sardinka2021@gmail.com>
2023-11-24 18:48:14 +0300

    var6 10%

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -6,9 +6,7 @@ package com.walking.intensive.chapter1.task1;
 public class Main {
     public static void main(String[] args) {
 
-        int age =1;
-        int rezult2 = 1 % 10;
-        System.out.println(" остаток от деления " + rezult2);
+        int age = 122;
         System.out.println(getAgeString(age));
     }
 
@@ -16,15 +14,18 @@ public class Main {
         String answerAge = " Ваш возраст ";
         int result = age % 10;
 
-        if (result >= 5 && result <= 20 || (result % 10) ==0) {
+        if (result >= 5 && result <= 20 || (result % 10) == 0) {
             return answerAge + age + " лет";
         }
+
         if (result >= 2 && result <= 4) {
             return answerAge + age + " года";
 
-        } else if (age == 1) {
+        }
+        else if (age == 1) {
             return answerAge + age + " год";
         }
+
         return answerAge + age + " год";
     }
 }
