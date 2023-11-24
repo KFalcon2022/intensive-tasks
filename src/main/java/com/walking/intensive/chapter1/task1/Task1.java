70192cc278adf3f34838123fd3526556eb3934ec
Boris <aksenov.boris8595@gmeil.com>
2023-11-24 17:26:39 +0500

    Укоротил условия используя операторы сравнения. Добавил несколько return'ов, отформатировал код через "контрол альт L", убрал двойные ветвления.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -12,20 +12,15 @@ public class Task1 {
     }
 
     static String getAgeString(int age) {
-        String getAgeStr;
-        if (age%100 == 11 || age%100 == 12 || age%100 == 13 || age%100 == 14) {
-            getAgeStr = " лет";
-        } else {
-            if (age % 10 == 1) {
-                getAgeStr = " год";
-            } else {
-                if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) {
-                    getAgeStr = " года";
-                } else {
-                    getAgeStr = " лет";
-                }
-            }
+        if (age % 100 >= 11 && age % 100 <= 14) {
+            return "Вам " + age + " лет";
         }
-        return "Вам " + age + getAgeStr;
+        if (age % 10 == 1) {
+            return "Вам " + age + " год";
+        }
+        if (age % 10 >= 2 && age % 10 <= 4) {
+            return "Вам " + age + " года";
+        }
+        return "Вам " + age + " лет";
     }
 }
