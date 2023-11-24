74ba44073c1bc928a82b6477db8924320432f372
vowiks <150813696+vowiks@users.noreply.github.com>
2023-11-25 01:16:46 +0300

    task1

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -1,19 +1,26 @@
-package com.walking.intensive.chapter1.task1;
-
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
- */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+// Для собственных проверок можете делать любые изменения в этом методе
         int age = 0;
 
         System.out.println(getAgeString(age));
+        int age = 32;
+
+        getAgeString(возраст);
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+// Место для вашего кода
+        статическая пустая строка getAgeString(возраст ввода) {
 
-        return null; // Заглушка. При реализации - удалить
-    }
-}
+            if (age < 0) {
+                System.out.println("Неверный возраст.");
+            } else if (age % 10 == 1 && age != 11) {
+                System.out.println("Вам " + age + " год.");
+            } еще, если (возраст % 10>= 2 && возраст % 10 <= 4 && возраст!= 12 && возраст!= 13 && возраст!= 14) {
+                System.out.println("Вам " + age + " года.");
+            } else {
+                System.out.println("Вам " + age + " лет.");
+            }
+
+            return null;
