ee6eeb89d2e08afaf2247b745a293ec28e61f567
U_M1CVT <akachamina@alfabank.ru>
2023-11-24 11:27:09 +0300

    Решение 3-й задачи

--- src/main/java/com/walking/intensive/chapter1/task3/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
  int year = 0;
-
+    System.out.println(isLeap(year));
     }
 
     static boolean isLeap(int year) {
         //        Место для вашего кода
-        boolean isLeap = (year%400 == 0 || year%4 == 0);
+        boolean isLeap = year%4 == 0 && year%100 != 0 || year%400 == 0;
 
         return isLeap;
     }
