01c6adbcb8512d0de8fb4d5b9f33af709b2e7835
smmotor <smmotor777@gmail.com>
2023-11-24 19:15:30 +0600

    Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -7,7 +7,7 @@ public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 0;
+        double b = 5;
         double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
@@ -26,8 +26,25 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double d = Math.pow(b, 2) - 4 * a * c;
+        double x1 = 0;
+        double x2 = 0;
+        if (a == 0 && b == 0 && c == 0) {
+            return String.format("Решений бесконечно");
+        } else if (a == 0 && b == 0 && c != 0) {
+            return String.format("Количество решений: 0.");
+        } else if (a == 0 && (b != 0 && c != 0)) {
+            x1 = -c / b;
+            return String.format("Количество решений: 1. Корень: %d", (int) x1);
+        } else if (d > 0) {
+            x1 = (-b - Math.sqrt(d)) / 2 * a;
+            x2 = (-b + Math.sqrt(d)) / 2 * a;
+            return String.format("Количество решений: 2. Корни: %d;%d", (int) x1, (int) x2);
+        } else if (d == 0) {
+            x1 = (-b - Math.sqrt(d)) / 2 * a;
+            return String.format("Количество решений: 1. Корень: %d", (int) x1);
+        } else {
+            return String.format("Количество решений: 0.");
+        }
     }
 }
