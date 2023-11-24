3b2a08b7fe80aeb01b375b2bac02920c32d1eac4
Oleg Klimchuk <koi196621@gmail.com>
2023-11-24 14:33:01 +0200

    ВЫСОКОСНЫЙ ГОД

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -4,6 +4,8 @@ package com.walking.intensive.chapter1.task1;
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Task1 {
+        private static final int[] AGE_ARRAY = new int[]{11, 12, 13, 14, 15, 16, 17, 17, 19, 111, 112, 113, 114};
+
     public static void main(String[] args) {
         int age = 112;
 
@@ -11,28 +13,21 @@ public class Task1 {
     }
 
     static String getAgeString(int age) {
-        String s = "Вам ";
 
         if (age < 0) {
-            return " Вы не родились";
+            return "Вы не родились";
         }
 
-        int[] ageArray = new int[]{11, 12, 13, 14, 15, 16, 17, 17, 19, 111, 112, 113, 114};
-        for (int id : ageArray) {
-            if (age == id) {
-                return s + age + " лет";
+        for (int exceptionAge : AGE_ARRAY) {
+            if (age == exceptionAge) {
+                return "Вам " + age + " лет";
             }
         }
 
-        switch (age % 10) {
-            case 1:
-                return s + age + " год";
-            case 2:
-            case 3:
-            case 4:
-                return s + age + " года";
-            default:
-                return s + age + " лет";
-        }
+        return switch (age % 10) {
+            case 1 -> "Вам " + age + " год";
+            case 2, 3, 4 -> "Вам " + age + " года";
+            default -> "Вам " + age + " лет";
+        };
     }
 }
