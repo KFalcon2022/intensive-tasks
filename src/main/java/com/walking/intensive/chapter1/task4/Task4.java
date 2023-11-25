3c87366e7c7e602e57c5ce400e3b80ce09247034
dmitryNet <d.evstafiev@crystals.ru>
2023-11-26 02:37:08 +0300

    task4 - solution

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,8 +26,33 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        double x1, x2;
+        double discriminant;
+
+        if (a == 0) {
+            if (b == 0) {
+                return "Решений бесконечно";
+            } else {
+                x1 = -c / b;
+                return "Количество решений: 1. Корень: " + (int) x1;
+            }
+        }
+
+        discriminant = (b * b) - (4 * a * c);
+
+        if (discriminant < 0) {
+            return "Количество решений: 0.";
+        }
+
+        if (discriminant == 0) {
+            x1 = -b / (2 * a);
+            return "Количество решений: 1. Корень: " + (int) x1;
+        } else {
+            x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            return "Количество решений: 2. Корни: " + (int) x1 + ";" + (int) x2;
+        }
+
     }
 }
