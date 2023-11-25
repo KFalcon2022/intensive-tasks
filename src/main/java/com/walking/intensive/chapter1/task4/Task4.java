9de7142fec41b8fdebee79afbf5ddcffd8954c90
dorogao5 <145500950+dorogao5@users.noreply.github.com>
2023-11-25 12:18:15 +0300

    correction of  branch

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -1,49 +1,33 @@
 package com.walking.intensive.chapter1.task4;
 
-import java.lang.Math;
-
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-parameter.html">ссылка</a>
  */
 public class Task4 {
     public static void main(String[] args) {
-
+//        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 5;
-        double c = -10;
+        double b = 0;
+        double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
     }
 
+    /**
+     * При формировании строки, корни уравнения должны указываться по возрастанию.
+     * <p>
+     * Примеры результирующей строки:
+     * <p>
+     * Количество решений: 2. Корни: -4;4
+     * <p>
+     * Количество решений: 1. Корень: 0
+     * <p>
+     * Количество решений: 0.
+     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        if (a == 0 && b == 0 && c == 0) {
-            return "Бесконечное множество решений";
-        }
-        if (a == 0 && b == 0 && c != 0 || b == 0 && c > 0) {
-            return "invalid numbers";
-        }
-        if (b == 0 && c < 0) {
-            double x1 = Math.sqrt(-c / a);
-            double x2 = -Math.sqrt(-c / a);
-            return "Количество решений: 2. Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
-        }
-        if (b == 0 && c == 0) {
-            return "Количество решений: 1. Корень: 0";
-        }
-        if (a == 0) {
-            return String.format("Количество решений: 1. Корень %f", -c / b);
-        }
-        double diskr = b * b - 4 * a * c;
-        double x1 = ((-b + Math.sqrt(diskr)) / 2 * a);
-        double x2 = ((-b - Math.sqrt(diskr)) / 2 * a);
-        if (diskr < 0) {
-            return "Количество решений: 0.";
-        }
-        if (diskr == 0) {
-            return String.format("Количество решений: 1. Корень: %f", x1);
-        }
-        return "Количество решений: 2. Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
+        //        Место для вашего кода
 
+        return null; // Заглушка. При реализации - удалить
     }
 }
