3e2cb87487bac8b23dccf46d10c883de387eaba2
Magpier <garson.ao@gmail.com>
2023-11-25 22:51:43 +0300

    Chapter1 Task 1/4 ver 2 Убрал swithcase добавил переменные age10 age100 заменил ими age%10 и age%100 соответственно

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -5,7 +5,6 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Main {
     public static void main(String[] args) {
-        int age = 0;
         for (int i = -1; i < 129; i++) {
             System.out.println(getAgeString(i));
         }
@@ -13,29 +12,34 @@ public class Main {
     }
 
     static String getAgeString(int age) {
+        int age10 = age % 10;
+        int age100 = age % 100;
         String s = "";
-        if (age > 127 || age < 0) {
+
+        if (age < 0 || age > 127) {
             return "Проверьте правильность ввода возраста";
         }
 
-        switch (age % 10) {
-            case 1:
-                if (age % 100 == 11) {
-                    s = "Вам " + age + " лет";
-                } else {
-                    s = "Вам " + age + " год";
-                }
-                break;
-            case 2, 3, 4:
-                if (age % 100 == 12 || age % 100 == 13 || age % 100 == 14) {
-                    s = "Вам " + age + " лет";
-                } else {
-                    s = "Вам " + age + " года";
-                }
-                break;
-            case 0, 5, 6, 7, 8, 9:
+        if (age10 == 1) {
+
+            if (age100 == 11) {
                 s = "Вам " + age + " лет";
+            } else {
+                s = "Вам " + age + " год";
+            }
+
+        } else if (age10 == 2 || age10 == 3 || age10 == 4) {
+
+            if (age100 == 12 || age100 == 13 || age100 == 14) {
+                s = "Вам " + age + " лет";
+            } else {
+                s = "Вам " + age + " года";
+            }
+
+        } else {
+            s = "Вам " + age + " лет";
         }
+
         return s;
     }
 }
