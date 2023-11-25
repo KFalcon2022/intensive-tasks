e19df711dabaf6581b1663ab77ba3f4543a060e1
DeaDMop03 <bl.target@yandex.ru>
2023-11-26 00:15:56 +0300

    resolve chapter1task1

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -6,12 +6,34 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+//        int floorAmount = 7;
+//        int entranceAmount = 5;
+//       int flatNumber =141;
+//        String flatLocation = getFlatLocation(floorAmount, entranceAmount, flatNumber);
+//        System.out.println(flatLocation);
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         //        Место для вашего кода
+        int flatsOnFloorAmmount = 4;
+        String result = "";
+        if (flatNumber < 1 || flatNumber > floorAmount*entranceAmount*flatsOnFloorAmmount)
+        {
+            result = "Такой квартиры не существует";
+        }
+        else {
+            int entranceNumber = ((flatNumber-1) / (floorAmount * flatsOnFloorAmmount)) + 1;
+            int floorNumber = ((flatNumber-1) - (floorAmount * flatsOnFloorAmmount*(entranceNumber - 1)))/flatsOnFloorAmmount + 1;
+            switch (flatNumber - (floorAmount * flatsOnFloorAmmount*(entranceNumber - 1))- (floorNumber -1)*flatsOnFloorAmmount){
+                case 1: result = flatNumber + " кв - " + entranceNumber +" подъезд, " + floorNumber + " этаж, слева от лифта, влево"; break;
+                case 2: result = flatNumber + " кв - " + entranceNumber +" подъезд, " + floorNumber + " этаж, слева от лифта, вправо"; break;
+                case 3: result = flatNumber + " кв - " + entranceNumber +" подъезд, " + floorNumber + " этаж, справа от лифта, влево"; break;
+                case 4: result = flatNumber + " кв - " + entranceNumber +" подъезд, " + floorNumber + " этаж, справа от лифта, вправо"; break;
 
-        return null; // Заглушка. При реализации - удалить
+            }
+
+             }
+        return result;
+        // Заглушка. При реализации - удалить
     }
 }
