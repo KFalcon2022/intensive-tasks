bd00d0f179715678fbe92866d92c1c87ba3bab88
Magpier <garson.ao@gmail.com>
2023-11-25 23:28:53 +0300

    Chapter1 Task 1/4

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -14,32 +14,28 @@ public class Main {
     static String getAgeString(int age) {
         int age10 = age % 10;
         int age100 = age % 100;
-        String s = "";
-
+        String ageString = "";
         if (age < 0 || age > 127) {
             return "Проверьте правильность ввода возраста";
         }
-
         if (age10 == 1) {
-
             if (age100 == 11) {
-                s = "Вам " + age + " лет";
+
+                ageString = "Вам " + age + " лет";
             } else {
-                s = "Вам " + age + " год";
+                ageString = "Вам " + age + " год";
             }
-
-        } else if (age10 == 2 || age10 == 3 || age10 == 4) {
-
-            if (age100 == 12 || age100 == 13 || age100 == 14) {
-                s = "Вам " + age + " лет";
-            } else {
-                s = "Вам " + age + " года";
-            }
-
-        } else {
-            s = "Вам " + age + " лет";
+            return ageString;
         }
-
-        return s;
+        if (age10 == 2 || age10 == 3 || age10 == 4) {
+            if (age100 == 12 || age100 == 13 || age100 == 14) {
+                ageString = "Вам " + age + " лет";
+            } else {
+                ageString = "Вам " + age + " года";
+            }
+        } else {
+            ageString = "Вам " + age + " лет";
+        }
+        return ageString;
     }
 }
