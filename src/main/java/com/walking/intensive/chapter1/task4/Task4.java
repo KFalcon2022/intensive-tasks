7af46432d08fdbbdc90938b8d2398af9f8e65598
Dmitry <milordoff@yandex.ru>
2023-11-25 12:05:44 +0300

    Chapter 1 Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,31 +26,32 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        double D = b * b - 4 * a * c;
-        double x1 = (-b + Math.sqrt(D)) / 2 * a;
-        double x2 = (-b - Math.sqrt(D)) / 2 * a;
+        double discriminant = b * b - 4 * a * c;
+        double root1 = (-b - Math.sqrt(discriminant)) / 2 * a;
+        double root2 = (-b + Math.sqrt(discriminant)) / 2 * a;
 
         if (a == 0 & b == 0 & c == 0) {
             return "Решений бесконечно";
-        } else if (a == 0 & b == 0 & c != 0) {
+        } else if (a == 0 & b == 0 & c != 0 || discriminant < 0 || a != 0 & b == 0 & c != 0 & -c / a < 0) {
             return "Количество решений: 0.";
         } else if (a == 0 & b != 0 & c != 0) {
             return "Количество решений: 1. Корень: " + c / -b;
         } else if (a != 0 & b == 0 & c != 0) {
-            if (-c / a < 0) {
-                return "Количество решений: 0.";
-            } else {
-                return "Количество решений: 2. Корни: " + -Math.sqrt(-c / a) + ";" + Math.sqrt(-c / a);
-            }
+            return "Количество решений: 2. Корни: " + -Math.sqrt(-c / a) + ";" + Math.sqrt(-c / a);
+        } else if (a != 0 & b != 0 & c != 0 || discriminant == 0) {
+            return "Количество решений: 1. Корень: " + root1;
         } else {
-            if (D < 0) {
-                return "Количество решений: 0.";
-            } else if (D == 0) {
-                return "Количество решений: 1. Корень: " + x1;
-            } else {
-                return "Количество решений: 2. Корни: " + -x1 + ";" + x2;
-            }
+            return formatRoots(root1, root2);
+        }
+    }
+
+    static String formatRoots(double root1, double root2) {            // метод для вывода корней в порядке возрастания
+        if (root1 < root2) {
+            return "Количество решений: 2. Корни: " + root1 + ";" + root2;
+        } else {
+            return "Количество решений: 2. Корни: " + root2 + ";" + root1;
         }
     }
 }
+
 
