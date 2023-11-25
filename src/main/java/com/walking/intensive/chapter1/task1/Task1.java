23b6cf799240be9fa6dec8e2bceb0e17d902ec56
DeaDMop03 <bl.target@yandex.ru>
2023-11-25 16:21:06 +0300

    resolve chapter1task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 1;
 
         System.out.println(getAgeString(age));
     }
@@ -14,6 +14,21 @@ public class Task1 {
     static String getAgeString(int age) {
 //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int longevityRecord = 127;
+        String yourAge = "";
+        if (age<0 || age > longevityRecord)
+        {
+            System.out.println("Ваш возраст "+ age + "? Вас не существует.");
+        }
+        else {
+            if (age % 10 == 1 && age != 11 && age != 111) {
+                yourAge = "Вам " + age + " год";
+            } else if ((age % 10 == 2 && age != 12 && age != 112) || (age % 10 == 3 && age != 13 && age != 113) || (age % 10 == 4 && age != 14 && age != 114)) {
+                yourAge = "Вам " + age + " года";
+            } else {
+                yourAge = "Вам " + age + " лет";
+            }
+        }
+ return yourAge;
     }
 }
