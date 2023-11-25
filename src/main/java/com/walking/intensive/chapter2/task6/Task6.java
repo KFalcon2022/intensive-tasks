ceaadf08be5a7fd2428c29b9f2d32ec4e42e29ce
Lunularis <s.n.volynkina@yandex.ru>
2023-11-25 20:12:54 +0700

    Chapter 2 Task 6 (new)

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -3,5 +3,40 @@ package com.walking.intensive.chapter2.task6;
 public class Task6 {
     public static void main(String[] args) {
 
+        System.out.println(getMaxCommonDivisor(0, 20));
+        System.out.println(getMaxCommonDivisor(24, 20));
+        System.out.println(getMinCommonMultiple(0, 20));
+        System.out.println(getMinCommonMultiple(24, 20));
+
+    }
+    public static int getMaxCommonDivisor(int x, int y) {
+
+        if (x == 0 || y == 0){
+            return 0;
+        }
+
+        if (x == y) {
+            return x;
+        }
+
+        int divisor = x - y;
+
+        if (divisor < 0) {
+            // x меньше y, значит наименьшие числа  x и -divisor
+            return getMaxCommonDivisor(x, -1 * divisor);
+        } else {
+            return getMaxCommonDivisor(y, divisor);
+        }
+    }
+
+    public static int getMinCommonMultiple(int x, int y) {
+
+        int maxDivisor = getMaxCommonDivisor(x, y);
+
+        if (maxDivisor == 0) {
+            return 0;
+        }
+
+        return x * y / maxDivisor;
     }
 }
