1b0fb39df3f5580a51b0fd69b9003662beb83e77
SkofieldAE <alexandrevd2107@gmail.com>
2023-11-25 15:59:47 +0300

    update_task1

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -6,37 +6,34 @@ package com.walking.intensive.chapter1.task4;
 public class Main {
     public static void main(String[] args) {
 
-        double a = 1;
-        double b = -8;
-        double c = 15;
+            double a = 1;
+            double b = -8;
+            double c = 15;
 
-        System.out.println(solveQuadraticEquation(a, b, c));
+            System.out.println(solveQuadraticEquation(a, b, c));
 
-    }
-    static String solveQuadraticEquation(double a, double b, double c) {
-        if (a == 0 && b == 0 && c == 0) {
-            return "Решений бесконечно";
-        } else if (a == 0 && b == 0) {
-            return "Количество решений: 0.";
-        } else if (a == 0 && c != 0) {
-            double x1 = -c / b;
-            return "Количество решений: 1. Корень: " +  x1;
         }
-        double discriminant = b * b - 4 * a * c;
-        if (discriminant > 0) {
-            double x1, x2;
-            x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            return "Количество решений: 2. Корни: -1;1 x1 " + x1 + ", x2 = " + x2;
+        static String solveQuadraticEquation(double a, double b, double c) {
+            if (a == 0 && b == 0 && c == 0) {
+                return "Решений бесконечно";
+            } else if (a == 0 && b == 0) {
+                return "Количество решений: 0.";
+            } else if (a == 0 && c != 0) {
+                return "Количество решений: 1. Корень: " + (-c / b);
+            }
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant > 0) {
+                double root = (-b - Math.sqrt(discriminant)) / (2 * a);
+                double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                return "Количество решений: 2. Корни: " + Math.min(root, root1) + " ; " + Math.max(root, root1);
 
-        } else if (discriminant == 0) {
-            double x;
-            x = -b / (2 * a);
-            return "Количество решений: 1, x = " + x;
+            } else if (discriminant == 0) {
+                return "Количество решений: 1" + (-b / (2 * a));
 
-        } else {
-            return "Количество решений: 0.";
+            } else {
+                return "Количество решений 0.";
+            }
         }
     }
-}
+
 
