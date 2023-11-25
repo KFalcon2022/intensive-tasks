9f22e91f9556b85a77d2e2fc375747b535f6e467
egor.djun <xedshotg@gmail.com>
2023-11-25 10:26:00 +0700

    fixed

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -30,36 +30,36 @@ public class Task4 {
         if (a == 0 && b == 0 && c == 0) {
             return "Решений бесконечно";
         }
-        if(a == 0 && b !=0 && c !=0){
-           return "Количество решений: 1. Корень: " + c;
+        if (a == 0 && b != 0 && c != 0) {
+            return "Количество решений: 1. Корень: " + c;
         }
         if (b == 0 && c != 0) {
-            double x = (-c) / a;
-            if (x > 0) {
-                double x1 = -Math.sqrt((-c) / a);
-                double x2 = Math.sqrt((-c) / a);
-                return "Количество решений: 2. Корни " + x1 + ";" + x2;
+            double multiplier = (-c) / a;
+            if (multiplier > 0) {
+                double rootOne = -Math.sqrt((-c) / a);
+                double rootTwo = Math.sqrt((-c) / a);
+                return "Количество решений: 2. Корни " + rootOne + ";" + rootTwo;
             }
-            if (x < 0){
+            if (multiplier < 0) {
                 return "Количество решений: 0.";
             }
         }
-        if (b != 0 && c == 0){
-            return "Количество решений: 2. Корни " + 0 + ";" + ((-b)/a);
+        if (b != 0 && c == 0) {
+            return "Количество решений: 2. Корни " + 0 + ";" + ((-b) / a);
         }
-        if (b == 0 && c == 0){
+        if (b == 0 && c == 0) {
             return "Количество решений: 1. Корень 0";
         }
 
         //полные квадратные уравнения
-        double d = Math.pow(b, 2) - 4 * a * c;
-        if (d < 0) {
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        if (discriminant < 0) {
             return "Количество решений: 0.";
         }
-        if (d > 0) {
-            double x1 = (-b - Math.sqrt(d)) / (2 * a);
-            double x2 = (-b + Math.sqrt(d)) / (2 * a);
-            return "Количество решений: 2. Корни " + x1 + ";" + x2;
+        if (discriminant > 0) {
+            double rootOne = (-b - Math.sqrt(discriminant)) / (2 * a);
+            double rootTwo = (-b + Math.sqrt(discriminant)) / (2 * a);
+            return "Количество решений: 2. Корни " + rootOne + ";" + rootTwo;
         }
         return "Количество решений: 1. Корень " + ((-b) / (2 * a));
     }
