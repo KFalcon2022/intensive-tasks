51ec4d1e78030d8cfa6687e8cbd8e5958dca2616
Sprite_Spirit <a.backer@yandex.com>
2023-11-27 01:06:22 +0300

    Задание 1 готово. [В методе main закомментировала ручную проверку]

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,19 +1,30 @@
 package com.walking.intensive.chapter1.task1;
 
+//import java.util.Random;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
+
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+       // Random random = new Random();
+       // int age = random.nextInt(128);
 
-        System.out.println(getAgeString(age));
+       // System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        String stringEnd;
 
-        return null; // Заглушка. При реализации - удалить
+        if (age % 10 == 1 && age != 11 && age != 111) {
+            stringEnd = "год";
+        } else if ((age % 10 > 1 && age % 10 < 5) && !(age >= 12 && age <= 15)) {
+            stringEnd = "года";
+        } else {
+            stringEnd = "лет";
+        }
+
+        return String.format("Вам %d %s", age, stringEnd);
     }
 }
