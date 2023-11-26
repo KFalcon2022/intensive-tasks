800af323319905dcf04074593ce12aa68b74f73c
KrisLu23 <krisbondarev@yandex.ru>
2023-11-26 18:09:42 +0300

    Chapter 1 Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -6,9 +6,9 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 
-        double a = 18;
-        double b = 0;
-        double c = -12;
+        double a = 12;
+        double b = 2;
+        double c = -18;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -26,21 +26,24 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-
-        if (a == 0 || b == 0 && -c / a < 0) {
+        if (a == 0 && b == 0 && c == 0) {
+            return "Решений бесконечно";
+        }
+        if (b == 0 && -c / a < 0) {
             return "Количество решений: 0.";
+        }
+        if (a == 0) {
+            return "Количество решений: 1. Корень: " + (-c) / b;
         }
         if (b == 0 && c == 0) {
             return "Количество решений: 1. Корень: 0";
         }
 
-        double x1;
-        double x2;
-        double discriminant;
-        discriminant = Math.pow(b, 2) - 4 * a * c;
-        x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
-        return "Количество решений: 2. Корни: " + x1 + ", " + x2;
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        double rootFirst = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double rootSecond = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+        return "Количество решений: 2. Корни: " + rootFirst + ", " + rootSecond;
     }
 }
