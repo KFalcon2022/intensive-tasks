c3389bb46d809f6f508d0efce569abaac4d184df
ArtemiyDS <27071999@mail.ru>
2023-11-26 21:00:23 +0300

    Chapter 1 Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -5,10 +5,9 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
+        double a = 1;
         double b = 0;
-        double c = 0;
+        double c = -1;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -26,8 +25,26 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0) {       // Рассмотрим случаи, когда исходное уравнение не предсатвляет собой квадратное уравнение
+            if (b != 0) {
+                return "Количество решений: 1. Корень: " + String.valueOf((int) (-c / b));
+            } else {
+                if (c != 0)
+                    return "Количество решений: 0.";
+                else
+                    return "Решений бесконечно";
+            }
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0)
+            return "Количество решений: 0.";
+        else if (discriminant == 0)
+            return "Количество решений: 1. Корень: " + String.valueOf((int) ((-b) / (2 * a)));
+        else {
+            double firstAnswer = (-b - Math.sqrt(discriminant)) / (2 * a);
+            double secondAnswer = (-b + Math.sqrt(discriminant)) / (2 * a);
+            return "Количество решений: 2. Корни: " + String.valueOf((int) firstAnswer) + ";" + String.valueOf((int) secondAnswer);
+        }
     }
 }
