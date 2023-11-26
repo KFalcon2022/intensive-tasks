ff13539d1edc5d881f53d6786e1fcac964a0863c
lambo064 <tasha_lambo@mail.ru>
2023-11-26 15:43:13 +0300

    chapter 2 task 6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -2,6 +2,18 @@ package com.walking.intensive.chapter2.task6;
 
 public class Task6 {
     public static void main(String[] args) {
+        System.out.println(getNOD(40, 60));
+        System.out.println(getNOK(40, 60));
+    }
 
+    public static int getNOK(int a, int b) {
+        return (a * b) / getNOD(a, b);
+    }
+
+    public static int getNOD(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return getNOD(b, a % b);
     }
 }
