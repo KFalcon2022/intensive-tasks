1fcfb41bc8df2ddadcea6e53ace96bb3380d1672
TitLukich <Nef-92@yandex.ru>
2023-11-27 00:10:03 +0300

    Chapter 2, Task 6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -1,26 +1,60 @@
 package com.walking.intensive.chapter2.task6;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-NOKNOD.html">ссылка</a>
  */
 public class Task6 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b;
+        do {
+            System.out.print("Введите положительное число а: ");
+            a = scanner.nextInt();
 
+            System.out.print("Введите положительное число b: ");
+            b = scanner.nextInt();
+        }
+        while (a < 1 || b < 1);
+
+        scanner.close();
+
+        System.out.printf("НОД числа %d и числа %d равен %d\n", a, b, getNod(a, b));
+        System.out.printf("НОК числа %d и числа %d равен %d\n", a, b, getNoc(a, b));
+        System.out.printf("НОД числа %d и числа %d равен %d\n", a, b, getNodByEuclideanAlgorithm(a, b));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        int result = Math.max(m, n);
+        for (int i = result; i % m + i % n != 0; i++) {
+            result = i;
+        }
+
+        if (result == Math.max(m, n)) {
+            return result;
+        }
+        return result + 1;
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        int result = Math.min(m, n);
+        for (int i = result; m % i + n % i != 0; i--) {
+            result = i;
+        }
+
+        if (result == Math.min(m, n)) {
+            return result;
+        }
+        return result - 1;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        if (n == 0) {
+            return m;
+        }
+        return getNodByEuclideanAlgorithm(n, m % n);
     }
 
 }
