667c219cb33e7379a3df1f73e7f0ebe7c4636903
Magpier <garson.ao@gmail.com>
2023-11-26 15:02:32 +0300

    Chapter2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Main.java
@@ -4,14 +4,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //  System.out.println(friendNumber(1000000));
-        // System.out.println(getDiviniterSum(284));
-        System.out.println(primeFactorizationSet(2355));
-
-
+        System.out.println(friendNumber(1000000));
     }
 
-    //Не элегантное решение без массивов и прочего BRUTEFORCE!!!
     static int friendNumber(int N) {
         int m;
         int n;
@@ -41,53 +36,5 @@ public class Main {
         }
         return 0;
     }
-
-    // решение с блекджеком и сетами
-    static Set<Integer> primeFactorizationSet(int a) {
-        boolean isChange;
-        Set<Integer> result = new TreeSet<>();
-        int number = Math.abs(a);
-        if (number == 0) {
-            System.out.println("Вы пытаетесь разложить 0 на простые множители");
-            return result;
-        }
-        do {
-            isChange = false;
-            if (number % 2 == 0) {
-                result.add(2);
-                number /= 2;
-                isChange = true;
-            } else {
-                for (int i = 3; i <= Math.sqrt(number); i = i + 2) {
-                    if (number % i == 0) {
-                        result.add(i);
-                        number /= i;
-                        isChange = true;
-                        break;
-                    }
-                }
-            }
-        } while (isChange);
-        result.add(1);
-        result.add(number);
-        return result;
-    }
-
-    static int getDiviniterSum(int a) {
-        Set<Integer> set = primeFactorizationSet(a);
-        Set<Integer> set2 = new TreeSet<>(set);
-        Iterator<Integer> iter1 = set.iterator();
-        Iterator<Integer> iter2 = set2.iterator();
-        while (iter1.hasNext()) {
-            while (iter2.hasNext()) {
-                set.add(iter1.next() * iter2.next());
-            }
-        }
-        System.out.println(set);
-        return 0;
-
-
-    }
-
 
 }
