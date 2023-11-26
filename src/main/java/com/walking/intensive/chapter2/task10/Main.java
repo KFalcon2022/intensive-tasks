f43f9828b4905e582de2b1dd71d1cd527f9e4e46
Magpier <garson.ao@gmail.com>
2023-11-26 17:49:05 +0300

    Chapter2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Main.java
@@ -2,6 +2,20 @@ package com.walking.intensive.chapter2.task10;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(isPalindrome("Was it a car or a cat I saw?"));
+        System.out.println(isPalindrome("Was it a car or a cat I saw?fd"));
 
+
+    }
+    static boolean isPalindrome(String s){
+        char[] sentanse = s.replaceAll("[^a-zA-Z]", "").toLowerCase().toCharArray();
+
+        for (int i = 0; i < sentanse.length/2; i++) {
+            if(sentanse[i]!=sentanse[sentanse.length-i-1]){
+                    return false;
+                }
+            }
+
+        return true;
     }
 }
