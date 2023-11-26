524d9337bf407457ec927584d8746de04626f25d
Magpier <garson.ao@gmail.com>
2023-11-26 03:14:36 +0300

    Chapter2 Task 6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,9 +5,14 @@ import java.util.*;
 public class Task6 {
     public static void main(String[] args) {
         System.out.println(primeFactorization(200356));
-        System.out.println(greatestCommonDivisor(54,24));
-        System.out.println(greatestCommonDivisor(124,1150));
-
+        System.out.println(greatestCommonDivisor(54, 24));
+        System.out.println(greatestCommonDivisor(124, 1150));
+        System.out.println(primeFactorization(80));
+        System.out.println(primeFactorization(100));
+        System.out.println(primeFactorization(256));
+        System.out.println(highestCommonFactor(100, 256));
+        System.out.println(highestCommonFactor(2560, 8051));
+        System.out.println(highestCommonFactor(0,0));
 
 
     }
@@ -15,24 +20,35 @@ public class Task6 {
     static int greatestCommonDivisor(int a, int b) {
         List<Integer> aPrimeList = primeFactorization(a);
         List<Integer> bPrimeList = primeFactorization(b);
-        int GCD = 1;
+        int gcd = 1;
         for (int i = 0; i < bPrimeList.size(); i++) {
             if (aPrimeList.contains(bPrimeList.get(i))) {
-                GCD*=bPrimeList.get(i);
+                gcd *= bPrimeList.get(i);
                 // Если возникнет ситуация когда лист содержит два и более одинаковых значения подряд {..,2,2..} мы зануляем
                 // первое найденое значение, чтобы он больше его не мог найти в методе contains
-                aPrimeList.set(aPrimeList.indexOf(bPrimeList.get(i)),0);
+                aPrimeList.set(aPrimeList.indexOf(bPrimeList.get(i)), 0);
             }
         }
-
-
-        return GCD;
+        return gcd;
     }
 
     static int highestCommonFactor(int a, int b) {
-
-
-        return 0;
+        Map<Integer, Integer> aPrimeMap = mapPrimeFactorization(a);
+        Map<Integer, Integer> bPrimeMap = mapPrimeFactorization(b);
+        int hcf = 1;
+        // Сливаем 2 мапы в одну где ключ - простое число, значение - количество этих чисел
+        for (Integer i : aPrimeMap.keySet()) {
+            if (bPrimeMap.containsKey(i) && bPrimeMap.get(i) < aPrimeMap.get(i)) {
+                bPrimeMap.put(i, aPrimeMap.get(i));
+            }
+            if (!bPrimeMap.containsKey(i)) {
+                bPrimeMap.put(i, aPrimeMap.get(i));
+            }
+        }
+        for (Integer i : bPrimeMap.keySet()) {
+            hcf *= Math.pow(i, bPrimeMap.get(i));
+        }
+        return hcf;
     }
 
     static List<Integer> primeFactorization(int a) {
@@ -67,6 +83,19 @@ public class Task6 {
         } while (number >= 1);
         Collections.sort(result);
         return result;
+    }
+
+    static Map<Integer, Integer> mapPrimeFactorization(int a) {
+        List<Integer> list = primeFactorization(a);
+        Map<Integer, Integer> primeMap = new HashMap<>();
+        for (Integer i : list) {
+            if (primeMap.containsKey(i)) {
+                primeMap.put(i, primeMap.get(i) + 1);
+            } else {
+                primeMap.put(i, 1);
+            }
+        }
+        return primeMap;
     }
 
 
