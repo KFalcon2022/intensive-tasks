463eb3aec183eca8fc6dd7e239159a6f20196925
Greem4 <i@greem4.ru>
2023-11-26 16:00:01 +0300

    chapter 2, task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -4,4 +4,22 @@ public class Task6 {
     public static void main(String[] args) {
 
     }
+
+    public static int findLCM(int num1, int num2) {
+        int maxNum = Math.max(num1, num2);
+        int lcm = maxNum;
+
+        while (lcm % num1 != 0 || lcm % num2 != 0) {
+            lcm += maxNum;
+        }
+
+        return lcm;
+    }
+
+    public static int findGCD(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return findGCD(num2, num1 % num2);
+    }
 }
