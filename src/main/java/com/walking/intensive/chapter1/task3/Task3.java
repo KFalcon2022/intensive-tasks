3813b5f0038e666e2989905936e505a81ac97acb
DeaDMop03 <bl.target@yandex.ru>
2023-11-26 11:50:20 +0300

    resolve chapter1task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -6,12 +6,24 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
         //Для собственных проверок можете делать любые изменения в этом методе
+        //int year = 2104;
+        //boolean isLeapYear = isLeap(year);
+        //System.out.println(isLeapYear);
 
     }
 
     static boolean isLeap(int year) {
         //Место для вашего кода
+        boolean result = false;
+        if (((year % 4 ==0) && (year % 100 !=0)) || (year % 400==0))
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
 
-        return false; // Заглушка. При реализации - удалить
+        return result; // Заглушка. При реализации - удалить
     }
 }
