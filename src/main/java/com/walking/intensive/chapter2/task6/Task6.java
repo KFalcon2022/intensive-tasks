dc6cbdd03491b32aa5a782c5eb764e024ec1e42f
apjam <apjam@yandex.ru>
2023-11-26 20:03:43 +0600

    task6 is done

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -2,6 +2,29 @@ package com.walking.intensive.chapter2.task6;
 
 public class Task6 {
     public static void main(String[] args) {
+        int a = 18, b = 48;
+        System.out.println("НОК: " + getNOK(a, b));
+        System.out.println("НОД: " + getNOD(a, b));
+    }
 
+    static int getNOK(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return (a * b) / getNOD(a, b);
+    }
+
+    static int getNOD(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        int biggest = Math.max(a, b);
+        int lowest = Math.min(a, b);
+
+        int remainder = biggest % lowest;
+        if (remainder == 0) {
+            return lowest;
+        }
+        return getNOD(lowest, remainder);
     }
 }
