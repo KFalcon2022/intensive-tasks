5a00b8f56b5658ae94b828642f645c4c6cc6e822
ArtemiyDS <27071999@mail.ru>
2023-11-26 20:13:10 +0300

    resolve task2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,13 +5,39 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        System.out.println(getFlatLocation(10,3,121));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        /*
+        Проверка на то, что номер квартиры - положительное число и
+        номер квартиры не превышает кол-во возможных квартир в доме
+         */
+        if ( flatNumber <= 0 || 4 * floorAmount * entranceAmount < flatNumber ) {
+            return "Такой квартиры не существует";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        flatNumber--; // Нумерация квартир будет начинаться не с 1, а с 0
+        String answer = "";
+
+        int flatInEntrance = 4 * floorAmount; // Кол-во квартир в одном подъезде = 4 * кол-во этажей
+        answer += String.valueOf(flatNumber / flatInEntrance + 1) + " подъезд," + String.valueOf((flatNumber % flatInEntrance) / 4 + 1) + " этаж,";
+
+        switch(flatNumber % 4){
+            case 0:
+                answer += "слева от лифта, влево";
+                break;
+            case 1:
+                answer += "слева от лифта, вправо";
+                break;
+            case 2:
+                answer += "справо от лифта, влево";
+                break;
+            case 3:
+                answer += "справа от лифта, вправо";
+                break;
+        }
+
+        return answer;
     }
 }
