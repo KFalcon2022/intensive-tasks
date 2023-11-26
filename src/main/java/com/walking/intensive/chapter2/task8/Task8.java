c3dfd32501bce609e56be18038ba50746ca7fdeb
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-11-26 21:49:36 +0300

    solution task8 comparison

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -1,7 +1,7 @@
 package com.walking.intensive.chapter2.task8;
 
 /**
- * Условие: <a href="http://geometry-math.ru/homework/Java-ticket.html">ссылка</a>
+ * РЈСЃР»РѕРІРёРµ: <a href="http://geometry-math.ru/homework/Java-ticket.html">СЃСЃС‹Р»РєР°</a>
  */
 public class Task8 {
     public static void main(String[] args) {
@@ -9,7 +9,7 @@ public class Task8 {
     }
 
     static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
+        // Р’Р°С€ РєРѕРґ
         return 0.0;
     }
 }
