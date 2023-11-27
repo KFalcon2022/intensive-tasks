6643b37967bf4ce8dcc1900403cf5062a96813d8
Pavlo <pavlokutsyn@gmail.com>
2023-11-28 00:43:49 +0100

    Chapter2 Task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,22 +5,49 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
+        int m = 10;
+        int n = 25;
 
+        //validation
+        if (validationInput(m, n)) {
+            System.out.println("НОК(" + m + ", " + n + ") = " + getNoc(m, n));
+            System.out.println("НОД(" + m + ", " + n + ") = " + getNod(m, n));
+            System.out.println("НОД (рекурсивниый Евклид) (" + m + ", " + n + ") = " + getNodByEuclideanAlgorithm(m, n));
+
+        } else {
+            System.out.println("Введённые числа не соответствуют условиям задачи");
+        }
+    }
+
+    static boolean validationInput(int m, int n) {
+        // проверка на то, что положительные
+        return m > 0 && n > 0;
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+
+        return Math.abs(m * n) / getNod(m, n);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        int nod = 1; // начально значение НОД
+        int min = Math.min(m, n); // находим наименьшее число между m і n
+
+        for (int i = 2; i <= min; i++) {
+            if (m % i == 0 && n % i == 0) {
+                nod = i; // обновляем НОД если нашли общий делитель
+            }
+        }
+
+        return nod;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        // Рекурсивная реализация алгоритма Евклида
+        if (n == 0) {
+            return m;
+        }
+        return getNodByEuclideanAlgorithm(n, m % n);
     }
 
 }
