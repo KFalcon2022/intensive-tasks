a4ea4a3b2ef0ccf5d52e6e5221845e6c43ad01f5
EkkoXIII <astaroth644@gmail.com>
2023-11-28 02:00:04 +0300

    Решение задачи 7. На значении до 15000 еще худо-бедно работает, но на больших задумывается на очень долго. Как оптимизировать перебор не придумал =( Подскажите, плиз, в каком направлении думать.

--- src/main/java/com/walking/intensive/chapter2/task7/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Main.java
@@ -1,7 +1,48 @@
-package com.walking.intensive.chapter2.task7;
+package src.main.java.com.walking.intensive.chapter2.task7;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Введите максимальное дружественное число (меньше 1 000 000): ");
+        int friendlyIntLimit = scanner.nextInt();
+
+        scanner.close();
+
+        if (friendlyIntLimit >= 1000000) {
+            System.out.println("Вы ввели число более 1 000 000.");
+        } else {
+            System.out.printf("Наибольшее число из пары дружественных чисел до %d: %d", friendlyIntLimit, getMaxFriendlyInt(friendlyIntLimit));
+        }
+    }
+
+    public static int getDivisorsSum(int integer) {
+        int result = 1;
+
+        for (int i = 2; i < Math.sqrt(integer); i++) {
+            if (integer % i == 0) {
+                result += i + integer / i;
+            }
+        }
+
+        return result;
+    }
+
+    public static int getMaxFriendlyInt(int maxInteger) {
+        int result = 0;
+        int sum = 0;
+
+        for (int i = 1; i <= maxInteger; i++) {
+            for (int j = i + 1; j <= maxInteger; j++) {
+                if ((i == getDivisorsSum(j)) && (getDivisorsSum(i) == j) && (i + j > sum)) {
+                    sum = i + j;
+                    result = Math.max(i, j);
+                }
+            }
+        }
+
+        return result;
     }
 }
