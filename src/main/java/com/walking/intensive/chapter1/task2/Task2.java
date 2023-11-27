124cd4d32bcf26ac4703e1c32477574a0f964532
Alphyra <ageyeva.sveta@gmail.com>
2023-11-27 20:28:37 +0300

    Chapter 1 Task 2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,13 +5,41 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        int floorAmount = 10;
+        int entranceAmount = 3;
+        int flatNumber = 121;
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int allFlats = floorAmount * entranceAmount * 4;
+        // Validation
+        if (flatNumber <= 0 || flatNumber > allFlats) {
+            return "Такой квартиры не существует";
+        }
+        // Entrance Number
+        int flatsAtEntrance = floorAmount * 4;
+        int entranceNum = ((flatNumber - 1) / flatsAtEntrance + 1);
+        // Floor Number
+        int floorNum;
+        if (flatNumber % flatsAtEntrance == 0) {
+            floorNum = floorAmount;
+        } else {
+            floorNum = ((flatNumber % flatsAtEntrance - 1) / 4 + 1);
+        }
+        // Direction on floor
+        String direction = "";
+        switch (flatNumber % 4) {
+            case 1 -> direction = "слева от лифта, влево";
+            case 2 -> direction = "слева от лифта, вправо";
+            case 3 -> direction = "справа от лифта, влево";
+            case 0 -> direction = "справа от лифта, вправо";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        return flatNumber + " кв - " +
+                entranceNum + " подъезд, " +
+                floorNum + " этаж, " +
+                direction;
     }
 }
