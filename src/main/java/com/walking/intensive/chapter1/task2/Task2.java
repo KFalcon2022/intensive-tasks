f126fd1427f94f206668c0d7843a8e6b5f70d440
ichigovizor <ichigovizor@gmail.com>
2023-11-27 09:46:24 +0600

    Chapter 1 Task 2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -14,7 +14,7 @@ public class Task2 {
         int flatQuantity = floorAmount * entranceAmount * 4;
         int position = flatNumber % 4;
         int entranceNumber = (flatNumber - 1)/(floorAmount * 4) + 1;
-        int floorNumber = (((flatNumber-(entranceNumber - 1) * (floorAmount * 4)) - 1) / 4) + 1;
+        int floorNumber = (((flatNumber - (entranceNumber - 1) * (floorAmount * 4)) - 1) / 4) + 1;
         String positionStr = switch (position) {
             case (0) -> "справа от лифта, вправо";
             case (1) -> "слева от лифта, влево";
@@ -23,7 +23,7 @@ public class Task2 {
             default -> "";
         };
 
-        if (flatNumber>flatQuantity|flatNumber < 1) {
+        if (flatNumber > flatQuantity | flatNumber < 1) {
             return "Такой квартиры не существует";
         } else if (floorAmount < 1) {
             return "Такого этажа не существует";
