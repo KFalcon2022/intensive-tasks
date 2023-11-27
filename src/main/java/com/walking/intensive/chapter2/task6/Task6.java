f02b96bb1dadec4a5d131a286e6f08e89f8f0280
KrisLu23 <krisbondarev@yandex.ru>
2023-11-27 13:47:23 +0300

    Chapter 2 Task 6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter2.task6;
 public class Task6 {
     public static void main(String[] args) {
         System.out.println(getNoc(12, 8));
-        System.out.println(getNod(56,48));
+        System.out.println(getNod(56, 48));
     }
 
     static int getNoc(int m, int n) {
@@ -14,31 +14,32 @@ public class Task6 {
         n = Math.abs(n);
         int max = Math.max(m, n);
         int min = Math.min(m, n);
-            int temp = max;
-            while (!(max % min == 0)) {
-                max += temp;
-            }
-            return max;
+        int temp = max;
+        while (!(max % min == 0)) {
+            max += temp;
         }
+        return max;
+    }
 
 
     static int getNod(int m, int n) {
         int nod = 0;
         m = Math.abs(m);
         n = Math.abs(n);
-        int max = Math.max(m,n);
-        int min = Math.min(m,n);
-        if (max % min == 0){
+        int max = Math.max(m, n);
+        int min = Math.min(m, n);
+        if (max % min == 0) {
             return nod = min;
         }
         for (int i = min; i > 0; i--) {
-            if (max % i == 0 && min % i ==0){
+            if (max % i == 0 && min % i == 0) {
                 return nod = i;
             }
-        } return nod;
+        }
+        return nod;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
+    static int getNodByEuclideanAlgorithm(int m, int n) {
         // Ваш код
         return 0;
     }
