57be95de029d3ffc8905d2c7d9bf9f17f830573f
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-27 15:11:11 +0300

    Task 2.1

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,32 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        System.out.println(getFriendlyPair(300));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
+    static int getFriendlyPair(int N) {
+        for (int i = N; i > 1; i--) {
+            int sum1number = 0;
+            int sum2number = 0;
+
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    sum1number += j;
+                }
+            }
+
+            for (int j = 1; j < sum1number; j++) {
+                if (sum1number % j == 0) {
+                    sum2number += j;
+                }
+            }
+
+            if (sum2number == i) {
+                return i;
+            }
+
+        }
         return 0;
     }
+
 }
