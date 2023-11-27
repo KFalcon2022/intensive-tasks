2975138e3db7f932f3f47ad17ce094abfe00ba29
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-27 16:23:22 +0300

    Chapter 2 Task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,24 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println("вероятность вытащить счастливый билет: " + getHappyTicketChance(999999) + "%");
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance(int ticketNumber) {
+        int count = 0;
+        for (int i = 1000; i <= ticketNumber; i++) {
+            int sum1 = i % 10 + i / 10 % 10 + i / 100 % 10;
+            int sum2 = i / 1000 % 10 + i / 10000 % 10 + i / 100000 % 10;
+            if (sum2 == sum1) {
+                count++;
+            }
+        }
+
+        System.out.println("всего счастливых чисел: " + count);
+        return (double) count / ticketNumber * 100;
     }
+
 }
+
+
+
