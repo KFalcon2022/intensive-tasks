a3f3aa3831cf03a73626c24aa2ce9ce0f02f0ebf
Alext715 <alexandrevd2107@gmail.com>
2023-11-27 11:33:01 +0300

    Chapter_2_Task_10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,24 @@
 package com.walking.intensive.chapter2.task10;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
 public class Task10 {
     public static void main(String[] args) {
+        Scanner palindrome = new Scanner(System.in);
 
+        System.out.println("Введите строку");
+        String inputString = palindrome.nextLine();
+
+        System.out.println(isPalindrome(inputString));
     }
+    static boolean isPalindrome(String inputString) {
+        String removeExcess = inputString.replaceAll("[^\\p{L}\\p{N}]+", "");
+        StringBuilder result = new StringBuilder(removeExcess);
+        StringBuilder reverse = result.reverse();
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+        return reverse.toString().equalsIgnoreCase(removeExcess);
     }
 }
