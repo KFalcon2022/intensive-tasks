4c96e69db8732cf8ab0c941d3b73914c868a3fe8
TitLukich <Nef-92@yandex.ru>
2023-11-27 12:49:26 +0300

    Chapter 2, Task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,25 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
+        System.out.println(getHappyTicketChance(1000000));
 
     }
-
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance(int ticketNumber) {
+        double count = 0;
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (sumDigit(i) == sumDigit(j)) {
+                    count++;
+                }
+            }
+        }
+        return count / ticketNumber;
+    }
+    static int sumDigit(int num) {
+        int sum = 0;
+        for (int i = num; i > 0; i /= 10) {
+            sum += i % 10;
+        }
+        return sum;
     }
 }
