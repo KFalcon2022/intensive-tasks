6c4e8ab952098208ab8927f8eaecd7bed1c5a7c9
Vbliznev <7492008@mail.ru>
2023-11-27 16:17:57 +0300

    Merge pull request #87 from KFalcon2022/revert
    Revert

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -5,40 +5,20 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 111;
 
-        System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        String year = "";
-        String result = "";
-
         int lastNumberFromAge = age % 10;
-
-        if (lastNumberFromAge == 1) {
-            year = "год";
+        if (lastNumberFromAge == 1 & age != 11 & age != 111) {
+            return ("Вам " + age + " " + "год");
         } else {
             if (lastNumberFromAge == 2 || lastNumberFromAge == 3 || lastNumberFromAge == 4) {
-                year = "года";
-            } else {
-                year = "лет";
-
+                if (age != 12 & age != 112 & age != 13 & age != 113 & age != 14 & age != 114) {
+                    return ("Вам " + age + " " + "года");
+                }
             }
+            return ("Вам " + age + " " + "лет");
         }
-        switch (age) {
-            case 11 -> year = "лет";
-            case 12 -> year = "лет";
-            case 13 -> year = "лет";
-            case 14 -> year = "лет";
-            case 111 -> year = "лет";
-            case 112 -> year = "лет";
-            case 113 -> year = "лет";
-            case 114 -> year = "лет";
-        }
-
-        result = ("Вам " + age + " " + year);
-        return result;
     }
 }
