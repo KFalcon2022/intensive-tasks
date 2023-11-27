1c47370bb5729ef0ceb80653ddf347a96303f018
SashaD32 <Sasha_D_32@mail.ru>
2023-11-27 11:23:49 +0700

    Correct Chapter1 Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -14,8 +14,6 @@ public class Task3 {
     static boolean isLeap(int year) {
 
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            return true;
-        }
-        return false;
+
     }
 }
