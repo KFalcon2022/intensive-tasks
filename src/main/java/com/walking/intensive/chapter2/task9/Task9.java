c6efcd56c5af68c7dd326867a3a8b466dadc8886
Boris <aksenov.boris8595@gmeil.com>
2023-11-27 22:42:30 +0500

    Треугольник рисуется, но думаю код можно и проще написать. Идее не понравилось += в циклах(в гугле посмотрел почему), вроде правильно использовал стрингбилдер(явно на ракете съездил в магазин).

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,11 +5,43 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
-
+        System.out.println(getPascalTriangle(15));
     }
 
-    static String getPascalTriangle(int n){
-        // Ваш код
-        return null;
+    static String getPascalTriangle(int n) {
+        StringBuilder sb = new StringBuilder();
+        int[][] arrayMy = new int[n + 1][n + 1];
+        int j = 0;
+        for (int i = 0; i <= n; i++) {
+            while (j <= i) {
+                if (j == 0 || j == i) {
+                    arrayMy[i][j] = 1;
+                } else {
+                    arrayMy[i][j] = arrayMy[i - 1][j] + arrayMy[i - 1][j - 1];
+                }
+                j++;
+            }
+            j = 0;
+        }
+        String stringResult = "";
+        String strMaxLength = "";
+        String strCurrent = "";
+        for (int i = 0; i <= n; i++) {
+            strMaxLength = sb.append(arrayMy[n][i]).append(" ").toString();
+        }
+        for (int i = 0; i <= n; i++) {
+            while (j <= i) {
+                strCurrent += arrayMy[i][j] + " ";
+                j++;
+            }
+            int lengthCurrentStr = (strMaxLength.length() - strCurrent.length()) / 2;
+            for (int k = 0; k <= lengthCurrentStr + 1; k++) {
+                strCurrent = " ".concat(strCurrent);
+            }
+            stringResult = sb.append(strCurrent).append("\n").toString();
+            strCurrent = "";
+            j = 0;
+        }
+        return stringResult;
     }
 }
