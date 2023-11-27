a6280cbb14e576b2f63406595b8b44a6d42b405d
NightwolfCB <nightwolf63@rambler.ru>
2023-11-27 21:03:13 +0300

    task1 fix

--- src/main/java/com/walking/intensive/chapter1/task1/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Main.java
@@ -23,14 +23,8 @@ public class Main {
                 || (decadeRemain == 3 & centuryRemain != 13)
                 || (decadeRemain == 4 & centuryRemain != 14)) {
             return "Вам " + age + " года";
-        } else if (centuryRemain == 11 || centuryRemain == 12
-                || centuryRemain == 14 || centuryRemain == 13
-                || decadeRemain == 5 || decadeRemain == 6
-                || decadeRemain == 7 || decadeRemain == 8
-                || decadeRemain == 9 || decadeRemain == 0) {
+        } else {
             return "Вам " + age + " лет";
         }
-
-        return "";
     }
 }
