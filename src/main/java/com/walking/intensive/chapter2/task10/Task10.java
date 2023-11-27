c8842c8df9d10f4317896bd2d0adb9a2ee92324f
Reilan <reilan86@gmail.com>
2023-11-27 16:59:18 +0300

    Chapter 2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -2,6 +2,26 @@ package com.walking.intensive.chapter2.task10;
 
 public class Task10 {
     public static void main(String[] args) {
+        System.out.println(getPalindrome("А роза упала на лапу азора!"));
+    }
 
+    public static String getPalindrome(String income) {
+
+        StringBuilder convertString = new StringBuilder();
+        convertString.append(income.toLowerCase().replaceAll("[^\\p{L}\\p{N}]", ""));
+
+        int start = 0;
+        int end = convertString.length() - 1;
+
+        while (start < end) {
+
+            if (convertString.charAt(end) != convertString.charAt(start)) {
+                return "Строка не палиндром.";
+
+            }
+            start++;
+            end--;
+        }
+        return "Строка является палиндромом.";
     }
 }
