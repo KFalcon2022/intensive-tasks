3242cee3fc1a5102ac3620b2502585799280c8e0
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-27 14:00:18 +0300

    Task 2.1

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -2,6 +2,25 @@ package com.walking.intensive.chapter2.task6;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        showNODandNOK(30, 18);
     }
+
+    static int findNOD_Evklid(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return findNOD_Evklid(b, a % b);
+    }
+
+    static int findNok(int a, int b) {
+        return a / findNOD_Evklid(a, b) * b;
+    }
+
+    static void showNODandNOK(int a, int b) {
+        System.out.printf("Нод чисел %d и %d : %d  \n", a, b, findNOD_Evklid(a, b));
+        System.out.printf("Нок чисел %d и %d : %d  ", a, b, findNok(a, b));
+    }
+
+
 }
