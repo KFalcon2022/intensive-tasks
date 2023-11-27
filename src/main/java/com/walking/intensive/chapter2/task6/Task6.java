2df02d9c33e887e2b6494d13d4e01c049ceb413d
k2336351 <k2336351@yandex.ru>
2023-11-27 12:38:10 +0300

    Solution Chapter 2  Task 6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -6,21 +6,56 @@ package com.walking.intensive.chapter2.task6;
 public class Task6 {
     public static void main(String[] args) {
 
+        int m = -9;
+        int n = 12;
+
+        System.out.println(
+                "D by Euclidean's Algorithm (" + m + "; " + n + ") = " +getNodByEuclideanAlgorithm(n, m) +
+                "\nD (" + m + "; " + n + ") = " + getNod(m, n) +
+                "\nK (" + m + "; " + n + ") = " + getNoc(m, n)
+        );
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+
+        return Math.abs(m * n) / getNod(m, n);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (m == 0 || n == 0) {
+            return m + n;
+        }
+
+        m = Math.abs(m);
+        n = Math.abs(n);
+
+        int nod = 1;
+
+        for (int i = 1; i <= m && i <= n; i++) {
+
+            if (m % i == 0 && n % i == 0) {
+                nod = i;
+            }
+        }
+
+        return nod;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+
+        m = Math.abs(m);
+        n = Math.abs(n);
+
+        if (n == 0) {
+            return m;
+        }
+
+        return getNodByEuclideanAlgorithm(n, m % n);
     }
 
 }
