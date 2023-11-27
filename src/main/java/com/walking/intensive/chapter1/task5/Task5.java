4c88b99263904dd4daee6d754031812c34f65047
k2336351 <k2336351@yandex.ru>
2023-11-27 03:08:35 +0300

    Solution Chapter 1  Task 5 (удалёны sout и null, применён реверс)

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -8,11 +8,12 @@ import java.util.Arrays;
 public class Task5 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 3;
-        double b = 4;
-        double c = 5;
+        double a = -3;
+        double b = 0;
+        double c = 3;
 
-        System.out.println("Площадь по формуле Герона:  " + getAreaByHeron(a, b, c) +
+        System.out.println("Треугольник существует: " + !isTriangleNotExist(a, b, c) +
+                "\nПлощадь по формуле Герона:  " + getAreaByHeron(a, b, c) +
                 "\nДлины высот треугольника: " + Arrays.toString(getHeights(a, b, c)) +
                 "\nДлины медиан треугольника: " + Arrays.toString(getMedians(a, b, c)) +
                 "\nДлины биссектрис треугольника: " + Arrays.toString(getBisectors(a, b, c)) +
@@ -22,16 +23,15 @@ public class Task5 {
                 "\nПлощадь, найденная по синусу: " + getAreaAdvanced(a, b, c));
     }
 
-    static double getAreaByHeron(double a, double b, double c) {
+    static double getAreaByHeron(double a, double b, double c) throws IllegalArgumentException {
 
-        if (isTriangleExist(a, b, c)) {
-
-            double halfPerimeter = (a + b + c) / 2;
-
-            return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+        if (isTriangleNotExist(a, b, c)) {
+            return 0;
         }
 
-        return 0;
+        double halfPerimeter = (a + b + c) / 2;
+
+        return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
     }
 
     /**
@@ -39,20 +39,20 @@ public class Task5 {
      */
     static double[] getHeights(double a, double b, double c) {
 
-        if (isTriangleExist(a, b, c)) {
-            double doubleArea = getAreaByHeron(a, b, c) * 2;
-
-            double[] heights = new double[3];
-            heights[0] = doubleArea / a;
-            heights[1] = doubleArea / b;
-            heights[2] = doubleArea / c;
-
-            Arrays.sort(heights);
-
-            return heights;
+        if (isTriangleNotExist(a, b, c)) {
+            return new double[3];
         }
 
-        return null;
+        double doubleArea = getAreaByHeron(a, b, c) * 2;
+
+        double[] heights = new double[3];
+        heights[0] = doubleArea / a;
+        heights[1] = doubleArea / b;
+        heights[2] = doubleArea / c;
+
+        Arrays.sort(heights);
+
+        return heights;
     }
 
     /**
@@ -60,19 +60,18 @@ public class Task5 {
      */
     static double[] getMedians(double a, double b, double c) {
 
-        if (isTriangleExist(a, b, c)) {
-
-            double[] medians = new double[3];
-            medians[0] = Math.sqrt(2 * (Math.pow(a, 2) + Math.pow(b, 2)) - Math.pow(c, 2)) / 2;
-            medians[1] = Math.sqrt(2 * (Math.pow(b, 2) + Math.pow(c, 2)) - Math.pow(a, 2)) / 2;
-            medians[2] = Math.sqrt(2 * (Math.pow(a, 2) + Math.pow(c, 2)) - Math.pow(b, 2)) / 2;
-
-            Arrays.sort(medians);
-
-            return medians;
+        if (isTriangleNotExist(a, b, c)) {
+            return new double[3];
         }
 
-        return null;
+        double[] medians = new double[3];
+        medians[0] = Math.sqrt(2 * (Math.pow(a, 2) + Math.pow(b, 2)) - Math.pow(c, 2)) / 2;
+        medians[1] = Math.sqrt(2 * (Math.pow(b, 2) + Math.pow(c, 2)) - Math.pow(a, 2)) / 2;
+        medians[2] = Math.sqrt(2 * (Math.pow(a, 2) + Math.pow(c, 2)) - Math.pow(b, 2)) / 2;
+
+        Arrays.sort(medians);
+
+        return medians;
     }
 
     /**
@@ -80,20 +79,20 @@ public class Task5 {
      */
     static double[] getBisectors(double a, double b, double c) {
 
-        if (isTriangleExist(a, b, c)) {
-
-            double perimeter = a + b + c;
-            double[] bisectors = new double[3];
-            bisectors[0] = Math.sqrt(b * c * perimeter * (perimeter - a * 2)) / (perimeter - a);
-            bisectors[1] = Math.sqrt(a * c * perimeter * (perimeter - b * 2)) / (perimeter - b);
-            bisectors[2] = Math.sqrt(a * b * perimeter * (perimeter - c * 2)) / (perimeter - c);
-
-            Arrays.sort(bisectors);
-
-            return bisectors;
+        if (isTriangleNotExist(a, b, c)) {
+            return new double[3];
         }
 
-        return null;
+        double perimeter = a + b + c;
+
+        double[] bisectors = new double[3];
+        bisectors[0] = Math.sqrt(b * c * perimeter * (perimeter - a * 2)) / (perimeter - a);
+        bisectors[1] = Math.sqrt(a * c * perimeter * (perimeter - b * 2)) / (perimeter - b);
+        bisectors[2] = Math.sqrt(a * b * perimeter * (perimeter - c * 2)) / (perimeter - c);
+
+        Arrays.sort(bisectors);
+
+        return bisectors;
     }
 
     /**
@@ -101,68 +100,59 @@ public class Task5 {
      */
     static double[] getAngles(double a, double b, double c) {
 
-        if (isTriangleExist(a, b, c)) {
-
-            double cosine1 = (Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b);
-            double cosine2 = (Math.pow(b, 2) + Math.pow(c, 2) - Math.pow(a, 2)) / (2 * b * c);
-            double cosine3 = (Math.pow(a, 2) + Math.pow(c, 2) - Math.pow(b, 2)) / (2 * a * c);
-
-            double[] angles = new double[3];
-            angles[0] = Math.toDegrees(Math.acos(cosine1));
-            angles[1] = Math.toDegrees(Math.acos(cosine2));
-            angles[2] = Math.toDegrees(Math.acos(cosine3));
-
-            Arrays.sort(angles);
-
-            return angles;
+        if (isTriangleNotExist(a, b, c)) {
+            return new double[3];
         }
 
-        return null;
+        double cosine1 = (Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b);
+        double cosine2 = (Math.pow(b, 2) + Math.pow(c, 2) - Math.pow(a, 2)) / (2 * b * c);
+        double cosine3 = (Math.pow(a, 2) + Math.pow(c, 2) - Math.pow(b, 2)) / (2 * a * c);
+
+        double[] angles = new double[3];
+        angles[0] = Math.toDegrees(Math.acos(cosine1));
+        angles[1] = Math.toDegrees(Math.acos(cosine2));
+        angles[2] = Math.toDegrees(Math.acos(cosine3));
+
+        Arrays.sort(angles);
+
+        return angles;
     }
 
     static double getInscribedCircleRadius(double a, double b, double c) {
 
-        if (isTriangleExist(a, b, c)) {
-            return (getAreaByHeron(a, b, c) * 2) / (a + b + c);
+        if (isTriangleNotExist(a, b, c)) {
+            return 0;
         }
 
-        return 0;
+        return (getAreaByHeron(a, b, c) * 2) / (a + b + c);
     }
 
     static double getCircumradius(double a, double b, double c) {
 
-        if (isTriangleExist(a, b, c)) {
-            return (a * b * c) / (getAreaByHeron(a, b, c) * 4);
+        if (isTriangleNotExist(a, b, c)) {
+            return 0;
         }
 
-        return 0;
+        return (a * b * c) / (getAreaByHeron(a, b, c) * 4);
     }
 
     static double getAreaAdvanced(double a, double b, double c) {
 
-        if (isTriangleExist(a, b, c)) {
-
-            double cosine = (Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b);
-            double sine = Math.sqrt(1 - Math.pow(cosine, 2));
-
-            return (a * b * sine) / 2;
+        if (isTriangleNotExist(a, b, c)) {
+            return 0;
         }
 
-        return 0;
+        double cosine = (Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b);
+        double sine = Math.sqrt(1 - Math.pow(cosine, 2));
+
+        return (a * b * sine) / 2;
     }
 
-    static boolean isTriangleExist(double a, double b, double c) {
+    static boolean isTriangleNotExist(double a, double b, double c) {
 
         boolean isValid = a > 0 && b > 0 && c > 0;
         boolean isExist = a + b > c && b + c > a && a + c > b;
 
-        if (!isValid || !isExist) {
-
-            System.out.println("Треугольник не существует или является вырожденным");
-
-            return false;
-        }
-
-        return true;
+        return !isValid || !isExist;
     }
 }
