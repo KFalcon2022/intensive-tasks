98af16a15025739122dd7a85574b99a43028fdbf
Lunularis <s.n.volynkina@yandex.ru>
2023-11-27 21:41:15 +0700

    Chapter 2 Task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Main.java
@@ -2,6 +2,26 @@ package com.walking.intensive.chapter2.task8;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(getHappyTicketChance(1));
+    }
 
+    static double getHappyTicketChance(int ticketNumber){
+        //максмальное значение суммы трех цифр: 27
+        //посчитаем вероятность выпадения каждого числа
+        int[] frequency = new int[28];
+
+        for (int i = 0; i < 1000; i++) {
+            int sum = i % 10 + (i / 10) % 10 + (i / 100);
+            frequency[sum]++;
+        }
+        //посчитаем вероятность того, что одновременно в правой и левой частях выпадет одинаковая сумма
+        double Probability = 0; // итоговая вероятность счастливого билета
+        double amount = 1000; //количество вариантов
+
+        for (int curFrequency : frequency) {
+            Probability += Math.pow((double) curFrequency / amount, 2);
+        }
+
+        return Probability;
     }
 }
