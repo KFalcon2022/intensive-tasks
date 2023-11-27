e63396ea66c72d4c6cccd6f3df79f7c477622ff4
ichigovizor <ichigovizor@gmail.com>
2023-11-27 10:58:43 +0600

    Chapter 1 Task 3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -10,8 +10,19 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
 
-        return false; // Заглушка. При реализации - удалить
+        if (year % 400 == 0) {
+            return true;
+
+        } else if (year % 100 == 0) {
+            return false;
+
+        } else if (year % 4 == 0) {
+            return true;
+
+        } else {
+            return false;
+        }
+
     }
 }
