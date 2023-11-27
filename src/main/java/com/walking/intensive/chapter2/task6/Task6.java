55f0abce0196c03ae200e123d31d30465598f552
Alext715 <alexandrevd2107@gmail.com>
2023-11-27 14:45:07 +0300

    Chapter_2_Task_6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -1,26 +1,38 @@
 package com.walking.intensive.chapter2.task6;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-NOKNOD.html">ссылка</a>
  */
 public class Task6 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Введите первое число m ");
+        int m = in.nextInt();
+
+        System.out.println("Введите второе число n ");
+        int n = in.nextInt();
+        in.close();
+
+
+        System.out.println("НОД " + getNod(m, n) + "\n" + "НОК " + getNoc(m, n));
+        System.out.println("НОД по алгоритму Евклида " + getNodByEuclideanAlgorithm(m, n));
     }
-
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        return m / getNod(m, n) * n;
     }
-
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        int result = 0;
+        for (int i = 1; i <= Math.max(m, n); i++) {
+            if (m % i == 0 && n % i == 0) {
+                result = i;
+            }
+        }
+        return result;
     }
-
     static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+        return n == 0 ? m : getNodByEuclideanAlgorithm(n, m % n);
     }
-
 }
