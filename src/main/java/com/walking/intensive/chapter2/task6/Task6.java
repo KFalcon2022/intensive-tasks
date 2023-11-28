5f64b039d2bb976786d46e2daf868d31059b63e2
ETSphere <elem174@proton.me>
2023-11-28 13:30:19 +0500

    Вроде работает (закоментил тест-итерацию рекурсии)

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -4,23 +4,46 @@ package com.walking.intensive.chapter2.task6;
  * Условие: <a href="https://geometry-math.ru/homework/Java-NOKNOD.html">ссылка</a>
  */
 public class Task6 {
+//    private static int countMSG;
+
     public static void main(String[] args) {
+        int a = 84;
+        int b = 90;
+
+
+        System.out.println("Наибольший общий делитель: \n" + getNod(a, b));
+        System.out.println("Рекурсия, алгоритм Евклида: \n" + getNodByEuclideanAlgorithm(a, b));
+        System.out.println("Наименьшее общее кратное: \n" + getNoc(a, b));
 
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        /* НОК(a, b)=a⋅b: ( a , b ) = a · b : НОД(a, b)
+        НОК двух чисел - это наименьшее положительное число НОК = |a · b| : НОД(a, b)
+         */
+
+        return Math.abs(m * n) / getNod(m, n);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+
+        while (n != 0) {
+            int tempBuffer = n;
+            n = m % n;
+            m = tempBuffer;
+        }
+        return m;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+
+//        countMSG++;
+        if (n == 0) {
+            return m;
+        } else {
+//            System.out.println("recurs " + countMSG + " ,b = " + n);  // self test
+            return getNodByEuclideanAlgorithm(n, m % n);
+        }
     }
 
 }
