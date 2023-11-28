23157fe4a7b192b6f0821ed3f13867431a6ed2f9
Alexey <lolex1@mail.ru>
2023-11-28 21:50:59 +0300

    Chapter 2 Task6

--- src/main/java/com/walking/intensive/chapter2/task6/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Main.java
@@ -3,53 +3,50 @@ package com.walking.intensive.chapter2.task6;
 public class Main {
     public static void main(String[] args) {
 
-    int number1 = 13;
-    int number2 = 191;
+        int number1 = 270;
+        int number2 = 192;
 
         System.out.println(getNOKAndNOD(number1, number2));
     }
 
-    public static boolean isNotCorrectInput(int number1, int number2){
-        if (number1 < 1 || number2 < 1){
-            return true;
-        }
-        return false;
+    public static boolean isNotCorrectInput(int number1, int number2) {
+        return number1 < 1 || number2 < 1;
     }
 
     public static boolean isSimple(int number1) {
-        for(int i = 2; i <= number1 / 2; i++) {
-            if(number1 % i == 0) {
+        for (int i = 2; i <= number1 / 2; i++) {
+            if (number1 % i == 0) {
                 return false;
             }
         }
         return true;
     }
 
-    public static int getMinNumber(int number1, int number2){
+    public static int getMinNumber(int number1, int number2) {
         return Math.min(number1, number2);
     }
 
-    public static int getMaxNumber(int number1, int number2){
+    public static int getMaxNumber(int number1, int number2) {
         return Math.max(number1, number2);
     }
 
-    public static String getNOKAndNOD (int number1, int number2){
-        if (isNotCorrectInput(number1, number2)){
+    public static String getNOKAndNOD(int number1, int number2) {
+        if (isNotCorrectInput(number1, number2)) {
             return "Вводимое значение не должны быть меньше или равно 0";
         }
 
         int minNumber = getMinNumber(number1, number2);
         int maxNumber = getMaxNumber(number1, number2);
 
-        if (minNumber == maxNumber){
+        if (minNumber == maxNumber) {
             return "Наименьшее общее кратное (НОК): " + maxNumber + "\nНаибольший общий делить (НОД): " + minNumber;
         }
 
-        if (isSimple(maxNumber) && isSimple(minNumber)){
+        if (isSimple(maxNumber) && isSimple(minNumber)) {
             return "Наименьшее общее кратное (НОК): " + maxNumber * minNumber + "\nНаибольший общий делить (НОД): 1";
         }
 
-        if (maxNumber % minNumber == 0){
+        if (maxNumber % minNumber == 0) {
             return "Наименьшее общее кратное (НОК): " + maxNumber + "\nНаибольший общий делить (НОД): " + minNumber;
         }
         int valueNOD = getNOD(maxNumber, minNumber);
@@ -58,15 +55,15 @@ public class Main {
         return "Наименьшее общее кратное (НОК): " + valueNOK + "\nНаибольший общий делить (НОД): " + valueNOD;
     }
 
-    public static int getNOD(int valueA, int valueB){
+    public static int getNOD(int valueA, int valueB) {
         boolean isNOD = true;
         int valueNOD = 0;
-        int remainDiv = 0;
-        int factorFromAB = 0;
-        while (isNOD){
+        int remainDiv;
+        int factorFromAB;
+        while (isNOD) {
             factorFromAB = valueA / valueB;
             remainDiv = valueA - (factorFromAB * valueB);
-            if (valueA % valueB == 0){
+            if (valueA % valueB == 0) {
                 valueNOD = valueB;
                 isNOD = false;
             }
