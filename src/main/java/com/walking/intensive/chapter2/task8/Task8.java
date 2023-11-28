0d181778505db85d0ad757142141d5a604044578
njmist <njmist@gmail.com>
2023-11-28 23:58:43 +0300

    chapter 2 task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter2.task8;
 
+import java.awt.*;
+
 /**
  * Условие: <a href="http://geometry-math.ru/homework/Java-ticket.html">ссылка</a>
  */
@@ -8,8 +10,19 @@ public class Task8 {
 
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance(int ticketNumber) {
+        double happyTicketAmount = 0;
+        for (int i = 0; i < 1000000; i++) {
+            int firstDigit = i % 10;
+            int secondDigit = i / 10 % 10;
+            int thirdDigit = i / 100 % 10;
+            int fourthDigit = i / 1000 % 10;
+            int fifthDigit = i / 10000 % 10;
+            int sixthDigit = i / 100000 % 10;
+            if (firstDigit + secondDigit + thirdDigit == fourthDigit + fifthDigit + sixthDigit) {
+                happyTicketAmount++; // Количество счастливых билетов
+            }
+        }
+        return happyTicketAmount / 1000000; // Вероятность выпадения счастливого билетика
     }
 }
