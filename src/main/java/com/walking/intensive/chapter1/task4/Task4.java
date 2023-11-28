42fde9fd7c755ae2c30cac68f7a8563f0eedeff8
U_M1CVT <akachamina@alfabank.ru>
2023-11-28 18:34:32 +0300

    Исправление по зачениям

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -6,9 +6,9 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 8;
-        double b = 30;
-        double c = 7;
+        double a = 1;
+        double b = 0;
+        double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -26,15 +26,32 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-       double Discriminant = Math.pow (b, 2) - 4 * a * c;
+
+        if (a == 0 && b == 0 && c == 0) {
+            return "Количество решений: Бесконечное множество";
+        }
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0";
+        }
+        if (a == 0) {
+            double root = - c / b;
+            if (root == 0){
+            return "Количество решений: 1. Корень: " + Math.abs(root);
+            }
+            return "Количество решений: 1. Корень: " + root;
+        }
+        double Discriminant = Math.pow(b, 2) - 4 * a * c;
 
         if (Discriminant == 0) {
-            double root = - b / (2 * a);
+            double root = -b / (2 * a);
+            if (root == 0){
+                return "Количество решений: 1. Корень: " + Math.abs(root);
+            }
             return "Количество решений: 1. Корень: " + root;
         }
         if (Discriminant > 0) {
-            double root1 = (- b - Math.sqrt(Discriminant)) / (2 * a);
-            double root2 = (- b + Math.sqrt(Discriminant)) / (2 * a);
+            double root1 = (-b - Math.sqrt(Discriminant)) / (2 * a);
+            double root2 = (-b + Math.sqrt(Discriminant)) / (2 * a);
             return "Количество решений: 2. Корни: " + Math.max(root1, root2) + ", " + Math.min(root1, root2);
         }
         return "Количество решений: 0";
