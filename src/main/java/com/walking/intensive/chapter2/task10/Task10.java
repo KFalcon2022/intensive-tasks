884c2c2d06d2f2a3a6acc9498250975c6891a3dc
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-28 15:12:25 +0300

    Chapter 2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,35 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome("ab bb  ba"));
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    static boolean isPalindrome(String inputString) {
+        char[] str = inputString
+                .toLowerCase()
+                .toCharArray();
+
+        int left = 0;
+        int right = str.length - 1;
+
+        while (left < right) {
+            if (!Character.isLetter(str[left])) {
+                left++;
+                continue;
+            }
+
+            if (!Character.isLetter(str[right])) {
+                right--;
+                continue;
+            }
+
+            if (str[left] != str[right]) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
     }
 }
