a4c098ac80f58df96fd66a926d4f186cd9f882d5
stogov_so <stogov_sergei@mail.ru>
2023-11-28 13:12:16 +0500

    Chapter2_task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,22 +5,64 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
+        int m = 0;
+        int n = 0;
 
+        System.out.println("НОК = " + getNoc(m, n));
+        System.out.println("НОД = " + getNod(m, n));
+        System.out.println("НОД = " + getNodByEuclideanAlgorithm(m, n));
+    }
+
+    static boolean checkValue(int m, int n) {
+        return m == 0 || n == 0;
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        if (checkValue(m, n)) {
+            return 0;
+        }
+
+        int maxValue = Math.max(m, n);
+        int minValue = Math.min(m, n);
+        int noc = maxValue;
+
+        while (noc % minValue != 0) {
+            noc += maxValue;
+        }
+
+        return Math.abs(noc);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        if (checkValue(m, n)) {
+            if (m != 0) {
+                return Math.abs(m);
+            }
+            if (n != 0) {
+                return Math.abs(n);
+            }
+
+            return 0;
+        }
+
+        int maxValue = Math.max(m, n);
+        int minValue = Math.min(m, n);
+        int temp;
+
+        while (minValue != 0) {
+            temp = minValue;
+            minValue = maxValue % minValue;
+            maxValue = temp;
+        }
+
+        return Math.abs(maxValue);
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
-    }
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        if (n == 0) {
+            return Math.abs(m);
+        }
 
+        return getNodByEuclideanAlgorithm(n, m % n);
+    }
 }
