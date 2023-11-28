dd602f6fe556b8ae04d426986328bd1da13ff122
Pavel Kuznetsov <9161194023@mail.ru>
2023-11-29 00:46:15 +0300

    solved chapter1 task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -6,12 +6,24 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
         //Для собственных проверок можете делать любые изменения в этом методе
+        int year = 1700;
+
+        System.out.println(isLeap(year));
 
     }
 
     static boolean isLeap(int year) {
         //Место для вашего кода
+        if(year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        }
+        return false;
 
-        return false; // Заглушка. При реализации - удалить
+
+        //return false; // Заглушка. При реализации - удалить
     }
 }
