da28c3462ce78ad0f21247aeea341b64e69e9cbc
Константин Синяков <k.siniakov@yandex.ru>
2023-11-28 22:30:46 +0300

    Chapter 2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,40 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        System.out.println(getFriendlyPair(100000));
+        System.out.println(getFriendlyPair(0));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        if (m < 1) {
+            throw new IllegalArgumentException("Допускаются только натуральных числа");
+        }
+
+        int potentialPair;
+        int delimiterSum;
+        int result = 0;
+
+        for (int i = 1; i <= m; i++) {
+            delimiterSum = getDelimiterSum(i);
+            potentialPair = getDelimiterSum(delimiterSum);
+
+            if (potentialPair == i && i < 1000000) {
+                result = Math.max(Math.max(delimiterSum, i), result);
+            }
+        }
+
+        return result;
+    }
+
+    static int getDelimiterSum(int m) {
+        int sum = 0;
+
+        for (int i = 1; i < m; i++) {
+            if (m % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 }
