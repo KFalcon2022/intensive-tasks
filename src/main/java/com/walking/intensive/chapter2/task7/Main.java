2c2ddf225fb94e6080e80607974f23090f6fcc49
Magpier <garson.ao@gmail.com>
2023-11-28 21:01:15 +0300

    Chapter2 Task 7 Убрал все в метод

--- src/main/java/com/walking/intensive/chapter2/task7/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Main.java
@@ -5,36 +5,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println(friendNumber(1000000));
+        System.out.println(friendNumber(300));
     }
 
     static int friendNumber(int N) {
-        int m;
-        int n;
-        int temp;
         if (N < 0 || N > 1000000) {
             System.out.println("Число вне диапазона");
             return 0;
         }
         for (int i = N; i >= 1; i--) {
-            m = i;
-            n = 0;
-            temp = 0;
-            for (int j = 1; j < i; j++) {
-                if (m % j == 0 && n != j) {
-                    n += j;
-                }
+            if (calculateNumber(calculateNumber(i)) == i) {
+                return i;
             }
-            for (int j = 1; j < n; j++) {
-                if (n % j == 0 && n != j) {
-                    temp += j;
-                }
-            }
-            if (temp == m) {
-                return m;
+        }
+        return 0;
+    }
+
+    static int calculateNumber(int cycle) {
+        int number = 0;
+        for (int i = 1; i < cycle; i++) {
+            if (cycle % i == 0 && number != i) {
+                number += i;
             }
 
         }
-        return 0;
+        return number;
     }
 
 }
