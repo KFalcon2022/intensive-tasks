0a54c9fce230f33048b2cf842f0e8bd3640e6b2c
SashaD32 <Sasha_D_32@mail.ru>
2023-11-28 12:48:16 +0700

    Correct Chapter1 Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -6,14 +6,13 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
         //Для собственных проверок можете делать любые изменения в этом методе
-        if (isLeap(1600)) {
+        if (isLeap(1601)) {
             System.out.println("ok");
         }
 
     }
     static boolean isLeap(int year) {
 
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
