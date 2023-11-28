43f486bc6bbc07cb761cf716fca607bff11e0e0b
DeaDMop03 <bl.target@yandex.ru>
2023-11-29 01:10:41 +0300

    chapter1task2 fix1

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -16,24 +16,26 @@ public class Task2 {
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         //        Место для вашего кода
         int flatsOnFloorAmmount = 4;
-        String result = "";
-        if (flatNumber < 1 || flatNumber > floorAmount*entranceAmount*flatsOnFloorAmmount)
+        String result;
+        if ((flatNumber < 1) || (flatNumber > floorAmount * entranceAmount * flatsOnFloorAmmount))
         {
             result = "Такой квартиры не существует";
-        }
-        else {
-            int entranceNumber = ((flatNumber-1) / (floorAmount * flatsOnFloorAmmount)) + 1;
-            int floorNumber = ((flatNumber-1) - (floorAmount * flatsOnFloorAmmount*(entranceNumber - 1)))/flatsOnFloorAmmount + 1;
-            switch (flatNumber - (floorAmount * flatsOnFloorAmmount*(entranceNumber - 1))- (floorNumber -1)*flatsOnFloorAmmount){
-                case 1: result = flatNumber + " кв - " + entranceNumber +" подъезд, " + floorNumber + " этаж, слева от лифта, влево"; break;
-                case 2: result = flatNumber + " кв - " + entranceNumber +" подъезд, " + floorNumber + " этаж, слева от лифта, вправо"; break;
-                case 3: result = flatNumber + " кв - " + entranceNumber +" подъезд, " + floorNumber + " этаж, справа от лифта, влево"; break;
-                case 4: result = flatNumber + " кв - " + entranceNumber +" подъезд, " + floorNumber + " этаж, справа от лифта, вправо"; break;
-
+        } else {
+            int entranceNumber = ((flatNumber - 1) / (floorAmount * flatsOnFloorAmmount)) + 1;
+            int floorNumber = ((flatNumber - 1) - (floorAmount * flatsOnFloorAmmount * (entranceNumber - 1)))/flatsOnFloorAmmount + 1;
+            result = flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber;
+            switch (flatNumber - (floorAmount * flatsOnFloorAmmount * (entranceNumber - 1))- (floorNumber - 1) * flatsOnFloorAmmount){
+                case 1: result = result + " этаж, слева от лифта, влево";
+                    break;
+                case 2: result = result + " этаж, слева от лифта, вправо";
+                    break;
+                case 3: result = result + " этаж, справа от лифта, влево";
+                    break;
+                case 4: result = result + " этаж, справа от лифта, вправо";
+                    break;
             }
 
         }
         return result;
-        // Заглушка. При реализации - удалить
     }
 }
