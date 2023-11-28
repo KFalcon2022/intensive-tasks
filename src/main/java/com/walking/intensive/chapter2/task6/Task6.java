aeb0a380b96877a494cda218b13c689b3e6a380b
ETSphere <elem174@proton.me>
2023-11-28 16:39:21 +0500

    Убрал свои пометки (комментарии) согласно "золотому правилу"

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -4,12 +4,10 @@ package com.walking.intensive.chapter2.task6;
  * Условие: <a href="https://geometry-math.ru/homework/Java-NOKNOD.html">ссылка</a>
  */
 public class Task6 {
-//    private static int countMSG;
 
     public static void main(String[] args) {
         int a = 84;
         int b = 90;
-
 
         System.out.println("Наибольший общий делитель: \n" + getNod(a, b));
         System.out.println("Рекурсия, алгоритм Евклида: \n" + getNodByEuclideanAlgorithm(a, b));
@@ -18,9 +16,6 @@ public class Task6 {
     }
 
     static int getNoc(int m, int n) {
-        /* НОК(a, b)=a⋅b: ( a , b ) = a · b : НОД(a, b)
-        НОК двух чисел - это наименьшее положительное число НОК = |a · b| : НОД(a, b)
-         */
 
         return Math.abs(m * n) / getNod(m, n);
     }
@@ -37,11 +32,9 @@ public class Task6 {
 
     static int getNodByEuclideanAlgorithm(int m, int n) {
 
-//        countMSG++;
         if (n == 0) {
             return m;
         } else {
-//            System.out.println("recurs " + countMSG + " ,b = " + n);  // self test
             return getNodByEuclideanAlgorithm(n, m % n);
         }
     }
