19505a2fbec4ccd869d894f7e8e6d99e6c1a476d
Sprite_Spirit <a.backer@yandex.com>
2023-11-28 16:31:43 +0300

    Chapter 1 Task 5 Удалила лишние блоки кода и форматирование. Дописала код, т.к. при переносе проекта часть отвалилась. Тест прогнала - сдан.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,30 +1,19 @@
 package com.walking.intensive.chapter1.task1;
 
-//import java.util.Random;
-
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 
 public class Task1 {
-    public static void main(String[] args) {
-       // Random random = new Random();
-       // int age = random.nextInt(128);
-
-       // System.out.println(getAgeString(age));
-    }
+    public static void main(String[] args) {}
 
     static String getAgeString(int age) {
-        String stringEnd;
-
         if (age % 10 == 1 && age != 11 && age != 111) {
-            stringEnd = "год";
-        } else if ((age % 10 > 1 && age % 10 < 5) && !(age >= 12 && age <= 15)) {
-            stringEnd = "года";
+            return "Вам " + age + " год";
+        } else if ((age % 10 > 1 && age % 10 < 5) && !(age >= 12 && age <= 15) && !(age % 100 == 12 || age % 100 == 13 || age % 100 == 14)) {
+            return "Вам " + age + " года";
         } else {
-            stringEnd = "лет";
+            return "Вам " + age + " лет";
         }
-
-        return String.format("Вам %d %s", age, stringEnd);
     }
 }
