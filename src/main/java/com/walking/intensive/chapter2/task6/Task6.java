ea6254b8fae8db98013f47bfd07e6c9af6f3cb74
sauza7 <sauza777@yandex.ru>
2023-11-28 16:08:48 +0300

    Chapter 2 Task 6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -6,21 +6,41 @@ package com.walking.intensive.chapter2.task6;
 public class Task6 {
     public static void main(String[] args) {
 
+        int m = 112;
+        int n = 14;
+
+        System.out.println("НОК чисел " + m + " и " + n + " равен: " + getNoc(m, n));
+        System.out.println("НОД чисел " + m + " и " + n + " равен: " + getNod(m, n));
+        System.out.println("НОД чисел " + m + " и " + n + " равен: " + getNodByEuclideanAlgorithm(m, n));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+
+        int noc = Math.max(m, n);
+
+        while (noc % m != 0 || noc % n != 0) {
+            noc++;
+        }
+
+        return noc;
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+
+        int nod = Math.min(m, n);
+
+        while (m % nod != 0 || n % nod != 0) {
+            nod--;
+        }
+
+        return nod;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+
+        if (Math.max(m, n) % Math.min(m, n) == 0) return Math.min(m, n);
+
+        return getNodByEuclideanAlgorithm(Math.abs(m - n), Math.min(m, n));
     }
 
 }
