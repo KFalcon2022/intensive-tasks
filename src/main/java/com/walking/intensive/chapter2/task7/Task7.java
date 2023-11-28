217dcce8d6b5e42aeb7c3f68f55b94749fa4c19a
k2336351 <k2336351@yandex.ru>
2023-11-29 01:49:18 +0300

    Solution Chapter 2  Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -6,10 +6,66 @@ package com.walking.intensive.chapter2.task7;
 public class Task7 {
     public static void main(String[] args) {
 
+        int m = 10000;
+
+        System.out.println("Friendly max: " + getFriendlyPair(m));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+
+        final int MAX_VALUE = 1_000_000;
+
+        m = Math.abs(m);
+
+        if (m >= MAX_VALUE) {
+            return 0;
+        }
+
+        int maxFriendlyNumbersSum = 0;
+        int maxFriendlyPair = 0;
+
+        for (int i = 1; i < m; i++) {
+
+            int iDivisorsSum = getDivisorsSum(i);
+
+            if (isNumbersFriendly(i, iDivisorsSum) && iDivisorsSum < m) {
+
+                int friendlyNumbersSum = i + iDivisorsSum;
+
+                if (friendlyNumbersSum > maxFriendlyNumbersSum) {
+
+                    maxFriendlyNumbersSum = friendlyNumbersSum;
+                    maxFriendlyPair = Math.max(i, iDivisorsSum);
+                }
+            }
+        }
+
+        return maxFriendlyPair;
+    }
+
+    static boolean isNumbersFriendly(int num1, int num2) {
+
+        if (num1 == num2) {
+            return false;
+        }
+
+        return num1 == getDivisorsSum(num2) && num2 == getDivisorsSum(num1);
+    }
+
+    static int getDivisorsSum(int number) {
+
+        number = Math.abs(number);
+
+        int divisorsSum = 0;
+
+        for (int i = 1; i < number; i++) {
+
+            if (number % i == 0) {
+
+                divisorsSum += i;
+            }
+        }
+
+        return divisorsSum;
     }
 }
