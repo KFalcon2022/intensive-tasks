a9a4eefecf94d5567a17c87960eac79cc88fbeea
RVishez <oliohcmpn@gmail.com>
2023-11-28 16:55:00 +0100

    Chapter 1 task 4 test добавлены фигурные скобки и пустые строки для читаемости

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -14,22 +14,30 @@ public class Task4 {
     }
 
     static String solveQuadraticEquation(double a, double b, double c) {
+
         double discriminant = (b * b - (4 * a * c));
         double x1, x2;
-        if (a == 0 && b == 0 && c == 0) return "Решений бесконечно";
+
+        if (a == 0 && b == 0 && c == 0) {
+            return "Решений бесконечно";
+        }
+
         if (a == 0 && b != 0) {
             x1 = (-c) / b;
             return "Количество решений: 1. Корень: " + (int) x1;
         }
+
         if (a != 0 && discriminant == 0) {
-            x1 = ((-b) / (2 * a));
+            x1 = (-b) / (2 * a);
             return "Количество решений: 1. Корень: " + (int) x1;
         }
+
         if (discriminant > 0) {
-            x1 = ((-b + Math.sqrt(discriminant)) / (2 * a));
-            x2 = ((-b - Math.sqrt(discriminant)) / (2 * a));
+            x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
             return "Количество решений: 2. Корни: " + (int) Math.min(x1, x2) + ";" + (int) Math.max(x1, x2);
         }
+
         return "Количество решений: 0.";
     }
 }
