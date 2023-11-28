0a1ae87c81808a4c4aab7e7ebe8bd8fda87c7908
RVishez <oliohcmpn@gmail.com>
2023-11-28 14:58:08 +0100

    Chapter 1 task 4 test final

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -6,28 +6,30 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
+        double a = 1;
         double b = 0;
-        double c = 0;
+        double c = 5;
 
         System.out.println(solveQuadraticEquation(a, b, c));
-
     }
 
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        double discriminant = (b * b - (4 * a * c));
+        double x1, x2;
+        if (a == 0 && b == 0 && c == 0) return "Решений бесконечно";
+        if (a == 0 && b != 0) {
+            x1 = (-c) / b;
+            return "Количество решений: 1. Корень: " + (int) x1;
+        }
+        if (a != 0 && discriminant == 0) {
+            x1 = ((-b) / (2 * a));
+            return "Количество решений: 1. Корень: " + (int) x1;
+        }
+        if (discriminant > 0) {
+            x1 = ((-b + Math.sqrt(discriminant)) / (2 * a));
+            x2 = ((-b - Math.sqrt(discriminant)) / (2 * a));
+            return "Количество решений: 2. Корни: " + (int) Math.min(x1, x2) + ";" + (int) Math.max(x1, x2);
+        }
+        return "Количество решений: 0.";
     }
 }
