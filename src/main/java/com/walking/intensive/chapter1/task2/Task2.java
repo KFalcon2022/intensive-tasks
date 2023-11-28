ad3c4e14e0920c41ac38b85f3fcdb306f5afb61b
Akarumai <yzavgoro124@gmail.com>
2023-11-28 17:30:30 +0300

    task2_version1

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,13 +5,27 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
+        int floorAmount = 10; //Кол-во этажей
+        int entranceAmount = 4; //Кол-во подъездов
+        int flatNumber = 155; //Номер квартиры
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        String flatLocation = "";
+        if (flatNumber <=0)
+            return "Ошибка ввода.";
+        if (flatNumber > 4 * floorAmount * entranceAmount)
+            return "Квартира номер " + flatNumber + " отсутсвует в этом доме.";
+        int entranceNumber = (flatNumber-1) / (floorAmount * 4) + 1; //Подъезд квартиры
+        int floorNumber = ((flatNumber - 1) / 4 + 1) - (floorAmount*(entranceNumber-1)); //Этаж квартиры
+        switch (flatNumber % 4) {
+            case 0 -> flatLocation = " справа от лифта и направо.";
+            case 1 -> flatLocation = " слева от лифта и налево.";
+            case 2 -> flatLocation = " слева от лифта и направо.";
+            case 3 -> flatLocation = " справа от лифта и налево.";
+        }
+        return "Квартира " + flatNumber + " расположена в подъезде " + entranceNumber + " на этаже "
+                + floorNumber + flatLocation;
     }
 }
