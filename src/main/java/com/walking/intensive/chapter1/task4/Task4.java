89353dea82e9016ecb054f8379bb8144c322c148
RVishez <oliohcmpn@gmail.com>
2023-11-28 14:59:48 +0100

    Chapter 1 task 4 test final без комменатриев

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         double a = 1;
         double b = 0;
         double c = 5;
