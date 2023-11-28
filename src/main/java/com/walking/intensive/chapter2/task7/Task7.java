ad688faa0c6ff2c2eb3863a122af703611a2318e
stogov_so <stogov_sergei@mail.ru>
2023-11-28 19:54:37 +0500

    Chapter2_task7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,39 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
+        int limit = 10000;
 
+        System.out.println(getFriendlyPair(limit));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int maxFriendlyNumber = 0;
+        int sumFriendlyPair = 0;
+
+        for (int i = m - 1; i > 0; i--) {
+            int friendlyNumber1 = getDivisors(i);
+            int friendlyNumber2 = getDivisors(friendlyNumber1);
+            int maxNumber = Math.max(friendlyNumber1, friendlyNumber2);
+            int sumNumbers = friendlyNumber1 + friendlyNumber2;
+
+            if (i == friendlyNumber2 && maxNumber < m && sumFriendlyPair < sumNumbers && friendlyNumber1 != friendlyNumber2) {
+                maxFriendlyNumber = maxNumber;
+                sumFriendlyPair = sumNumbers;
+            }
+        }
+
+        return maxFriendlyNumber;
+    }
+
+    static int getDivisors(int number) {
+        int result = 0;
+
+        for (int i = number - 1; i > 0; i--) {
+            if (number % i == 0) {
+                result += i;
+            }
+        }
+
+        return result;
     }
 }
