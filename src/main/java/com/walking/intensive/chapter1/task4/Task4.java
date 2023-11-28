2c908a8e602bc4216fcf5fe2073e009362518e39
Sprite_Spirit <a.backer@yandex.com>
2023-11-28 17:17:37 +0300

    Chapter 1 Task 4 Переименовала переменные (квадратные корни x1 и x2 в squareRoot1 и squareRoot2 соответственно). Переименовала D в discriminant. Убрала форматирование строк. Привела ответы к типу int. Комментарии удалила. Тест пройден. Благодарю.

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -5,13 +5,7 @@ package com.walking.intensive.chapter1.task4;
  */
 
 public class Task4 {
-    public static void main(String[] args) {
-//        double a = 1;
-//        double b = -3;
-//        double c = 2;
-//
-//        System.out.println(solveQuadraticEquation(a, b, c));
-    }
+    public static void main(String[] args) {}
 
     /**
      * При формировании строки, корни уравнения должны указываться по возрастанию.
@@ -26,43 +20,38 @@ public class Task4 {
      */
 
     static String solveQuadraticEquation(double a, double b, double c) {
-        // x1 - первый корень, x2 - второй корень
-        double x1;
-        double x2;
+        double squareRoot1;
+        double squareRoot2;
 
-        String solveString = "Количество решений:";
-
-        // Если a равно 0, то уравнение перестает быть квадратным
         if (a == 0) {
             if (b == 0) {
                 if (c == 0) {
-                    return String.format("%s Бесконечное кол-во решений", solveString);
+                    return "Решений бесконечно";
                 } else {
-                    return String.format("%s 0", solveString);
+                    return "Количество решений: 0.";
                 }
             } else {
-                x1 = - c / b;
-                return String.format("%s 1. Корень: %.1f", solveString, x1);
+                squareRoot1 = - c / b;
+                return "Количество решений: 1. Корень: " + (int) squareRoot1;
             }
         }
 
-        // Вычисляем дискриминант
-        double D = b * b - 4 * a * c;
+        double discriminant = b * b - 4 * a * c;
 
-        if (D > 0) {
-            x1 = (-b + Math.sqrt(D) / (2 * a));
-            x2 = (-b - Math.sqrt(D) / (2 * a));
-            if (x1 > x2) {
-                double temp = x1;
-                x1 = x2;
-                x2 = temp;
+        if (discriminant > 0) {
+            squareRoot1 = (-b + Math.sqrt(discriminant) / (2 * a));
+            squareRoot2 = (-b - Math.sqrt(discriminant) / (2 * a));
+            if (squareRoot1 > squareRoot2) {
+                double temp = squareRoot1;
+                squareRoot1 = squareRoot2;
+                squareRoot2 = temp;
             }
-            return String.format("%s 2. Корень: %.1f; %.1f", solveString, x1, x2);
-        } else if (D == 0) {
-            x1 = - b / (2 * a);
-            return String.format("%s 1. Корень: %.1f", solveString, x1);
+            return "Количество решений: 2. Корни: " + (int) squareRoot1 + ";" + (int) squareRoot2;
+        } else if (discriminant == 0) {
+            squareRoot1 = - b / (2 * a);
+            return "Количество решений: 1. Корень: " + (int) squareRoot1;
         } else {
-            return String.format("%s 0.", solveString);
+            return "Количество решений: 0.";
         }
     }
 }
