07ce0e48155dbd36eacfefe013c49cb77ab07f3c
mirchello <mrumilov@gmail.com>
2023-11-29 00:50:37 +0300

    часть 2 задача 9 первое решение

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -1,15 +1,40 @@
 package com.walking.intensive.chapter2.task9;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-pascalTriangle.html">ссылка</a>
  */
 public class Task9 {
     public static void main(String[] args) {
-
+        getPascalTriangle(30);
     }
 
     static String getPascalTriangle(int n){
-        // Ваш код
+        Integer[][] pascalArray = new Integer[n][n];
+        String[] pascalArrayString = new String[n];
+        pascalArray[0][0] = 1;
+        pascalArrayString[0] = pascalArray[0][0].toString();
+        for (int i=1; i<n; i++){
+            for (int j=0; j<=i; j++){
+                if (j==0 || j == i){
+                    pascalArray[i][j] = 1;
+                } else {
+                    pascalArray[i][j] = pascalArray[i-1][j-1]+pascalArray[i-1][j];
+                }
+            }
+            pascalArrayString[i] = Arrays.stream(pascalArray[i])
+                                         .filter(t -> t != null)
+                                         .map(Object::toString)
+                                         .collect(Collectors.joining(" "));
+        }
+        for (int i = 0; i<n; i++){
+            int N = (pascalArrayString[n-1].length() - pascalArrayString[i].length())/2;
+            pascalArrayString[i] = String.join("", Collections.nCopies(N, " ")) + pascalArrayString[i];
+            System.out.println(pascalArrayString[i]);
+        }
         return null;
     }
 }
