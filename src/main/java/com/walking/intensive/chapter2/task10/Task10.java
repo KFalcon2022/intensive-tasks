098cd415d72118697d0fbe43e4bb224bc544fce2
ArtemiyDS <27071999@mail.ru>
2023-11-28 22:57:55 +0300

    Chapter 2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,21 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
+        String str = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
 
+        System.out.println(isPalindrome(str));
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    static boolean isPalindrome(String inputString){
+        // \\pP - регулярное выражение, благодаря которому можно удалить всю пунктуацию
+        String resultString = inputString.toLowerCase().replaceAll(" ", "").replaceAll("\\pP", "");
+
+        int length = resultString.length();
+        for (int i = 0; i < length / 2; i++) {      // Не стал использовать метод reverse() класса StringBuilder :D
+            if(resultString.charAt(i) != resultString.charAt(length - i - 1))
+                return false;
+        }
+
+        return true;
     }
 }
