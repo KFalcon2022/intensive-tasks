0cfa2954bfb4ed126086fff66a41939c54dbd862
Boris <aksenov.boris8595@gmeil.com>
2023-11-29 04:08:19 +0500

    Писать даже не знаю что. Найти формулы в гугле да вписать их. Правда над рекурсией поломать пришлось немного голову, не сразу вышло. Если надо, то можно переписать через циклы просто перебором.

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,22 +5,31 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-
+        System.out.println(getNoc(10, 25));
+        System.out.println(getNodByEuclideanAlgorithm(18, 15));
+        System.out.println(getNod(25, 15));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        return Math.abs(m * n) / getNodByEuclideanAlgorithm(m, n);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        while (m != 0 && n != 0) {
+            if (m > n) {
+                m = m % n;
+            } else {
+                n = n % m;
+            }
+        }
+        return m + n;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        if (n != 0 && m != 0) {
+            return m > n ? getNodByEuclideanAlgorithm(m % n, n) : getNodByEuclideanAlgorithm(m, n % m);
+        }
+        return n + m;
     }
 
 }
