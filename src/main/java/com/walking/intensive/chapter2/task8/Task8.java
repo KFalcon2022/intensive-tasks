98e0ddd7df5ad681ca15aef45cb34e8cd30858cf
Pavlo <pavlokutsyn@gmail.com>
2023-11-28 19:37:09 +0100

    Chapter2 Task8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,34 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println(getHappyTicketChance());
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance() {
+        int totalTickets = 1000000;
+        int happyTickets = 0;
+
+        for (int i = 0; i < totalTickets; i++) {
+            if (isTicketHappy(i)) {
+                happyTickets++;
+            }
+        }
+
+        return (double) happyTickets / totalTickets;
+    }
+
+    static boolean isTicketHappy(int ticketNumber) {
+        int firstPart = ticketNumber / 1000;
+        int secondPart = ticketNumber % 1000;
+        return sumOfDigits(firstPart) == sumOfDigits(secondPart);
+    }
+
+    static int sumOfDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
     }
 }
