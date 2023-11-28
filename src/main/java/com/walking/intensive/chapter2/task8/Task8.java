cdb57fe002e6c7f7a98dd984324f9e9680ce9200
Константин Синяков <k.siniakov@yandex.ru>
2023-11-28 23:14:43 +0300

    Chapter 2 Task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,27 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println(getHappyTicketChance());
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance() {
+        int a, b, c, d, e, f;
+        int count = 0;
+        final int limit = 999999;
+
+        for (int i = 0; i <= limit; i++) {
+            a = i / 100000;
+            b = (i / 10000) % 10;
+            c = (i / 1000) % 10;
+            d = (i / 100) % 10;
+            e = (i / 10) % 10;
+            f = i % 10;
+
+            if (a + b + c == d + e + f) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return ((double) count) / limit;
     }
 }
