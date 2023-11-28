5451092beb1619d517de232ed78110c5fb5b2d75
tvadim1 <tvadim1@yandex.ru>
2023-11-28 17:58:01 +0600

    Initial commit

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,35 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println(getHappyTicketChance(999999));
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance(int ticketNumber) {
+        int collisionNumber = 0;
+
+        for (int i = 0; i <= 999; i++) {
+            for (int j = 0; j <= 999; j++) {
+
+                if ((i == j) || (getSum(i) == getSum(j))) {
+                    System.out.println(i + "\t" + j);
+                    collisionNumber++;
+                }
+            }
+        }
+
+        return (double) collisionNumber / 1000000;
+    }
+
+    private static int getSum(int number) {
+        int sum = 0;
+        int currentDigit;
+
+        while (number > 0) {
+            currentDigit = number % 10;
+            sum += currentDigit;
+            number /= 10;
+        }
+
+        return sum;
     }
 }
