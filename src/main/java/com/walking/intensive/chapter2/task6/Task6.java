2aefea14c8318a54c062f78e1fe858cc0a59f814
Greem4 <i@greem4.ru>
2023-11-28 11:11:30 +0300

    chapter 2, task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,22 +5,28 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-
+        System.out.println(getNoc(4, 5));
+        System.out.println(getNod(24, 36));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        return (m * n) / getNodByEuclideanAlgorithm(m, n);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        while (n != 0) {
+            int temp = n;
+            n = m % n;
+            m = temp;
+        }
+        return m;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        if (n == 0) {
+            return m;
+        }
+        return getNodByEuclideanAlgorithm(n, m % n);
     }
 
 }
