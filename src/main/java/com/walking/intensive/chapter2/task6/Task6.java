6590605d3c3d77b52359d678d06ecb67c620e61b
Greem4 <i@greem4.ru>
2023-11-28 11:07:05 +0300

    chapter 2, task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,41 +5,28 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-        System.out.println(findGCD(4,5));
-        System.out.println(findLCM(4,5));
-    }
-
-    public static int findLCM(int num1, int num2) {
-        int maxNum = Math.max(num1, num2);
-        int lcm = maxNum;
-
-        while (lcm % num1 != 0 || lcm % num2 != 0) {
-            lcm += maxNum;
-        }
-
-        return lcm;
-    }
-
-    public static int findGCD(int num1, int num2) {
-        if (num2 == 0) {
-            return num1;
-        }
-        return findGCD(num2, num1 % num2);
+        System.out.println(getNoc(4, 5));
+        System.out.println(getNod(24, 36));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        return (m * n) / getNodByEuclideanAlgorithm(m, n);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+       while (n != 0) {
+           int temp = n;
+           n = m % n;
+           m = temp;
+        }
+        return m;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        if (n == 0) {
+            return m;
+        }
+        return getNodByEuclideanAlgorithm(n, m % n);
     }
 
 }
