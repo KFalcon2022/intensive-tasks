e0efe6180410a67d3eb05c9d1125c803bbcb386d
SenyaDiamond <SenyaDiamond@gmail.com>
2023-11-28 10:10:49 +0300

    Chapter 2 Task 8 v. 1.0

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -9,7 +9,21 @@ public class Task8 {
     }
 
     static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+        int positiveresults = 0;
+        for (int i = 0; i <= 999_999; i++)
+        {
+            if (isHappyTicket(i)) {
+                positiveresults++;
+            }
+        }
+        return positiveresults/999_999d;
+    }
+
+    static boolean isHappyTicket (int ticketNumber) {
+        String stringTicketNumber = Integer.toString(ticketNumber);
+        while (stringTicketNumber.length() < 6) {
+            stringTicketNumber = "0" + stringTicketNumber;
+        }
+        return  (stringTicketNumber.charAt(0) + stringTicketNumber.charAt(1) + stringTicketNumber.charAt(2) == stringTicketNumber.charAt(3) + stringTicketNumber.charAt(4) + stringTicketNumber.charAt(5));
     }
 }
