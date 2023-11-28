1fddd5756a4928d17e0a54d5ff29c654eeecab86
LVyshlov <Leonvy@mail.ru>
2023-11-28 18:19:27 +0300

    Chapter1 task3 fixed

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -7,17 +7,12 @@ public class Task3 {
     public static void main(String[] args) {
         //Для собственных проверок можете делать любые изменения в этом методе
         int year = 2023;
-        isLeap(year);
+        System.out.println(isLeap(year));
 
     }
 
     static boolean isLeap(int year) {
-        if (year % 4 == 0 && year % 100 !=0 || year % 400 == 0) {
-            System.out.println(year+" Високосный");
-            return true;
-        }
-        System.out.println(year+" не високосный");
+        return year % 4 == 0 && year % 100 !=0 || year % 400 == 0;
 
-        return false;// Заглушка. При реализации - удалить
     }
 }
