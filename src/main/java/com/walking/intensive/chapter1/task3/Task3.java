c2600f65e635a1db89271f9270c1c8b30b815c31
LVyshlov <Leonvy@mail.ru>
2023-11-28 17:01:53 +0300

    Chapter1 task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -6,12 +6,18 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
         //Для собственных проверок можете делать любые изменения в этом методе
+        int year = 2023;
+        isLeap(year);
 
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
+        if (year % 4 == 0 && year % 100 !=0 || year % 400 == 0) {
+            System.out.println(year+" Високосный");
+            return true;
+        }
+        System.out.println(year+" не високосный");
 
-        return false; // Заглушка. При реализации - удалить
+        return false;// Заглушка. При реализации - удалить
     }
 }
