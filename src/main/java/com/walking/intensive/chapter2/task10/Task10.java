777d64c4d8b483ea505a28eb287c9ad505c766dd
Greem4 <i@greem4.ru>
2023-11-29 13:05:44 +0300

    chapter 2, task10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,20 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        Task10 task10 = new Task10();
+        System.out.println(task10.isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум."));
+        // В условии не было static, по этому я не стал дописывать static, а сделал вызов метода через объект.
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    boolean isPalindrome(String inputString) {
+        String cleanStr = inputString.replaceAll("[^а-яА-Я]", "")
+                .toLowerCase();
+
+        for (int i = 0; i < cleanStr.length() / 2; i++) {
+            if (cleanStr.charAt(i) != cleanStr.charAt(cleanStr.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
