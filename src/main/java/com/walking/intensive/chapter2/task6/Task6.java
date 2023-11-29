8c7f011b299a86e760d5923d1a75f180bdafcd6a
KrisLu23 <krisbondarev@yandex.ru>
2023-11-29 10:32:33 +0300

    Chapter 2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,37 +5,17 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-        System.out.println(getNoc(12, 8));
-        System.out.println(getNod(56,48));
+
     }
 
     static int getNoc(int m, int n) {
-        m = Math.abs(m);
-        n = Math.abs(n);
-        int max = Math.max(m, n);
-        int min = Math.min(m, n);
-            int temp = max;
-            while (!(max % min == 0)) {
-                max += temp;
-            }
-            return max;
-        }
-
+        // Ваш код
+        return 0;
+    }
 
     static int getNod(int m, int n) {
-        int nod = 0;
-        m = Math.abs(m);
-        n = Math.abs(n);
-        int max = Math.max(m,n);
-        int min = Math.min(m,n);
-        if (max % min == 0){
-            return nod = min;
-        }
-        for (int i = min; i > 0; i--) {
-            if (max % i == 0 && min % i ==0){
-                return nod = i;
-            }
-        } return nod;
+        // Ваш код
+        return 0;
     }
 
     static int getNodByEuclideanAlgorithm(int m, int n){
