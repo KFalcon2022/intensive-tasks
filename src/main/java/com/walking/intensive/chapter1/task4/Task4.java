d1ad45345cf00f3e7ee19cc8bef9e9b28474cb30
rovbo85@gmail.com <rovbo85@gmail.com>
2023-11-29 13:09:41 +0500

    Внесены изменения в соответствии с замечаниями рецензента, а именно: - Удалил переменную answer и лишнюю инициализацию переменных firstRoot и secondRoot; - Метод String.format заменил на конвертацию через (int); - Способы возврата ответов переписал; - Избавился от лишних вложений;

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -6,42 +6,32 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
 
   public static void main(String[] args) {
-    double a = 1;
-    double b = 0;
-    double c = 0;
+    double a = 2;
+    double b = 3;
+    double c = -5;
     System.out.println(solveQuadraticEquation(a, b, c));
   }
 
   static String solveQuadraticEquation(double a, double b, double c) {
-    String answer;
-    double firstRoot;
-    double secondRoot;
     if (a == 0 && b != 0 && c != 0) {
-      answer = "Количество решений: 1. Корень: " + String.format("%.0f", (-c / b));
+      return "Количество решений: 1. Корень: " + (int) (-c / b);
     } else if (a == 0 && b == 0 && c == 0) {
-      answer = "Решений бесконечно";
-    } else if (a == 0 && b == 0 && c == 5) {
-      answer = ("Количество решений: 0.");
-
-    } else {
-      double discriminant = Math.pow(b, 2) - 4 * a * c;
-      if (discriminant > 0) {
-        firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
-        secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
-        if (firstRoot < secondRoot) {
-          answer = "Количество решений: 2. Корни: " + String.format("%.0f", firstRoot) + ";"
-              + String.format("%.0f", secondRoot);
-        } else {
-          answer = "Количество решений: 2. Корни: " + String.format("%.0f", secondRoot) + ";"
-              + String.format("%.0f", firstRoot);
-        }
-      } else if (discriminant == 0) {
-        firstRoot = b / (2 * a);
-        answer = "Количество решений: 1. Корень: " + String.format("%.0f", firstRoot);
-      } else {
-        answer = ("Количество решений: 0.");
-      }
+      return "Количество решений бесконечно.";
+    } else if (a == 0 && b == 0) {
+      return "Количество решений: 0.";
     }
-    return answer;
+    double discriminant = Math.pow(b, 2) - 4 * a * c;
+    if (discriminant > 0) {
+      double firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+      double secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+      return firstRoot < secondRoot ? "Количество решений: 2. Корни: " + (int) firstRoot + ";"
+          + (int) secondRoot
+          : "Количество решений: 2. Корни: " + (int) secondRoot + ";" + (int) firstRoot;
+    }
+    if (discriminant == 0) {
+      return "Количество решений: 1. Корень: " + (int) (b / (2 * a));
+    } else {
+      return ("Количество решений: 0.");
+    }
   }
 }
