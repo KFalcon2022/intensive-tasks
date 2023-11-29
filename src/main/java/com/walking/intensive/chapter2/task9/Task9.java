c371bb2fd0f2401d1cc14ac15ce37a6153bc77af
njmist <njmist@gmail.com>
2023-11-29 23:01:17 +0300

    chapter 2 task 9

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,11 +5,32 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
+        int n = 50;
 
+        getPascalTriangle(n);
     }
 
-    static String getPascalTriangle(int n){
-        // Ваш код
+    static String getPascalTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            int lastStringLength = getTriangleString(n).length();
+            int currentStringLength = getTriangleString(i).length();
+
+            for (int spaces = 0; spaces < (lastStringLength - currentStringLength) / 2 + 1; spaces++) {
+                System.out.print(" ");
+            }
+            System.out.println(getTriangleString(i));
+        }
         return null;
+    }
+
+    static String getTriangleString(int n) {
+        String triangleString = " ";
+        long number = 1;
+
+        for (int i = 0; i < n; i++) {
+            triangleString = triangleString + number + " ";
+            number = number * (n - 1 - i) / (i + 1);
+        }
+        return triangleString;
     }
 }
