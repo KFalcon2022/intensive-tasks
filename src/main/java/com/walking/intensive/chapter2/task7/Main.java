7cb9153ff06a4d050305df6aa2a41b47445f90b7
apjam <apjam@yandex.ru>
2023-11-29 23:19:58 +0600

    task7 is done

--- src/main/java/com/walking/intensive/chapter2/task7/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Main.java
@@ -1,7 +1,48 @@
 package com.walking.intensive.chapter2.task7;
 
 public class Main {
-    public static void main(String[] args) {
+    static final int N = 1000000;
 
+    public static void main(String[] args) {
+        System.out.println(getFriends(99999));
+    }
+
+    static int getFriends(int n) {
+        if (n >= N) {
+            return 0;
+        }
+        int bSum, aSum, b;
+        for (int a = n; a > 0; --a) {
+            aSum = getSum(a);
+            b = aSum;
+
+            if (aSum >= N || a == b) {
+                continue;
+            }
+
+            bSum = getSum(b);
+
+            if (a == bSum) {
+                return Math.max(a, b);
+            }
+        }
+        return 0;
+    }
+
+    static int getSum(int x) {
+        // уменьшить количество итераций
+        int cut = x % 2 == 0 ? x / 2 : x / 3;
+        int sum = 0;
+
+        for (int i = cut; i > 0; --i) {
+            if (x % i == 0) {
+                sum += i;
+            }
+            if (sum >= N) {
+                break;
+            }
+        }
+
+        return sum;
     }
 }
