e451fef2d0f114d20828001257f9149810520968
SenyaDiamond <SenyaDiamond@gmail.com>
2023-11-29 16:38:34 +0300

    Chapter 2 Task 9 v 1.0

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -6,10 +6,35 @@ package com.walking.intensive.chapter2.task9;
 public class Task9 {
     public static void main(String[] args) {
 
+
     }
 
-    static String getPascalTriangle(int n){
-        // Ваш код
-        return null;
+    static String getPascalTriangle(int n) {
+        String result = "";
+        String baseOfTriangle = "";
+        for (int j = 0; j <= n; j++) {
+            baseOfTriangle += (getFactorial(n)) / (getFactorial(j) * getFactorial(n - j)) + " ";
+        }
+
+        for (int i = 0; i < n; i++) {
+            String currentLine = "";
+            for (int j = 0; j <= i; j++) {
+                currentLine += (getFactorial(i)) / (getFactorial(j) * getFactorial(i - j)) + " ";
+            }
+            for (int s = 0; s < ((baseOfTriangle.length() - currentLine.length()) / 2); s++) {
+                result += " ";
+            }
+            result += currentLine + "\n";
+        }
+        result += baseOfTriangle;
+        return result;
+    }
+
+    static long getFactorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
