331c1c967f0e955b794bacbe7a94c15b460a517c
Boris <aksenov.boris8595@gmeil.com>
2023-11-29 17:31:33 +0500

    Передавать ticketNumder нет смысла. Возможно в задаче стоило добавить "Найти вероятность получения счастливого билета в промежутке от 0 до n", где n - ticketNumber. Или стоило передавать количество цифр в билете, и тогда можно было бы найти шанс для 4, 6, 8 значных биетов(и в таком случае мой код переписывать пришлось бы).

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,23 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println(getHappyTicketChance(120000));
     }
 
     static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+        int numberLuckyTickets = 0;
+        int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+        for (int i = 0; i < 999999; i++) {
+            a = i / 100000;
+            b = i % 100000 / 10000;
+            c = i % 10000 / 1000;
+            d = i % 1000 / 100;
+            e = i % 100 / 10;
+            f = i % 10;
+            if (a+d+c == d+e+f) {
+                numberLuckyTickets++;
+            }
+        }
+        return numberLuckyTickets != 0 ? (double) numberLuckyTickets / 999999 : 0;
     }
 }
