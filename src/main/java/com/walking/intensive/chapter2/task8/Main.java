9e6de7935528d1d700637891ca95e3c40552cb6b
lambo064 <tasha_lambo@mail.ru>
2023-11-29 13:27:42 +0300

    chapter 2 task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Main.java
@@ -6,30 +6,35 @@ public class Main {
     }
 
     public static double getProbability() {
-        double luckyWariants = 0;
+        double luckyWariants = 1;
         double allWariants = 1;
+
         for (int i = 0; i < 1000000; i++) {
+
             if (isTicketLucky(i)) {
                 luckyWariants += 1;
             }
+
             allWariants += 1;
         }
-        return luckyWariants / allWariants * 100;
+
+        return luckyWariants / allWariants ;
     }
 
     public static boolean isTicketLucky(int ticket) {
-        int firstsum = 0;
-        int secondsum = 0;
+        int firstSum = 0;
+        int secondSum = 0;
 
         for (int i = 0; i < 3; i++) {
-            firstsum = firstsum + ticket % 10;
+            firstSum = firstSum + ticket % 10;
             ticket = ticket / 10;
         }
 
         for (int i = 0; i < 3; i++) {
-            secondsum = secondsum + ticket % 10;
+            secondSum += ticket % 10;
             ticket = ticket / 10;
         }
-        return firstsum == secondsum;
+
+        return firstSum == secondSum;
     }
 }
