a39566911eeb41042040f037ca01b6bd5fe7e8a5
Oleg Klimchuk <koi196621@gmail.com>
2023-11-29 15:50:08 +0200

    Task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -4,31 +4,26 @@ package com.walking.intensive.chapter2.task6;
  * Условие: <a href="https://geometry-math.ru/homework/Java-NOKNOD.html">ссылка</a>
  */
 public class Task6 {
-    public static void main(String[] args) {
 
-        System.out.println(getNod(25,15));
-    }
-
-    static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
-    }
-
-    static int getNod(int m, int n) {
-
-            while (m != n) {
-                if (m > n) {
-                    m = m - n;
-                } else {
-                    n = n - m;
-                }
-            }
-            return m;
+public static void main(String[] args) {
+    int n = 25;
+    int m = 15;
+    System.out.println("НОК чисел " + n + " и " + m + " равен " + getNoc(n, m));
+    System.out.println("НОД чисел " + n + " и " + m + " равен " + getNod(n, m));
+}
+    //    тод для нахождения НОД двух чисел
+    public static int getNod(int n, int m) {
+        if (m == 0) {
+            return n;
         }
-
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+        return getNod(m, n % m);
     }
+
+    // Метод для нахождения НОК двух чисел
+    public static int getNoc(int n, int m) {
+        int gcd = getNod(n, m);
+        return (n * m) / gcd;
+    }
+
 
 }
