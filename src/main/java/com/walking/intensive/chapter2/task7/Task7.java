81f728122fbd1291b6fa8adddf6935b878fcec11
Vbliznev <7492008@mail.ru>
2023-11-29 17:44:37 +0300

    New Pull Request

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,32 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        getFriendlyPair(100000);
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
+    static int getFriendlyPair(int m) {
+        int number1 = 0;
+        int number2 = 0;
+        int summNumbers = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    number1 = number1 + j;
+                }
+            }
+            for (int j = 1; j < number1; j++) {
+                if (number1 % j == 0) {
+                    number2 = number2 + j;
+                }
+            }
+            if (i == number2 && i != number1) {
+                System.out.println("Число " + i + " число " + number1 + " - дружественные");
+                summNumbers = summNumbers + 1;
+            }
+            number2 = 0;
+            number1 = 0;
+        }
+        System.out.println("Всего таких чисел - " + summNumbers);
         return 0;
     }
 }
