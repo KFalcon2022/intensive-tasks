2dffe3a3c3b5898ca5032b1277550d38cf39f7e6
k2336351 <k2336351@yandex.ru>
2023-11-30 00:36:25 +0300

    Solution Chapter 2  Task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -6,10 +6,33 @@ package com.walking.intensive.chapter2.task8;
 public class Task8 {
     public static void main(String[] args) {
 
+        System.out.printf("\nThe odds to get a happy ticket are %.2f",getHappyTicketChance(999_999));
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance(int ticketNumber) {
+
+        int happyTicketAmount = 0;
+
+        for (int i = 0; i <= ticketNumber; i++) {
+
+            if (isHappyTicket(i)) {
+
+                happyTicketAmount++;
+            }
+        }
+
+        return (double) happyTicketAmount / 1_000_000;
+    }
+
+    static boolean isHappyTicket(int ticketNumber) {
+
+        int firstDigit = ticketNumber / 100_000;
+        int secondDigit = (ticketNumber % 100_000) / 10_000;
+        int thirdDigit = (ticketNumber % 10_000) / 1_000;
+        int fourthDigit = (ticketNumber % 1_000) / 100;
+        int fifthDigit = (ticketNumber % 100) / 10;
+        int sixthDigit = (ticketNumber % 10);
+
+        return firstDigit + secondDigit + thirdDigit == fourthDigit + fifthDigit + sixthDigit;
     }
 }
