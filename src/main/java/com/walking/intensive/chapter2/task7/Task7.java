2c30eac11a7679d47d06a4592b24bb53133990c7
k2336351 <k2336351@yandex.ru>
2023-11-29 22:39:32 +0300

    Solution Chapter 2  Task 7 (для ускорения работы программы изменён код в методе getFriendlyPair())

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -6,10 +6,65 @@ package com.walking.intensive.chapter2.task7;
 public class Task7 {
     public static void main(String[] args) {
 
+        int m = 99_999;
+        int maxFriendlyPair = getFriendlyPair(m);
+        int minFriendlyPair = getDivisorsSum(maxFriendlyPair);
+
+        System.out.println("Friendly max: " + maxFriendlyPair +
+                         "\nFriendly min: " + minFriendlyPair);
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+
+        final int MAX_VALUE = 1_000_000;
+
+        m = Math.abs(m);
+
+        if (m >= MAX_VALUE) {
+            return 0;
+        }
+
+        /*
+         * Выполнение кода в предыдущем коммите для максимального значения занимало около 4,5 часов(!)
+         * Выполнение нового кода для максимального значения занимает около 2 минут
+        */
+
+        int maxFriendlyPair = m - 1;
+        int minFriendlyPair = getDivisorsSum(maxFriendlyPair);
+
+        while (!isNumbersFriendly(maxFriendlyPair, minFriendlyPair) && maxFriendlyPair > 0 ||
+                minFriendlyPair > m) {
+
+            maxFriendlyPair--;
+            minFriendlyPair = getDivisorsSum(maxFriendlyPair);
+        }
+
+        return maxFriendlyPair;
+    }
+
+    static boolean isNumbersFriendly(int num1, int num2) {
+
+        if (num1 == num2) {
+            return false;
+        }
+
+        return num1 == getDivisorsSum(num2) && num2 == getDivisorsSum(num1);
+    }
+
+    static int getDivisorsSum(int number) {
+
+        number = Math.abs(number);
+
+        int divisorsSum = 0;
+
+        for (int i = 1; i < number; i++) {
+
+            if (number % i == 0) {
+
+                divisorsSum += i;
+            }
+        }
+
+        return divisorsSum;
     }
 }
