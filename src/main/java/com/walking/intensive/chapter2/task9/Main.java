dff428433ab20fcbb5d446b7acfb3069947fbc1c
user <tasiqq@gmail.com>
2023-11-29 20:25:07 +0300

    Chapter 2, Task 9; - переделан способ выравнивания треугольника.

--- src/main/java/com/walking/intensive/chapter2/task9/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Main.java
@@ -1,6 +1,6 @@
-/*Напишите метод, в который вводится натуральное число N, а метод возвращает треугольник Паскаля.
-Число N - степень или количество этажей в треугольнике.
-P.S. Треугольник должен быть выровнен по центру.
+/*пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ, пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ N, пїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
+пїЅпїЅпїЅпїЅпїЅ N - пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
+P.S. пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.
  */
 
 package com.walking.intensive.chapter2.task9;
@@ -8,26 +8,39 @@ package com.walking.intensive.chapter2.task9;
 public class Main {
     public static void main(String[] args) {
 
-        drawPascalTriangle(10);
+        drawPascalTriangle(20);
     }
+
     public static void drawPascalTriangle(int n) {
+
+        StringBuilder maxString = new StringBuilder();
         for (int i = 0; i < n; i++) {
 
+            maxString.append(getFactorial(n - 1) / (getFactorial(i) * getFactorial(n - 1 - i)));
+            if (i != n - 1) {
+                maxString.append(" ");
+            }
+        }
+        int maxLength = maxString.length();
+        System.out.println("РџРѕСЃР»РµРґРЅСЏСЏ СЃС‚СЂРѕРєР° " + maxString);
+        System.out.println("Р”Р»РёРЅР° РїРѕСЃР»РµРґРЅРµР№ СЃС‚СЂРѕРєРё " + maxLength);
 
-            StringBuilder string = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+
+            StringBuilder triangleLine = new StringBuilder();
 
             for (int j = 0; j <= i; j++) {
 
-                string.append(getFactorial(i) / (getFactorial(j) * getFactorial(i - j)));
-                if (j != i){
-                    string.append("       ");
+                triangleLine.append(getFactorial(i) / (getFactorial(j) * getFactorial(i - j)));
+                if (j != i) {
+                    triangleLine.append(" ");
                 }
             }
-            for (int j=0;j<(n-i)+1; j++){
-                System.out.print("    ");
-            }
 
-            System.out.println(string);
+            for (int k = 0; k < (maxLength - triangleLine.length()) / 2; k++) {
+                System.out.print(" ");
+            }
+            System.out.println(triangleLine);
         }
     }
 
