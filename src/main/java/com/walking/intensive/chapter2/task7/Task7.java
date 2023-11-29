43394162d5aa31e0c99b47dec08e082143a45706
Greem4 <i@greem4.ru>
2023-11-29 11:09:13 +0300

    chapter 2, task7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,39 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        System.out.println(getFriendlyPair(520));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int maxFriendlySum = 0;
+        int maxFriendlyNum = 0;
+
+        for (int i = 2; i < m; i++) {
+            int sum1 = getDivisorsSum(i);
+            int sum2 = getDivisorsSum(sum1);
+
+            if (i == sum2 && i != sum1 && sum1 < m) {
+                if (sum1 > maxFriendlySum) {
+                    maxFriendlySum = sum1;
+                    maxFriendlyNum = i;
+                }
+            }
+        }
+        return maxFriendlyNum;
     }
+
+    static int getDivisorsSum(int n) {
+        int sum = 1;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i * i != n) {
+                    sum += n / i;
+                }
+            }
+        }
+        return sum;
+    }
+
+
 }
