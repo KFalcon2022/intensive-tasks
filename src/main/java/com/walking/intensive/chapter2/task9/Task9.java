3ddfd1346a3fc1ccdf0c125d8418fc348160d18a
mirchello <mrumilov@gmail.com>
2023-11-29 10:39:52 +0300

    часть 2 задача 9 переформатирование и легкий рефакторинг

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -12,27 +12,28 @@ public class Task9 {
         getPascalTriangle(30);
     }
 
-    static String getPascalTriangle(int n){
+    static String getPascalTriangle(int n) {
         Integer[][] pascalArray = new Integer[n][n];
         String[] pascalArrayString = new String[n];
         pascalArray[0][0] = 1;
         pascalArrayString[0] = pascalArray[0][0].toString();
-        for (int i=1; i<n; i++){
-            for (int j=0; j<=i; j++){
-                if (j==0 || j == i){
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
                     pascalArray[i][j] = 1;
                 } else {
-                    pascalArray[i][j] = pascalArray[i-1][j-1]+pascalArray[i-1][j];
+                    pascalArray[i][j] = pascalArray[i - 1][j - 1] + pascalArray[i - 1][j];
                 }
             }
             pascalArrayString[i] = Arrays.stream(pascalArray[i])
-                                         .filter(t -> t != null)
-                                         .map(Object::toString)
-                                         .collect(Collectors.joining(" "));
+                    .filter(t -> t != null)
+                    .map(Object::toString)
+                    .collect(Collectors.joining(" "));
         }
-        for (int i = 0; i<n; i++){
-            int N = (pascalArrayString[n-1].length() - pascalArrayString[i].length())/2;
-            pascalArrayString[i] = String.join("", Collections.nCopies(N, " ")) + pascalArrayString[i];
+        for (int i = 0; i < n; i++) {
+            String whitespaceString = " ";
+            int whitespaceCount = (pascalArrayString[n - 1].length() - pascalArrayString[i].length()) / 2;
+            pascalArrayString[i] = String.join("", Collections.nCopies(whitespaceCount, whitespaceString)) + pascalArrayString[i];
             System.out.println(pascalArrayString[i]);
         }
         return null;
