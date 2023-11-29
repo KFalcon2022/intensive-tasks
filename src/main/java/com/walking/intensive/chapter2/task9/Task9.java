7a717f134ede3ef9a92bf55678a659bf8f77c61e
вашÐSahaPWNZ <007saha@inbox.ru>
2023-11-29 17:03:54 +0300

    Chapter 2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,11 +5,37 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
-
+        getPascalTriangle(5);
     }
 
-    static String getPascalTriangle(int n){
-        // Ваш код
+    static String getPascalTriangle(int N) {
+        int CnK = 1;
+        String maxStr = "";
+        maxStr += 1 + " ";
+        for (int k = 1; k < N - 1; k++) {
+            CnK *= ((N - 1) - k + 1);
+            CnK /= k;
+            maxStr += CnK + " ";
+        }
+        maxStr += 1;
+        System.out.println(maxStr.length());
+        for (int n = 0; n < N; n++) {
+            int maxCnK = 0;
+            String strN = "";
+            CnK = 1;
+            strN += CnK + " ";
+            for (int k = 1; k <= n; k++) {
+                CnK *= (n - k + 1);
+                CnK /= k;
+                strN += CnK + " ";
+                if (CnK > maxCnK){
+                    maxCnK = CnK;
+                }
+            }
+
+                System.out.println(" ".repeat(maxStr.length() / 3 -n) + strN + " ".repeat(maxStr.length() / 3-n));
+
+        }
         return null;
     }
 }
