ddb1f81f43b21362e1205a63e99afdc02f663f43
SkofieldAE <alexandrevd2107@gmail.com>
2023-11-30 00:04:01 +0300

    update chapter 2 task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -14,16 +14,27 @@ public class Task10 {
 
         System.out.println(isPalindrome(inputString));
     }
+
     static boolean isPalindrome(String inputString) {
         int leftOnRight = 0, rightOnLeft = inputString.length() - 1;
 
-        while(leftOnRight < rightOnLeft){
-            if(inputString.charAt(leftOnRight) != inputString.charAt(rightOnLeft)){
-                return false;
+        while (leftOnRight < rightOnLeft) {
+            char left = Character.toLowerCase(inputString.charAt(leftOnRight));
+            char right = Character.toLowerCase(inputString.charAt(rightOnLeft));
+
+            if (Character.isLetter(left) && Character.isLetter(right)) {
+                if (left != right) {
+                    return false;
+                }
+                leftOnRight++;
+                rightOnLeft--;
+            } else if (!Character.isLetter(left)) {
+                leftOnRight++;
+            } else if (!Character.isLetter(right)) {
+                rightOnLeft--;
             }
-            leftOnRight++;
-            rightOnLeft--;
         }
         return true;
     }
 }
+
