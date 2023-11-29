1421d0b2a48e23e44212c9d21179b9203bdc5357
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-11-29 21:57:55 +0300

    task10 solution

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -4,15 +4,22 @@ package com.walking.intensive.chapter2.task10;
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
 public class Task10 {
-    public static boolean isPalindrome(String inputString){
-        String str = inputString.replaceAll("[^A-Za-zА-Яа-я]", "").toLowerCase();
-        StringBuffer stringBuffer = new StringBuffer(str);
-        stringBuffer.reverse();
-        String reversedStr = stringBuffer.toString();
 
-        return str.equals(reversedStr);
-    }
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум."));
+        String inputString = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
+        System.out.println(isPalindrome(inputString));
+    }
+
+    public static boolean isPalindrome(String inputString) {
+        String str = inputString.toLowerCase().replaceAll("[^а-я0-9]", "");
+        int lastIndex = str.length() - 1;
+
+        for(int i = 0, j = lastIndex; i <= lastIndex; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
