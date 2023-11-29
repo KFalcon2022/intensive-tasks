2e99b4425788676ec8932ebad41a6b5d630477b0
Alexey <lolex1@mail.ru>
2023-11-29 21:30:30 +0300

    Task5

--- src/main/java/com/walking/intensive/chapter1/task5/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Main.java
@@ -22,19 +22,21 @@ public class Main {
         }
 
     static double getAreaByHeron(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a && a * b * c != 0) {
+
+        if (isTriangleExist(a, b, c)) {
+
             double p = (a + b + c) / 2;
 
             return Math.sqrt(p * (p - a) * (p - b) * (p - c));
         }
 
-        System.out.println("Треугольник не существует");
-
         return 0.0;
     }
 
     static double[] getHeights(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a && a * b * c != 0) {
+
+        if (isTriangleExist(a, b, c)) {
+
             double hA = (2 / a) * getAreaByHeron(a, b, c);
             double hB = (2 / b) * getAreaByHeron(a, b, c);
             double hC = (2 / c) * getAreaByHeron(a, b, c);
@@ -42,13 +44,13 @@ public class Main {
             return getMinAvgMaxValue(hA, hB, hC);
         }
 
-        System.out.println("Треугольник не существует");
-
         return new double[]{0.0};
     }
 
     static double[] getMedians(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a && a * b * c != 0) {
+
+        if (isTriangleExist(a, b, c)) {
+
             double mA = 0.5 * Math.sqrt(2 * (b * b) + 2 * (c * c) - a * a);
             double mB = 0.5 * Math.sqrt(2 * (a * a) + 2 * (c * c) - b * b);
             double mC = 0.5 * Math.sqrt(2 * (a * a) + 2 * (b * b) - c * c);
@@ -56,13 +58,13 @@ public class Main {
             return getMinAvgMaxValue(mA, mB, mC);
         }
 
-        System.out.println("Треугольник не существует");
-
         return new double[]{0.0};
     }
 
     static double[] getBisectors(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a && a * b * c != 0) {
+
+        if (isTriangleExist(a, b, c)) {
+
             double lA = Math.sqrt((b * c) * (b + c + a) * (b + c - a)) / (b + c);
             double lB = Math.sqrt((a * c) * (a + c + b) * (a + c - b)) / (a + c);
             double lC = Math.sqrt((a * b) * (a + b + c) * (a + b - c)) / (a + b);
@@ -70,13 +72,13 @@ public class Main {
             return getMinAvgMaxValue(lA, lB, lC);
         }
 
-        System.out.println("Треугольник не существует");
-
         return new double[]{0.0};
     }
 
     static double[] getAngles(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a && a * b * c != 0) {
+
+        if (isTriangleExist(a, b, c)) {
+
             double cosA = Math.acos((a * a + c * c - b * b) / (2 * a * c));
             double cosB = Math.acos((a * a + b * b - c * c) / (2 * a * b));
             double cosY = Math.acos((b * b + c * c - a * a) / (2 * b * c));
@@ -84,44 +86,46 @@ public class Main {
             return getMinAvgMaxValue(Math.toDegrees(cosA), Math.toDegrees(cosB), Math.toDegrees(cosY));
         }
 
-        System.out.println("Треугольник не существует");
-
         return new double[]{0.0};
     }
 
     static double getInscribedCircleRadius(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a && a * b * c != 0) {
+
+        if (isTriangleExist(a, b, c)) {
+
             return (2 * getAreaByHeron(a, b, c)) / (a + b + c);
         }
-
-        System.out.println("Треугольник не существует");
 
         return 0.0;
     }
 
     static double getCircumradius(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a && a * b * c != 0) {
+
+        if (isTriangleExist(a, b, c)) {
+
             return (a * b * c) / (4 * getAreaByHeron(a, b, c));
         }
-        System.out.println("Треугольник не существует");
 
         return 0.0;
     }
 
     static double getAreaAdvanced(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a && a * b * c != 0) {
+
+        if (isTriangleExist(a, b, c)) {
+
             double cosA = (b * b + c * c - a * a) / (2 * b * c);
             double sinA = Math.sqrt(1 - cosA * cosA);
 
             return 0.5 * c * b * sinA;
         }
-        System.out.println("Треугольник не существует");
 
         return 0.0;
     }
 
     static double[] getMinAvgMaxValue(double valueA, double valueB, double valueC) {
+
         double minValue = Math.min(valueA, valueB);
+
         if (minValue > valueC) {
             minValue = valueC;
         }
@@ -135,5 +139,11 @@ public class Main {
         double avgValue = (valueA + valueB + valueC) - (minValue + maxValue);
 
         return new double[]{minValue, avgValue, maxValue};
+    }
+
+    static boolean isTriangleExist (double a, double b, double c){
+
+        return  (a + b > c && a + c > b && b + c > a && a * b * c != 0);
+
     }
 }
