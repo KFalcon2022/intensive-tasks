eaff76e52d6f15e8ceeb386cc9415c0243de4aff
SenyaDiamond <SenyaDiamond@gmail.com>
2023-11-29 17:36:39 +0300

    Chapter 2 Task 10 v 1.0

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,34 @@
 package com.walking.intensive.chapter2.task10;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
 public class Task10 {
     public static void main(String[] args) {
+        System.out.println(isPalindrome("Was it a car or a cat I saw!!!"));
+        System.out.println(isPalindrome("Caroline please!"));
 
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    static boolean isPalindrome(String inputString) {
+        String clearLetters = "";
+        for (int i = 0; i < inputString.length(); i++) {
+            clearLetters += Character.isLetter(inputString.charAt(i)) ? inputString.charAt(i) : "";
+        }
+        clearLetters = clearLetters.toLowerCase();
+        String reverseString = "";
+        for (int i = clearLetters.length() - 1; i >= 0; i--) {
+            reverseString += clearLetters.charAt(i);
+
+        }
+        //System.out.println(clearLetters);
+        //System.out.println(reverseString);
+        if (reverseString.equals(clearLetters)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
