4ef35ced96e9cb3ad01e362b854abc79bd213977
sauza7 <sauza777@yandex.ru>
2023-11-29 18:18:41 +0300

    Chapter 2. task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,39 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
+        int m = 15000;
 
+        if (m < 2) {
+            System.out.println("Число должно быть больше 1.");
+            return;
+        }
+
+        if (getFriendlyPair(m) == 0) {
+            System.out.println("Дружественных чисел меньше заданного нет.");
+        } else {
+            System.out.println("Максимально близкое дружественное число, меньше или равное заданному: " + getFriendlyPair(m));
+        }
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+
+        while (m != 0 && !(m == getSumDividers(getSumDividers(m)) && getSumDividers(m) < m)) {
+            m--;
+        }
+
+        return m;
+    }
+
+    static int getSumDividers(int n) {
+
+        int sumDividers = 0;
+
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sumDividers += i;
+            }
+        }
+
+        return sumDividers;
     }
 }
