24ec9889663f2f58ec2be18c8b0adc41fb150d44
smmotor <smmotor777@gmail.com>
2023-11-29 20:20:34 +0600

    Chapter 2 Task 6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,22 +5,37 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-
+        System.out.println(getNod(98, 35));
+        System.out.println(getNoc(98, 35));
+        System.out.println(getNodByEuclideanAlgorithm(98, 35));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        int getNod = getNod(m, n);
+        int mult = m * n;
+        int result = mult / getNod;
+        return result;
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        int maxNum = Math.max(Math.abs(m), Math.abs(n));
+        int nod = 1;
+        if (n == 0 || m == n) {
+            return Math.abs(m);
+        }
+        for (int i = 2; i < maxNum; i++) {
+            if (Math.abs(m) % i == 0 && Math.abs(n) % i == 0) {
+                nod = i;
+            }
+        }
+        return nod;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        if (n == 0) {
+            return Math.abs(m);
+        }
+        return getNodByEuclideanAlgorithm(n, m % n);
     }
 
 }
