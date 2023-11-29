e5fde95ad261266dee02a258be1cf44214f10c3f
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-11-30 00:13:13 +0300

    first solution

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -6,21 +6,32 @@ package com.walking.intensive.chapter2.task6;
 public class Task6 {
     public static void main(String[] args) {
 
+        System.out.println(getNoc(24, 54));
+        System.out.println(getNod(24, 54));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+
+        return (m * n) / getNod(m, n);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+
+        while (m != n) {
+            if (m > n) {
+                m -= n;
+            } else {
+                n -= m;
+            }
+        }
+
+        return m;
     }
 
     static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+
+
+        return m;
     }
 
 }
