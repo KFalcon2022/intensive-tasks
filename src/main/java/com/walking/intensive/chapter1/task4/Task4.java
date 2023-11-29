f1393588b70ea378581d533a52f7c932616a2af1
rovbo85@gmail.com <rovbo85@gmail.com>
2023-11-29 23:48:51 +0500

    Внесены изменения в соответствии с замечаниями рецензента, а именно: - Удалил лишнии else if; - Добавил пустые сроки между if; - Удалил лишние else

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -13,13 +13,19 @@ public class Task4 {
   }
 
   static String solveQuadraticEquation(double a, double b, double c) {
+
     if (a == 0 && b != 0 && c != 0) {
       return "Количество решений: 1. Корень: " + (int) (-c / b);
-    } else if (a == 0 && b == 0 && c == 0) {
+    }
+
+    if (a == 0 && b == 0 && c == 0) {
       return "Количество решений бесконечно.";
-    } else if (a == 0 && b == 0) {
+    }
+
+    if (a == 0 && b == 0) {
       return "Количество решений: 0.";
     }
+
     double discriminant = Math.pow(b, 2) - 4 * a * c;
     if (discriminant > 0) {
       double firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
@@ -28,10 +34,11 @@ public class Task4 {
           + (int) secondRoot
           : "Количество решений: 2. Корни: " + (int) secondRoot + ";" + (int) firstRoot;
     }
+
     if (discriminant == 0) {
       return "Количество решений: 1. Корень: " + (int) (b / (2 * a));
-    } else {
-      return ("Количество решений: 0.");
     }
+
+    return ("Количество решений: 0.");
   }
 }
