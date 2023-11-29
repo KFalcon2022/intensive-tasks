6a5154eb64861801a669758c89c93fda16017846
KrisLu23 <krisbondarev@yandex.ru>
2023-11-29 10:29:14 +0300

    Chapter 2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -3,13 +3,41 @@ package com.walking.intensive.chapter2.task7;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/java-friendly-number.html">ссылка</a>
  */
+
 public class Task7 {
     public static void main(String[] args) {
-
+        int value = 100000;
+        if (isCorrectInput(value)) {
+            System.out.println(getFriendlyPair(value));
+        }
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int firstValue;
+        int secondValue;
+        int result = 0;
+        for (int i = 1; i < m; i++) {
+            firstValue = getSumDivisors(i);
+            secondValue = getSumDivisors(firstValue);
+
+            if (secondValue == i) {
+                result = Math.max(firstValue, secondValue);
+            }
+        }
+        return result;
+    }
+
+    static int getSumDivisors(int a) {
+        int sum = 0;
+        for (int i = 1; i < a; i++) {
+            if (a % i == 0)
+                sum += i;
+        }
+        return sum;
+    }
+
+
+    static boolean isCorrectInput(int m) {
+        return (m > 0 || m < 1000000);
     }
 }
