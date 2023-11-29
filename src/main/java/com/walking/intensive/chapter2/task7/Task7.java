cfe15c25ad3d97b0613c97d4da2f8401d02b2126
Boris <aksenov.boris8595@gmeil.com>
2023-11-29 16:30:05 +0500

    Вроде верно находит. Возможно стоит попробовать сумму находить через рекурсию отдельным методом. Может быть займусь.

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,31 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        System.out.println(getFriendlyPair(12500));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int maxNumber = 0, sum1 = 1, sum2 = 1;
+        for (int i = m - 1; i > 0; i--) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    sum1 += j;
+                    sum1 += i / j;
+                }
+            }
+            for (int j = 2; j <= Math.sqrt(sum1); j++) {
+                if (sum1 % j == 0) {
+                    sum2 += j;
+                    sum2 += sum1 / j;
+                }
+            }
+            if (sum2 == i && i != sum1 && sum1 < m) {
+                maxNumber = i;
+                break;
+            }
+            sum2 = 1;
+            sum1 = 1;
+        }
+        return maxNumber;
     }
 }
