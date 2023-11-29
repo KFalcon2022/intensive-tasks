c24366b11ecb45be4d6a68837e6283568d4c02b0
U_M1CVT <akachamina@alfabank.ru>
2023-11-29 17:25:39 +0300

    Решение 2-й задачи 1 урока

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,13 +5,24 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
 
+        System.out.println(getFlatLocation(5, 3, 60));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int apartmentNumber = 4 * floorAmount * entranceAmount;
+        if (flatNumber > apartmentNumber || flatNumber <= 0 || floorAmount == 0 || entranceAmount == 0) {
+            return "Ошибка входных данных";
+        }
+        int flatEntrance = (flatNumber / (4 * floorAmount));
+        int apartmentEntrance = (flatNumber - ((flatEntrance - 1) * floorAmount * 4));
+        int flatFloor = (apartmentEntrance / 4);
+        String sideLift = ((apartmentEntrance % 4 == 1) || (apartmentEntrance % 4 == 2)) ? " слева" : " справа";
+        String roundAfterLift = (apartmentEntrance % 2 == 1) ? " влево" : " вправо";
 
-        return null; // Заглушка. При реализации - удалить
+        String result = String.format(flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloor + " этаж, " + sideLift + " от лифта, " + roundAfterLift);
+
+        return result;
+
     }
 }
