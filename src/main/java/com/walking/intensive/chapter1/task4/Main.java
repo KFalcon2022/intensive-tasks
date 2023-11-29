eaa9616604261245bee1a558a2440b9398a09a5e
NightwolfCB <nightwolf63@rambler.ru>
2023-11-29 23:06:31 +0300

    task4 fix

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -16,7 +16,25 @@ public class Main {
         Scanner inputThird = new Scanner(System.in);
         System.out.println("c = ");
         double c = inputThird.nextDouble();
-        System.out.println(solveQuadraticEquation(a, b, c));
+        // this should be completely redundant since
+        // if a equals 0 it's no more quadratic equation
+        // <a href="https://en.wikipedia.org/wiki/Quadratic_equation#:~:text=equation%20is%20linear%2C%20not%20quadratic"></a>
+        if (a == 0 & b != 0 & c != 0) {
+            // if a equals 0 there should be an exception
+            System.out.println("Количество решений: 1. Корень: " + Math.round((-c) / b));
+        } else if (a >= 0 & b == 0 & c > 0) {
+            System.out.println("Количество решений: 0.");
+        } else if (a == 0 & b == 0 & c == 0) {
+            System.out.println("Решений бесконечно");
+        } else if (b == 0 & ((a > 0 & c < 0) || (a < 0 & c > 0))) {
+            double x = Math.round(Math.sqrt((-c) / a));
+            System.out.println("Количество решений: 2. Корни: " + -Math.round(x) + ";" + Math.round(x));
+        } else if (a != 0 & b != 0 & c != 0) {
+            System.out.println(solveQuadraticEquation(a, b, c));
+        } else {
+            System.out.println("Количество решений: 1. Корень: 0");
+        }
+
     }
 
     /**
@@ -30,6 +48,7 @@ public class Main {
      * <p>
      * Количество решений: 0.
      */
+
     static String solveQuadraticEquation(double a, double b, double c) {
         double discriminant = b*b - 4 * a * c;
 
@@ -37,17 +56,17 @@ public class Main {
             return "Количество решений: 0.";
         } else if (discriminant == 0) {
             double x = (-b) / 2 * a;
-            return "Количество решений: 1. Корень: " + x;
+            return "Количество решений: 1. Корень: " + Math.round(x);
         } else if (discriminant > 0) {
             double xFirst = ((-b) + Math.sqrt(discriminant)) / 2 * a;
             double xSecond = ((-b) - Math.sqrt(discriminant)) / 2 * a;
             if (xFirst >= xSecond) {
-                return "Количество решений: 2. Корни: " + xFirst + ";" + xSecond;
-            } else if (xFirst < xSecond) {
-                return "Количество решений: 2. Корни: " + xSecond + ";" + xFirst;
+                return "Количество решений: 2. Корни: " + Math.round(xFirst) + ";" + Math.round(xSecond);
+            } else {
+                return "Количество решений: 2. Корни: " + Math.round(xSecond) + ";" + Math.round(xFirst);
             }
         }
-
+        
         return "";
     }
 }
