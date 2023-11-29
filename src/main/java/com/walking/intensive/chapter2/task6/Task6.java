3e128f92e276dec6b82012b1ab08883c1bdc6c52
Makoto-2005 <makoto2005@yandex.ru>
2023-11-29 17:28:00 +0300

    solution of task "LCM and GCD"

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -6,21 +6,35 @@ package com.walking.intensive.chapter2.task6;
 public class Task6 {
     public static void main(String[] args) {
 
+        int m = 16;
+        int n = 20;
+
+        System.out.println(getNodByEuclideanAlgorithm(m, n));
+        System.out.println(getNod(m, n));
+        System.out.println(getNoc(m, n));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+        return Math.abs(m * n) / getNod(m, n);
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        while (m != 0 && n != 0) {
+            if (m > n) {
+                m = m % n;
+            }
+            if (m == 0) {
+                return m + n;
+            }
+            n %= m;
+        }
+        return m + n;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        if (n == 0) {
+            return m;
+        }
+        return getNodByEuclideanAlgorithm(n, m % n);
     }
-
 }
