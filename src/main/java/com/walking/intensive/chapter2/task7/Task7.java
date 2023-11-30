205bc4bd4f6459f67f5bd6bf1ff745e0334f943f
Boris <aksenov.boris8595@gmeil.com>
2023-12-01 00:43:02 +0500

    Исправил, решение было неправильное. Теперь должно быть все верно

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,44 +5,37 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-        System.out.println(getFriendlyPair(250));
+        System.out.println(getFriendlyPair(1000000));
+//        System.out.println(getSumFactorsNumber());
     }
 
     static int getFriendlyPair(int m) {
-        int maxNumber = 0, maxSum = 0, sum1 = 1, sum2 = 1;
-        for (int i = m - 1; i > 0; i--) {   //проходим по всем числам от m-1 до 0
-            for (int j = 2; j <= Math.sqrt(i); j++) {   //находим сумму множителей первого числа и записываем в sum1
-                if (i % j == 0) {
-                    sum1 += j;
-                    sum1 += i / j;
-                }
-            }
-            //если сумма числа i и сумма множителей числа i больше ранее найденой суммы дружественной пары - идем дальше.
-            //если же сумма меньше - то нет смысла считать есть у числа i дружественное число, так как пара нам все равно не подходит.
+        int maxNumber = 0;
+        int maxSum = 0;
+        int sum1 = 1;
+        int sum2 = 1;
+        for (int i = m - 1; i > 0; i--) {
+            sum1 = getSumFactorsNumber(i);
             if (maxSum < i + sum1) {
-                for (int j = 2; j <= Math.sqrt(sum1); j++) {    //сумма множителей второго числа и записываем в sum2
-                    if (sum1 % j == 0) {
-                        sum2 += j;
-                        sum2 += sum1 / j;
-                    }
-                }
-                /*
-                 * Проверки(условие слишком громоздкое, мне не нравится, но работает)
-                 * 1. Число i не равно сумме множителей числа i(sum1)
-                 * 2. Сумма множителей числа sum1 = числу i
-                 * 3. Максимальная найденная сумма чисел i и sum1 меньше, чем текущая сумма i и sum1
-                 * 4. sum1(сумма множителей числа i) меньше, чем m
-                 * 5. sum2(сумма множителей числа sum1) меньше, чем m
-                 * Если все условия истины - перезаписываем максимальную сумму и максимальное значение
-                 */
-                if (sum1 != i && sum2 == i && maxSum < sum1 + i && sum1 < m && sum2 < m) {
-                    maxSum = sum1 + i;
-                    maxNumber = Math.max(sum1, i);
-                }
+                sum2 = getSumFactorsNumber(sum1);
+            }
+            if (sum1 != i && sum2 == i && maxSum < sum1 + i && sum1 < m && sum2 < m) {
+                maxSum = sum1 + i;
+                maxNumber = Math.max(sum1, i);
             }
             sum2 = 1;
-            sum1 = 1;
         }
         return maxNumber;
+    }
+
+    static int getSumFactorsNumber(int n) {
+        int sumFactorsNumber = 1;
+        for (int j = 2; j <= Math.sqrt(n); j++) {
+            if (n % j == 0) {
+                sumFactorsNumber += j;
+                sumFactorsNumber += n / j;
+            }
+        }
+        return sumFactorsNumber;
     }
 }
