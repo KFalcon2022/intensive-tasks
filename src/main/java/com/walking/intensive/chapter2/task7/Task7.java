996d76633f06ad9dc26e3279f9c2f0ce7ae70f12
Alext715 <alexandrevd2107@gmail.com>
2023-11-30 11:26:27 +0300

    Chapter 2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -6,10 +6,38 @@ package com.walking.intensive.chapter2.task7;
 public class Task7 {
     public static void main(String[] args) {
 
+        int maxFriendlyNumber = getFriendlyPair(1000000);
+
+        System.out.println("Максимальное дружественное число: " + maxFriendlyNumber);
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int maxFriendlyNumber = 0;
+
+        for (int num1 = 1; num1 < m; num1++) {
+            int num2 = getSum(num1);
+
+            if (num1 < num2 && num2 < m && getSum(num2) == num1) {
+                maxFriendlyNumber = num2;
+            }
+        }
+
+        return maxFriendlyNumber;
+    }
+
+    public static int getSum(int number) {
+        int sum = 1;
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                sum += i;
+                int divisor = number / i;
+                if (divisor != i) {
+                    sum += divisor;
+                }
+            }
+        }
+
+        return sum;
     }
 }
