7002458cd0f5b3c15cd89de131dceaacb85cf5c1
kriss.gord <kriss.gord@gmail.com>
2023-11-30 15:04:08 +0500

    chapter 1 task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -5,29 +5,35 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 0;
-        double c = 0;
+        double b = 4;
+        double c = 5;
 
         System.out.println(solveQuadraticEquation(a, b, c));
-
     }
 
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        String resultCount = "Количество решений: %d.";
+
+        if (a == 0 && b == 0 && c == 0) {
+            return "Количество решений: бесконечно";
+        }
+
+        if (a == 0 && b != 0 && c != 0) {
+            return String.format(resultCount, 1) + String.format(" Корень: %,.2f", c * (-1) / b);
+        }
+
+        double discr = (b * b) - (4 * a * c);
+
+        if (discr == 0) {
+            return String.format(resultCount, 1) + String.format(" Корень: %,.2f", (-1) * b / (2 * a));
+        }
+        if (discr > 0) {
+            return String.format(resultCount, 2) + String.format(" Корни: %,.2f;%,.2f", ((-1) * b + java.lang.Math.sqrt(discr)) / (2 * a),
+                    ((-1) * b - java.lang.Math.sqrt(discr)) / (2 * a));
+        }
+
+        return String.format(resultCount, 0);
     }
 }
