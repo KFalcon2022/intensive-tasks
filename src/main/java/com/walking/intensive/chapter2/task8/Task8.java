fb89f1a8a020a282932e40ebe7731dd33b6179bf
sauza7 <sauza777@yandex.ru>
2023-11-30 14:19:40 +0300

    Chapter 2. task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -6,10 +6,37 @@ package com.walking.intensive.chapter2.task8;
 public class Task8 {
     public static void main(String[] args) {
 
+        int ticketNumber = 999999;
+
+        if (ticketNumber < 1 || ticketNumber > 999999) {
+            System.out.println("Некорректный номер.");
+            return;
+        }
+
+        System.out.println("Вероятность счастливого билета: " + getHappyTicketChance(ticketNumber));
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance(int ticketNumber) {
+
+        if (ticketNumber < 1000) {
+            return 0;
+        }
+
+        int sumHappyTickets = 0;
+
+        for (int i = 1001; i <= ticketNumber; i++) {
+
+            int i1 = i / 100000 % 10,
+                    i2 = i / 10000 % 10,
+                    i3 = i / 1000 % 10,
+                    i4 = i / 100 % 10,
+                    i5 = i / 10 % 10,
+                    i6 = i % 10;
+
+            if (i1 + i2 + i3 == i4 + i5 + i6) {
+                sumHappyTickets++;
+            }
+        }
+        return (double) sumHappyTickets / ticketNumber;
     }
 }
