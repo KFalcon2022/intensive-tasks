337fd3c2478578a470e374173a035b4ba6a91d8f
lambo064 <tasha_lambo@mail.ru>
2023-11-30 15:07:36 +0300

    chapter 2 task 9

--- src/main/java/com/walking/intensive/chapter2/task10/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Main.java
@@ -2,6 +2,14 @@ package com.walking.intensive.chapter2.task10;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome("rerer"));
     }
+
+    public static boolean isPalindrome(String example) {
+        String str = example.replaceAll(" ", "").replaceAll("\\p{Punct}", "");
+
+        return str.equalsIgnoreCase(String.valueOf(new StringBuilder(str).reverse()));
+    }
+
+
 }
