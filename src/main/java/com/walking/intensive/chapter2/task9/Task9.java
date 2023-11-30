25b0e7c4673820815eb5ecc7e5e395123ab83faa
dimapros <dima.ru.98@mail.ru>
2023-11-30 20:06:27 +1000

    Сhapter2.Task9.

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -10,10 +10,33 @@ public class Task9 {
         System.out.println("Введите число: ");
         int n = sc.nextInt();
 
-        System.out.println(printTriangle(n));
+        System.out.println(printPascalTriangle(n));
     }
 
-    private static int[][] getPascalTriangle(int n) {
+    public static String printPascalTriangle(int n) {
+        int[][] triangle = getPascalNumbers(n);
+        int quantityWhiteSpaces = triangle.length;
+        String whiteSpace = " ";
+        String answer = "";
+
+        for (int i = 0; i < triangle.length; i++) {
+            answer += whiteSpace.repeat(quantityWhiteSpaces);
+
+            for (int j = 0; j < triangle[i].length; j++) {
+
+                if (triangle[i][j] == 0) {
+                    answer += "";
+                } else {
+                    answer += triangle[i][j] + whiteSpace;
+                }
+            }
+            quantityWhiteSpaces--;
+            answer += "\n";
+        }
+        return answer;
+    }
+
+    private static int[][] getPascalNumbers(int n) {
         int[][] triangle = new int[n][n * 2];
 
         for (int i = 0; i < triangle.length; i++) {
@@ -22,41 +45,13 @@ public class Task9 {
 
                 if (i == 0 && j == 0 || j == 0) {
                     triangle[i][j] = 1;
-                }
-
-                else if(i == 0 && j != 0){
+                } else if (i == 0 && j != 0) {
                     triangle[i][j] = 0;
-                }
-
-                else{
+                } else {
                     triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
                 }
             }
         }
         return triangle;
-    }
-    public static String printTriangle(int n){
-        int[][] triangle = getPascalTriangle(n);
-        int quantityWhiteSpaces = triangle.length;
-        String whiteSpace = " ";
-        String answer = "";
-
-        for(int i = 0; i < triangle.length; i++){
-            answer+= whiteSpace.repeat(quantityWhiteSpaces);
-
-            for(int j = 0; j < triangle[i].length; j++){
-
-                if(triangle[i][j] == 0){
-                    answer+= "";
-                }
-
-                else{
-                    answer+= triangle[i][j] + whiteSpace;
-                }
-            }
-            quantityWhiteSpaces--;
-            answer+= "\n";
-        }
-        return answer;
     }
 }
