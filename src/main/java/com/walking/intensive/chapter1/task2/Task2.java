2163f16827ecea1f4bb9f6c02df4aa2c5a4a148d
dimapros <dima.ru.98@mail.ru>
2023-11-30 22:44:16 +1000

    Chapter1.Task2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -1,17 +1,56 @@
 package com.walking.intensive.chapter1.task2;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
- */
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Введите количество этажей: ");
+        int floorAmount = sc.nextInt();
+
+        System.out.println("Введите количество подъездов: ");
+        int entranceAmount = sc.nextInt();
+
+        System.out.println("Введите номер квартиры: ");
+        int flatNumber = sc.nextInt();
+
+        sc.close();
+
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int amountFlatsOnFloor = 4;
+        int amountFlats = floorAmount * entranceAmount * amountFlatsOnFloor;
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber > amountFlats || flatNumber <= 0) {
+            return "Такой квартиры не существует";
+        }
+
+        if (floorAmount <= 0) {
+            return "Количество этажей не может быть меньше 1.";
+        }
+
+        if (entranceAmount <= 0) {
+            return "Количество подъездов не может быть меньше 1.";
+        }
+
+        int searchableEntrance = (int) Math.ceil(flatNumber / (floorAmount * (double) amountFlatsOnFloor));
+        int searchableFloor = (int) (Math.ceil(flatNumber / (double) amountFlatsOnFloor)) % floorAmount;
+
+        if (searchableFloor == 0) {
+            searchableFloor = floorAmount;
+        }
+
+        String location = flatNumber + " кв - " + searchableEntrance + " подъезд, " + searchableFloor + " этаж, ";
+        String turn = switch (flatNumber % amountFlatsOnFloor) {
+            case 0 -> "справа от лифта, вправо";
+            case 1 -> "слева от лифта, влево";
+            case 2 -> "слева от лифта, вправо";
+            case 3 -> "справа от лифта, влево";
+            default -> "";
+        };
+        return location + turn;
     }
 }
