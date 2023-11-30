f5f87493631fc483bc94c4f06a333a69819f5a2c
njmist <njmist@gmail.com>
2023-11-30 23:00:58 +0300

    chapter 2 task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,33 @@
 package com.walking.intensive.chapter2.task10;
 
 /**
- * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
+ * ???????: <a href="https://geometry-math.ru/homework/Java-palindrome.html">??????</a>
  */
 public class Task10 {
     public static void main(String[] args) {
+        String inputString = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
 
+        if (isPalindrome(inputString)){
+            System.out.println("Строка является палиндромом");
+        } else {
+            System.out.println("Строка не является палиндромом");
+        }
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    static boolean isPalindrome(String inputString) {
+        String letterString = "";
+
+        for (int i = 0; i < inputString.length();i++){
+            if (Character.isLetterOrDigit(inputString.charAt(i))){
+                letterString = letterString + inputString.charAt(i);
+            }
+        }
+        String reverseLetterString = "";
+
+        for (int i = 0; i < letterString.length(); i++ ){
+            reverseLetterString = letterString.charAt(i) + reverseLetterString;
+        }
+
+        return reverseLetterString.equalsIgnoreCase(letterString);
     }
 }
