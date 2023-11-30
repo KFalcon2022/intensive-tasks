773c4341579a604af663d41ec21736c7d9e5f575
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-11-30 21:32:31 +0300

    first solution

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -11,15 +11,58 @@ public class Task10 {
     }
 
     public static boolean isPalindrome(String inputString) {
-        String str = inputString.toLowerCase().replaceAll("[^а-я0-9]", "");
-        int lastIndex = str.length() - 1;
+        int lastIndex = inputString.length() - 1;
 
-        for(int i = 0, j = lastIndex; i <= lastIndex; i++, j--) {
-            if (str.charAt(i) != str.charAt(j)) {
+        for (int i = 0, j = lastIndex; i <= lastIndex; i++, j--) {
+            if (getCharByIndex(inputString, i, true) != getCharByIndex( inputString, lastIndex - i, false)) {
+
                 return false;
             }
         }
 
         return true;
+    }
+
+    public static char getCharByIndex(String str, int index, boolean isStart) {
+        if (isRussianLetter(str.charAt(index))) {
+
+            if (isUpperCase(str.charAt(index))) {
+                return toLowerCase(str.charAt(index));
+            }
+
+            return str.charAt(index);
+        }
+        if(isStart) {
+            index++;
+
+            return getCharByIndex(str, index, true);
+        }
+        index--;
+
+        return getCharByIndex(str, index, false);
+    }
+
+    public static boolean isRussianLetter(char simbol) {
+        if (simbol >= 192 && simbol <= 223) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isUpperCase(char simbol) {
+        if (simbol >= 192 && simbol <= 223) {
+            return true;
+        }
+
+        return false;
+    }
+    public static char toLowerCase(char simbol) {
+        if (isUpperCase(simbol)) {
+
+            return (char) (simbol + 32);
+        }
+
+        return simbol;
     }
 }
