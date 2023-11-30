497eac8968d35c4ad5ce963d48d199198fb6e68c
Oleg <kovalyovop@yandex.ru>
2023-11-30 15:53:44 +0300

    Chapter2Task7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -6,10 +6,48 @@ package com.walking.intensive.chapter2.task7;
 public class Task7 {
     public static void main(String[] args) {
 
+        System.out.println(getDivisors(220));
+        System.out.println(getDivisors(284));
+        System.out.println(getDivisors(440));
+        System.out.println(getDivisors(880));
+        System.out.println(getDivisors(1760));
+        System.out.println();
+        System.out.println(getFriendlyPair(1000));
+        System.out.println(getFriendlyPair(5000));
+        System.out.println(getFriendlyPair(10000));
+        System.out.println(getFriendlyPair(15000));
+        System.out.println(getFriendlyPair(20000));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        if (m < 1 || m > 1000_000) {
+            return 0;
+        }
+
+        int maxFriendlyNumber = 0;
+
+        for (int i = 1; i < m; i++) {
+            int friendlyOne = getDivisors(i);
+            int friendlyTwo = getDivisors(friendlyOne);
+            int maxNumber = Math.max(friendlyOne, friendlyTwo);
+
+            if (i == friendlyTwo && maxNumber < m && friendlyOne != friendlyTwo) {
+                maxFriendlyNumber = maxNumber;
+            }
+        }
+
+        return maxFriendlyNumber;
+    }
+
+    static int getDivisors(int m) {
+        int result = 0;
+
+        for (int i = 1; i < m; i++) {
+            if (m % i == 0) {
+                result += i;
+            }
+        }
+
+        return result;
     }
 }
