504fb4e8a01ebe55b7ac18b77dc556a3020c0e4d
Pavel Kuznetsov <9161194023@mail.ru>
2023-11-30 03:10:07 +0300

    solved chapter1 task2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,13 +5,43 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
 
+        int floorAmount = 5;
+        int entranceAmount = 3;
+        int flatNumber = 20;
+
+        System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if ((floorAmount * 4 * entranceAmount < flatNumber) || flatNumber < 0) {
+            return "Такой квартиры не существует";
+        }
+
+        int entranceNumber = (int) Math.ceil((double) flatNumber / (floorAmount * 4));
+        int floorNumber;
+
+        if (flatNumber % (floorAmount * 4) == 0) {
+            floorNumber = floorAmount;
+        } else {
+            floorNumber = (int) Math.ceil((double) (flatNumber % (floorAmount * 4)) / 4);
+        }
+
+        switch (flatNumber % 4) {
+            case 0:
+                return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, "
+                        + "справа от лифта, вправо";
+            case 1:
+                return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, "
+                        + "слева от лифта, влево";
+            case 2:
+                return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, "
+                        + "слева от лифта, вправо";
+            case 3:
+                return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, "
+                        + "справа от лифта, влево";
+        }
+        return null;
     }
 }
