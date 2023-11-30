928f0e5bd14d594c6b01989722194acae49e3e5d
KrisLu23 <krisbondarev@yandex.ru>
2023-11-30 11:35:49 +0300

    Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,37 @@
 package com.walking.intensive.chapter2.task10;
 
+import java.util.Locale;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум."));
+        System.out.println(isPalindrome("Горд дох, ход дрог."));
+        System.out.println(isPalindrome("Никогда не говори никогда."));
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    static boolean isPalindrome(String inputString) {
+        inputString = inputString.replaceAll("[ ,.!?]", "").toLowerCase(Locale.ROOT);
+
+        char[] arr = new char[inputString.length()];
+
+        for (int i = 0; i < inputString.length(); i++) {
+            arr[i] = inputString.charAt(i);
+        }
+
+        char[] pall = new char[inputString.length()];
+
+        for (int i = 1; i < arr.length + 1; i++) {
+            pall[i - 1] = arr[arr.length - i];
+        }
+
+        for (int i = 0; i < inputString.length(); i++) {
+            if (pall[i] != arr[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
