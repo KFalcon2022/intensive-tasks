4ebab2cd6ae915516c3b176c624e7ad2b7586a71
Magpier <garson.ao@gmail.com>
2023-11-30 21:02:44 +0300

    Chapter2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Main.java
@@ -4,13 +4,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(isPalindrome("Was it a car or a cat I saw?"));
         System.out.println(isPalindrome("Was it a car or a cat I saw?fd"));
-
-
+        System.out.println(isPalindrome("abcdcba"));
+        System.out.println(isPalindrome("abba"));
     }
-    static boolean isPalindrome(String s){
-        StringBuilder sb = new StringBuilder(s.replaceAll("[^a-zA-Z]", "").toLowerCase());
-        if(!sb.toString().contentEquals(sb.reverse())){
-            return false;
+
+    static boolean isPalindrome(String s) {
+        String string = s.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        for (int i = 0; i < string.length() / 2; i++) {
+            if (string.charAt(i) != string.charAt(string.length() - i - 1)) {
+                return false;
+            }
         }
         return true;
     }
