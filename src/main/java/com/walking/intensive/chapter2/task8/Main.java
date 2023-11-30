b340ea63e328b40bc3b41696fd97d32c5a69b4a1
EkkoXIII <astaroth644@gmail.com>
2023-11-30 17:17:38 +0300

    Решение задачи 8

--- src/main/java/com/walking/intensive/chapter2/task8/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Main.java
@@ -1,7 +1,41 @@
-package com.walking.intensive.chapter2.task8;
+package src.main.java.com.walking.intensive.chapter2.task8;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Вероятность выпадения счастливого билета с в диапазоне номеров от 000000 до 999999: " + getSuccessProbability());
+    }
 
+    public static boolean isLuckyTicket(int ticketNumber) {
+        boolean result = false;
+        int divisor = 10;
+        int firstHalfSum = 0;
+        int secondHaldSum = 0;
+
+        for (int i = 0; i <= 5; i++) {
+            if (i < 3) {
+                firstHalfSum += ticketNumber / (int) Math.pow(divisor, i) % divisor;
+            } else {
+                secondHaldSum += ticketNumber / (int) Math.pow(divisor, i) % divisor;
+            }
+        }
+
+        if (firstHalfSum == secondHaldSum) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public static float getSuccessProbability() {
+        int ticketNumberLimit = 999999;
+        int successCount = 0;
+
+        for (int i = 0; i <= ticketNumberLimit; i++) {
+            if (isLuckyTicket(i)) {
+                successCount++;
+            }
+        }
+
+        return (float) successCount / ticketNumberLimit;
     }
 }
