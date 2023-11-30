7cab9c2817e4253a95bc02d0a64f81b867afdf61
Oleg <kovalyovop@yandex.ru>
2023-11-30 16:46:04 +0300

    Chapter2Task8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,26 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println(isLuckyTicket(1));
+        System.out.println(getHappyTicketChance(1));
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance(int ticketNumber) {
+        double count = 0;
+
+        for (int i = 0; i <= 999_999; i++) {
+            if (isLuckyTicket(i)) {
+                count++;
+            }
+        }
+
+        return count / 1000_000; // делим на общее число вероятных значений, включая 0
+    }
+
+    static boolean isLuckyTicket(int ticketNumber) { // дополнительные скобки для читаемости
+        int firstPart = (ticketNumber / 100_000) + (ticketNumber / 10_000 % 10) + (ticketNumber / 1000 % 10);
+        int secondPart = (ticketNumber / 100 % 10) + (ticketNumber / 10 % 10) + (ticketNumber % 10);
+
+        return firstPart == secondPart;
     }
 }
