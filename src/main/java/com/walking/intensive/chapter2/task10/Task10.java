1bda2233c1a6db2de206ccfb3e30cddece3e5fa7
ChiffaRi <testiktt20@gmail.com>
2023-11-30 22:25:03 +0500

    Chapter 2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,37 @@
 package com.walking.intensive.chapter2.task10;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
 public class Task10 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите палиндром: ");
+        String inputString = in.nextLine();
+        in.close();
 
+        if (isPalindrome(inputString)) {
+            System.out.print("Фраза - палиндром.");
+        } else {
+            System.out.print("Фраза не палиндром.");
+        }
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
+    static boolean isPalindrome(String inputString) {
+        String str = inputString.replaceAll("[,.!? -]", "").toLowerCase();
+        int check = 0;
+        int j = str.length();
+        for (int i = 0; i < str.length() / 2; i++, j--) {
+            if (str.charAt(i) == str.charAt(j - 1)) {
+                check++;
+            }
+        }
+
+        if (check == j - 1) {
+            return true;
+        }
         return false;
     }
 }
