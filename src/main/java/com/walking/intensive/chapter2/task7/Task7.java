3bd05c62f0bb58d64b52e210d6b2ecf9383c5b15
Boris <aksenov.boris8595@gmeil.com>
2023-11-30 23:45:22 +0500

    Исправил, решение было неправильное. Теперь должно быть все верно

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,27 +5,40 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-        System.out.println(getFriendlyPair(12500));
+        System.out.println(getFriendlyPair(250));
     }
 
     static int getFriendlyPair(int m) {
-        int maxNumber = 0, sum1 = 1, sum2 = 1;
-        for (int i = m - 1; i > 0; i--) {
-            for (int j = 2; j <= Math.sqrt(i); j++) {
+        int maxNumber = 0, maxSum = 0, sum1 = 1, sum2 = 1;
+        for (int i = m - 1; i > 0; i--) {   //проходим по всем числам от m-1 до 0
+            for (int j = 2; j <= Math.sqrt(i); j++) {   //находим сумму множителей первого числа и записываем в sum1
                 if (i % j == 0) {
                     sum1 += j;
                     sum1 += i / j;
                 }
             }
-            for (int j = 2; j <= Math.sqrt(sum1); j++) {
-                if (sum1 % j == 0) {
-                    sum2 += j;
-                    sum2 += sum1 / j;
+            //если сумма числа i и сумма множителей числа i больше ранее найденой суммы дружественной пары - идем дальше.
+            //если же сумма меньше - то нет смысла считать есть у числа i дружественное число, так как пара нам все равно не подходит.
+            if (maxSum < i + sum1) {
+                for (int j = 2; j <= Math.sqrt(sum1); j++) {    //сумма множителей второго числа и записываем в sum2
+                    if (sum1 % j == 0) {
+                        sum2 += j;
+                        sum2 += sum1 / j;
+                    }
                 }
-            }
-            if (sum2 == i && i != sum1 && sum1 < m) {
-                maxNumber = i;
-                break;
+                /*
+                 * Проверки(условие слишком громоздкое, мне не нравится, но работает)
+                 * 1. Число i не равно сумме множителей числа i(sum1)
+                 * 2. Сумма множителей числа sum1 = числу i
+                 * 3. Максимальная найденная сумма чисел i и sum1 меньше, чем текущая сумма i и sum1
+                 * 4. sum1(сумма множителей числа i) меньше, чем m
+                 * 5. sum2(сумма множителей числа sum1) меньше, чем m
+                 * Если все условия истины - перезаписываем максимальную сумму и максимальное значение
+                 */
+                if (sum1 != i && sum2 == i && maxSum < sum1 + i && sum1 < m && sum2 < m) {
+                    maxSum = sum1 + i;
+                    maxNumber = Math.max(sum1, i);
+                }
             }
             sum2 = 1;
             sum1 = 1;
