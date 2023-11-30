b35231c347ba8f97c30ff49609dd8dd865b3f6bf
njmist <njmist@gmail.com>
2023-11-30 23:14:20 +0300

    chapter 2 task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -7,7 +7,7 @@ public class Task10 {
     public static void main(String[] args) {
         String inputString = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
 
-        if (isPalindrome(inputString)){
+        if (isPalindrome(inputString)) {
             System.out.println("Строка является палиндромом");
         } else {
             System.out.println("Строка не является палиндромом");
@@ -17,14 +17,14 @@ public class Task10 {
     static boolean isPalindrome(String inputString) {
         String letterString = "";
 
-        for (int i = 0; i < inputString.length();i++){
-            if (Character.isLetterOrDigit(inputString.charAt(i))){
+        for (int i = 0; i < inputString.length(); i++) {
+            if (Character.isLetterOrDigit(inputString.charAt(i))) {
                 letterString = letterString + inputString.charAt(i);
             }
         }
         String reverseLetterString = "";
 
-        for (int i = 0; i < letterString.length(); i++ ){
+        for (int i = 0; i < letterString.length(); i++) {
             reverseLetterString = letterString.charAt(i) + reverseLetterString;
         }
 
