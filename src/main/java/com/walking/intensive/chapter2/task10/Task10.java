947a0aa9a4ea35091a1a05eb3941bec8240ecd3f
sauza7 <sauza777@yandex.ru>
2023-12-01 17:50:37 +0300

    Chapter 2. task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,25 @@
 package com.walking.intensive.chapter2.task10;
 
 /**
- * РЈСЃР»РѕРІРёРµ: <a href="https://geometry-math.ru/homework/Java-palindrome.html">СЃСЃС‹Р»РєР°</a>
+ * ???????: <a href="https://geometry-math.ru/homework/Java-palindrome.html">??????</a>
  */
 public class Task10 {
     public static void main(String[] args) {
 
+        String str1 = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
+        String str2 = "О, Муза! Ранясь шилом опыта, ты помолишься на разум.";
+
+        System.out.println(isPalindrome(str1));
+        System.out.println(isPalindrome(str2));
     }
 
-    boolean isPalindrome(String inputString){
-        // Р’Р°С€ РєРѕРґ
-        return false;
+    static boolean isPalindrome(String inputString) {
+
+        String lowerCaseString = inputString.toLowerCase();
+        String regex = "[^а-яa-z]";
+        String stringWithoutSymbols = lowerCaseString.replaceAll(regex , "");
+        StringBuilder stringWithoutSymbolsReverse = new StringBuilder(stringWithoutSymbols).reverse();
+
+        return stringWithoutSymbols.compareTo(stringWithoutSymbolsReverse.toString()) == 0;
     }
 }
