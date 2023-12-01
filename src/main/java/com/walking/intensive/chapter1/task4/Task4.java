0d9b140df55f5d9db9b663c05224aed135b2a7f7
Captain-Greg <antonovgeorgij12@gmail.com>
2023-12-01 17:12:37 +0300

    Chapter 1 Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -5,9 +5,9 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        double a = 1;
+        double b = 1;
+        double c = 1;
 
         System.out.println(solveQuadraticEquation(a, b, c));
 
@@ -15,27 +15,28 @@ public class Task4 {
 
     static String solveQuadraticEquation(double a, double b, double c) {
 
-        double x1, x2;
-        double discriminant = b * b - 4 * a * c;
-
         if (a == 0 && b == 0 && c == 0) {
             return ("Решений бесконечно");
-        } else if (a == 0 && b != 0) {
-            x1 = -c / b;
-            return ("Количество решений: 1. Корень: " + (int) x1);
-        } else if (a == 0) {
-            return ("Количество решений: 0.");
         }
 
-        if (discriminant < 0) {
+        double x1, x2;
+
+        if (a == 0 && b != 0) {
+            x1 = -c / b;
+            return ("Количество решений: 1. Корень: " + (int) x1);
+        }
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (a == 0 || discriminant < 0) {
             return ("Количество решений: 0.");
-        } else if (discriminant == 0) {
+        }
+        if (discriminant == 0) {
             x1 = -b / (2 * a);
             return ("Количество решений: 1. Корень: " + (int) x1);
-        } else {
-            x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            return ("Количество решений: 2. Корни: " + (int) x1 + ";" + (int) x2);
         }
+        x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        return ("Количество решений: 2. Корни: " + (int) x1 + ";" + (int) x2);
     }
 }
