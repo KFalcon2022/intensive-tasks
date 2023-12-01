396dfeb9cb5d44629f346da465a1ed99ba732e1e
Oleg Klimchuk <koi196621@gmail.com>
2023-12-01 16:40:33 +0200

    chapter2.task10  KOI

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,45 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
+        String str1 = "radar";
+        String str2 = "hello";
+        String str3 = "А роза упала на лапу Азора";
 
+        if (isPalindrome(str1)) {
+            System.out.println(str1 + " - это палиндром.");
+        } else {
+            System.out.println(str1 + " - это не палиндром.");
+        }
+
+        if (isPalindrome(str2)) {
+            System.out.println(str2 + " - это палиндром.");
+        } else {
+            System.out.println(str2 + " - это не палиндром.");
+        }
+
+        if (isPalindrome(str3)) {
+            System.out.println(str3 + " - это палиндром.");
+        } else {
+            System.out.println(str3 + " - это не палиндром.");
+        }
     }
 
-    boolean isPalindrome(String inputString){
+    public static boolean isPalindrome(String inputString) {
         // Ваш код
-        return false;
+        // Убираем пробелы и приводим строку к нижнему регистру
+        inputString = inputString.replaceAll(" ", "").toLowerCase();
+//        inputString = inputString.toLowerCase();
+
+        int left = 0;
+        int right = inputString.length() - 1;
+
+        while (left < right) {
+            if (inputString.charAt(left) != inputString.charAt(right)) {
+                return false; // Если символы не совпадают, строка не палиндром
+            }
+            left++;
+            right--;
+        }
+        return true; // Если все символы совпадают, строка палиндром
     }
 }
--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,41 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
+        int number1 = 1000000; //220;
+        int number2 = 0; //284;
 
+        if (number1 == 0 || number2 == 0) {
+            System.out.println( "Должны бать меньше 1 000 000");
+//            return 0;
+        }
+
+        if ((number1 >= 1000000 & number2 >= 1000000)) {
+            System.out.println( "Должны бать меньше 1 000 000");
+        }
+
+        if (areFriendlyNumbers(number1, number2)) {
+            System.out.println(number1 + " и " + number2 + " являются дружественной парой чисел.");
+        } else {
+            System.out.println(number1 + " и " + number2 + " не являются дружественной парой чисел.");
+        }
+    }
+
+    public static boolean areFriendlyNumbers(int number1, int number2) {
+        int sum1 = getFriendlyPair(number1);
+        int sum2 = getFriendlyPair(number2);
+
+        return (sum1 == number2) && (sum2 == number1);
     }
 
     static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+        int sum = 0;
+
+        for (int i = 1; i <= m / 2; i++) {
+            if (m % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 }
