30add432dd83cfc8fedb9b95c014088e616889a1
Stas <shadrin060388@gmail.com>
2023-12-01 17:13:15 +0300

    Chapter 2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,30 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        int m = 1184;
+        System.out.println(getFriendlyPair(m));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int n = 0;
+        if (m > 0 && m < 1_000_000) {
+            for (int i = 1; i < m; i++) {
+                if (m % i == 0 && n < 1_000_000) {
+                    n += i;
+                }
+            }
+
+            int reverse_m = 0;
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    reverse_m += i;
+                }
+            }
+
+            if (reverse_m == m) {
+                return Math.max(n, m);
+            }
+        }
+        return -1;
     }
 }
