6339786763ab29aaa4e96773101be7878d6d1ca7
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-12-01 19:56:23 +0300

    solved chapter2.task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -8,6 +8,7 @@ public class Task6 {
 
         System.out.println(getNoc(24, 54));
         System.out.println(getNod(24, 54));
+        System.out.println(getNodByEuclideanAlgorithm(24, 54));
     }
 
     static int getNoc(int m, int n) {
@@ -17,21 +18,35 @@ public class Task6 {
 
     static int getNod(int m, int n) {
 
-        while (m != n) {
+        while (m != 0 || n != 0) {
             if (m > n) {
-                m -= n;
+                if (m % n == 0) {
+
+                    return n;
+                }
+                m %= n;
             } else {
-                n -= m;
+                if (n % m == 0) {
+
+                    return m;
+                }
+                n %= m;
             }
         }
 
-        return m;
+        return m + n;
     }
 
     static int getNodByEuclideanAlgorithm(int m, int n){
+        if (m % n == 0) {
 
+            return n;
+        }
+        if (n % m == 0) {
 
-        return m;
+            return m;
+        }
+
+        return m > n ? getNodByEuclideanAlgorithm(m % n, n) : getNodByEuclideanAlgorithm(m, n % m);
     }
-
 }
