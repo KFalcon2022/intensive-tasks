6b5e75de3c7e38bc294f58db2af2e922dd482ab4
Greem4 <i@greem4.ru>
2023-12-01 14:22:07 +0300

    chapter 2, task8.1

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,14 +5,10 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-        System.out.println(getHappyTicketChance(123456));
+        System.out.println(getHappyTicketChance());
     }
 
-    private static int calculateSumDigits(int number) {
-        return number / 100000 % 10 + number / 10000 % 10 + number / 1000 % 10 + number / 100 % 10 + number / 10 % 10 + number % 10;
-    }
-
-    static double getHappyTicketChance(int ticketNumber) {
+    static double getHappyTicketChance() {
         int count = 0;
         for (int i = 0; i <= 999999; i++) {
             int firstHalf = i / 1000;
@@ -21,13 +17,11 @@ public class Task8 {
                 count++;
             }
         }
-        if (ticketNumber >= 0 && ticketNumber <= 999999) {
-            int firstHalf = ticketNumber / 1000;
-            int secondHalf = ticketNumber % 1000;
-            if (calculateSumDigits(firstHalf) == calculateSumDigits(secondHalf)) {
-                return 1.0 / 999999;
-            }
-        }
         return (double) count / 999999;
+    }
+
+    private static int calculateSumDigits(int number) {
+        return number / 100000 % 10 + number / 10000 % 10 + number
+                / 1000 % 10 + number / 100 % 10 + number / 10 % 10 + number % 10;
     }
 }
