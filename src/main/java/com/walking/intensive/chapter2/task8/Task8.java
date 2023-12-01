96f9cd9942b1e14b27581e6117c94cb0d926971c
ganka-xn <jxn@inbox.ru>
2023-12-01 18:24:41 +0300

    Chapter 2, task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -6,10 +6,32 @@ package com.walking.intensive.chapter2.task8;
 public class Task8 {
     public static void main(String[] args) {
 
+        System.out.println(getHappyTicketChance());
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance() {
+        int count = 0;
+
+        for (int i = 0; i < 1000000; i++) {
+            if (isTicketHappy(i)) {
+                count++;
+            }
+        }
+
+        return count / 1000000.0;
     }
+
+    static boolean isTicketHappy(int ticketNumber) {
+        int leftSum = (ticketNumber / 100000) +
+                      (ticketNumber / 10000) % 10 +
+                      (ticketNumber / 1000) % 10;
+
+        int rightSum = (ticketNumber / 100) % 10 +
+                       (ticketNumber / 10) % 10 +
+                        ticketNumber % 10;
+
+        return leftSum == rightSum;
+    }
+
+
 }
