97c48d841f9b248605b739c39e60a08c31c302d0
U_M1CVT <akachamina@alfabank.ru>
2023-12-01 14:23:15 +0300

    Исправление по замечаниям

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 
-        System.out.println(getFlatLocation(5, 3, 60));
+        System.out.println(getFlatLocation(5, 3, 34));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
@@ -22,8 +22,8 @@ public class Task2 {
         int flatEntrance = (flatNumber / (apartmentOnFloor * floorAmount));
         int apartmentEntrance = (flatNumber - ((flatEntrance - 1) * floorAmount * apartmentOnFloor));
         int flatFloor = (apartmentEntrance / apartmentOnFloor);
-        String sideLift = ((apartmentEntrance % apartmentOnFloor == 1) || (apartmentEntrance % apartmentOnFloor == 2)) ? " слева" : " справа";
-        String roundAfterLift = (apartmentEntrance % 2 == 1) ? " влево" : " вправо";
+        String sideLift = ((apartmentEntrance % apartmentOnFloor == 1) || (apartmentEntrance % apartmentOnFloor == 2)) ? "слева" : " права";
+        String roundAfterLift = (apartmentEntrance % 2 == 1) ? "влево" : "вправо";
 
         String result = String.format(flatNumber + " кв - " + flatEntrance + " подъезд, " + flatFloor + " этаж, " + sideLift + " от лифта, " + roundAfterLift);
 
