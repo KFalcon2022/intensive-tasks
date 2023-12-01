cac8e0713bad9264f8a9ff68f58d1ddabbece3b1
egor.djun <xedshotg@gmail.com>
2023-12-01 11:56:08 +0700

    Исправил, но я сам не додумался, пришлось схемы подсматривать :(

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,41 +5,49 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-        int a = 13;
-        int b = 19;
+        int a = 15;
+        int b = 15;
 
-        System.out.println(getNoc(a, b));
-        System.out.println(getNod(a, b));
+        System.out.println("Наименьшее общее кратное чисел " + a + " и " + b + " = " + getNoc(a, b));
+        System.out.println("Наибольший общий делитель чисел " + a + " и " + b + " = " + getNod(a, b));
     }
 
     static int getNoc(int m, int n) {
+        if (m == n) {
+            return 1;
+        }
+
         int noc;
         int index = 1;
 
         while (true) {
             if (index % m == 0 && index % n == 0) {
                 noc = index;
+
                 break;
             }
+
             index++;
         }
+
         return noc;
     }
 
     static int getNod(int m, int n) {
-        int nod = 0;
+        if (m == n) {
+            return m;
+        }
 
-        for (int i = Integer.MAX_VALUE; i >= 1; i--) {
-            if (m % i == 0 && n % i == 0) {
-                nod = i;
-                break;
+        int numberOne = m;
+        int numberTwo = n;
+        while (numberOne != numberTwo) {
+            if (numberOne < numberTwo) {
+                numberTwo = numberTwo - numberOne;
+            } else {
+                numberOne = numberOne - numberTwo;
             }
         }
-        return nod;
-    }
 
-    static int getNodByEuclideanAlgorithm(int m, int n) {
-
-        return 0;
+        return numberOne;
     }
 }
