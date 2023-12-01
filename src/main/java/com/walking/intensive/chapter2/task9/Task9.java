14e1dd3c79979d27f40faa7a5416951165d50d54
ganka-xn <jxn@inbox.ru>
2023-12-01 20:58:53 +0300

    Chapter 2, task 9

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,11 +5,36 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
-
+        getPascalTriangle(40);
     }
 
-    static String getPascalTriangle(int n){
-        // Ваш код
-        return null;
+    static void getPascalTriangle(int m) {
+        int widthMax = getPascalLine(m - 1).length();
+
+        for (int n = 0; n < m; n++) {
+            String line = getPascalLine(n);
+            int width = line.length();
+            for (int i = 0; i < (widthMax - width) / 2; i++) {
+                System.out.print(" ");
+            }
+            System.out.println(line);
+        }
+    }
+
+    static String getPascalLine(int n){
+        long current, previous = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int k = 0; k <= n; k++) {
+            if (k == 0) {
+                current = 1;
+            } else {
+                current = previous * (n + 1 - k) / k;
+            }
+            sb.append(current).append(" ");
+            previous = current;
+        }
+
+        return sb.toString();
     }
 }
