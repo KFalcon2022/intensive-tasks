5aab9bf52725a47cfb77abfaff744f86b356a2fb
ganka-xn <jxn@inbox.ru>
2023-12-01 16:24:00 +0300

    Chapter 2, task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,47 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
 
+        System.out.println(getFriendlyPair(1000000));
+        System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int maxFriendly = 0;
+//        int minFriendly = 0;
+        int maxFriendlySum = 0;
+
+        for (int testA = m - 1; testA > 1; testA--) {
+            int testB = getDivisorsSum(testA);
+            if (testB < m && testB != testA) {
+                int divisorsSumB = getDivisorsSum(testB);
+                if (testA == divisorsSumB && maxFriendlySum < (testA + testB) ) {
+                    maxFriendlySum = testA + testB;
+                    maxFriendly = testA;
+//                    minFriendly = testB;
+                }
+            }
+        }
+
+//        System.out.printf("Friendly pair: %d, %d\n", maxFriendly, minFriendly);
+        return maxFriendly;
+    }
+
+    public static int getDivisorsSum(int a) {
+        int divisorsSum = 1;
+        int i = 2;
+
+        while (i * i < a) {
+            if (a % i == 0) {
+                divisorsSum += i;
+                if (i != (a / i)) {
+                    divisorsSum += a / i;
+                }
+            }
+            i++;
+        }
+
+        return divisorsSum;
     }
 }
