3fcc5de1e48c8b906717d98953f8dd247fc9dc5a
Pavlo <pavlokutsyn@gmail.com>
2023-12-01 11:50:09 +0100

    Chapter2_Task10 исправил автоматические изменения IDE

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,31 +5,38 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
+        Task10 task = new Task10();
         String testString = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
-        System.out.println("Is palindrome: " + isPalindrome(testString));
+        System.out.println("Is palindrome: " + task.isPalindrome(testString));
     }
 
-    static boolean isPalindrome(String inputString) {
+    boolean isPalindrome(String inputString) {
         String cleanedString = "";
+
         for (int i = 0; i < inputString.length(); i++) {
             char currentChar = inputString.charAt(i);
             if (Character.isLetterOrDigit(currentChar)) {
                 cleanedString += currentChar;
             }
         }
+
         cleanedString = cleanedString.toLowerCase();
+
         int leftIndex = 0;
         int rightIndex = cleanedString.length() - 1;
 
         while (leftIndex < rightIndex) {
             char leftChar = cleanedString.charAt(leftIndex);
             char rightChar = cleanedString.charAt(rightIndex);
+
             if (leftChar != rightChar) {
                 return false;
             }
+
             leftIndex++;
             rightIndex--;
         }
+
         return true;
     }
 }
