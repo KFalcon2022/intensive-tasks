0e8c4d97f744db03ed4bbfd80ce87ead93c9f4e1
dimapros <dima.ru.98@mail.ru>
2023-12-02 01:07:33 +1000

    Сhapter2.Task10.

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,36 +5,48 @@ import java.util.Scanner;
 public class Task10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String receivedSuggestion = sc.nextLine();
+        String inputString = sc.nextLine();
 
-        System.out.println(isPalindrome(receivedSuggestion));
+        System.out.println(isPalindrome(inputString));
     }
 
-    static boolean isPalindrome(String receivedSuggestion){
-        String suggestion = receivedSuggestion;
-        suggestion = getFormatString(suggestion);
+    static boolean isPalindrome(String inputString) {
+        StringBuilder suggestion1 = new StringBuilder(inputString.replaceAll("\\s", ""));
+        StringBuilder suggestion2 = new StringBuilder(inputString.replaceAll("\\s", ""));
 
-        for(int i = 0; i < suggestion.length(); i++){
-            if(suggestion.charAt(i) != getReverseString(suggestion)[i]){
-                return false;
+        suggestion1 = getFormatStringBuilder(suggestion1);
+        suggestion2 = getFormatStringBuilder(suggestion2);
+        suggestion1.reverse();
+
+        if (suggestion1.length() <= 1) {
+            return false;
+        }
+
+        if (suggestion1.toString().toLowerCase().equals(suggestion2.toString().toLowerCase())) {
+            return true;
+        }
+        return false;
+    }
+
+    private static StringBuilder getFormatStringBuilder(StringBuilder suggestion) {
+        final String PUNCTUATION = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < suggestion.length(); i++) {
+            boolean hasPunctuation = false;
+
+            for (int j = 0; j < PUNCTUATION.length(); j++) {
+
+                if (suggestion.charAt(i) == PUNCTUATION.charAt(j)) {
+                    hasPunctuation = true;
+                    break;
+                }
+            }
+
+            if (!hasPunctuation) {
+                answer.append(suggestion.charAt(i));
             }
         }
-        return true;
-    }
-
-    private static char[] getReverseString(String suggestion){
-        char[] newSuggestion = new char[suggestion.length()];
-
-        for(int i = 0; i < suggestion.length(); i++){
-            newSuggestion[(suggestion.length() - 1) - i] = suggestion.charAt(i);
-        }
-        return newSuggestion;
-    }
-
-    private static String getFormatString(String suggestion){
-        suggestion = (suggestion.toLowerCase()).replaceAll("\\p{IsPunctuation}", "");
-        suggestion = suggestion.replaceAll(" ", "");
-
-        return suggestion;
+        return answer;
     }
 }
