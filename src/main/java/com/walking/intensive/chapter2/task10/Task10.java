7e25767dbeace1f1536986e345a5351cb4bac02e
Pavlo <pavlokutsyn@gmail.com>
2023-12-01 11:40:49 +0100

    Chapter2 Task10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,31 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        String testString = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
+        System.out.println("Is palindrome: " + isPalindrome(testString));
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    static boolean isPalindrome(String inputString) {
+        String cleanedString = "";
+        for (int i = 0; i < inputString.length(); i++) {
+            char currentChar = inputString.charAt(i);
+            if (Character.isLetterOrDigit(currentChar)) {
+                cleanedString += currentChar;
+            }
+        }
+        cleanedString = cleanedString.toLowerCase();
+        int leftIndex = 0;
+        int rightIndex = cleanedString.length() - 1;
+
+        while (leftIndex < rightIndex) {
+            char leftChar = cleanedString.charAt(leftIndex);
+            char rightChar = cleanedString.charAt(rightIndex);
+            if (leftChar != rightChar) {
+                return false;
+            }
+            leftIndex++;
+            rightIndex--;
+        }
+        return true;
     }
 }
