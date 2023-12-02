48622caaca5beba17783f005cbaceeb41aa5e302
Константин Синяков <k.siniakov@yandex.ru>
2023-12-03 02:45:16 +0300

    Chapter 2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,23 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome("a"));
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+        char[] badSymbols = {'!', '?', ' ', ',', '.', '-', '\'', '\"'};//
+        for (char symbol : badSymbols
+        ) {
+            inputString = inputString.replace(String.valueOf(symbol), "");
+        }
+        inputString = inputString.toLowerCase();
+
+        for (int i = 0; i < inputString.length() / 2; i++) {
+            if (inputString.charAt(i) != inputString.charAt(inputString.length() - (i + 1))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
