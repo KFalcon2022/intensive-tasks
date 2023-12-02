76aa372be7f4530b4564bafc167074d2a1a2bab9
Boris <aksenov.boris8595@gmeil.com>
2023-12-02 20:53:24 +0500

    Решение без регулярки. Но пришлось добавить строку с цифрами и английскими буквами.

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,12 +5,26 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Madam, I'm Adam ??..!:^"));
+        System.out.println(isPalindrome("Niagara o roar ag!!/.!ain"));
     }
 
-    static boolean isPalindrome(String inputString){
-        inputString = inputString.replaceAll("[^A-Za-z0-9]", "");
-        StringBuilder sbReverse = new StringBuilder(inputString.replaceAll("[^A-Za-z0-9]", "")).reverse();
-        return inputString.equalsIgnoreCase(sbReverse.toString());
+    static boolean isPalindrome(String inputString) {
+        String strListAdditionalCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        inputString = inputString.toLowerCase();
+        int counterFromBeginning = 0;
+        int counterFromEnd = inputString.length() - 1;
+        while (counterFromBeginning <= counterFromEnd) {
+            if (strListAdditionalCharacters.indexOf(inputString.charAt(counterFromBeginning)) != -1 && strListAdditionalCharacters.indexOf(inputString.charAt(counterFromEnd)) != -1) {
+                if (inputString.charAt(counterFromBeginning) != inputString.charAt(counterFromEnd)) {
+                    return false;
+                }
+                counterFromEnd--;
+                counterFromBeginning++;
+            } else {
+                counterFromBeginning += strListAdditionalCharacters.indexOf(inputString.charAt(counterFromBeginning)) == -1 ? 1 : 0;
+                counterFromEnd += strListAdditionalCharacters.indexOf(inputString.charAt(counterFromEnd)) == -1 ? -1 : 0;
+            }
+        }
+        return true;
     }
 }
