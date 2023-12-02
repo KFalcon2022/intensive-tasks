47884ceb386dd8d0f3b819cf4521fa59ef50e219
lambo064 <tasha_lambo@mail.ru>
2023-12-02 22:12:33 +0300

    chapter 2 task 9

--- src/main/java/com/walking/intensive/chapter2/task10/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Main.java
@@ -1,14 +1,40 @@
 package com.walking.intensive.chapter2.task10;
 
+import static java.lang.Character.isLetter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("rerer"));
+        System.out.println(isPalindrome("aaavaaa"));
     }
 
     public static boolean isPalindrome(String example) {
-        String str = example.replaceAll(" ", "").replaceAll("\\p{Punct}", "");
+        String stringWithoutPunctuation = removePunctuation(example);
+        return stringWithoutPunctuation.equalsIgnoreCase(rewerseString(stringWithoutPunctuation));
+    }
 
-        return str.equalsIgnoreCase(String.valueOf(new StringBuilder(str).reverse()));
+    public static String removePunctuation(String string) {
+
+        String stringWithoutPunctuation = "";
+
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (isLetter(c)) {
+                stringWithoutPunctuation += c;
+            }
+        }
+        return stringWithoutPunctuation;
+    }
+
+    public static String rewerseString(String string) {
+
+            char[] array = string.toCharArray();
+            String result = "";
+
+            for (int i = array.length - 1; i >= 0; i--) {
+                result += array[i];
+            }
+
+            return result;
     }
 
 
