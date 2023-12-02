0ad7f7227646b9cb2fa28aa43c7d5b18e7050cd9
Captain-Greg <antonovgeorgij12@gmail.com>
2023-12-02 12:58:31 +0300

    Chapter 1 Task 3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -9,9 +9,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-            return true;
-        }
-        return false;
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
     }
 }
