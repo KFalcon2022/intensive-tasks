2c339cd773c52bc3b900d26bc869afa612bd7dce
BogdanKaliuzhniy <kaliuzhnb@gmail.com>
2023-12-02 18:20:13 +0300

    Chapter 1, task 3, fixed v2

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -14,7 +14,7 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-            return year % 400 == 0 && year % 100 == 0 || year % 100 != 0 && year % 4 == 0;
+        return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
     }
 
 }
