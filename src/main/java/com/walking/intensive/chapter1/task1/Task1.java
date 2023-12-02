56af92c70a2356438ddaec88231391c11939dc56
Legion <heaven.brain@gmail.com>
2023-12-02 20:41:05 +0500

    Completed 1 task

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,14 +6,23 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 1;
+        while (age < 127) {
+            age++;
+            System.out.println(getAgeString(age));
+        }
 
-        System.out.println(getAgeString(age));
+
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        if (age % 10 == 1 && age % 100 != 11) {
+            return ("Вам " + age + " год");
+        } else if ((age % 10) >= 2 && (age % 10) <= 4 && ((age % 100) < 10 || (age % 100) >= 20)) {
+            return ("Вам " + age + " года");
+        } else {
+            return ("Вам " + age + " лет");
+        }
 
-        return null; // Заглушка. При реализации - удалить
     }
 }
