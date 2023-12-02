00b4f33d39a85532078359e82ca1ad10c9c27233
BogdanKaliuzhniy <kaliuzhnb@gmail.com>
2023-12-02 11:17:45 +0300

    Chapter 1, task 4, refactored

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -8,8 +8,8 @@ import java.lang.Math;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
+        double a = 1;
+        double b = 3;
         double c = 0;
 
         System.out.println(solveQuadraticEquation(a, b, c));
@@ -28,65 +28,33 @@ public class Task4 {
      */
 
     static String solveQuadraticEquation(double a, double b, double c) {
-        double x1;
-        double x2;
-        double discriminant;
 
         if (a == 0 && a == b && b == c) {
             return "Решений бесконечно";
-        } else if (a != 0 && b == 0 && c == 0) {
-            return "Количество решений: 1. Корень: 0";
-        } else if (a == 0) {
-            return solveLinearEquation(b, c);
-        } else if (b != 0 && c != 0) {
-            discriminant = Math.pow(b, 2) - 4 * a * c;
-            if (discriminant < 0) {
-                return "Количество решений: 0.";
-            } else if (discriminant == 0) {
-                x1 = -b / 2 * a;
-                return "Количество решений: 1. Корень: " + (int) x1;
-            } else {
-                x1 = (-b - Math.sqrt(discriminant)) / 2 * a;
-                x2 = (-b + Math.sqrt(discriminant)) / 2 * a;
-                return "Количество решений: 2. Корни: " + compareRoots(x1, x2);
-            }
-        } else {
-            return solveIncompleteQuadraticEquation(a, b, c);
         }
-    }
 
-    static String solveIncompleteQuadraticEquation(double a, double b, double c) {
-        double x1;
-        double x2;
-
-//      Если b == 0, то квадратное уравнение принимает вид неполного уравнения: ax^2 + c = 0
-        if (a != 0 && b == 0) {
-            if (-c / a > 0) {
-                x1 = Math.sqrt(-c / a);
-                x2 = -Math.sqrt(-c / a);
-                return "Количество решений: 2. Корни: " + compareRoots(x1, x2);
-            }
-//      Если c == 0, то квадратное уравнение принимает вид неполного уравнения: ax^2 + bx = 0.
-        } else if (a != 0 && c == 0) {
-            x1 = -b / a;
-            return "Количество решений: 2. Корни: " + compareRoots(x1, 0); // Один из корней всегда равен нулю.
-        }
-        return "Количество решений: 0.";
-    }
-
-    static String solveLinearEquation(double b, double c) {
-        double x;
-
-//      Если a == 0, то это линейное уравнение типа bx + c = 0;
-        if (b != 0) {
-            x = c / b;
-            return "Количество решений: 1. Корень: " + (int) x;
-        } else {
+        double discriminant = b * b - 4 * a * c;
+        if (a == 0 && b == 0 || discriminant < 0) {
             return "Количество решений: 0.";
         }
-    }
 
-    static String compareRoots(double x1, double x2) {
-        return (x1 < x2 ? (int) x1 + ";" + (int) x2 : (int) x2 + ";" + (int) x1);
+        if (a == 0) {
+            return "Количество решений: 1. Корень: " + (int) (c / b);
+        }
+
+        if (c == 0 && b != 0) {
+            double singleRoot = -b / a;
+            return "Количество решений: 2. Корни: "
+                    + (int) Math.min(singleRoot, 0) + ";" + (int) Math.max(singleRoot, 0);
+        }
+
+        if (discriminant > 0) {
+            double firstRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+            double secondRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+            return "Количество решений: 2. Корни: " +
+                    (int) Math.min(firstRoot, secondRoot) + ";" + (int) Math.max(firstRoot, secondRoot);
+        } else {
+            return "Количество решений: 1. Корень: " + (int) (-b / (2 * a));
+        }
     }
 }
