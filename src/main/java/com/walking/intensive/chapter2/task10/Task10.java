2d6f2f5288af2f6aa518202a83e7e467ba8912fe
tvadim1 <tvadim1@yandex.ru>
2023-12-02 18:17:21 +0600

    Исправлено

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,7 +1,6 @@
 package com.walking.intensive.chapter2.task10;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
@@ -9,10 +8,8 @@ import java.nio.charset.StandardCharsets;
 public class Task10 {
     public static void main(String[] args) {
         /* Онлайн компилятор, где кодировка windows-1251 работает:
-
         https://www.jdoodle.com/online-java-compiler
-
-         */
+        */
 
         String palindrome = "Учуя молоко, я около мяучу";
         String encodedInputString;
@@ -20,9 +17,9 @@ public class Task10 {
         try {
             byte[] byteArray = palindrome.getBytes("windows-1251");
             encodedInputString = new String(byteArray, "windows-1251");
-                // пробовал и с StandardCharsets.UTF_8
-                // проверка работает и так, и так
-                // нужно для корректного отображения в консоли
+            // пробовал и с StandardCharsets.UTF_8
+            // проверка работает и так, и так
+            // нужно для корректного отображения в консоли
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
@@ -32,26 +29,27 @@ public class Task10 {
 
     public static boolean isPalindrome(String inputString) {
         char[] symbol = inputString.toCharArray();
-        //System.out.println(symbol);
         StringBuilder formattedPalindrome = new StringBuilder();
 
-        for (int i = 0; i < symbol.length; i++) {               //фильтруем по char
-            if ((symbol[i] >= 1040 && symbol[i] <= 1103) ||     //UTF-8 Cyrillic (А-Я, а-я)
+        for (int i = 0; i < symbol.length; i++) {
+            //далее комментарий для себя, чтобы знать как было изначально
+            /*if ((symbol[i] >= 1040 && symbol[i] <= 1103) ||     //UTF-8 Cyrillic (А-Я, а-я)
                     symbol[i] == 1025 ||                        //буква Ё
                     symbol[i] == 1105 ||                        //буква ё
                     (symbol[i] >= 65 && symbol[i] <= 90) ||     //UTF-8 Basic Latin (A-Z)
-                    (symbol[i] >= 97 && symbol[i] <= 122)       //UTF-8 Basic Latin (a-z)
-            ) {
+                    (symbol[i] >= 97 && symbol[i] <= 122))      //UTF-8 Basic Latin (a-z)*/
+            if (Character.isLetter(symbol[i])) {
                 formattedPalindrome.append(symbol[i]);
             }
         }
+
         String upcasedPalindrome = formattedPalindrome.toString().toUpperCase();
-        //System.out.println(upcasedPalindrome);
 
         for (int i = 0; i < upcasedPalindrome.length() / 2; i++) {
-            if (upcasedPalindrome.charAt(i) !=
-                    upcasedPalindrome.charAt(upcasedPalindrome.length() - 1 - i)) return false;
-
+            if (upcasedPalindrome.charAt(i)
+                    != upcasedPalindrome.charAt(upcasedPalindrome.length() - 1 - i)) {
+                return false;
+            }
         }
 
         return true;
