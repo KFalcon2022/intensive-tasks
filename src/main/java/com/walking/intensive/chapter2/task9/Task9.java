2479ae00d8f94a8702ca9b1ef1044e1410fb2a0b
Константин Синяков <k.siniakov@yandex.ru>
2023-12-03 02:00:56 +0300

    Chapter 2 Task 9

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,11 +5,72 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
-
+        System.out.println(getPascalTriangle(20));
     }
 
-    static String getPascalTriangle(int n){
-        // Ваш код
-        return null;
+    static String getPascalTriangle(int n) {
+        String[] data = new String[n];
+        String buff = "";
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            buff = getSummedLine(buff);
+            data[i] = buff;
+        }
+
+        int maxLineLength = buff.length();
+
+        for (int i = 0; i < n; i++) {
+            result = result.concat(repeatSpace((maxLineLength - data[i].length()) / 2) + data[i] + "\n");
+        }
+
+        return result;
     }
+
+    static String repeatSpace(int n) {
+        return " ".repeat(Math.max(0, n));
+    }
+
+    private static String getSummedLine(String line) {
+        if (line.isEmpty()) {
+            return "1";
+        }
+
+        if (line.length() == 1) {
+            return "1 1";
+        }
+
+        String result = "";
+        boolean hasLeft = false, hasRight = false;
+        int left = 0, right = 0;
+        int buffer = 0;
+        char activeSymbol;
+
+        for (int i = 0; i < line.length(); i++) {
+            activeSymbol = line.charAt(i);
+            if (' ' == activeSymbol) {
+                if (!hasLeft) {
+                    hasLeft = true;
+                    left = buffer;
+                } else {
+                    hasRight = true;
+                    right = buffer;
+                }
+
+                if (hasRight) {
+                    result = result.concat(" " + (left + right));
+                    left = right;
+
+                    hasRight = false;
+                }
+                buffer = 0;
+            } else {
+                buffer = buffer * 10 + (activeSymbol - '0');
+            }
+        }
+
+        return "1" + result + " " + (left + buffer) + " 1";
+    }
+
+
 }
