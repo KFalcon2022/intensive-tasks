c74514d4a81f71304ec65fe14b5fdfd4bf3bd60d
Alexey <lolex1@mail.ru>
2023-12-02 13:21:37 +0300

    Chapter 2 Task7

--- src/main/java/com/walking/intensive/chapter2/task7/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Main.java
@@ -2,56 +2,44 @@ package com.walking.intensive.chapter2.task7;
 
 public class Main {
     public static void main(String[] args) {
-        int numberN = 17296;
-        int numberM = 18416;
-        System.out.println(friendlyNumbs(numberN, numberM));
 
+        int numberN = 999999;
+        System.out.println(getFriendlyNumbs(numberN));
     }
 
-    public static boolean isNotCorrectInput(int n, int m){
+    public static boolean isNotCorrectInput(int n) {
 
-       return n == m || ((n < 0 || n > 999_999) || (m < 0 || m > 999_999));
+        return n < 0 || n > 999_999;
     }
 
-    public static boolean isFriendlyNumbs(int n, int totalN, int m, int totalM){
+    public static int getFriendlyNumbs(int numberN) {
 
-        return n == totalM & m == totalN;
-    }
-
-    public static int friendlyNumbs(int numberN, int numberM) {
-
-        if (isNotCorrectInput(numberN , numberM)){
+        if (isNotCorrectInput(numberN)) {
 
             return 0;
         }
 
-        int totalN = getTotalN(numberN);
-        int totalM = getTotalM(numberM);
-        int total = 0;
-
-        if (isFriendlyNumbs(numberN, totalN, numberM, totalM)){
-
-            total = Math.min(totalN, totalM);
-        }
-
-        return total;
+        return getMinTotalN(numberN);
     }
 
-    public static int getTotalN(int numberN){
+    public static int getMinTotalN(int numberN) {
 
-        int totalN = 0;
+        int total = 0;
 
-        for (int i = numberN / 2; i > 0 ; i--){
+        for (int i = numberN; i > 0; i--) {
 
-            if (numberN % i == 0) {
-                totalN += i;
+            total = getTotalM(i, numberN);
+
+            if (total <= i && getTotalM(total, i) == i) {
+
+                return total;
             }
         }
 
-        return totalN;
+        return 0;
     }
 
-    public static int getTotalM(int numberM) {
+    public static int getTotalM(int numberM, int checkNumb) {
 
         int totalM = 0;
 
@@ -60,15 +48,16 @@ public class Main {
             if (numberM % i == 0) {
                 totalM += i;
 
-                if (totalM > 999_999) {
-
+                if (totalM > checkNumb) {
                     return 0;
                 }
             }
         }
+        if (totalM == numberM) {
+
+            return 0;
+        }
 
         return totalM;
     }
-
-
 }
