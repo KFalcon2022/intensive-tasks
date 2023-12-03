39ae6d11cebb3253dc4fc47ba76d2b7d05027065
egor.djun <xedshotg@gmail.com>
2023-12-03 13:35:19 +0700

    Chapter2, Task8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,40 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println(getHappyTicketChance());
     }
 
     static double getHappyTicketChance() {
-        // Ваш код
-        return 0.0;
+        double countLuckyTicket = 0.0;
+        int maxNumber = 999_999;
+
+        for (int i = 0; i <= maxNumber; i++) {
+            if (isLucky(i)) {
+                countLuckyTicket++;
+            }
+        }
+
+        return countLuckyTicket / maxNumber;
+    }
+
+    static boolean isLucky(int number) {
+        String string = String.valueOf(number);
+
+        while (string.length() % 6 != 0) {
+            string = "0" + string;
+        }
+
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int i = 0; i < string.length() / 2; i++) {
+            num1 = num1 + Integer.parseInt(String.valueOf(string.charAt(i)));
+        }
+
+        for (int i = string.length() / 2; i < string.length(); i++) {
+            num2 = num2 + Integer.parseInt(String.valueOf(string.charAt(i)));
+        }
+
+        return num1 == num2;
     }
 }
