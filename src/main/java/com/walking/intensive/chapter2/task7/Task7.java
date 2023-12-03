c4e06e6859bfc9199d6e8d6b3907c324fdb5a34c
DearCuba <rovbo85@gmail.com>
2023-12-03 11:42:03 +0500

    Выполнил задание 7 в главе 2.

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -4,12 +4,27 @@ package com.walking.intensive.chapter2.task7;
  * Условие: <a href="https://geometry-math.ru/homework/java-friendly-number.html">ссылка</a>
  */
 public class Task7 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int m = 1000000;
+        System.out.println("Нибольшее число из пары дружественных чисел: " + getFriendlyPair(m));
     }
 
     static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+
+        while (m != getSumOfDivisors(getSumOfDivisors(m)) || getSumOfDivisors(m) >= m) {
+            m--;
+        }
+        return m;
+    }
+
+    static int getSumOfDivisors(int m) {
+        int sumOfDivisors = 0;
+        for (int i = 1; i <= m / 2; i++) {
+            if (m % i == 0) {
+                sumOfDivisors +=  i;
+            }
+        }
+        return sumOfDivisors;
     }
 }
