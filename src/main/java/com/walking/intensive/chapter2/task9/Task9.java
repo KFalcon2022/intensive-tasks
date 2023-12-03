b04322e793f8ee5460e1dfee622d60f67838e3f6
k2336351 <k2336351@yandex.ru>
2023-12-04 02:26:48 +0300

    Solution Chapter 2  Task 9

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,11 +5,34 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
-
+        System.out.println(getPascalTriangle(25));
     }
 
-    static String getPascalTriangle(int n){
-        // Ваш код
-        return null;
+    static String getPascalTriangle(int n) {
+        String result = String.format("%s", "");
+        int maxRowLength = getRow(n).length();
+
+        for (int i = 0; i < n; i++) {
+            String row = getRow(i);
+            int spacesQuantity = (maxRowLength - row.length()) / 2;
+            String spaces = String.format("%" + spacesQuantity + "s", " ");
+
+            result = result.concat(spaces).concat(row).concat("\n");
+        }
+        return result;
+    }
+
+    static String getRow(int rowIndex) {
+        long element = 1L; // element(0,0) = 0! / (0! * (0 - 0)!)
+        String rowResult = String.format("%d%s", element, " ");
+
+        for (int i = 0; i < rowIndex; i++) {
+            element = element * (rowIndex - i) / (i + 1);
+            String elementResult = String.format("%d%s", element, " ");
+
+            rowResult = rowResult.concat(elementResult);
+        }
+
+        return rowResult;
     }
 }
