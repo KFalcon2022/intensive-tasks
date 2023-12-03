8efd538819e7a9cbac39d92462ca8ca4bb0aeb6f
SenyaDiamond <SenyaDiamond@gmail.com>
2023-12-03 10:58:30 +0300

    Chapter 2 Task 7 v 1.2

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,41 +5,36 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
     }
 
     static int getFriendlyPair(int m){
+        int maxFriendlyNumber = 0;
+        int maxSumFriendlyNumbers = 0;
+
         if (m >= 1_000_000) {
             return 0;
         }
-
-        int maxFriendlyNumber = 0;
-        int maxSumFriendlyNumbers = 0;
         for (int i = m; i > 1 ; i--)
         {
-            long firstNumberDivisorsSum = getDivisorsSum(i);
-            for (int j = 1; j < m ; j++) {
-                long secondNumberDivisorsSum = getDivisorsSum(j);
-                if (i != j && firstNumberDivisorsSum == j && secondNumberDivisorsSum == i && (i + j > maxSumFriendlyNumbers)) {
-                    maxSumFriendlyNumbers = i + j;
-                    maxFriendlyNumber = Math.max(i, j);
-                }
+            int firstNumberDivisorsSum = getDivisorsSum(i);
+            int secondNumberDivisorsSum = getDivisorsSum(firstNumberDivisorsSum);
+
+            if (i == secondNumberDivisorsSum && firstNumberDivisorsSum != secondNumberDivisorsSum && (i + secondNumberDivisorsSum) > maxSumFriendlyNumbers) {
+                maxSumFriendlyNumbers = i + secondNumberDivisorsSum;
+                maxFriendlyNumber = i;
             }
         }
         return maxFriendlyNumber;
     }
 
-    static long getDivisorsSum(int a) {
-        long sum = 0;
-        if (a > 0) {
-            for (int i = 1; i < a; i++) {
-                if (a % i == 0) {
-                    sum += i;
-                }
+    static int getDivisorsSum(int a) {
+        int sum = 0;
+
+        for (int i = 1; i < a; i++) {
+            if (a % i == 0) {
+                sum += i;
             }
-            return sum;
-        } else {
-            return 0;
         }
+        return sum;
     }
 }
