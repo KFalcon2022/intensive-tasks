177f523aa4b7be1b2c38b5ed1f778cc4a99f3af6
TitLukich <Nef-92@yandex.ru>
2023-12-03 16:14:20 +0300

    Chapter 3, Task 12

--- src/main/java/com/walking/intensive/chapter3/task12/Task12.java
+++ src/main/java/com/walking/intensive/chapter3/task12/Task12.java
@@ -1,13 +1,22 @@
 package com.walking.intensive.chapter3.task12;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-balls.html">МЯЧИКИ</a>
  */
 public class Task12 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Введите строку: ");
+        String baskets = scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println(Arrays.toString(getNumberOfMovements(baskets)));
     }
-
     /**
      * Подсчет минимального количества операций, необходимых для перемещения всех мячиков в i-ю ячейку.
      *
@@ -15,7 +24,14 @@ public class Task12 {
      * @return массив с минимальным количеством операций
      */
     static int[] getNumberOfMovements(String baskets) {
-        // Ваш код
-        return new int[]{};
+        int[] answer = new int[baskets.length()];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer.length; j++) {
+                answer[i] += Character.getNumericValue(baskets.charAt(j)) * Math.abs(j - i);
+            }
+        }
+
+        return answer;
     }
 }
