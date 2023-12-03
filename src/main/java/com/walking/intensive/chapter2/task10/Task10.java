94fcc236d63117accbb185d9994e7cea09ba5b75
SenyaDiamond <SenyaDiamond@gmail.com>
2023-12-03 10:00:26 +0300

    Chapter 2 Task 10 v 1.1

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -9,26 +9,22 @@ public class Task10 {
     public static void main(String[] args) {
         System.out.println(isPalindrome("Was it a car or a cat I saw!!!"));
         System.out.println(isPalindrome("Caroline please!"));
-
     }
 
     static boolean isPalindrome(String inputString) {
         String clearLetters = "";
+        String reverseString;
+
         for (int i = 0; i < inputString.length(); i++) {
-            clearLetters += Character.isLetter(inputString.charAt(i)) ? inputString.charAt(i) : "";
+            if (Character.isLetter(inputString.charAt(i))) {
+                clearLetters += inputString.charAt(i);
+            }
         }
         clearLetters = clearLetters.toLowerCase();
-        String reverseString = "";
+        reverseString = "";
         for (int i = clearLetters.length() - 1; i >= 0; i--) {
             reverseString += clearLetters.charAt(i);
-
         }
-        //System.out.println(clearLetters);
-        //System.out.println(reverseString);
-        if (reverseString.equals(clearLetters)) {
-            return true;
-        } else {
-            return false;
-        }
+        return reverseString.equals(clearLetters);
     }
 }
