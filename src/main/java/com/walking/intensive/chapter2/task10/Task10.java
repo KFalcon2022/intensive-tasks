543ade34e42b979310189b9951d3528af6ad1fe5
njmist <njmist@gmail.com>
2023-12-04 22:55:12 +0300

    chapter 2 task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-        String inputString = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
+        String inputString = "Муза!!! Ранясь шилом опыта, ты помолишься на разум!!!";
 
         if (isPalindrome(inputString)) {
             System.out.println("Строка является палиндромом");
@@ -15,19 +15,26 @@ public class Task10 {
     }
 
     static boolean isPalindrome(String inputString) {
-        String letterString = "";
+        if (inputString == "" || inputString == null) { // Проверим, есть ли смысл методу работать
+            return false;
+        }
 
-        for (int i = 0; i < inputString.length(); i++) {
-            if (Character.isLetterOrDigit(inputString.charAt(i))) {
-                letterString = letterString + inputString.charAt(i);
+        for (int i = 0, j = inputString.length() - 1; i < inputString.length() / 2 || j > inputString.length() / 2; i++, j--) { // Разбиваю строку пополам и иду по символам сразу с обоих концов
+            if (!Character.isLetterOrDigit(inputString.charAt(i))) { // Если символ в первой половине не буква и не цифра,
+                j++;                                                 // пропускаю эту итерацию, пропуская этот символ,
+                continue;                                            // но не пропуская тот символ во второй половине, что должен был быть в прошлой итерации
+            }
+
+            if (!Character.isLetterOrDigit(inputString.charAt(j))) { // Аналогично предыдущей проверке, но для второй половины строки
+                i--;
+                continue;
+            }
+
+            if (inputString.toLowerCase().charAt(i) != inputString.toLowerCase().charAt(j)) { // И если буква или цифра с первой половины не совпадает с её отражением со второй половины, то это не палиндром
+                return false;
             }
         }
-        String reverseLetterString = "";
 
-        for (int i = 0; i < letterString.length(); i++) {
-            reverseLetterString = letterString.charAt(i) + reverseLetterString;
-        }
-
-        return reverseLetterString.equalsIgnoreCase(letterString);
+        return true; // А если прошли все проверки, то палиндром! И обошлись без создания строк
     }
 }
