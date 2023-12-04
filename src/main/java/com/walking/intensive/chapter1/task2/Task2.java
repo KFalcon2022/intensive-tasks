522cc1f0bc6b46f73e86b09fc0b5261cd9f54e68
Akarumai <yzavgoro124@gmail.com>
2023-12-04 11:36:24 +0300

    task2_version4

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -5,26 +5,37 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-        int floorAmount = -10; //Кол-во этажей
-        int entranceAmount = -1; //Кол-во подъездов
+
+        int floorAmount = 10; //Кол-во этажей
+        int entranceAmount = 1; //Кол-во подъездов
         int flatNumber = 15; //Номер квартиры
+
         System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        String flatLocation = "";
-        if (flatNumber <= 0 || floorAmount <= 0 || entranceAmount <= 0)
+
+        final int flatsOnFloor = 4;//Кол-во квартир на одном этаже
+
+        if (flatNumber <= 0 || floorAmount <= 0 || entranceAmount <= 0) {
             return "Ошибка ввода. Число не может быть отрицательным или равным нулю.";
-        if (flatNumber > 4 * floorAmount * entranceAmount)
+        }
+
+        if (flatNumber > flatsOnFloor * floorAmount * entranceAmount) {
             return "Квартира номер " + flatNumber + " отсутсвует в этом доме.";
-        int entranceNumber = (flatNumber - 1) / (floorAmount * 4) + 1; //Подъезд квартиры
-        int floorNumber = ((flatNumber - 1) / 4 + 1) - (floorAmount * (entranceNumber - 1)); //Этаж квартиры
-        switch (flatNumber % 4) {
+        }
+
+        int entranceNumber = (flatNumber - 1) / (floorAmount * flatsOnFloor) + 1; //Подъезд квартиры
+        int floorNumber = ((flatNumber - 1) / flatsOnFloor + 1) - (floorAmount * (entranceNumber - 1)); //Этаж квартиры
+        String flatLocation = "";
+
+        switch (flatNumber % flatsOnFloor) {
             case 0 -> flatLocation = " справа от лифта и направо.";
             case 1 -> flatLocation = " слева от лифта и налево.";
             case 2 -> flatLocation = " слева от лифта и направо.";
             case 3 -> flatLocation = " справа от лифта и налево.";
         }
+
         return "Квартира " + flatNumber + " расположена в подъезде " + entranceNumber + " на этаже "
                 + floorNumber + flatLocation;
     }
