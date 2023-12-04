f7e211d3ba1a538b576721d264738811729dfbfb
sauza7 <sauza777@yandex.ru>
2023-12-04 13:58:38 +0300

    Chapter 3. task 11

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -6,6 +6,9 @@ package com.walking.intensive.chapter3.task11;
 public class Task11 {
     public static void main(String[] args) {
 
+        int[] array = new int[]{1, 4, 2, 5, 3};
+
+        System.out.println(getSumSubarraysOddLength(array));
     }
 
     /**
@@ -15,7 +18,21 @@ public class Task11 {
      * @return сумма всех нечетныз подмассивов
      */
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+
+        int sumSubarraysOddLength = 0;
+        int lengthOfMinSubbarray = 1;
+
+        while (array.length / lengthOfMinSubbarray != 0) {
+
+            for (int i = 0; i <= array.length - lengthOfMinSubbarray; i++) {
+
+                for (int j = i; j < i + lengthOfMinSubbarray; j++) {
+                    sumSubarraysOddLength += array[j];
+                }
+            }
+
+            lengthOfMinSubbarray += 2;
+        }
+        return sumSubarraysOddLength;
     }
 }
