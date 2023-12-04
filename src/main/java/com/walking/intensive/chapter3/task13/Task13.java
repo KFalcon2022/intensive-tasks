1978595ccf815122d2104b71e9ad88a4d9d0888b
egor.djun <xedshotg@gmail.com>
2023-12-04 16:00:03 +0700

    Chapter 3, Task 13

--- src/main/java/com/walking/intensive/chapter3/task13/Task13.java
+++ src/main/java/com/walking/intensive/chapter3/task13/Task13.java
@@ -5,18 +5,33 @@ package com.walking.intensive.chapter3.task13;
  */
 public class Task13 {
     public static void main(String[] args) {
-
+        int[] plants = {1, 1, 1, 4, 2, 3};
+        int wateringCanVolume = 4;
+        System.out.println(getCountSteps(plants, wateringCanVolume));
     }
 
-    /**
-     * Получить количество шагов необходимых для полива всех растений.
-     *
-     * @param plants            массив с количеством литров воды необходимому i-му растению
-     * @param wateringCanVolume объем лейки
-     * @return количество шагов необходимое для полива всех растений
-     */
     static int getCountSteps(int[] plants, int wateringCanVolume) {
-        // Ваш код
-        return 0;
+        int wateringCan = wateringCanVolume;
+        int count = 0;
+
+        for (int i = 0; i < plants.length; i++) {   //поливаем пока есть вода
+            wateringCan = wateringCan - plants[i];
+            count++;
+
+            if (i == plants.length - 1) {           // когда полили последнюю грядку, заканчиваем работу
+                return count;
+            }
+
+            if (wateringCan <= plants[i]) {         // вода закончилась
+                count += i + 1;                     // идем к реке
+                wateringCan = wateringCanVolume;    // наполняем лейку
+
+                for (int j = 0; j <= i; j++) {      // возращаемя с водой
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
