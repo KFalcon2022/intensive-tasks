0e5b370151b861d503b1c2f764db441390374cae
ganka-xn <jxn@inbox.ru>
2023-12-04 17:51:07 +0300

    Chapter 2, task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,28 @@
 package com.walking.intensive.chapter2.task10;
 
+import java.util.Locale;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        String str = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
+        System.out.println(str + " – " + isPalindrome(str) + " палиндром.");
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    static boolean isPalindrome(String inputString){
+        inputString = inputString.replaceAll("\\p{Punct}+|\\s+", "").toLowerCase();
+        char[] input = inputString.toCharArray();
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] != input[input.length - i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
     }
+
+
 }
