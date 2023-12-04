894b88bd7e44c66ab69c825d005c46a32de0d0d9
Oleg <kovalyovop@yandex.ru>
2023-12-04 15:37:27 +0300

    Chapter2Task10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,35 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome("А "));
+        System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум."));
+        System.out.println(isPalindrome("Муза, ранясь шилом опыта, ты помолишься на разумы"));
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+        StringBuilder firstLine = new StringBuilder(inputString.toLowerCase());
+        StringBuilder secondLine = new StringBuilder(inputString.toLowerCase());
+
+        firstLine = correctionLine(firstLine);
+        secondLine = correctionLine(secondLine);
+        secondLine.reverse();
+        // обрабатываем вариант, при котором введена одна буква, но имеются пробелы и прочие символы
+        if (firstLine.length() < 2) {
+            return false;
+        }
+
+        return firstLine.compareTo(secondLine) == 0;
+    }
+
+    static StringBuilder correctionLine(StringBuilder s) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetter(s.charAt(i))) {
+                result.append(s.charAt(i));
+            }
+        }
+
+        return result;
     }
 }
