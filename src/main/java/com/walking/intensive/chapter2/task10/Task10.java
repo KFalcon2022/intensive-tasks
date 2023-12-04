b708ad5d13e9b16b220b96d73953501b3b5fcbad
KrisLu23 <krisbondarev@yandex.ru>
2023-12-04 10:28:07 +0300

    Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,37 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
+        System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум."));
+        System.out.println(isPalindrome("Горд дох, ход дрог."));
+        System.out.println(isPalindrome("Никогда не говори никогда."));
 
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+        int start = 0;
+        int end = inputString.length() - 1;
+
+        boolean isTrue = true;
+
+        while (start < end) {
+
+            char fromStart = inputString.toLowerCase().charAt(start);
+            char fromEnd = inputString.toLowerCase().charAt(end);
+
+            if (Character.isLetter(inputString.toLowerCase().charAt(start)) && Character.isLetter(inputString.toLowerCase().charAt(end))) {
+
+                if (fromStart != fromEnd) {
+                    isTrue = false;
+                }
+                start++;
+                end--;
+
+            } else if (!Character.isLetter(fromStart)) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return isTrue;
     }
 }
