11979ae70644b5657289880ae8da659b5a5366fa
Boris <aksenov.boris8595@gmeil.com>
2023-12-04 20:49:58 +0500

    Ответ находит.

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,22 +1,28 @@
 package com.walking.intensive.chapter3.task14;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
  */
 public class Task14 {
     public static void main(String[] args) {
-
+        int[][] arrayInputPoints = new int[][]{{1, 1}, {1, 2}, {2, 2}, {2, 1}};
+        int[][] arrayInputQueries = new int[][]{{3, 2, 1}, {2, 2, 1}, {0, 0, 2}};
+        System.out.println(Arrays.toString(getCountOfPoints(arrayInputPoints, arrayInputQueries)));
     }
 
-    /**
-     * Расчет количества точек попавших в каждую окружность.
-     *
-     * @param points  массив с координатами точек
-     * @param queries массив с координатами центра и радиусом окружностей
-     * @return массив с количеством точек внутри окружностей
-     */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] arrayOutput = new int[queries.length];
+        for (int j = 0; j < queries.length; j++) {
+            for (int[] point : points) {
+                double xCoordinate = Math.pow((queries[j][0] - point[0]), 2);
+                double yCoordinate = Math.pow((queries[j][1] - point[1]), 2);
+                if (queries[j][2] >= Math.sqrt(xCoordinate + yCoordinate)) {
+                    arrayOutput[j]++;
+                }
+            }
+        }
+        return arrayOutput;
     }
 }
