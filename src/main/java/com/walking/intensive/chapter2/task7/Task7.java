84695042ad648757b9b537d0432351d750489877
RVishez <oliohcmpn@gmail.com>
2023-12-05 23:42:44 +0100

    Chapter 2 task 7 solved

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,43 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        System.out.println(getFriendlyPair(200));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+
+        int sumOfFriendlyPair = 0;
+        int maxSumOfFriendlyPair = 0;
+        int maxFriendlyNumber = 0;
+
+        if (m >= 1000000) {
+            return -1;
+        }
+
+        while (m != 0) {
+            if ((sumOfDividers(sumOfDividers(m)) == m) && (sumOfDividers(m) < m)) {
+                sumOfFriendlyPair = sumOfDividers(m) + m;
+                if (sumOfFriendlyPair > maxSumOfFriendlyPair) {
+                    maxSumOfFriendlyPair = sumOfFriendlyPair;
+                    maxFriendlyNumber = Math.max(sumOfDividers(m), m);
+                }
+            }
+            m--;
+        }
+
+        return maxFriendlyNumber;
+    }
+
+    static int sumOfDividers(int m) {
+
+        int sumOfDividers = 0;
+
+        for (int i = 1; i < m; i++) {
+            if (m % i == 0) {
+                sumOfDividers += i;
+            }
+        }
+
+        return sumOfDividers;
     }
 }
