a0dd4b019350513151f6f2ad9121e9aca1643e8d
Oleg <kovalyovop@yandex.ru>
2023-12-05 11:29:28 +0300

    Chapter2Task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,31 +5,7 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-        System.out.println(getNoc(1000, 2000));
-        System.out.println(getNod(1000, 2000));
-        System.out.println(getNodByEuclideanAlgorithm(1000, 2000));
-    }
 
-    static int getNoc(int m, int n) {
-        return m / getNod(m, n) * n;
-    }
-
-    static int getNod(int m, int n) {
-        while (n != 0) {
-            int temp = n;
-            n = m % n;
-            m = temp;
-        }
-
-        return m;
-    }
-
-    static int getNodByEuclideanAlgorithm(int m, int n) {
-        if (n == 0) {
-            return m;
-        }
-
-        return getNodByEuclideanAlgorithm(n, m % n);
     }
 
     static int getNoc(int m, int n) {
