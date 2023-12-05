7b6b41c0efff78c8477581f993f22cf3885c08a8
Alexey <lolex1@mail.ru>
2023-12-05 19:53:36 +0300

    Chapter 2 Task9

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -6,10 +6,53 @@ package com.walking.intensive.chapter2.task9;
 public class Task9 {
     public static void main(String[] args) {
 
+        System.out.println(getPascalTriangle(20));
+//        System.out.println(getStringFactorial(13));
+//        System.out.println(getFactorial(11));
+
     }
 
     static String getPascalTriangle(int n){
-        // Ваш код
-        return null;
+       String f = "";
+
+        for (int i = 0; i <= n; i++){
+            f = f + "\n" + getStringFactorial(i);
+
+        }
+
+        return f;
+    }
+
+    public static long getFactorial(long f){
+
+        long result = 1;
+
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static String getStringFactorial(int stringF){
+
+        StringBuilder result = new StringBuilder(getFactorial(stringF) / (getFactorial(0) * getFactorial(stringF)) + "");
+
+        for (int i = 1; i <= stringF; i++){
+            String x = " " + getFactorial(stringF) / (getFactorial(i) * getFactorial(stringF - i));
+            result.append(x);
+        }
+
+        return result.toString();
+    }
+
+    public static String getSpace(int spaceNumb){
+
+        String space = "";
+
+        for (int i = 0; i < spaceNumb; i++){
+            space = space + " ";
+        }
+
+        return space;
     }
 }
