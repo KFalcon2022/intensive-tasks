15cc45563b96e63c60093b4af05fb06355e4f14c
Oleg <kovalyovop@yandex.ru>
2023-12-05 12:54:21 +0300

    Chapter3Task12

--- src/main/java/com/walking/intensive/chapter3/task12/Task12.java
+++ src/main/java/com/walking/intensive/chapter3/task12/Task12.java
@@ -1,21 +1,38 @@
 package com.walking.intensive.chapter3.task12;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-balls.html">МЯЧИКИ</a>
  */
 public class Task12 {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(getNumberOfMovements("110")));
+        System.out.println(Arrays.toString(getNumberOfMovements("112")));
     }
 
-    /**
-     * Подсчет минимального количества операций, необходимых для перемещения всех мячиков в i-ю ячейку.
-     *
-     * @param baskets Строка с указанием наличия или отсутсвия мячика
-     * @return массив с минимальным количеством операций
-     */
+    static boolean isValidChar(char c) {
+        return c != '0' && c != '1';
+    }
+
     static int[] getNumberOfMovements(String baskets) {
-        // Ваш код
-        return new int[]{};
+        int[] minNum = new int[baskets.length()];
+
+        for (int i = 0; i < baskets.length(); i++) { // по условиям задачи принимаем символ '0' || '1'
+            if (isValidChar(baskets.charAt(i))) {
+                return new int[]{0};
+            }
+        }
+
+        for (int i = 0; i < baskets.length(); i++) {
+
+            for (int j = 0; j < baskets.length(); j++) {
+                if (baskets.charAt(j) == '1') {
+                    minNum[i] += Math.abs(j - i);
+                }
+            }
+        }
+
+        return minNum;
     }
 }
