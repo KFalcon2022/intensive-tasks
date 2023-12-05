6b6ce808274665ea00fdce9b12181ae37a6364e1
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-12-05 23:35:35 +0300

    first

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,11 +1,17 @@
 package com.walking.intensive.chapter3.task14;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
  */
 public class Task14 {
     public static void main(String[] args) {
+        int[][] points_1 = {{1, 3}, {3, 3}, {5, 3}, {2, 2}};
+        int[][] queries_1 = {{2, 3, 1}, {4, 3, 1}, {1, 1, 2}};
+        int[] answer_1 = {3, 2, 2};
 
+        System.out.println(Arrays.toString(getCountOfPoints(points_1, queries_1)));
     }
 
     /**
@@ -16,7 +22,22 @@ public class Task14 {
      * @return массив с количеством точек внутри окружностей
      */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] result = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int x = (queries[i][0]);
+            int y = (queries[i][1]);
+            int raduis = (queries[i][2]);
+
+            int temp = 0;
+            for (int j = 0; j < points.length; j++) {
+                if (Math.abs(x- points[j][0]) <= raduis && Math.abs(y - points[j][1]) <= raduis) {
+                    temp++;
+                }
+            }
+            result[i] = temp;
+        }
+        System.out.println(Arrays.toString(result));
+        return result;
     }
 }
