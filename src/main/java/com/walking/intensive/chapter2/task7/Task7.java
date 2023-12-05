5dafd3bbd8c0e46edaedafbb2db5a5f23c2ae5e0
smmotor <smmotor777@gmail.com>
2023-12-05 23:20:35 +0600

    Chapter 2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -1,15 +1,39 @@
 package com.walking.intensive.chapter2.task7;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/java-friendly-number.html">ссылка</a>
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        System.out.println(getFriendlyPair(1000000));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int firstPairNum = 0;
+        int secondPairNum = 0;
+        for (int i = 1; i < m; i++) {
+            int j = getSumDivisors(i);
+            if (i < j && i == getSumDivisors(j)) {
+                firstPairNum = i;
+                secondPairNum = j;
+            }
+        }
+        if (firstPairNum < secondPairNum  && secondPairNum < m ) {
+            return secondPairNum;
+        } else {
+            return firstPairNum;
+        }
+    }
+
+    static int getSumDivisors(int m) {
+        int sum = 0;
+        for (int i = 1; i < m / 2 + 1; i++) {
+            if (m % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
