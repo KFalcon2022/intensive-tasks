410a66e7643d91b199ca7b68eb504656614a37fe
lambo064 <tasha_lambo@mail.ru>
2023-12-05 23:24:56 +0300

    chapter 2 task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Main.java
@@ -1,41 +1,32 @@
 package com.walking.intensive.chapter2.task10;
 
-import static java.lang.Character.isLetter;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("aaavaaa"));
+        System.out.println(isPalindrome("aa.aba fAa"));
     }
 
     public static boolean isPalindrome(String example) {
-        String stringWithoutPunctuation = removePunctuation(example);
-        return stringWithoutPunctuation.equalsIgnoreCase(rewerseString(stringWithoutPunctuation));
-    }
+        example = example.toLowerCase();
+        for (int i = 0; i < example.length() - 1; i++) {
 
-    public static String removePunctuation(String string) {
+            int j = example.length() - 1 - i;
 
-        String stringWithoutPunctuation = "";
+            if (example.charAt(j) == '.' || example.charAt(j) == ',' || example.charAt(j) == ' ' || example.charAt(j) == '?') {
+                j--;
+            }
 
-        for (int i = 0; i < string.length(); i++) {
-            char c = string.charAt(i);
-            if (isLetter(c)) {
-                stringWithoutPunctuation += c;
+            if (example.charAt(i) == '.' || example.charAt(i) == ',' || example.charAt(i) == ' ' || example.charAt(i) == '?') {
+                i++;
+            }
+
+            if (i > j) {
+                break;
+            }
+
+            if (example.charAt(j) != example.charAt(i)) {
+                return false;
             }
         }
-        return stringWithoutPunctuation;
+        return true;
     }
-
-    public static String rewerseString(String string) {
-
-            char[] array = string.toCharArray();
-            String result = "";
-
-            for (int i = array.length - 1; i >= 0; i--) {
-                result += array[i];
-            }
-
-            return result;
-    }
-
-
 }
