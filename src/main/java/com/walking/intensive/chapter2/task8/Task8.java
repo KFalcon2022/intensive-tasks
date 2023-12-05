053b9ceccf10921eba240919c7468b7bc4cf4d34
apjam <apjam@yandex.ru>
2023-12-05 21:47:54 +0600

    task8 is done

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -4,12 +4,40 @@ package com.walking.intensive.chapter2.task8;
  * Условие: <a href="http://geometry-math.ru/homework/Java-ticket.html">ссылка</a>
  */
 public class Task8 {
-    public static void main(String[] args) {
 
+    static final int TICKETS_QUANTITY = 1000000;
+
+    public static void main(String[] args) {
+        double result = getHappyTicketChance();
+        System.out.println(result);
     }
 
-    static double getHappyTicketChance(int ticketNumber){
-        // Ваш код
-        return 0.0;
+    static double getHappyTicketChance() {
+        int countHappy = 0;
+
+        for (int i = 1000; i < TICKETS_QUANTITY; ++i) {
+            if (getSumNumbers(getFirstPart(i)) == getSumNumbers(getSecondPart(i))) {
+                ++countHappy;
+            }
+        }
+
+        return (double) countHappy / TICKETS_QUANTITY;
+    }
+
+    static int getFirstPart(int number) {
+        return number / 1000;
+    }
+
+    static int getSecondPart(int number) {
+        return number % 1000;
+    }
+
+    static int getSumNumbers(int number) {
+        int sum = 0;
+        for (int i = number % 10; number != 0; i = number % 10) {
+            sum += i;
+            number /= 10;
+        }
+        return sum;
     }
 }
