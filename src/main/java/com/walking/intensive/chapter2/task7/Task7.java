cde9589c1dfe2fcea7b127f32d3389b5e5e4c87c
rovbo85@gmail.com <rovbo85@gmail.com>
2023-12-05 10:57:33 +0500

    Внесены изменения в соответствии с замечаниями рецензента, а именно: - Изменил условия цикла while так, что бы при отрицательном значении переменной m он не уходли в бесконечноть;

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -6,13 +6,13 @@ package com.walking.intensive.chapter2.task7;
 public class Task7 {
 
     public static void main(String[] args) {
-        int m = 1000000;
+        int m = 130000;
         System.out.println("Нибольшее число из пары дружественных чисел: " + getFriendlyPair(m));
     }
 
     static int getFriendlyPair(int m){
 
-        while (m != getSumOfDivisors(getSumOfDivisors(m)) || getSumOfDivisors(m) >= m) {
+        while (m >= 0 & m != getSumOfDivisors(getSumOfDivisors(m)) | getSumOfDivisors(m) >= m) {
             m--;
         }
         return m;
