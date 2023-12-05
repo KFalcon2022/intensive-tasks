5212cb29c61752cedbc8048a0a36d320ceeb623d
KrisLu23 <krisbondarev@yandex.ru>
2023-12-05 12:18:17 +0300

    Task 12

--- src/main/java/com/walking/intensive/chapter3/task12/Task12.java
+++ src/main/java/com/walking/intensive/chapter3/task12/Task12.java
@@ -1,21 +1,34 @@
 package com.walking.intensive.chapter3.task12;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-balls.html">МЯЧИКИ</a>
  */
 public class Task12 {
     public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(getNumberOfMovements("1001")));
     }
 
-    /**
-     * Подсчет минимального количества операций, необходимых для перемещения всех мячиков в i-ю ячейку.
-     *
-     * @param baskets Строка с указанием наличия или отсутсвия мячика
-     * @return массив с минимальным количеством операций
-     */
+
     static int[] getNumberOfMovements(String baskets) {
-        // Ваш код
-        return new int[]{};
+        int[] arr = new int[baskets.length()];
+        int val = Integer.parseInt(baskets);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arr[i] = val % 10;
+            val /= 10;
+        }
+
+        int[] result = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] == 1) {
+                    result[i] += Math.abs(j - i);
+                }
+            }
+        }
+        return result;
     }
 }
