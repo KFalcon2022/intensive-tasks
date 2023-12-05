5cdfa53b71495e7452859e2e26aedd5ed528e854
njmist <njmist@gmail.com>
2023-12-05 22:12:21 +0300

    chapter 2 task 13

--- src/main/java/com/walking/intensive/chapter3/task13/Task13.java
+++ src/main/java/com/walking/intensive/chapter3/task13/Task13.java
@@ -5,18 +5,37 @@ package com.walking.intensive.chapter3.task13;
  */
 public class Task13 {
     public static void main(String[] args) {
+        int[] plants = {2, 2, 1, 2, 5};
+        int wateringCanVolume = 5;
 
+        System.out.println("Количество шагов, нужное для полива всех растений: " + getCountSteps(plants, wateringCanVolume));
     }
 
-    /**
-     * Получить количество шагов необходимых для полива всех растений.
-     *
-     * @param plants            массив с количеством литров воды необходимому i-му растению
-     * @param wateringCanVolume объем лейки
-     * @return количество шагов необходимое для полива всех растений
-     */
     static int getCountSteps(int[] plants, int wateringCanVolume) {
-        // Ваш код
-        return 0;
+        for (int i = 0; i < plants.length; i++){        // Делаем проверку, нет ли растения, требующего больше воды, чем мы можем нести в лейке
+            if (wateringCanVolume < plants[i]){
+                return 0;
+            }
+        }
+
+        int currentPosition = -1;                           // Где мы сейчас
+        int wateringCanCurrentVolume = wateringCanVolume;   // Сколько воды в лейке сейчас
+        int counter = 0;                                    // Заводим счетчик
+
+        while (currentPosition < plants.length - 1) {                        // Пока не польём последнее растение
+            currentPosition++;                                               // Смотрим следующее растение
+
+            if (wateringCanCurrentVolume >= plants[currentPosition]) {       // Если в лейке достаточно воды для полива
+                wateringCanCurrentVolume -= plants[currentPosition];         // Поливаем растение
+                counter++;                                                   // Увеличиваем счётчик (делаем шаг вперед)
+
+            } else {                                                         // Если в лейке недостаточно воды для полива следующего растения
+                counter += currentPosition * 2;                              // Идём к реке и возвращаемся на ту позицию, где были
+                wateringCanCurrentVolume = wateringCanVolume;                // Теперь у нас полная лейка воды!
+                currentPosition--;                                           // Сбрасываем позицию, типа посмотрели, надо заново глянуть
+            }
+        }
+
+        return counter;
     }
 }
