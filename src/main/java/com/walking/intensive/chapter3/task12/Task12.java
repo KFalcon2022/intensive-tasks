fa8a61ba6dbfbc70ce14bbc76d4f93e11fac1091
ArtemiyDS <27071999@mail.ru>
2023-12-05 22:06:15 +0300

    Chapter 3 Task 12

--- src/main/java/com/walking/intensive/chapter3/task12/Task12.java
+++ src/main/java/com/walking/intensive/chapter3/task12/Task12.java
@@ -5,17 +5,27 @@ package com.walking.intensive.chapter3.task12;
  */
 public class Task12 {
     public static void main(String[] args) {
+        String str = "110";
+
+        int[] answer = getNumberOfMovements(str);
+
+        for(int tmp: answer)
+            System.out.println(tmp);
 
     }
 
-    /**
-     * Подсчет минимального количества операций, необходимых для перемещения всех мячиков в i-ю ячейку.
-     *
-     * @param baskets Строка с указанием наличия или отсутсвия мячика
-     * @return массив с минимальным количеством операций
-     */
     static int[] getNumberOfMovements(String baskets) {
-        // Ваш код
-        return new int[]{};
+        char[] charMas = baskets.toCharArray(); // создаем массив char "1" и "0"
+        int[] result = new int[charMas.length];
+
+        for (int i = 0; i < charMas.length; i++) {
+            for (int j = 0; j < charMas.length; j++) {
+                if (charMas[j] == '1' && i != j)
+                    result[i] += Math.abs(i - j);
+            }
+        }
+
+        return result;
     }
+
 }
