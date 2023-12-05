c8a006ea58f9d62c1bb73efb93bfd36103c82aaa
k2336351 <k2336351@yandex.ru>
2023-12-06 00:28:18 +0300

    Solution Chapter 2  Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -8,8 +8,48 @@ public class Task10 {
 
     }
 
-    boolean isPalindrome(String inputString){
-        // Ваш код
-        return false;
+    static boolean isPalindrome(String inputString) {
+        if (isNullOrEmpty(inputString)) {
+            return false;
+        }
+
+        String preparedString = getPreparedString(inputString);
+
+        if (preparedString.length() == 1) {
+            return false;
+        }
+
+        StringBuilder reversedStringBuilder;
+        reversedStringBuilder = new StringBuilder(preparedString);
+
+        return preparedString.equalsIgnoreCase(reversedStringBuilder.reverse().toString());
+    }
+
+    static boolean isNullOrEmpty(String inputString) {
+        return inputString == null || inputString.isEmpty();
+    }
+
+    static String getPreparedString(String inputString) {
+
+        if (isNullOrEmpty(inputString)) {
+            return "";
+        }
+
+        StringBuilder preparedStringBuilder;
+        preparedStringBuilder = new StringBuilder();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            char character = inputString.charAt(i);
+
+            if ((character >= ' ' && character <= '/') ||
+                    (character >= ':' && character <= '?') ||
+                    (character >= '[' && character <= '_')) {
+                continue;
+            }
+
+            preparedStringBuilder.append(character);
+        }
+
+        return preparedStringBuilder.toString();
     }
 }
