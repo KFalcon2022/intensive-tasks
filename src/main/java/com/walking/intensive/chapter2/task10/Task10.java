7a960b2f0aec4dded37d1319f71b4c604dc645eb
sauza7 <sauza777@yandex.ru>
2023-12-05 13:33:43 +0300

    Chapter 2. task 10 Third Solution

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -7,7 +7,7 @@ public class Task10 {
     public static void main(String[] args) {
 
         String str1 = "Муза! Ранясь шилом опыта, ты помолишься на разум.";
-        String str2 = "О, Муза! Ранясь шилом опыта, ты помолишься на разум.";
+        String str2 = "О Муза! Ранясь шилом опыта, ты помолишься на разум.";
 
         System.out.println(isPalindrome(str1));
         System.out.println(isPalindrome(str2));
@@ -15,21 +15,41 @@ public class Task10 {
 
     static boolean isPalindrome(String inputString) {
 
-        char[] arrayOfChars = new char[inputString.length()];
-        int lastLetterIndex = 0;
+        int indexLeftLetter = 0;
+        boolean noLettersInString = true;
+//Проверка на наличие букв в строке, для недопущения ошибки при пустой, либо строке только из символов.
+// Как сопутствующее - поиск индекса первой буквы.
+        while (indexLeftLetter < inputString.length() && noLettersInString) {
 
-        for (int i = 0 ; i < inputString.length() ; i++) {
-            if ( Character.isLetter(inputString.charAt(i)) ) {
-                arrayOfChars[lastLetterIndex++] = Character.toLowerCase (inputString.charAt(i) );
+            if (Character.isLetter(inputString.charAt(indexLeftLetter++))) {
+                noLettersInString = false;
             }
         }
 
-        for (int j = 0 ; j < lastLetterIndex ; j++) {
-            if (arrayOfChars[j] != arrayOfChars[lastLetterIndex-1-j]) {
+        if (noLettersInString) {
+            return false;
+        }
+// Конец проверки.
+        indexLeftLetter--;
+        int indexRightLetter = inputString.length() - 1;
+
+        while (indexLeftLetter <= indexRightLetter) {
+
+            while (!Character.isLetter(inputString.charAt(indexLeftLetter))) {
+                indexLeftLetter++;
+            }
+
+            while (!Character.isLetter(inputString.charAt(indexRightLetter))) {
+                indexRightLetter--;
+            }
+
+            if (Character.toLowerCase(inputString.charAt(indexLeftLetter)) != Character.toLowerCase(inputString.charAt(indexRightLetter))) {
                 return false;
             }
-        }
 
+            indexLeftLetter++;
+            indexRightLetter--;
+        }
         return true;
     }
 }
