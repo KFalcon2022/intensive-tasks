14f2573d6e5c82787f1a8e7250f13b64cdc834bf
Alexey <lolex1@mail.ru>
2023-12-05 20:15:07 +0300

    Chapter 2 Task8+

--- src/main/java/com/walking/intensive/chapter2/task8/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static double getChanceHappyTicket(int tickets){
 
-        return  (double) getCountHappyTicket(tickets) / 999_999;
+        return  (double) getCountHappyTicket(tickets) / 1_000_000;
     }
 
     public static int getCountHappyTicket(int tickets){
@@ -28,29 +28,18 @@ public class Main {
     public static boolean isTotalEqual(int number){
 
         if (number < 10_000){
-            return (number / 1000) == getTotal1000(number);
+            return (number / 1000) == (number / 100 % 10 + number / 10 % 10 + number % 10);
         }
 
         if (number < 100_000){
-           return (number / 10000 + number / 1000 % 10) == getTotal1000(number);
+           return (number / 10000 + number / 1000 % 10) == (number / 100 % 10 + number / 10 % 10 + number % 10);
         }
 
         if (number < 1_000_000){
-            return getTotalFirst1000(number) == getTotal1000(number);
+            return (number / 100000 + number / 10000 % 10 + number / 1000 % 10) == (number / 100 % 10 + number / 10 % 10 + number % 10);
         }
 
         return false;
 
     }
-
-    public static int getTotal1000(int number){
-
-        return (number / 100 % 10 + number / 10 % 10 + number % 10);
-    }
-
-    public static int getTotalFirst1000(int number){
-
-        return (number / 100000 + number / 10000 % 10 + number / 1000 % 10);
-    }
-
 }
