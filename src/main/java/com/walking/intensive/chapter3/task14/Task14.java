8460e6a25ed89a747cd2db031380ead40d2fb7dc
ArtemiyDS <27071999@mail.ru>
2023-12-05 21:40:09 +0300

    Chapter 3 Task 14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,22 +1,34 @@
 package com.walking.intensive.chapter3.task14;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
- */
+import java.lang.Math;
+
 public class Task14 {
     public static void main(String[] args) {
+        int[][] points = {{1, 3}, {3, 3}, {5, 3}, {2, 2}};
+        int[][] queries = {{2, 3, 1}, {4, 3, 1}, {1, 1, 2}};
+
+        int[] answer = getCountOfPoints(points,queries);
+
+        for(int tmp: answer)
+            System.out.println(tmp);
 
     }
 
-    /**
-     * Расчет количества точек попавших в каждую окружность.
-     *
-     * @param points  массив с координатами точек
-     * @param queries массив с координатами центра и радиусом окружностей
-     * @return массив с количеством точек внутри окружностей
-     */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] result = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if (isPointIncludedInTheCircle(points[j][0], points[j][1], queries[i][0], queries[i][1], queries[i][2])){
+                    result[i]++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static boolean isPointIncludedInTheCircle(int x1, int y1, int x0, int y0, int r) {
+        return ((Math.pow(x1 - x0, 2) + Math.pow(y1 - y0, 2)) <= r * r);    // Если выражение верно, то точка входит в пределах круга
     }
 }
