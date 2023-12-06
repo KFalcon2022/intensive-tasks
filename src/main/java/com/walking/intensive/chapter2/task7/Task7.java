24c6901657d2281fdf62e17ab052b53fe9d64642
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-12-06 17:22:44 +0300

    Chapter2Task7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -1,4 +1,4 @@
-package com.walking.intensive.chapter2.task7;
+package main.java.com.walking.intensive.chapter2.task7;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/java-friendly-number.html">ссылка</a>
@@ -8,8 +8,34 @@ public class Task7 {
 
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int sumDemominators1;
+        int sumDemominators2;
+
+        for (int i = 1; i < m; i++) {
+            sumDemominators1 = searchDenominators(i);
+
+            for (int j = 1; j < m; j++) {
+                sumDemominators2 = searchDenominators(j);
+
+                if (i == j) continue;
+
+                if (sumDemominators1 == j && sumDemominators2 == i) {
+
+                    return Math.max(i, j);
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int searchDenominators(int num) {
+        int sumDenominators = 0;
+        for (int n = 1; n < num; n++) {
+            if (num % n == 0) {
+                sumDenominators += n;
+            }
+        }
+        return sumDenominators;
     }
 }
