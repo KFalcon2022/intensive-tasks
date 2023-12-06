0245e29e3ae742789c472bc31fd06938e818bbfc
ganka-xn <jxn@inbox.ru>
2023-12-06 16:40:04 +0300

    Скорректировано по замечаниям

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,68 +5,47 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-        int[][] matrix = {
-                {-12, 9},
-                {0, -8},
-                {111, 85},
-                {-12, -4},
-                {54, 54},
-                {72, 96},
-                {0, 6},
-                {0, 0},
-                {15, 1}
-        };
-        for (int i = 0; i < matrix.length; i++) {
-            int a = matrix[i][0];
-            int b = matrix[i][1];
-            System.out.printf("НОД (%s, %s) = %s ", a, b, getNodByEuclideanAlgorithm(a, b));
-            System.out.printf("НОД Евклид (%s, %s) = %s ", a, b, getNod(a, b));
-            System.out.printf("НОК (%s, %s) = %s\n", a, b, getNoc(a, b));
-        }
+        int a = 96;
+        int b = 72;
+
+        System.out.printf("НОК (%s, %s) = %s\n", a, b, getNoc(a, b));
+        System.out.printf("НОД Евклид (%s, %s) = %s\n", a, b, getNodByEuclideanAlgorithm(a, b));
+        System.out.printf("НОД (%s, %s) = %s\n", a, b, getNod(a, b));
     }
 
     static int getNoc(int m, int n) {
-        int noc = 0;
-
-        try {
-            if (m == 0 || n == 0) {
-                throw new Exception();
-            }
-            noc = m * n / getNodByEuclideanAlgorithm(m, n);
-        } catch (Exception e) {
-            System.out.print("Не имеет решения! ");
-//            e.printStackTrace();
-        }
-
-        return noc;
-    }
-
-    static int getNod(int m, int n) {
-        if (n == 0) {
-            return Math.abs(m);
-        }
-        return Math.abs(getNod(n, m % n));
+        return (m * n) / getNod(m, n);
     }
 
     static int getNodByEuclideanAlgorithm(int m, int n) {
-        int gcd = 1;
+        if (n == 0) {
+            return Math.abs(m);
+        }
+        return Math.abs(getNodByEuclideanAlgorithm(n, m % n));
+    }
 
+    static int getNod(int m, int n) {
         if (m == 0 && n == 0) {
             return 0;
-        } else if (m == 0) {
-            return Math.abs(n);
-        } else if (n == 0) {
-            return Math.abs(m);
-        } else {
-            m = Math.abs(m);
-            n = Math.abs(n);
+        }
 
-            for (int i = 2; i < Math.min(m, n); i++) {
-                while (m % i == 0 && n % i == 0) {
-                    gcd *= i;
-                    m /= i;
-                    n /= i;
-                }
+        if (m == 0) {
+            return Math.abs(n);
+        }
+
+        if (n == 0) {
+            return Math.abs(m);
+        }
+
+        m = Math.abs(m);
+        n = Math.abs(n);
+        int gcd = 1;
+
+        for (int i = 2; i < Math.min(m, n); i++) {
+            while (m % i == 0 && n % i == 0) {
+                gcd *= i;
+                m /= i;
+                n /= i;
             }
         }
 
