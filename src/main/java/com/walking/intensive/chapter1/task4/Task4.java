c5f0268747c0db4495fa9b5cf5849e983932f1c7
Alphyra <ageyeva.sveta@gmail.com>
2023-12-06 21:23:13 +0300

    Chapter 1 Task 4 v3

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -8,9 +8,9 @@ import java.text.DecimalFormat;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 1;
-        double b = 0;
-        double c = -5;
+        double a = 4;
+        double b = -2;
+        double c = 0;
         System.out.println(solveQuadraticEquation(a, b, c));
     }
 
@@ -27,47 +27,39 @@ public class Task4 {
      */
     static String solveQuadraticEquation(double a, double b, double c) {
         DecimalFormat formatter = new DecimalFormat("#.##");
-        if (a == 0 && b == 0 && c != 0) {
+        if (a == 0 && b == 0) {
+            if (c == 0) {
+                return "Решений бесконечно";
+            }
             return "Количество решений: 0.";
         }
         if (((a == 0 && b != 0) || (a != 0 && b == 0)) && c == 0) {
             return "Количество решений: 1. Корень: 0";
         }
-        if (a == 0 && b != 0 && c != 0) {
+        if (a == 0 && b != 0) {
             return "Количество решений: 1. Корень: " + formatter.format(-c / b);
         }
-        if (a != 0 && b == 0 && c != 0) {
+        if (a != 0 && b == 0) {
             double div = -c / a;
             if (div < 0) {
                 return "Количество решений: 0.";
             }
-            return solveWithDiscriminant(a, b, c);
+            return solveUsingDiscriminant(a, b, c);
         }
-        if (a != 0 && b != 0 && c == 0) {
-            double div = -b / a;
-            if (div < 0) {
-                return "Количество решений: 2. Корни: " + div + ";0";
-            }
-            return "Количество решений: 2. Корни: 0;" + div;
-        }
-        if (a != 0 && b != 0 && c != 0) {
-            return solveWithDiscriminant(a, b, c);
-        }
-        return "Решений бесконечно";
+        return solveUsingDiscriminant(a, b, c);
     }
 
-    static String solveWithDiscriminant(double a, double b, double c) {
+    static String solveUsingDiscriminant(double a, double b, double c) {
         DecimalFormat formatter = new DecimalFormat("#.##");
         double discriminant = b * b - 4 * a * c;
         if (discriminant > 0) {
-            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            return "Количество решений: 2. Корни: " + formatter.format(x2) + ";" + formatter.format(x1);
-        } else {
-            if (discriminant == 0) {
-                return "Количество решений: 1.  Корень: " + formatter.format(-b / (2 * a));
-            }
-            return "Количество решений: 0.";
+            double x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            return "Количество решений: 2. Корни: " + formatter.format(x1) + ";" + formatter.format(x2);
         }
+        if (discriminant == 0) {
+            return "Количество решений: 1.  Корень: " + formatter.format(-b / (2 * a));
+        }
+        return "Количество решений: 0.";
     }
 }
