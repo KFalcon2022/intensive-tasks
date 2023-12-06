e24b63191626d76cf027c68147880a5d70926913
TitLukich <Nef-92@yandex.ru>
2023-12-06 23:08:12 +0300

    Chapter 3, Task 14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  */
 public class Task14 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(getCountOfPoints(new int[][]{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}}, new int[][] {{1, 2, 2}, {2, 2, 2}, {4, 3, 2}, {4, 3, 3}})));
+        System.out.println(Arrays.toString(getCountOfPoints(new int[][]{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}}, new int[][]{{1, 2, 2}, {2, 2, 2}, {4, 3, 2}, {4, 3, 3}})));
     }
 
     /**
@@ -18,17 +18,19 @@ public class Task14 {
      * @return массив с количеством точек внутри окружностей
      */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-       int [] answer = new int[queries.length];
+        int[] answer = new int[queries.length];
+
         for (int i = 0; i < queries.length; i++) {
             int count = 0;
+
             for (int[] point : points) {
                 if (Math.sqrt(Math.pow(point[0] - queries[i][0], 2) + Math.pow(point[1] - queries[i][1], 2)) <= queries[i][2]) {
                     count++;
                 }
             }
-            answer [i] = count;
+            answer[i] = count;
         }
-        // Ваш код
+
         return answer;
     }
 }
