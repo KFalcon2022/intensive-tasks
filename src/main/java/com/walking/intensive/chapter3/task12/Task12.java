98a7f4826f6c742cdece981928bf8b5a45bb4486
ganka-xn <jxn@inbox.ru>
2023-12-07 00:21:50 +0300

    Ch.3 Task12

--- src/main/java/com/walking/intensive/chapter3/task12/Task12.java
+++ src/main/java/com/walking/intensive/chapter3/task12/Task12.java
@@ -1,10 +1,14 @@
 package com.walking.intensive.chapter3.task12;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-balls.html">МЯЧИКИ</a>
  */
 public class Task12 {
     public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(getNumberOfMovements("011101")));
 
     }
 
@@ -15,7 +19,45 @@ public class Task12 {
      * @return массив с минимальным количеством операций
      */
     static int[] getNumberOfMovements(String baskets) {
-        // Ваш код
-        return new int[]{};
+        int[] moveCount = new int[baskets.length()];
+        int sumOfBalls = 0;
+
+        String[] splitBaskets = baskets.split("");
+
+        int[] initialBaskets = new int[splitBaskets.length];
+        for (int i = 0; i < splitBaskets.length; i++) {
+            initialBaskets[i] = Integer.parseInt(splitBaskets[i]);
+            sumOfBalls += initialBaskets[i];
+        }
+
+        int[] resultBaskets = initialiseBaskets(initialBaskets);
+
+        for (int i = 0; i < resultBaskets.length; i++) { // перекладываем мячики в i корзину
+            while (resultBaskets[i] != sumOfBalls) {
+                for (int j = 0; j < resultBaskets.length; j++) {
+                    if (resultBaskets[j] != 0 && j > i) {
+                        resultBaskets[j - 1]++;
+                        resultBaskets[j]--;
+                        moveCount[i]++;
+                    } else if (resultBaskets[j] != 0 && j < i) {
+                        resultBaskets[j + 1]++;
+                        resultBaskets[j]--;
+                        moveCount[i]++;
+                    }
+                }
+            }
+            resultBaskets = initialiseBaskets(initialBaskets);
+        }
+        return moveCount;
+    }
+
+    static int[] initialiseBaskets(int[] baskets) {
+        int[] resultBaskets = new int[baskets.length];
+
+        for (int i = 0; i < baskets.length; i++) {
+            resultBaskets[i] = baskets[i];
+        }
+
+        return resultBaskets;
     }
 }
