6119f19ba6db1e48be204dd465471036fe20a741
DeaDMop03 <bl.target@yandex.ru>
2023-12-06 18:39:45 +0300

    chapter2task6 release

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -9,18 +9,43 @@ public class Task6 {
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        int greaterNumber = Math.max(m, n);
+        int smallerNumber = Math.min(m, n);
+        int noc = greaterNumber;
+        while (noc % smallerNumber != 0) {
+            noc += noc;
+        }
+        return noc;
+
+
     }
 
     static int getNod(int m, int n) {
         // Ваш код
-        return 0;
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        return getNoc(m, n) / Math.abs(m * n);
     }
 
     static int getNodByEuclideanAlgorithm(int m, int n){
         // Ваш код
-        return 0;
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+
+            while (m != n) {
+                if (m > n) {
+                    m = m - n;
+                } else {
+                    n = n - m;
+                }
+            }
+            return m;
     }
 
 }
