a4cdf2697c735b026c05f083e59ce13d036b9807
stogov_so <stogov_sergei@mail.ru>
2023-12-07 01:23:22 +0500

    Chapter2_task9

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,11 +5,47 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
-
+        System.out.println(getPascalTriangle(25));
     }
 
-    static String getPascalTriangle(int n){
-        // Ваш код
-        return null;
+    static String getPascalTriangle(int n) {
+        StringBuilder triangle = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            int numberTriangle = 1;
+
+            StringBuilder lineTriangle = new StringBuilder();
+
+            for (int j = 1; j <= i; j++) {
+                numberTriangle *= (i - j + 1);
+                numberTriangle /= j;
+
+                lineTriangle.append(" ").append(numberTriangle);
+            }
+
+            int currentLength = lineTriangle.length() + 1;
+            int countSpace = (getMaxLength(n) - currentLength) / 2;
+
+            String space = " ";
+
+            triangle.append(space.repeat(countSpace)).append(numberTriangle).append(lineTriangle).append("\n");
+        }
+
+        return triangle.toString();
+    }
+
+    static int getMaxLength(int n) {
+        StringBuilder lastLine = new StringBuilder("1");
+
+        int numberLastLine = 1;
+
+        for (int i = 1; i <= n; i++) {
+            numberLastLine *= (n - i + 1);
+            numberLastLine /= i;
+
+            lastLine.append(" ").append(numberLastLine);
+        }
+
+        return lastLine.length();
     }
 }
