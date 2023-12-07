decc58079592d8c68c2f2fed2056763dace4515d
KrisLu23 <krisbondarev@yandex.ru>
2023-12-07 12:28:40 +0300

    Task 15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -5,11 +5,55 @@ package com.walking.intensive.chapter3.task15;
  */
 public class Task15 {
     public static void main(String[] args) {
+        int[][] array = {{9, 3, 6}, {8, 0, 1}, {3, 6, 7}};
+        System.out.println(getMaxFloors(array));
 
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+        int result = 0;
+
+        for (int i = 0; i < city.length; i++) {
+
+            for (int j = 0; j < city[i].length; j++) {
+                int maxPermissibleValue = Math.min(getMaxHorizontalValue(city[i]), getMaxVerticalValue(city, j));
+
+                if (city[i][j] < maxPermissibleValue) {
+                    result += maxPermissibleValue - city[i][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    static int getMaxHorizontalValue(int[] array) {
+        int maxValue = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+
+            if (maxValue < array[i]) {
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+
+    static int getMaxVerticalValue(int[][] array, int index) {
+
+        int[] tempArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            tempArray[i] = array[i][index];
+        }
+
+        int maxValue = 0;
+
+        for (int i = 0; i < tempArray.length; i++) {
+
+            if (maxValue < tempArray[i]) {
+                maxValue = tempArray[i];
+            }
+        }
+        return maxValue;
     }
 }
