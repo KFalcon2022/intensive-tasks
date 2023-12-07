29061a100b430aef0afe3b3ce59e33d56dbd6466
lambo064 <tasha_lambo@mail.ru>
2023-12-08 00:28:10 +0300

    chapter 3 task 11

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
-
+        System.out.print(getSumSubarraysOddLength(new int[]{1, 4, 2, 5, 3}));
     }
 
     /**
@@ -15,7 +15,32 @@ public class Task11 {
      * @return сумма всех нечетныз подмассивов
      */
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+            int[] cuttedArray = new int[array.length - i];
+            int k = 0;
+            for (int j = i; j < array.length; j++) {
+                cuttedArray[k] = array[j];
+                k++;
+                if (k > cuttedArray.length) {
+                    break;
+                }
+            }
+            result+=get(cuttedArray);
+        }
+
+        return result;
+    }
+
+    public static int get(int[] array) {
+        int result = 0;
+        for (int j = 1; j < array.length + 1; j++) {
+
+            for (int i = 0; i < j; i++) {
+                result += array[i];
+            }
+            j++;
+        }
+        return result;
     }
 }
