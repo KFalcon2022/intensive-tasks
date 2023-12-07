ebe565aed4debd5f7d432be7aa36d72b3293309d
ETSphere <elem174@proton.me>
2023-12-07 16:00:06 +0500

    Криво, но работает.

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -6,6 +6,9 @@ package com.walking.intensive.chapter3.task11;
 public class Task11 {
     public static void main(String[] args) {
 
+        int[] array = {1, 4, 2, 5, 3};
+
+        System.out.println("Output: " + getSumSubarraysOddLength(array));
     }
 
     /**
@@ -15,7 +18,21 @@ public class Task11 {
      * @return сумма всех нечетныз подмассивов
      */
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+
+        int sum =0;
+        int n = array.length;
+
+        for (int i = 0; i <n ; i++) {
+
+            for (int j = i; j <n ; j+=2) {
+
+                for (int k = i; k <=j ; k++) {
+
+                    sum += array[k];
+                }
+            }
+        }
+
+        return sum;
     }
 }
