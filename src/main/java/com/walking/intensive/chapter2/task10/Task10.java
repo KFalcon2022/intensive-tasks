4fa61ee4f534eef74e8ed759203eb5dc7560cb1e
bergen <bergy@yandex.ru>
2023-12-08 02:34:54 +0300

    chapter2.task10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,41 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
     }
 
-    boolean isPalindrome(String inputString){
+    static boolean isPalindrome(String inputString) {
+        String is = inputString;
         // Ваш код
+        int i = 0;
+        int j = is.length() - 1;
+        is = is.toUpperCase();
+
+        while (i < j) {
+            if (is.charAt(i) == is.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                if (isSpaceOrPunctuation(is.charAt(i))) {
+                    i++;
+                    continue;
+                }
+                if (isSpaceOrPunctuation(is.charAt(j))) {
+                    j--;
+                    continue;
+                }
+//                System.out.println(i + "i- " + is.charAt(i) + " " + j + "j - " + is.charAt(j));
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Знаю, что есть способ проверки на знак пунктуации - это сравнить с другим регистром, но не нашел как сделать
+    // это в java. Если подскажите - буду благодарен.
+    static boolean isSpaceOrPunctuation (char c) {
+        if (c == '.' || c == '!' || c == ' ' || c == ',' ) {
+            return true;
+        }
         return false;
     }
 }
