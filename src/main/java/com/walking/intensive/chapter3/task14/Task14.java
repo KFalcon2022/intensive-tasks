9d1ff96f3da30ae964ad20daffe4e0404fd956da
egor.djun <xedshotg@gmail.com>
2023-12-07 14:26:04 +0700

    Chapter 3, Task 14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,22 +1,49 @@
 package com.walking.intensive.chapter3.task14;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
  */
 public class Task14 {
     public static void main(String[] args) {
+        int[][] points = {{1, 3}, {3, 3}, {5, 3}, {2, 2}};
+        int[][] queries = {{2, 3, 1}, {4, 3, 1}, {1, 1, 2}};
 
+        System.out.println(Arrays.toString(getCountOfPoints(points, queries)));
     }
 
-    /**
-     * Расчет количества точек попавших в каждую окружность.
-     *
-     * @param points  массив с координатами точек
-     * @param queries массив с координатами центра и радиусом окружностей
-     * @return массив с количеством точек внутри окружностей
-     */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int answerLen = queries.length;
+        int[] answer = new int[answerLen];
+
+        for (int i = 0; i < answerLen; i++) {
+            answer[i] = amountPoints(i, points, queries);
+        }
+
+        return answer;
+    }
+
+    static int amountPoints(int index, int[][] points, int[][] queries) {
+        int pointA, pointB, circleRadius, pointX, pointY;
+        boolean isTrue;
+        int amountPoints = 0;
+
+        pointA = queries[index][0];
+        pointB = queries[index][1];
+        circleRadius = queries[index][2];
+
+        for (int i = 0; i < points.length; i++) {
+            pointX = points[i][0];
+            pointY = points[i][1];
+
+            isTrue = Math.pow(pointA - pointX, 2) + Math.pow(pointB - pointY, 2) <= Math.pow(circleRadius, 2);
+
+            if (isTrue) {
+                amountPoints++;
+            }
+        }
+
+        return amountPoints;
     }
 }
