145c34f2d6b4c107ab62d38a88cebaf1d27d7866
njmist <njmist@gmail.com>
2023-12-07 19:58:18 +0300

    chapter 3 task 15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -5,11 +5,41 @@ package com.walking.intensive.chapter3.task15;
  */
 public class Task15 {
     public static void main(String[] args) {
+        int[][] city = {{1, 0, 2}, {1, 2, 1}, {3, 2, 0}};
 
+        System.out.println(getMaxFloors(city));
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+        int count = 0;                          // Вводим счётчик
+
+        for (int i = 0; i < city.length; i++) {                         // i номер строчки
+            for (int j = 0; j < city[i].length; j++) {                  // j номер колонки
+                count += getMaxBuildingHere(i, j, city) - city[i][j];   // Вычитаем максимальное количество этажей из нынешнего в точке, добавляем этажи к счётчику
+            }
+        }
+
+        return count; // Вот и ответ, сколько этажей можно добавить без изменения линии горизонта
     }
+
+    static int getMaxBuildingHere(int stringNumber, int columnNumber, int[][] city) { // Для этого пересечения колонки и столбца
+        int stringMax = 0;
+
+        for (int i = 0; i < city[stringNumber].length; i++) {
+            if (city[stringNumber][i] > stringMax) {
+                stringMax = city[stringNumber][i];      // Находим максимальное число на строчке
+            }
+        }
+        int columnMax = 0;
+
+        for (int i = 0; i < city.length; i++) {
+            if (city[i][columnNumber] > columnMax) {
+                columnMax = city[i][columnNumber]; // Находим максимальное число на колонке
+            }
+        }
+
+        return Math.min(stringMax, columnMax); // Берем минимальное из них, тогда линия горизонта не пострадает ни с какой стороны!
+    }
+
+
 }
