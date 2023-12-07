2f19dab7671dc1de2864f0aedb0019350222ce46
RVishez <oliohcmpn@gmail.com>
2023-12-07 23:50:16 +0100

    Chapter 2 task 8 solved

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -1,15 +1,35 @@
 package com.walking.intensive.chapter2.task8;
 
+import javax.sound.midi.Soundbank;
+
 /**
  * Условие: <a href="http://geometry-math.ru/homework/Java-ticket.html">ссылка</a>
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println(getHappyTicketChance());
     }
 
     static double getHappyTicketChance() {
-        // Ваш код
-        return 0.0;
+
+        int i = 0;
+        int sumEvents = 0;
+
+        while (i < 1000000) {
+
+            String ticketNumber = String.format("%06d", i);
+
+            int firstNumber = Integer.parseInt(ticketNumber.substring(0, 1)) + Integer.parseInt(ticketNumber.substring(1, 2)) + Integer.parseInt(ticketNumber.substring(2, 3));
+            int secondNumber = Integer.parseInt(ticketNumber.substring(3, 4)) + Integer.parseInt(ticketNumber.substring(4, 5)) + Integer.parseInt(ticketNumber.substring(5, 6));
+
+            if (firstNumber == secondNumber) {
+                sumEvents++;
+            }
+
+            i++;
+        }
+
+        return (double) sumEvents / 999999.0;
     }
+
 }
