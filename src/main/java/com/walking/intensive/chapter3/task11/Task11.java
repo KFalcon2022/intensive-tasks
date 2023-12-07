82f68090269f8928a7bcfa084b1f8b88a17589ab
Pavlo <pavlokutsyn@gmail.com>
2023-12-07 15:28:12 +0100

    Chapter2_Task11 "прямой подход"

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -5,17 +5,31 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
-
+        int[] array = {1, 4, 2, 5, 3};
+        System.out.println(getSumSubarraysOddLength(array));
     }
 
     /**
-     * Подсчет суммы всех возможных подмассивов нечетной длины.
-     *
-     * @param array массив целых чисел
-     * @return сумма всех нечетныз подмассивов
+     * прохожусь по каждому елементу масива
+     * генерирую все возможные подмасивы непарной длинны
+     * делаю проверку? если масив выходит за границы основного масива, тогда прерываю расчёт
+     * (мне подсказали без прирывания выдавало Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5)
+     * сумирую елемемнты масива
      */
+
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+        int sum = 0;
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            for (int subArrayLength = 1; subArrayLength <= n; subArrayLength += 2) {
+                if (i + subArrayLength > n) {
+                    break;
+                }
+                for (int j = i; j < i + subArrayLength; j++) {
+                    sum += array[j];
+                }
+            }
+        }
+        return sum;
     }
 }
