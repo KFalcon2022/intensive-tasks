1a702f291fb2b22fed1935b43ba9f9c2cdd72dc8
ArtemiyDS <27071999@mail.ru>
2023-12-08 21:55:21 +0300

    Chapter 3 Task 15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -5,11 +5,45 @@ package com.walking.intensive.chapter3.task15;
  */
 public class Task15 {
     public static void main(String[] args) {
+        int[][] city = {{3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}};
 
+        System.out.println(getMaxFloors(city));;
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+        int maxFloors = 0;
+        int minFloorBoundary; // минимальное значение среди max значения в строке и в столбце для определенного (i,j)
+
+        for (int[] ints : city) {
+            for (int j = 0; j < city.length; j++) {
+                minFloorBoundary = Math.min(getMaxFloorInRow(city, j), getMaxFloorInColumn(ints));
+                if (ints[j] < minFloorBoundary)
+                    maxFloors += minFloorBoundary - ints[j];
+            }
+        }
+
+        return maxFloors;
+    }
+
+    static int getMaxFloorInRow(int[][] city, int index) {
+        int maxFloorInRow = 0;
+
+        for (int[] ints : city) {
+            if (maxFloorInRow < ints[index])
+                maxFloorInRow = ints[index];
+        }
+
+        return maxFloorInRow;
+    }
+
+    static int getMaxFloorInColumn(int[] city) {
+        int maxFloorInColumn = 0;
+
+        for (int j : city) {
+            if (maxFloorInColumn < j)
+                maxFloorInColumn = j;
+        }
+
+        return maxFloorInColumn;
     }
 }
