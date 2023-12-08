bca65bf7258e302d1fb5b157e6c7ceeb26fb5afd
DevTorchez <torchez@yandex.ru>
2023-12-08 20:26:16 +0300

    Resolve Chapter1.Task5

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -1,71 +1,98 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-triangle.html">ссылка</a>
  */
 public class Task5 {
+    public static double[] returnedArray = new double[3];
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-
     }
 
     static double getAreaByHeron(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        double p = (a + b + c) / 2;
+        double triangleS = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return triangleS;
     }
 
-    /**
-     * Располагайте высоты по возрастанию.
-     */
     static double[] getHeights(double a, double b, double c) {
-        //        Место для вашего кода
+        double p = (a + b + c) / 2;
+        double triangleS = 2 * Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        returnedArray[0] = triangleS / a;
+        returnedArray[1] = triangleS / b;
+        returnedArray[2] = triangleS / c;
+        Arrays.sort(returnedArray);
 
-        return null; // Заглушка. При реализации - удалить
+        return returnedArray;
     }
 
-    /**
-     * Располагайте медианы по возрастанию.
-     */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
+        double aa = a * a;
+        double bb = b * b;
+        double cc = c * c;
 
-        return null; // Заглушка. При реализации - удалить
+        returnedArray = new double[]{0, 0, 0};
+
+        returnedArray[0] = Math.sqrt(2 * bb + 2 * cc - aa) / 2;
+        returnedArray[1] = Math.sqrt(2 * aa + 2 * cc - bb) / 2;
+        returnedArray[2] = Math.sqrt(2 * aa + 2 * bb - cc) / 2;
+        Arrays.sort(returnedArray);
+
+        return returnedArray;
     }
 
-    /**
-     * Располагайте биссектрисы по возрастанию.
-     */
     static double[] getBisectors(double a, double b, double c) {
-        //        Место для вашего кода
+        double ab = a + b;
+        double ac = a + c;
+        double bc = b + c;
 
-        return null; // Заглушка. При реализации - удалить
+        returnedArray = new double[]{0, 0, 0};
+
+        returnedArray[0] = Math.sqrt(a * b * (ab + c) * (ab - c)) / ab; //c
+        returnedArray[1] = Math.sqrt(a * c * (ac + b) * (ac - b)) / ac; //b
+        returnedArray[2] = Math.sqrt(b * c * (bc + a) * (bc - a)) / bc; //a
+        Arrays.sort(returnedArray);
+
+        return returnedArray;
     }
 
-    /**
-     * Располагайте углы по возрастанию.
-     */
     static double[] getAngles(double a, double b, double c) {
-        //        Место для вашего кода
+        returnedArray = new double[]{0, 0, 0};
 
-        return null; // Заглушка. При реализации - удалить
+        returnedArray[0] = Math.toDegrees(Math.acos((b * b + c * c - a * a) / (2 * b * c))); //alpha /a
+        returnedArray[1] = Math.toDegrees(Math.acos((a * a + c * c - b * b) / (2 * a * c))); //betta /b
+        returnedArray[2] = Math.toDegrees(Math.acos((a * a + b * b - c * c) / (2 * a * b))); //gamma /c
+        Arrays.sort(returnedArray);
+
+        return returnedArray;
     }
 
     static double getInscribedCircleRadius(double a, double b, double c) {
-        //        Место для вашего кода
+        double p = (a + b + c) / 2;
+        double innerRadius = Math.sqrt(((p - a) * (p - b) * (p - c)) / p);
 
-        return 0; // Заглушка. При реализации - удалить
+        return innerRadius;
     }
 
     static double getCircumradius(double a, double b, double c) {
-        //        Место для вашего кода
+        double p = (a + b + c) / 2;
+        double outerRadius = (a * b * c) / (4 * Math.sqrt(p * (p - a) * (p - b) * (p - c)));
 
-        return 0; // Заглушка. При реализации - удалить
+        return outerRadius;
     }
 
     static double getAreaAdvanced(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return 0; // Заглушка. При реализации - удалить
+        double alpha = Math.toDegrees(Math.acos((b * b + c * c - a * a) / (2 * b * c))); //alpha /a
+        double sinAlpha = Math.sqrt(1 - Math.cos(alpha) * Math.cos(alpha));
+        double triangleS = (a * b * sinAlpha) / 2;
+
+        return triangleS;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(returnedArray);
     }
 }
