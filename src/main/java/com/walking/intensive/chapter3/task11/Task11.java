bd2dd652d965a3df8a27b839233101fc95f28d5c
ArtemiyDS <27071999@mail.ru>
2023-12-08 22:28:14 +0300

    Chapter 3 Task 11

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -5,17 +5,22 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
+        int[] massive = {1, 4, 2, 5, 3};
 
+        System.out.println(getSumSubarraysOddLength(massive));
     }
 
-    /**
-     * Подсчет суммы всех возможных подмассивов нечетной длины.
-     *
-     * @param array массив целых чисел
-     * @return сумма всех нечетныз подмассивов
-     */
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+        int sumSubarraysOddLength = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j += 2) {
+                for (int k = i; k <= j; k++) {
+                    sumSubarraysOddLength += array[k];
+                }
+            }
+        }
+
+        return sumSubarraysOddLength;
     }
 }
