3efb744abf39081b9968187b1545800ded8be19b
KrisLu23 <krisbondarev@yandex.ru>
2023-12-08 16:36:03 +0300

    Task 11

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -5,7 +5,8 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
-
+        int[] array = {1, 4, 2, 5, 3};
+        System.out.println(getSumSubarraysOddLength(array));
     }
 
     /**
@@ -15,7 +16,39 @@ public class Task11 {
      * @return сумма всех нечетныз подмассивов
      */
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+
+        int sum = 0;
+
+        for (int i = 1; i <= array.length; i += 2) {
+
+            for (int j = 0; j < array.length - i + 1; j++) {
+
+                int[] tempArray = getSubArray(array, i, j);
+                sum += getSumArray(tempArray);
+            }
+        }
+        return sum;
+    }
+
+    static int[] getSubArray(int[] array, int length, int index) {
+
+        int[] tempArray = new int[length];
+
+        for (int i = 0; i < length; i++) {
+
+            tempArray[i] = array[index];
+            index++;
+        }
+        return tempArray;
+    }
+
+    static int getSumArray(int[] array) {
+
+        int sum = 0;
+
+        for (int cell: array) {
+            sum+= cell;
+        }
+        return sum;
     }
 }
