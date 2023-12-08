db11f9e10ba43717ecd111805c88245bbafd3780
DevTorchez <torchez@yandex.ru>
2023-12-08 20:22:56 +0300

    Resolve Chapter1.Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -5,29 +5,49 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 0;
-        double c = 0;
+        double b = 1;
+        double c = 2;
 
         System.out.println(solveQuadraticEquation(a, b, c));
-
     }
 
-    /**
-     * При формировании строки, корни уравнения должны указываться по возрастанию.
-     * <p>
-     * Примеры результирующей строки:
-     * <p>
-     * Количество решений: 2. Корни: -4;4
-     * <p>
-     * Количество решений: 1. Корень: 0
-     * <p>
-     * Количество решений: 0.
-     */
     static String solveQuadraticEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        int setRoots;
+        int x1, x2 = 0;
+        String outputTail;
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b == 0 && c != 0) return "Количество решений: 0.";
+        if (a == 0 && b == 0 && c == 0) return "Решений бесконечно";
+
+        if (a == 0 && b != 0) {
+            setRoots = 1;
+            x1 = (int) ((c / b) * -1);
+            return "Количество решений: " + setRoots + ". Корень: " + x1;
+        }
+
+        if (b == 0 && c == 0) {
+            setRoots = 1;
+            x1 = 0;
+            return "Количество решений: " + setRoots + ". Корень: " + x1;
+        }
+
+        //Количество корней и решений
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant == 0) {
+            setRoots = 1;
+            x1 = (int) ((-1 * b) / (2 * a));
+            outputTail = " Корень: " + x1;
+        } else if (discriminant > 0) {
+            setRoots = 2;
+            outputTail = " Корни: ";
+            x1 = (int) ((-1 * b - Math.sqrt(discriminant)) / (2 * a));
+            x2 = (int) ((-1 * b + Math.sqrt(discriminant)) / (2 * a));
+
+        } else return "Количество решений: 0.";
+
+        String result = "Количество решений: " + setRoots + "." + outputTail + x1 + ";" + x2;
+
+        return result;
     }
 }
