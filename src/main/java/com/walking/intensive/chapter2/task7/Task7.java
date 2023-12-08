0491376225517512496ac7361271ea6c6c5148c9
mirchello <mrumilov@gmail.com>
2023-12-08 11:49:52 +0300

    chapter 2 task 7 init solution

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,40 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        System.out.println(getFriendlyPair(1000000));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    public static int getSumOfDivisors(int n) {
+        if (n < 2) {
+            return 0;
+        }
+
+        int sum = 1; // 1 всегда делитель
+        int sqrtN = (int) Math.sqrt(n);
+
+        for (int factor = 2; factor <= sqrtN; factor++) {
+            if (n % factor == 0) {
+                sum += factor;
+                int pairedFactor = n / factor;
+                if (pairedFactor != factor) { // Исключить корень квадратный n, если n - полный квадрат
+                    sum += pairedFactor;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    static int getFriendlyPair(int m) {
+        int maxSum = 0;
+        int result = 0;
+        for (int i = m - 1; i > 0; i--) {
+            int sumOfDivisiors = getSumOfDivisors(i);
+            if (i == getSumOfDivisors(sumOfDivisiors) && (i + sumOfDivisiors > maxSum) && Math.max(i, sumOfDivisiors) < m) {
+                maxSum = m + sumOfDivisiors;
+                result = Math.max(i, sumOfDivisiors);
+            }
+        }
+        return result;
     }
 }
