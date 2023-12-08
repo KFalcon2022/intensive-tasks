a16bbdef013fc823a04519bc318ff3440960daaa
RVishez <oliohcmpn@gmail.com>
2023-12-08 16:28:34 +0100

    Chapter 2 task 10 solved

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,27 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
+        System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум."));
 
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+
+        if (inputString.length() < 2) {
+            return false;
+        }
+
+        String inputStringFormatted = inputString.toLowerCase().replaceAll("[\\s\\p{P}]", "");
+
+        int stringLength = inputStringFormatted.length();
+
+        for (int i = 0, k = stringLength - 1; i < stringLength && k >= 0; i++, k--) {
+
+            if (inputStringFormatted.charAt(i) != inputStringFormatted.charAt(k)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
