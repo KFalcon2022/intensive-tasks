01427fb34a892555ad08122be01ca988cb0e021b
lambo064 <tasha_lambo@mail.ru>
2023-12-08 23:36:29 +0300

    chapter 3 task 14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter3.task14;
  */
 public class Task14 {
     public static void main(String[] args) {
-
+        getCountOfPoints(new int[][]{{1, 3}, {3, 3}, {5, 3}, {2, 2}}, new int[][]{{2, 3, 1}, {4, 3, 1}, {1, 1, 2}});
     }
 
     /**
@@ -16,7 +16,16 @@ public class Task14 {
      * @return массив с количеством точек внутри окружностей
      */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] pointsInside = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if ((Math.pow(points[j][0] - queries[i][0], 2) + Math.pow(points[j][1] - queries[i][1], 2)) <= Math.pow(queries[i][2], 2)) {
+                    pointsInside[i]++;
+                }
+            }
+        }
+        return pointsInside;
     }
+
+
 }
