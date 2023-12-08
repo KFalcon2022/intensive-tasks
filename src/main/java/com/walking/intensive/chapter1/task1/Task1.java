b068ae46ba51620d2f1b273f5902ac5820c36566
DevTorchez <torchez@yandex.ru>
2023-12-08 20:05:42 +0300

    Resolve Chapter1.Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -5,15 +5,26 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
-        System.out.println(getAgeString(age));
+        //int age = 0;
+        for (int age = 500; age <= 700; age++) {
+            System.out.println(getAgeString(age));
+        }
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        String tail = "";
+        int lastChar = age % 10;
+        int doubleDec = age % 100;
+        String temp = String.valueOf(age);
 
-        return null; // Заглушка. При реализации - удалить
+        if (lastChar == 0 || (lastChar > 4 && lastChar < 21) || (doubleDec > 4 && doubleDec < 21)) {
+            tail = " лет";
+        } else if (lastChar == 1 && age % 100 != 11) {
+            tail = " год";
+        } else if (lastChar >= 2 && lastChar <= 4) {
+            tail = " года";
+        }
+
+        return "Вам " + age + tail;
     }
 }
