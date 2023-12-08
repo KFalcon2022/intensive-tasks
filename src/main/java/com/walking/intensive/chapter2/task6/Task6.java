e7b2ed844c1cbce8ceb52885ba4c93700a423146
mirchello <mrumilov@gmail.com>
2023-12-08 10:06:01 +0300

    chapter 2 task 6 упрощение алгоритма

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-        System.out.println(getNodByEuclideanAlgorithm(16, 24));
+        System.out.println(getNodByEuclideanAlgorithm(116150, 232704));
     }
 
     static int getNoc(int m, int n) {
@@ -13,61 +13,26 @@ public class Task6 {
     }
 
     static int getNod(int m, int n) {
-        int result;
-        int multTwoGlobal = 0;
-        while (m != n && m != 0 && n != 0) {
-            boolean mIsEven = (m & 1) == 0;
-            boolean nIsEven = (n & 1) == 0;
-            if (mIsEven && nIsEven) {
-                m >>= 1;
-                n >>= 1;
-                multTwoGlobal++;
-            } else if (mIsEven) {
-                m >>= 1;
-            } else if (nIsEven) {
-                n >>= 1;
-            } else if (m > n) {
-                m = (m - n) >> 1;
+        while (m != 0 && n != 0) {
+            if (m > n) {
+                m %= n;
             } else {
-                n = (n - m) >> 1;
+                n %= m;
             }
         }
-        if (m != 0 && n != 0) {
-            result = m;
-        } else if (m == 0 && n != 0) {
-            result = n;
-        } else if (n == 0 && m != 0) {
-            result = m;
-        } else {
-            result = -1;
-        }
-        result = multTwoGlobal > 0 ? result << multTwoGlobal : result;
 
-        return result;
+        return m + n;
     }
 
     static int getNodByEuclideanAlgorithm(int m, int n) {
-        if (m == n) {
-            return n;
-        } else if (m == 0 && n != 0) {
-            return n;
-        } else if (n == 0 && m != 0) {
-            return m;
+        if (m == 0 || n == 0) {
+            return m + n;
+        } else if (m > n) {
+            return getNodByEuclideanAlgorithm(m % n, n);
+        } else {
+            return getNodByEuclideanAlgorithm(m, n % m);
         }
 
-        boolean mIsEven = (m & 1) == 0;
-        boolean nIsEven = (n & 1) == 0;
-        if (mIsEven && nIsEven) {
-            return getNodByEuclideanAlgorithm(m >> 1, n >> 1) << 1;
-        } else if (mIsEven) {
-            return getNodByEuclideanAlgorithm(m >> 1, n);
-        } else if (nIsEven) {
-            return getNodByEuclideanAlgorithm(m, n >> 1);
-        } else if (m > n) {
-            return getNodByEuclideanAlgorithm((m - n) >> 1, n);
-        } else {
-            return getNodByEuclideanAlgorithm(m, (n - m) >> 1);
-        }
     }
 
 }
