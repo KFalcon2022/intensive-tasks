be2230c3f33d2aea7dc95c20c4799823fd91a79b
user <tasiqq@gmail.com>
2023-12-09 22:19:24 +0300

    Chapter 4 Task 16

--- src/main/java/com/walking/intensive/chapter4/task16/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Main.java
@@ -1,7 +1,32 @@
 package com.walking.intensive.chapter4.task16;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[] array = {1, 3, 2, 5, 4};
+        System.out.println(Arrays.toString(doSelectionSort(array)));
+
+    }
+
+    public static int[] doSelectionSort(int[] array) {
+
+        int length = array.length;
+
+        for (int i = 0; i < length; i++) {
+            int minValue = array[i];
+            int minValueIndex = i;
+            for (int j = i; j < length; j++) {
+                if (array[j] < minValue) {
+                    minValue = array[j];
+                    minValueIndex = j;
+                }
+            }
+            array[minValueIndex] = array[i];
+            array[i] = minValue;
+        }
+
+        return array;
     }
 }
