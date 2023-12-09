fbcc32fbe7e5f228590e77bacc29b9326cbed18e
Magpier <garson.ao@gmail.com>
2023-12-09 13:48:57 +0300

    Chapter3 Task 11 ver1

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -1,11 +1,16 @@
 package com.walking.intensive.chapter3.task11;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Subarray.html">Подмассив</a>
  */
 public class Task11 {
     public static void main(String[] args) {
-
+        int[] test = {1, 4, 2, 5, 3};
+        int[] test2 = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(getSumSubarraysOddLength(test));
+        System.out.println(getSumSubarraysOddLength(test2));
     }
 
     /**
@@ -15,7 +20,22 @@ public class Task11 {
      * @return сумма всех нечетныз подмассивов
      */
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = array.length; j >= i; j--) {
+                if ((j - i) % 2 == 1) {
+                    sum += sumOddArray(Arrays.copyOfRange(array, i, j));
+                }
+            }
+        }
+        return sum;
+    }
+
+    static int sumOddArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
     }
 }
