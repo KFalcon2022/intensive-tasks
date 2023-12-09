bce950f4fe6242a80c80213644afe116af43d0f3
Alexey <lolex1@mail.ru>
2023-12-09 12:15:19 +0300

    Chapter 2 Task9

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -1,52 +1,55 @@
 package com.walking.intensive.chapter2.task9;
 
+import static java.lang.System.*;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-pascalTriangle.html">ссылка</a>
  */
 public class Task9 {
     public static void main(String[] args) {
 
-        System.out.println(getPascalTriangle(20));
-//        System.out.println(getStringFactorial(13));
-//        System.out.println(getFactorial(11));
-
+        out.println(getPascalTriangle(20));
     }
 
     static String getPascalTriangle(int n){
-       String f = "";
+        StringBuilder maxLengthStr = new StringBuilder();
 
-        for (int i = 0; i <= n; i++){
-            f = f + "\n" + getStringFactorial(i);
-
+        for (int i = n; i >= 0; i--){
+            String x = " " + getFactorial(n) / (getFactorial(i) * getFactorial(n - i));
+            maxLengthStr.append(x);
         }
 
-        return f;
+        StringBuilder resultTriangle = new StringBuilder();
+
+        for (int i = 0; i <= n; i++){
+            resultTriangle.append("\n").append(getStringFactorial(i, maxLengthStr.length()));
+        }
+
+        return resultTriangle.toString();
+    }
+
+    public static String getStringFactorial(int stringF,int maxStringLength){
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i <= stringF; i++){
+            String x = " " + getFactorial(stringF) / (getFactorial(i) * getFactorial(stringF - i));
+            result.append(x);
+        }
+
+        return getSpace((maxStringLength - result.length()) / 2) + result;
     }
 
     public static long getFactorial(long f){
-
         long result = 1;
 
         for (int i = 1; i <= f; i++) {
             result = result * i;
         }
+
         return result;
     }
 
-    public static String getStringFactorial(int stringF){
-
-        StringBuilder result = new StringBuilder(getFactorial(stringF) / (getFactorial(0) * getFactorial(stringF)) + "");
-
-        for (int i = 1; i <= stringF; i++){
-            String x = " " + getFactorial(stringF) / (getFactorial(i) * getFactorial(stringF - i));
-            result.append(x);
-        }
-
-        return result.toString();
-    }
-
     public static String getSpace(int spaceNumb){
-
         String space = "";
 
         for (int i = 0; i < spaceNumb; i++){
