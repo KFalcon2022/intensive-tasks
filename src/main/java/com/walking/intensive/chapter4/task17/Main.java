f6957d2fcdd2a0fe5d63e06e7d7efa5d31b50f32
user <tasiqq@gmail.com>
2023-12-09 22:29:46 +0300

    Chapter 4 Task 17

--- src/main/java/com/walking/intensive/chapter4/task17/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Main.java
@@ -1,7 +1,29 @@
 package com.walking.intensive.chapter4.task17;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        System.out.println(Arrays.toString(doBubbleSort(array)));
+    }
+
+    public static int[] doBubbleSort(int[] array) {
+
+        int length = array.length;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int maxValue = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = maxValue;
+                }
+            }
+        }
+
+        return array;
     }
 }
