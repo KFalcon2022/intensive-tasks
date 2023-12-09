2af33552b83cd807ce4c86a56d85a4c4bc997926
DeaDMop03 <bl.target@yandex.ru>
2023-12-09 19:04:20 +0300

    chapter2task7 release

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -8,8 +8,34 @@ public class Task7 {
 
     }
 
+    static int getSumOfDenominators (int m) {
+        int sumOfDenominators = 1;
+        //int i;
+        for (int i = 2; i <= (int) Math.floor(Math.sqrt(m)); i++) {
+            if (m == Math.pow(i, 2)) {
+                sumOfDenominators += i;
+            } else if (m % i == 0) {
+                sumOfDenominators += i + (m / i);
+            }
+        }
+        return sumOfDenominators;
+    }
+
     static int getFriendlyPair(int m){
         // Ваш код
-        return 0;
+        int maxFriendlyNumber = 1000000;
+        int maxFoundNumber = 0;
+        if (m < maxFriendlyNumber) {
+            for (int i = 2; i < m; i++) {
+                int maybeFriendlyPair = getSumOfDenominators(i);
+                if ((getSumOfDenominators(maybeFriendlyPair) == i) && maybeFriendlyPair < maxFriendlyNumber && maybeFriendlyPair != i) {
+                    int maxOfFriendlyPairs = Math.max(maybeFriendlyPair, i);
+                    if (maxOfFriendlyPairs > maxFoundNumber) {
+                        maxFoundNumber = maxOfFriendlyPairs;
+                    }
+                }
+            }
+        }
+        return maxFoundNumber;
     }
 }
