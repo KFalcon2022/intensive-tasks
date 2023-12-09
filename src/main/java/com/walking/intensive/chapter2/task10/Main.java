2e6fb3ae05f31c59344e4315622dc259ccbaddc4
Lunularis <s.n.volynkina@yandex.ru>
2023-12-09 12:06:29 +0700

    Chapter 2 Task 10: Учитывает буквы разных регистров

--- src/main/java/com/walking/intensive/chapter2/task10/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Main.java
@@ -3,26 +3,27 @@ package com.walking.intensive.chapter2.task10;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(isPalindrome("!!!8545487"));
+        System.out.println(isPalindrome("!!!85P4ip5487"));
     }
 
     static boolean isPalindrome(String inputString) {
 
         int i = 0, j = inputString.length() - 1; //i - указатель, который бежит от начала к середине, j - от конца к середине
+        String lowCaseString = inputString.toLowerCase();
 
         while (i < j) {
-            char chI = inputString.charAt(i);
+            char chI = lowCaseString.charAt(i);
 
             while (!Character.isLetter(chI) && i < j) {//двигаем указатель, пока не дойдем до буквы
                 i++;
-                chI = inputString.charAt(i);
+                chI = lowCaseString.charAt(i);
             }
 
-            char chJ = inputString.charAt(j);
+            char chJ = lowCaseString.charAt(j);
 
             while (!Character.isLetter(chJ) && i < j) {
                 j--;
-                chJ = inputString.charAt(j);
+                chJ = lowCaseString.charAt(j);
             }
 
             //System.out.println("Compare: " + chI + " and " + chJ);
