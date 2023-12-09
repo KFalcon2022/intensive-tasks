b069fb2971e0574b5e7dacc9f824c905987eff18
DeaDMop03 <bl.target@yandex.ru>
2023-12-09 22:20:41 +0300

    chapter2task8 release

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -1,15 +1,29 @@
 package com.walking.intensive.chapter2.task8;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="http://geometry-math.ru/homework/Java-ticket.html">ссылка</a>
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println(getHappyTicketChance());
     }
 
     static double getHappyTicketChance() {
         // Ваш код
-        return 0.0;
+        double sumOfCombinations = 0.0;
+        int[] numberOfCombinations = new int[28];
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 0; k <= 9; k++) {
+                    numberOfCombinations[i + j + k]++;
+                }
+            }
+        }
+        for (int numberOfCombination : numberOfCombinations) {
+            sumOfCombinations += Math.pow(numberOfCombination, 2);
+        }
+        return sumOfCombinations / 1000000;
     }
 }
