a8164f3f95f0b3ffb8553ed77d83f23205f7b043
lambo064 <tasha_lambo@mail.ru>
2023-12-09 22:47:40 +0300

    chapter 3 task 15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -5,11 +5,39 @@ package com.walking.intensive.chapter3.task15;
  */
 public class Task15 {
     public static void main(String[] args) {
-
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+        int count = 0;
+        for (int i = 0; i < city.length; i++) {
+
+            int[] column = new int[city.length];
+            int maxRowValue = getMaxValue(city[i]);
+
+            for (int j = 0; j < city.length; j++) {
+
+                for (int k = 0; k < city.length; k++) {
+                    column[k] = city[k][j];
+                }
+
+                int maxColumnValue = getMaxValue(column);
+
+                if (city[i][j] < maxRowValue && city[i][j] < maxColumnValue) {
+                    count += Math.min(maxRowValue, maxColumnValue) - city[i][j];
+                }
+            }
+        }
+        return count;
     }
+
+    public static int getMaxValue(int[] row) {
+        int maxValue = 0;
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] > maxValue) {
+                maxValue = row[i];
+            }
+        }
+        return maxValue;
+    }
+
 }
