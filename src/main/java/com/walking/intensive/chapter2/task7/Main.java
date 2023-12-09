14a56cf9679754b74e2dff8179de89fe1e4c3ba2
Lunularis <s.n.volynkina@yandex.ru>
2023-12-09 10:35:50 +0700

    Chapter 2 Task 7

--- src/main/java/com/walking/intensive/chapter2/task7/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Main.java
@@ -2,6 +2,35 @@ package com.walking.intensive.chapter2.task7;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(getFriendlyPair(10000));
+        //System.out.println(getDivisorSum(284));
 
+    }
+
+    static int getFriendlyPair(int n) {
+
+        for(int i=n; i>0; i--){
+            int friendN = getDivisorSum(i);
+            int friendDivisorSum = getDivisorSum(friendN);
+
+            if (friendDivisorSum == i && i != friendN){
+                //System.out.println("Friends: "+i +", "+friendN+" summ: "+(i+friendN));
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    static int getDivisorSum(int n) {
+
+        int sum = 0;
+
+        for (int i = 1; i < n / 2 + 1; i++) { //нет смысла искать делитель в диапазне от n/2 и выше
+            if (n % i == 0){
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
