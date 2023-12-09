ae6a31f7b7e29b353c77b1e088f6b150ea93e42a
Lunularis <s.n.volynkina@yandex.ru>
2023-12-09 11:22:08 +0700

    Chapter 2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Main.java
@@ -3,5 +3,39 @@ package com.walking.intensive.chapter2.task10;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(isPalindrome("!!!8545487"));
     }
+
+    static boolean isPalindrome(String inputString) {
+
+        int i = 0, j = inputString.length() - 1; //i - указатель, который бежит от начала к середине, j - от конца к середине
+
+        while (i < j) {
+            char chI = inputString.charAt(i);
+
+            while (!Character.isLetter(chI) && i < j) {//двигаем указатель, пока не дойдем до буквы
+                i++;
+                chI = inputString.charAt(i);
+            }
+
+            char chJ = inputString.charAt(j);
+
+            while (!Character.isLetter(chJ) && i < j) {
+                j--;
+                chJ = inputString.charAt(j);
+            }
+
+            //System.out.println("Compare: " + chI + " and " + chJ);
+            if (chI != chJ || !Character.isLetter(chI) ) {
+                //System.out.println("not palindrome");
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
 }
