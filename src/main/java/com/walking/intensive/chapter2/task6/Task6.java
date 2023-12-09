59a7288e795fb6ce7f24a293732db5987c09f252
Magpier <garson.ao@gmail.com>
2023-12-09 11:13:28 +0300

    Chapter2 Task 6 ver3 Поправил некоретное вычисление для отрицательных чисел

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -1,16 +1,20 @@
 package com.walking.intensive.chapter2.task6;
 
-import java.util.*;
-
 public class Task6 {
     public static void main(String[] args) {
         System.out.println(getHighestCommonFactor(40, 40));
         System.out.println(getGreatestCommonDivisor(1256, 2044));
-
+        System.out.println(getHighestCommonFactor(-231, -140));
+        System.out.println(getGreatestCommonDivisor(-231, -140));
+        System.out.println(getGreatestCommonDivisor(-140, -231));
+        System.out.println(getGreatestCommonDivisor(-2, -5));
+        System.out.println(getGreatestCommonDivisor(-5, -2));
 
     }
 
     static int getGreatestCommonDivisor(int number1, int number2) {
+        number1 = Math.abs(number1);
+        number2 = Math.abs(number2);
         if (number1 < number2) {
             int temp = number1;
             number1 = number2;
@@ -24,6 +28,6 @@ public class Task6 {
     }
 
     static int getHighestCommonFactor(int number1, int number2) {
-        return number1*number2/(getGreatestCommonDivisor(number1,number2));
+        return number1 * number2 / (getGreatestCommonDivisor(number1, number2));
     }
 }
