2d2d1baa33f96070485b4159ae8d9231d6e75ba3
Lunularis <s.n.volynkina@yandex.ru>
2023-12-10 18:10:08 +0700

    Chapter 4 Task 18

--- src/main/java/com/walking/intensive/chapter4/task18/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Main.java
@@ -3,5 +3,46 @@ package com.walking.intensive.chapter4.task18;
 public class Main {
     public static void main(String[] args) {
 
+        int[] answer = shakerSort(new int[]{9,8,7,6,5,4,3,2,1});
+        for (int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i]);
+        }
+
+    }
+
+    public static int[] shakerSort(int[] array) {
+
+        int leftBorder = 0;
+        int rightBorder = array.length-1;
+        boolean reshuffle = true;//была перестановка
+
+        while (reshuffle && leftBorder < rightBorder) {
+            reshuffle = false;
+
+            for (int i = leftBorder; i < rightBorder; i++) {
+                if (array[i] > array[i + 1]) {
+                    reshuffle = true;
+                    int buffer = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = buffer;
+                }
+            }
+
+            rightBorder--;
+
+            for (int i = rightBorder; i > leftBorder; i--) {
+                if (array[i] < array[i - 1]) {
+                    reshuffle = true;
+                    int buffer = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = buffer;
+                }
+            }
+
+            leftBorder++;
+
+        }
+
+        return array;
     }
 }
