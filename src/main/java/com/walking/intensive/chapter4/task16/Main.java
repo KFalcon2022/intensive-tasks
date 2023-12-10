eef2972e1d67fda2a738f26dfd6290d2aebc48f4
k2336351 <k2336351@yandex.ru>
2023-12-11 02:06:32 +0300

    Solution Chapter 4 Task 16

--- src/main/java/com/walking/intensive/chapter4/task16/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Main.java
@@ -1,7 +1,32 @@
 package com.walking.intensive.chapter4.task16;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] array = {0, 5, 2, 3, 1, 2, 3, 10, 6, 8};
 
+        System.out.println(Arrays.toString(getSelectionSortedArray(array)));
+    }
+
+    static int[] getSelectionSortedArray(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int minValueIndex = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minValueIndex]) {
+                    minValueIndex = j;
+                }
+            }
+
+            if (minValueIndex != i) {
+                int temp = array[i];
+                array[i] = array[minValueIndex];
+                array[minValueIndex] = temp;
+            }
+        }
+
+        return array;
     }
 }
