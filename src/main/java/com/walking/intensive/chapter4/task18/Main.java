ebbe45c450c99d29c36561281f9fd7717286e2cc
SkofieldAE <alexandrevd2107@gmail.com>
2023-12-10 16:13:57 +0300

    chapter 4 task 18

--- src/main/java/com/walking/intensive/chapter4/task18/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Main.java
@@ -1,7 +1,29 @@
 package com.walking.intensive.chapter4.task18;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+    public class Main {
+        public static void main(String[] args) {
+            int[] numbers = {10, 11, 1, 5, 8, 16, -4};
+
+            System.out.println(Arrays.toString(sortArray(numbers)));
+        }
+
+        public static int[] sortArray(int[] numbers) {
+            boolean swap = true;
+
+            while (swap) {
+                swap = false;
+                for (int i = 0; i < numbers.length - 1; i++) {
+                    if (numbers[i] > numbers[i + 1]) {
+                        swap = true;
+                        int tmp = numbers[i];
+                        numbers[i] = numbers[i + 1];
+                        numbers[i + 1] = tmp;
+                    }
+                }
+            }
+
+            return numbers;
+        }
     }
-}
