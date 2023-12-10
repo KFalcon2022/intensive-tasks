3ebdd90bd51e973c8ba170aa9cf3ff7275c8ffdc
lambo064 <tasha_lambo@mail.ru>
2023-12-10 22:52:59 +0300

    chapter 4 task 16

--- src/main/java/com/walking/intensive/chapter4/task16/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Main.java
@@ -2,6 +2,25 @@ package com.walking.intensive.chapter4.task16;
 
 public class Main {
     public static void main(String[] args) {
+        sortArray(new int[]{1, 3, 7, 6, 7, 2, 8});
+    }
 
+    public static void sortArray(int[] unSortedArray) {
+        for (int i = 0; i < unSortedArray.length; i++) {
+
+            int min = unSortedArray[i];
+            int minPlace = i;
+
+            for (int j = i + 1; j < unSortedArray.length; j++) {
+                if (unSortedArray[j] < min) {
+                    minPlace = j;
+                    min = unSortedArray[j];
+                }
+            }
+
+            unSortedArray[minPlace] = unSortedArray[i];
+            unSortedArray[i] = min;
+            System.out.print(unSortedArray[i]);
+        }
     }
 }
