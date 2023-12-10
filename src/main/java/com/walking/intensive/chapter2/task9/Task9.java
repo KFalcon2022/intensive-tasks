401d76706c500a88a1b0cb8ac94d386e2c33d8d8
DeaDMop03 <bl.target@yandex.ru>
2023-12-10 23:17:13 +0300

    chapter2task9 release

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,11 +5,45 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
+        //System.out.println(getPascalTriangle(10));
+    }
 
+    static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    static int getNextNumberOfTriangle (int row, int column) {
+        return getFactorial(row) / (getFactorial (row - column) * getFactorial(column));
+        //https://cf.ppt-online.org/files1/slide/t/TryEkBvHFfmAN2cX5UlqMeazhWgYJQx7n4Pp18IKC/slide-17.jpg
+        //https://cf2.ppt-online.org/files2/slide/j/JSMbARr8WZNUaxmTz6E1HfOosFcqt94ik3vBwnC5j/slide-2.jpg
+    }
+
+    static String getSpaces (int n) {
+        String spaces = "";
+        for (int i = 0; i < (n); i++) {
+            spaces += " ";
+        }
+        return spaces;
+    }
+
+    static String getARow (int n) {
+        String aRow = "";
+        for (int i = 0; i <= n; i++) {
+            aRow += Integer.toString(getNextNumberOfTriangle(n , i)) + " ";
+        }
+        return aRow;
     }
 
     static String getPascalTriangle(int n){
         // Ваш код
-        return null;
+        String pascalTriangle = "";
+        for (int i = 0; i <= n; i++) {
+            pascalTriangle += getSpaces((getARow(n).length() - getARow(i).length()) / 2) + getARow(i) + getSpaces((getARow(n).length() - getARow(i).length())) + "\n";
+            }
+        return pascalTriangle;
     }
 }
