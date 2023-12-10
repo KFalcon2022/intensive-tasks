70e50a14becae8697c925b42445d8447eef4c0b3
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-12-10 15:40:23 +0300

    Chapter3Task11

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -1,21 +1,32 @@
-package com.walking.intensive.chapter3.task11;
+package main.java.com.walking.intensive.chapter3.task11;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Subarray.html">Подмассив</a>
  */
 public class Task11 {
     public static void main(String[] args) {
-
+        System.out.println(getSumSubarraysOddLength(new int[]{1, 4, 2, 5, 3}));
     }
 
-    /**
-     * Подсчет суммы всех возможных подмассивов нечетной длины.
-     *
-     * @param array массив целых чисел
-     * @return сумма всех нечетныз подмассивов
-     */
-    static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+    public static int getSumSubarraysOddLength(int[] array) {
+        int result = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            result += array[i];
+            for (int j = array.length - 1; j > i; j--) {
+                if ((j - i) % 2 == 0) {
+                    result += sumArray(i, j, array);
+                }
+            }
+        }
+        return result;
+    }
+
+    static int sumArray(int i, int j, int[] array) {
+        int sumArray = 0;
+        for (int k = i; k <= j; k++) {
+            sumArray += array[k];
+        }
+        return sumArray;
     }
 }
