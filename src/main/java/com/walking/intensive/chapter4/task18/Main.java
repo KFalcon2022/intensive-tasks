cf1d7a69cb6f65de3ef11f5b9dd4210f0549597f
lambo064 <tasha_lambo@mail.ru>
2023-12-10 23:32:14 +0300

    chapter 4 task 18

--- src/main/java/com/walking/intensive/chapter4/task18/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Main.java
@@ -2,6 +2,46 @@ package com.walking.intensive.chapter4.task18;
 
 public class Main {
     public static void main(String[] args) {
+        shakerSort(new int[]{4, 8, 6, 5, 7, 9, 3, 45, 6, 7, 8, 9});
+    }
 
+    public static void shakerSort(int[] array) {
+
+        int count = 0;
+        int left = 0;
+        int right = array.length - 1;
+
+        for (int i = 0; i < array.length-1; i++) {
+
+            for (int j = left; j < right; j++) {
+                if (array[j] > array[j + 1]) {
+                    int max = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = max;
+                    count++;
+                }
+            }
+
+            right--;
+
+            for (int j = right; j > left; j--) {
+                if (array[j] < array[j - 1]) {
+                    int min = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = min;
+                    count++;
+                }
+            }
+
+            left++;
+
+            if (count == 0 || left >= right) {
+                break;
+            }
+        }
+
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
     }
 }
