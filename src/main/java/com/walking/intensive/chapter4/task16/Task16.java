abb71a1fe255e8d864fcb68419acc6ffda9d261b
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-12-10 17:44:29 +0300

    start

---
+++ src/main/java/com/walking/intensive/chapter4/task16/Task16.java
@@ -0,0 +1,16 @@
+package com.walking.intensive.chapter4.task16;
+
+/**
+ * Условие: <a href="https://geometry-math.ru/homework/%D0%A1hoicee-Sort.html">ссылка</a>
+ */
+public class Task16 {
+    public static void main(String[] args) {
+
+    }
+
+
+    static boolean isPalindrome(String inputString) {
+        // Ваш код
+        return false;
+    }
+}
