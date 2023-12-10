27e32412622bb0cec235707332b928aa69b099fa
Magpier <garson.ao@gmail.com>
2023-12-10 07:46:32 +0300

    Chapter3 Task 12 ver1

--- src/main/java/com/walking/intensive/chapter3/task12/Task12.java
+++ src/main/java/com/walking/intensive/chapter3/task12/Task12.java
@@ -1,11 +1,16 @@
 package com.walking.intensive.chapter3.task12;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-balls.html">МЯЧИКИ</a>
  */
 public class Task12 {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(checkAndConvert("110000")));
+        System.out.println(Arrays.toString(getNumberOfMovements("110000")));
+        System.out.println(Arrays.toString(getNumberOfMovements("001011")));
+        System.out.println(Arrays.toString(getNumberOfMovements("fgf")));
     }
 
     /**
@@ -15,7 +20,34 @@ public class Task12 {
      * @return массив с минимальным количеством операций
      */
     static int[] getNumberOfMovements(String baskets) {
-        // Ваш код
-        return new int[]{};
+        int[] input = checkAndConvert(baskets);
+        int[] result = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                if (input[j] == 1) {
+                    result[i] += Math.abs(i - j);
+                }
+            }
+        }
+        return result;
     }
+
+    static int[] checkAndConvert(String string) {
+        int[] result = new int[string.length()];
+        try {
+            for (int i = 0; i < result.length; i++) {
+                if (string.substring(i, i + 1).equals("1") || string.substring(i, i + 1).equals("0")) {
+                    result[i] = Integer.parseInt(string.substring(i, i + 1));
+                } else {
+                    throw new Exception();
+                }
+            }
+            return result;
+        } catch (Exception ex) {
+            System.err.println("Wrong input, return 111");
+            return new int[]{1, 1, 1};
+        }
+
+    }
+
 }
