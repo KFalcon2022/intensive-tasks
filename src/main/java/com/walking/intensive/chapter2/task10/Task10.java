166170e11891824d99a80f2d0dc6221499112e98
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-12-10 17:58:43 +0300

    Chapter2Task10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,10 +5,14 @@ package main.java.com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        String a = "В русской литературе достоверно известно";
     }
 
     boolean isPalindrome(String inputString) {
+        if (!isStringValid(inputString)) {
+            return false;
+        }
+
         inputString = inputString.toLowerCase();
 
         for (int i = 0, j = inputString.length() - 1; i < inputString.length() && j >= 0; i++, j--) {
@@ -26,5 +30,15 @@ public class Task10 {
             }
         }
         return true;
+    }
+
+    static boolean isStringValid(String string) {
+        int validSymbols = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLetter(string.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
