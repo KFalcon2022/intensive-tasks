94d9c9eeffe213fdb044e7c72a9b86c3bf547b56
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-12-10 21:36:08 +0300

    first commit

---
+++ src/main/java/com/walking/intensive/chapter4/task18/Task18.java
@@ -0,0 +1,14 @@
+package com.walking.intensive.chapter4.task18;
+
+/**
+ * Условие: <a href="https://geometry-math.ru/homework/Shaker-Sort.html">ссылка</a>
+ */
+public class Task18 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void shakerSort(int[] inputArray) {
+        // Ваш код
+    }
+}
