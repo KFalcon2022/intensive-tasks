98fd3af8b25f48cd2b12d7b60a97a068b1fe426e
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-12-10 21:28:33 +0300

    my choice sort

--- src/main/java/com/walking/intensive/chapter4/task16/Task16.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Task16.java
@@ -1,16 +1,58 @@
 package com.walking.intensive.chapter4.task16;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/%D0%A1hoicee-Sort.html">ссылка</a>
  */
 public class Task16 {
     public static void main(String[] args) {
 
+        int[] arr;
+        arr = new int[]{0, 2, 7, 1, 3, 4, 18, 5, 16, 6, 20, 19, 21, 17};
+        getChoiceSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
+    public static void getChoiceSort(int[] inputArray) {
+        if (inputArray.length >= 1 && !isSorted(inputArray)) {
+            choiceSort(inputArray);
+        }
+    }
 
-    static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+    static void choiceSort(int[] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+            int minIndex = minIndex(inputArray, i);
+            changeItemArray(inputArray, i, minIndex);
+        }
+    }
+
+    public static int minIndex(int[] inputArray, int startPosition) {
+        int minItem = 2147483647;
+        int minIndex = -1;
+        for (int i = startPosition; i < inputArray.length; i++) {
+            if (minItem > inputArray[i]) {
+                minIndex = i;
+                minItem = inputArray[i];
+            }
+        }
+
+        return minIndex;
+    }
+
+    public static void changeItemArray(int[] inputArray, int targetIndex, int minIndex) {
+        int temp = inputArray[targetIndex];
+        inputArray[targetIndex] = inputArray[minIndex];
+        inputArray[minIndex] = temp;
+    }
+
+    public static boolean isSorted(int[] inputArray) {
+        for (int i = 0; i < inputArray.length - 1; i++) {
+            if (inputArray[i] > inputArray[i + 1]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
