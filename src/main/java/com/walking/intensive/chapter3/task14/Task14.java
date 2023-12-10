af856bb660d52421ba7cd03020165943060bedab
SahaPWNZ <007saha@inbox.ru>
2023-12-10 21:47:55 +0300

    Chapter 3 Task 14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -4,8 +4,13 @@ package com.walking.intensive.chapter3.task14;
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
  */
 public class Task14 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int[][] arrayInputPoints = new int[][]{{1, 3}, {3, 3}, {5, 3}, {2, 2}};
+        int[][] arrayInputQueries = new int[][]{{2, 3, 1}, {4, 3, 1}, {1, 1, 2}};
+        for (int a : getCountOfPoints(arrayInputPoints, arrayInputQueries)) {
+            System.out.println(a);
+        }
     }
 
     /**
@@ -16,7 +21,19 @@ public class Task14 {
      * @return массив с количеством точек внутри окружностей
      */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] arrayResult = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+
+            for (int[] point : points) {
+                double xCordinate = Math.pow((queries[i][0] - point[0]), 2);
+                double yCordinate = Math.pow((queries[i][1] - point[1]), 2);
+                if (queries[i][2] >= Math.sqrt(xCordinate + yCordinate)) {
+                    arrayResult[i]++;
+                }
+            }
+        }
+
+        return arrayResult;
     }
 }
