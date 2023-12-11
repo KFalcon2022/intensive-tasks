64d087aa1e6daf86f26ebd879cc5b5867edc9ff6
Pavlo <pavlokutsyn@gmail.com>
2023-12-11 23:21:47 +0100

    Chapter3_Task14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -5,7 +5,16 @@ package com.walking.intensive.chapter3.task14;
  */
 public class Task14 {
     public static void main(String[] args) {
+        // Пример входных данных
+        int[][] points = {{1, 3}, {3, 3}, {5, 3}, {2, 2}};
+        int[][] queries = {{2, 3, 1}, {4, 3, 1}, {1, 1, 2}};
 
+        int[] answer = getCountOfPoints(points, queries);
+
+        // Вывод результатов
+        for (int count : answer) {
+            System.out.println(count);
+        }
     }
 
     /**
@@ -16,7 +25,28 @@ public class Task14 {
      * @return массив с количеством точек внутри окружностей
      */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] answer = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int count = 0;
+            for (int[] point : points) {
+                if (isInsideCircle(point, queries[i])) {
+                    count++;
+                }
+            }
+            answer[i] = count;
+        }
+
+        return answer;
+    }
+
+    static boolean isInsideCircle(int[] point, int[] query) {
+        // query = [x_center, y_center, radius]
+        int dx = point[0] - query[0];
+        int dy = point[1] - query[1];
+        int distanceSquared = dx * dx + dy * dy;
+        int radiusSquared = query[2] * query[2];
+
+        return distanceSquared <= radiusSquared;
     }
 }
