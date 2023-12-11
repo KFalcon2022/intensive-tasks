8348d696f2649f916fcc5e27ee4526e0a6275434
mirchello <mrumilov@gmail.com>
2023-12-11 15:24:10 +0300

    chapter 2 task 8 init solution

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,27 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
+        System.out.println(getHappyTicketChance());
+    }
 
+    static int getDigitsSum(int n){
+        String strN = String.valueOf(n);
+        int result=0;
+        for (int i=0; i<strN.length(); i++){
+            result += Integer.parseInt(String.valueOf(strN.charAt(i)));
+        }
+        return result;
     }
 
     static double getHappyTicketChance() {
-        // Ваш код
-        return 0.0;
+        double[] probabilities = new double[28];
+        double result = 0;
+        for (int i=0; i<=999; i++){
+            probabilities[getDigitsSum(i)]++;
+        }
+        for (int i=0; i<=13; i++){
+            result += 2*Math.pow(probabilities[i]/1000, 2);
+        }
+        return result;
     }
 }
