00f3e85cc02eb9c4ef05cf3afbf056d540037cfa
egor.djun <xedshotg@gmail.com>
2023-12-11 20:51:33 +0700

    Chapter 4, Task 16

--- src/main/java/com/walking/intensive/chapter4/task16/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Main.java
@@ -1,7 +1,40 @@
 package com.walking.intensive.chapter4.task16;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] initsArray = {-4, 2, -1, 3, 1, 7, 24, 9};
 
+        System.out.println(Arrays.toString(initsArray));
+        sortSelection(initsArray);
+        System.out.println(Arrays.toString(initsArray));
+    }
+
+    static int[] sortSelection(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = getMinIndex(array, i);
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+
+        return array;
+    }
+
+    static int getMinIndex(int[] array, int index) {
+        int min = Integer.MAX_VALUE;
+        int minIndex = 0;
+        
+        for (int i = index; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
     }
 }
