6d68efc7557cf1f87e7a857f41905a7d49a854b8
AVayshutis <avayshutis@yandex-team.ru>
2023-12-11 16:48:46 +0300

    Added fixes for the first task of the first chapter.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -7,30 +7,33 @@ public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         int age = 0;
+        
+        printAge(age);
+    }
 
+    static void printAge(int age) {
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 
         String declension = "Вам " + age;
-        int mod = age % 10;
+        String resultString = null;
+        int moduloDivision10 = age % 10;
+        int moduloDivision100 = age % 100;
 
-        if (age >= 0 && age < 150) {
-
-            if (age == 0 || (age >= 5 && age <= 20) || (age >= 105 && age <= 120)) {
-                declension = declension + " лет";
-            } else if (mod == 1) {
-                declension = declension + " год";
-            } else if (mod >= 2 && mod <= 4) {
-                declension = declension + " годa";
-            } else {
-                declension = declension + " лет";
-            }
-        } else {
-            declension = "Указан некорректный возраст!";
+        if (age < 0 || age > 150) {
+            return "Указан некорректный возраст!";
         }
 
-        return declension; // Заглушка. При реализации - удалить
+        if (moduloDivision10 == 0 || (moduloDivision100 >= 5 && moduloDivision100 <= 20) || (moduloDivision10 >= 5 && moduloDivision10 <= 10)) {
+            resultString = declension + " лет";
+        } else if (moduloDivision10 >= 2 && moduloDivision10 <= 4) {
+            resultString = declension + " годa";
+        } else if (moduloDivision10 == 1) {
+            resultString = declension + " год";
+        }
+
+        return resultString; // Заглушка. При реализации - удалить
     }
 }
