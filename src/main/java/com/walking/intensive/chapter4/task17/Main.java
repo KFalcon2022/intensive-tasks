d444d4562fd946c7a348865b5f39caf0fea2e7ec
k2336351 <k2336351@yandex.ru>
2023-12-11 20:35:02 +0300

    Solution Chapter 4 Task 17

--- src/main/java/com/walking/intensive/chapter4/task17/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Main.java
@@ -1,7 +1,34 @@
 package com.walking.intensive.chapter4.task17;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] array = {6, 4, 3, 5, 1, 8, 1, 3, 0};
 
+        System.out.println(Arrays.toString(getBubbleSortedArray(array, array.length)));
+    }
+
+    static int[] getBubbleSortedArray(int[] array, int arrayLength) {
+        boolean isSorted = true;
+
+        for (int i = 0; i < arrayLength - 1; i++) {
+            int nextIndex = i + 1;
+
+            if (array[i] > array[nextIndex]) {
+
+                int temp = array[i];
+                array[i] = array[nextIndex];
+                array[nextIndex] = temp;
+
+                isSorted = false;
+            }
+        }
+
+        if (isSorted) {
+            return array;
+        }
+
+        return getBubbleSortedArray(array, arrayLength - 1);
     }
 }
