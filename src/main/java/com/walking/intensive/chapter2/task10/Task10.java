1b5839ac03054ac2f45bf31c3a7ce8144cf0779e
DevTorchez <torchez@yandex.ru>
2023-12-11 19:53:33 +0300

    Resolve Chapter1.Task10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,19 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        //System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум"));
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+        //https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html
+        String temp = inputString.replaceAll("[\\p{Punct}+\\s+]", "").toLowerCase();
+        if (temp.length() < 2) return false;
+
+        String reverse = "";
+        for (int i = temp.length() - 1; i >= 0; i--){
+            reverse += temp.charAt(i);
+        }
+
+        return temp.equals(reverse);
     }
 }
