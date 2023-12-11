72182254f248e34e750a6a011326d2ce705b1e3d
stogov_so <stogov_sergei@mail.ru>
2023-12-11 19:33:29 +0500

    Chapter3_task11

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -5,17 +5,26 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
-
+        int[] array = {1, 4, 2, 5, 3, 6};
+        System.out.println(getSumSubarraysOddLength(array));
     }
 
-    /**
-     * Подсчет суммы всех возможных подмассивов нечетной длины.
-     *
-     * @param array массив целых чисел
-     * @return сумма всех нечетныз подмассивов
-     */
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+        int lengthArray = array.length;
+        int result = 0;
+
+        for (int i = 0; i < lengthArray; i++) {
+            int lengthSubarrays = i + 1;
+
+            while (lengthSubarrays <= lengthArray) {
+                for (int j = i; j < lengthSubarrays; j++) {
+                    result += array[j];
+                }
+
+                lengthSubarrays += 2;
+            }
+        }
+
+        return result;
     }
 }
