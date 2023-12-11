de8ab0566103d3d9332f92f7c2a9c6e20c12cadb
DevTorchez <torchez@yandex.ru>
2023-12-11 19:48:55 +0300

    Resolve Chapter1.Task7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -4,12 +4,49 @@ package com.walking.intensive.chapter2.task7;
  * Условие: <a href="https://geometry-math.ru/homework/java-friendly-number.html">ссылка</a>
  */
 public class Task7 {
-    public static void main(String[] args) {
+    static int numOne; //Для доступа и контроля из main
+    static int numTwo;
 
+    public static void main(String[] args) {
+        System.out.println("getFriendlyPair() " + getFriendlyPair(1000000));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        numOne = 0;
+        numTwo = -1;
+
+        for (int i = 200; i <= m; i++) {
+            int getSum_i = getSum(i);
+            int j = getSum(getSum_i);
+            //if (i + j > m) break;
+            if (getSum(getSum(i)) == i && getSum(i) != i) {
+                //if (i + getSum(i) > m) {
+                if (i + getSum(i) > 1000000) {
+                    break;
+                } else {
+                    numOne = i;
+                    numTwo = getSum(i);
+                }
+            }
+        }
+        if (numOne == 0 && numTwo == -1) {
+            System.out.println("Дружественных чисесл на интервале от 1 до " + m + " нет.");
+            return 0;
+        }
+        return Math.max(numOne, numTwo);
+    }
+
+    static int getSum(int num) {
+        int counter = 1;
+
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                counter += i;
+                counter += num / i;
+            }
+        }
+        if (Math.sqrt(num) % 1 == 0)
+            counter += Math.sqrt(num);
+        return counter;
     }
 }
