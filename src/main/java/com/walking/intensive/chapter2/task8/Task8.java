f6e1d9996b485670b50ad0622872df2a05f23510
mirchello <mrumilov@gmail.com>
2023-12-11 23:41:07 +0300

    chapter 2 task 8 рефакторинг и переработка метода подсчета суммы цифр в числе

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -8,24 +8,45 @@ public class Task8 {
         System.out.println(getHappyTicketChance());
     }
 
-    static int getDigitsSum(int n){
-        String strN = String.valueOf(n);
-        int result=0;
-        for (int i=0; i<strN.length(); i++){
-            result += Integer.parseInt(String.valueOf(strN.charAt(i)));
+    static double getHappyTicketChance() {
+        int maxDigitsCount = 6;
+        double[] probabilities;
+        double result = 0;
+        int maxHalfSumOfDigits = (maxDigitsCount / 2) * 9 + 1;
+
+        if (maxDigitsCount % 2 == 1) {
+            return 0;
+        } else {
+            probabilities = new double[maxHalfSumOfDigits];
         }
+
+        int maxHalfNumber = 9;
+        for (int i = 0; i < maxDigitsCount / 2 - 1; i++) {
+            maxHalfNumber *= 10;
+            maxHalfNumber += 9;
+        }
+
+        for (int i = 0; i <= maxHalfNumber; i++) {
+            probabilities[getDigitsSum(i)]++;
+        }
+
+        int halfProbabilityCount = (maxHalfSumOfDigits + 1) / 2 - 1;
+        for (int i = 0; i <= halfProbabilityCount; i++) {
+            result += 2 * Math.pow(probabilities[i] / Math.pow(10, maxDigitsCount / 2), 2);
+        }
+        if (maxHalfSumOfDigits % 2 != 0) {
+            result += Math.pow(probabilities[halfProbabilityCount + 1] / Math.pow(10, maxDigitsCount / 2), 2);
+        }
+
         return result;
     }
 
-    static double getHappyTicketChance() {
-        double[] probabilities = new double[28];
-        double result = 0;
-        for (int i=0; i<=999; i++){
-            probabilities[getDigitsSum(i)]++;
+    static int getDigitsSum(int n) {
+        int result = 0;
+        while (n / 10 > 0) {
+            result += n % 10;
+            n /= 10;
         }
-        for (int i=0; i<=13; i++){
-            result += 2*Math.pow(probabilities[i]/1000, 2);
-        }
-        return result;
+        return result + n;
     }
 }
