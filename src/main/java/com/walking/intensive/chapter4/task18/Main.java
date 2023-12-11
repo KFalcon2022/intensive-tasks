c8a5b2c5aba01a6fb33bf0ef9d1967cbeb96a61e
ganka-xn <jxn@inbox.ru>
2023-12-11 23:53:00 +0300

    Ch.4 Task18

--- src/main/java/com/walking/intensive/chapter4/task18/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Main.java
@@ -1,7 +1,45 @@
 package com.walking.intensive.chapter4.task18;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] testArray = new int[20];
 
+        for (int i = 0; i < testArray.length; i++) {
+            testArray[i] = (int) (Math.random() * 100);
+        }
+
+        System.out.println(Arrays.toString(testArray));
+        System.out.println(Arrays.toString(shakerSort(testArray)));
+    }
+
+    static int[] shakerSort(int[] array) {
+        int firstIndex = 0;
+        int lastIndex = array.length - 1;
+
+        while (firstIndex < lastIndex) {
+            for (int i = firstIndex; i < lastIndex; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temporaryValue = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temporaryValue;
+                }
+            }
+
+            lastIndex--;
+
+            for (int i = lastIndex; i > firstIndex; i--) {
+                if (array[i - 1] > array[i]) {
+                    int temporaryValue = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temporaryValue;
+                }
+            }
+
+            firstIndex++;
+        }
+
+        return array;
     }
 }
