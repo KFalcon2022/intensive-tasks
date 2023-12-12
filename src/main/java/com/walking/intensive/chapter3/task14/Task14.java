0ca83d2f5405f2d9ac666f7e72d236fe96988e82
stogov_so <stogov_sergei@mail.ru>
2023-12-12 20:32:30 +0500

    Chapter3_task14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,22 +1,36 @@
 package com.walking.intensive.chapter3.task14;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
  */
 public class Task14 {
     public static void main(String[] args) {
-
+        int[][] points = {{1, 3}, {3, 3}, {5, 3}, {2, 2}};
+        int[][] queries = {{2, 3, 1}, {4, 3, 1}, {1, 1, 2}};
+        System.out.println(Arrays.toString(getCountOfPoints(points, queries)));
     }
 
-    /**
-     * Расчет количества точек попавших в каждую окружность.
-     *
-     * @param points  массив с координатами точек
-     * @param queries массив с координатами центра и радиусом окружностей
-     * @return массив с количеством точек внутри окружностей
-     */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] countOfPoints = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int capacityOneCircle = 0;
+
+            for (int[] point : points) {
+                int lengthAxisX = point[0] - queries[i][0];
+                int lengthAxisY = point[1] - queries[i][1];
+                int radius = queries[i][2];
+
+                if (Math.pow(lengthAxisX, 2) + Math.pow(lengthAxisY, 2) <= Math.pow(radius, 2)) {
+                    capacityOneCircle++;
+                }
+            }
+
+            countOfPoints[i] = capacityOneCircle;
+        }
+
+        return countOfPoints;
     }
 }
