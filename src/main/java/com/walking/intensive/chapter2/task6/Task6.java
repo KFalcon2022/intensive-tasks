63029cc48e15e019755ba39ea872f4f6381c5b22
BogdanKaliuzhniy <kaliuzhnb@gmail.com>
2023-12-12 07:48:00 +0300

    Chapter 2, task 6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,22 +5,49 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-
+        int m = 1600;
+        int n = 25472;
+        System.out.println("NOC: " + getNoc(m, n));
+        System.out.println("NOD: " + getNod(m, n));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
+        if (m == n && n == 0) {
+            return 123;
+        }
+
+        if (getNod(m, n) == 1) {
+            return m * n;
+        }
+
+        int biggest = Math.max(m, n);
+        int smaller = Math.min(m, n);
+        int temp;
+
+        for (int i = 1; i < biggest; i++) {
+            temp = biggest * i % smaller;
+            if (temp == 0) {
+                return biggest * i;
+            }
+        }
         return 0;
     }
+
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        int biggest = Math.max(m, n);
+        int smaller = Math.min(m, n);
+        int temp = biggest % smaller;
+
+        while (temp != 0) {
+            biggest = smaller;
+            smaller = temp;
+            temp = biggest % smaller;
+        }
+        return smaller;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
+    static int getNodByEuclideanAlgorithm(int m, int n) {
         return 0;
     }
-
 }
