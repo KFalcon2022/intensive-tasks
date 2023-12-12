821058441dcdf0358806b9859971b63e4f4306da
mbishy <mbishy@yandex.ru>
2023-12-12 22:31:01 +0500

    1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,14 +6,25 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 125;
 
         System.out.println(getAgeString(age));
+
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age < 0 || age > 127) {
+            return "Введеный возраст некорректен";
+        } else {
+            if (age > 10 && age < 15 || age > 110 && age < 115) {
+                return "Вам " + age + " лет.";
+            } else if (age == 1 || age % 10 == 1) {
+                return "Вам " + age + " год.";
+            } else if (age > 1 && age < 5 || age % 10 > 1 && age % 10 < 5) {
+                return "Вам " + age + " года.";
+            } else {
+                return "Вам " + age + " лет.";
+            }
+        }
     }
 }
