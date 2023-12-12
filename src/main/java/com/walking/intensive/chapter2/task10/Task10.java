0b423440463349af92327361753fc063748a4f06
mirchello <mrumilov@gmail.com>
2023-12-12 13:02:40 +0300

    chapter 2 task 10 init solution

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,11 +5,24 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome("Муза, ранясь шилом опыта, ты помолишься на разумы"));
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+        String stringCleared = "";
+
+        for (int i = 0; i < inputString.length(); i++) {
+            stringCleared += Character.isLetter(inputString.charAt(i)) ?
+                    Character.toUpperCase(inputString.charAt(i)) :
+                    "";
+        }
+
+        for (int i =0; i<stringCleared.length()/2-1; i++){
+            if (stringCleared.charAt(i)!=stringCleared.charAt(stringCleared.length()-1-i)){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
