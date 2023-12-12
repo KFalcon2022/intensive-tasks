4187a1287cb26e97a8c1308e732451cf3cf76da6
Pavlo <pavlokutsyn@gmail.com>
2023-12-12 11:52:47 +0100

    Chapter3_Task15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -5,11 +5,33 @@ package com.walking.intensive.chapter3.task15;
  */
 public class Task15 {
     public static void main(String[] args) {
-
+        // Пример использования
+        int[][] city = {{2, 1}, {1, 3}};
+        System.out.println("Максимальное количество этажей, которые можно добавить: " + getMaxFloors(city));
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+        int n = city.length;
+        int[] maxRow = new int[n];
+        int[] maxCol = new int[n];
+
+        // Находим максимальные значения по строкам и столбцам
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                maxRow[i] = Math.max(maxRow[i], city[i][j]);
+                maxCol[j] = Math.max(maxCol[j], city[i][j]);
+            }
+        }
+
+        // Вычисляем возможное увеличение для каждого здания
+        int totalIncrease = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int possibleIncrease = Math.min(maxRow[i], maxCol[j]) - city[i][j];
+                totalIncrease += possibleIncrease;
+            }
+        }
+
+        return totalIncrease;
     }
 }
