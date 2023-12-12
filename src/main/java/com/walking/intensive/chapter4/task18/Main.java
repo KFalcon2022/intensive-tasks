6d7e2d06ff7031df498d45d8f2c72eafab858df8
k2336351 <k2336351@yandex.ru>
2023-12-13 02:32:52 +0300

    Solution Chapter 4 Task 18

--- src/main/java/com/walking/intensive/chapter4/task18/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Main.java
@@ -1,7 +1,45 @@
 package com.walking.intensive.chapter4.task18;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] array = {5, 10, 7, 1, 4, 16, 3, 0, 8, 12, 17, 6};
 
+        System.out.println(Arrays.toString(getShakerSortedArray(array, array.length, 0)));
+    }
+
+    static int[] getShakerSortedArray(int[] array, int arrayLength, int lastIndexForMinValueCheck) {
+        boolean isSorted = true;
+
+        for (int i = lastIndexForMinValueCheck; i < arrayLength - 1; i++) {
+            int nextIndex = i + 1;
+
+            if (array[i] > array[nextIndex]) {
+                int temp = array[i];
+                array[i] = array[nextIndex];
+                array[nextIndex] = temp;
+
+                isSorted = false;
+            }
+        }
+
+        for (int i = arrayLength - 2; i > lastIndexForMinValueCheck; i--) {
+            int previousIndex = i - 1;
+
+            if (array[i] < array[previousIndex]) {
+                int temp = array[i];
+                array[i] = array[previousIndex];
+                array[previousIndex] = temp;
+
+                isSorted = false;
+            }
+        }
+
+        if (isSorted) {
+            return array;
+        }
+
+        return getShakerSortedArray(array, arrayLength - 1, lastIndexForMinValueCheck + 1);
     }
 }
