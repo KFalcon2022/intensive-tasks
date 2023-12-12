78a873a43246dd43752f7e2b88adf67c36ac7cf7
njmist <njmist@gmail.com>
2023-12-12 12:35:32 +0300

    chapter 4 task 20

--- src/main/java/com/walking/intensive/chapter4/task20/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task20/Main.java
@@ -1,7 +1,49 @@
 package com.walking.intensive.chapter4.task20;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] array = {2, 0, -5, 100, 6, 2, 7, 8};
 
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(sortHeap(array)));
+    }
+
+    static int[] sortHeap(int[] array) { // Метод для сортировки массива кучей
+        for (int i = array.length / 2 - 1; i >= 0; i--) { // начиная с конца первой половины массива
+            makeHeap(array, i, array.length); // Строим кучу
+        }
+        for (int i = array.length - 1; i >= 0; i--) {
+            swapElements(array, 0, i); // Меняем нулевой элемент кучи (самый большой) с последним элементом массива
+            makeHeap(array, 0, i); // Строим кучу без последнего элемента (отсортированного)
+        }
+
+        return array; // Возвращаем отсортированный массив
+    }
+
+    static void makeHeap(int[] array, int i, int heapEnd) { // Метод для создания кучи
+        int rootIndex = i; // обозначаем индекс корня
+        int leftChild = 2 * i + 1; // обозначаем индекс левого потомка
+        int rightChild = 2 * i + 2; // обозначаем индекс правого потомка
+
+        if (leftChild < heapEnd && array[leftChild] > array[rootIndex]) { // Если индекс левого потомка входит длину массива и при его значение больше корневого
+            rootIndex = leftChild; // назначаем новый корень для последующей рекурсии
+        }
+        if (rightChild < heapEnd && array[rightChild] > array[rootIndex]) {
+            rootIndex = rightChild; // Аналогично проверяем правого потомка
+        }
+        if (rootIndex != i) { // Если какой-то потомок больше корня
+            swapElements(array, i, rootIndex); // Меняем местами потомка с корнем
+            makeHeap(array, rootIndex, heapEnd); // Запускаем метод для этого участка еще раз
+        }
+        // Если ничего не меняли, то выходим из рекурсии
+    }
+
+    static void swapElements(int[] array, int firstElementIndex, int secondElementIndex) { // Метод для перестановки элементов массива
+        int temp = array[firstElementIndex];
+
+        array[firstElementIndex] = array[secondElementIndex];
+        array[secondElementIndex] = temp;
     }
 }
