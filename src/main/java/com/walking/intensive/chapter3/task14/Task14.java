98231aa4c2d537fbc1a30652d4ac447190850a58
SenyaDiamond <SenyaDiamond@gmail.com>
2023-12-12 15:33:40 +0300

    Chapter 3 Task 14 v 1.0

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter3.task14;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
  */
@@ -16,7 +18,19 @@ public class Task14 {
      * @return массив с количеством точек внутри окружностей
      */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] answer = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
+            for (int[] point : points) {
+                int pointX = Math.abs(point[0] - query[0]);
+                int pointY = Math.abs(point[1] - query[1]);
+                if (Math.sqrt(Math.pow(pointX, 2) + Math.pow(pointY, 2)) <= query[2]) {
+                    answer[i]++;
+                }
+            }
+        }
+
+        return answer;
     }
 }
