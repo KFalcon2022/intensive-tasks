6c49d60dbf27db96cfd35b08c5e135a0d649f87e
ganka-xn <jxn@inbox.ru>
2023-12-12 15:20:17 +0300

    Ch.4 Task 17 Если за полный проход шага 1 по массиву не был переставлен ни 1 элемент, сортировка окончена. Скорректировано.

--- src/main/java/com/walking/intensive/chapter4/task17/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Main.java
@@ -1,7 +1,37 @@
 package com.walking.intensive.chapter4.task17;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] testArray = new int[20];
 
+        for (int i = 0; i < testArray.length; i++) {
+            testArray[i] = (int) (Math.random() * 100);
+        }
+
+        System.out.println(Arrays.toString(testArray));
+        System.out.println(Arrays.toString(bubbleSort(testArray)));
+    }
+
+    static int[] bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int countSorted = 0;
+
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temporaryValue = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temporaryValue;
+                    countSorted++;
+                }
+            }
+
+            if (countSorted == 0) {
+                break;
+            }
+        }
+
+        return array;
     }
 }
