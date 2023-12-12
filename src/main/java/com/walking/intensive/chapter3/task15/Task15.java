a9968bb9fc8c8b350c72fc9d7dcb012fe0770fda
stogov_so <stogov_sergei@mail.ru>
2023-12-13 01:43:25 +0500

    Chapter3_task15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -5,11 +5,35 @@ package com.walking.intensive.chapter3.task15;
  */
 public class Task15 {
     public static void main(String[] args) {
-
+        int[][] city = {{2, 1}, {1, 3}};
+        System.out.println(getMaxFloors(city));
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+        int countFloorsBuilt = 0;
+
+        for (int[] blocks : city) {
+            for (int i = 0; i < blocks.length; i++) {
+                int maxHeightFromNorthToSouth = 0;
+                int maxHeightFromWestToEast = 0;
+
+                for (int j = 0; j < city.length; j++) {
+                    if (city[j][i] > maxHeightFromNorthToSouth) {
+                        maxHeightFromNorthToSouth = city[j][i];
+                    }
+                    if (blocks[j] > maxHeightFromWestToEast) {
+                        maxHeightFromWestToEast = blocks[j];
+                    }
+                }
+
+                int maxAvailableHeight = Math.min(maxHeightFromWestToEast, maxHeightFromNorthToSouth);
+
+                if (maxAvailableHeight > blocks[i]) {
+                    countFloorsBuilt += maxAvailableHeight - blocks[i];
+                }
+            }
+        }
+
+        return countFloorsBuilt;
     }
 }
