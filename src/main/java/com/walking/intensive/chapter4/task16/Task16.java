64c058d2587618397b0403accf4c0232bd800541
ArtemiyDS <27071999@mail.ru>
2023-12-13 22:12:36 +0300

    Chapter 4 Task 16

--- src/main/java/com/walking/intensive/chapter4/task16/Task16.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Task16.java
@@ -5,11 +5,31 @@ package com.walking.intensive.chapter4.task16;
  */
 public class Task16 {
     public static void main(String[] args) {
+        int[] array = {4, 6, 12, 5, 4, 6, -1, -10};
 
+        int[] result = sortBySelection(array);
+        for (int num : result)
+            System.out.print(num + " ");
     }
 
     private static int[] sortBySelection(int[] array) {
+        int tmp; // tmp - переменная для переинициализации
+        int indexOfMinNumber;
 
-        return new int[]{};
+        for (int i = 0; i < array.length; i++) {
+            indexOfMinNumber = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[indexOfMinNumber] > array[j]) {
+                    indexOfMinNumber = j;       // запоминаем индекс переменной, где на данный момент содержится минимальное значение
+                }
+            }
+
+            tmp = array[i];
+            array[i] = array[indexOfMinNumber];
+            array[indexOfMinNumber] = tmp;
+
+        }
+
+        return array;
     }
 }
