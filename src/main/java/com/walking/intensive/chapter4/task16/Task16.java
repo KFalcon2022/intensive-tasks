ba3b6d96c879de04f3292f914c384669d56412d6
Pavlo <pavlokutsyn@gmail.com>
2023-12-13 18:58:08 +0100

    Chapter4_Task16

--- src/main/java/com/walking/intensive/chapter4/task16/Task16.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Task16.java
@@ -5,11 +5,26 @@ package com.walking.intensive.chapter4.task16;
  */
 public class Task16 {
     public static void main(String[] args) {
-
+        int[] array = {64, 25, 12, 22, 11}; // Пример массива
+        array = sortBySelection(array);
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
     }
 
     private static int[] sortBySelection(int[] array) {
-
-        return new int[]{};
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Обмен элементов
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        return array;
     }
 }
