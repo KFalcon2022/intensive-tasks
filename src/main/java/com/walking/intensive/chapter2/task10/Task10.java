96a3a26c1532eaf60940400dedf70288a2dbffc9
Константин Синяков <k.siniakov@yandex.ru>
2023-12-13 19:56:52 +0300

    Chapter 2 Task 10. Поправил замечания

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -5,31 +5,41 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Мат и тут и там"));
+        System.out.println(isPalindrome("!!! ."));
     }
 
     static boolean isPalindrome(String inputString) {
         int leftShift = 0, rightShift = 0;
+        boolean result = false;
+
         for (int i = 0; i < inputString.length(); i++) {
             while (!Character.isAlphabetic(inputString.charAt(leftShift + i))) {
                 leftShift++;
+                if ((leftShift + i) == inputString.length()) {
+                    break;
+                }
             }
             while (!Character.isAlphabetic(inputString.charAt(inputString.length() - (rightShift + i + 1)))) {
                 rightShift++;
+                if (inputString.length() == (rightShift + i + 1)) {
+                    break;
+                }
             }
             int leftPosition = leftShift + i;
             int rightPosition = inputString.length() - (rightShift + i + 1);
 
-            if ( leftPosition >= rightPosition){
+            if (leftPosition >= rightPosition) {
                 break;
             }
 
             if (Character.toUpperCase(inputString.charAt(leftPosition)) != Character.toUpperCase(inputString.charAt(rightPosition))) {
-                return false;
+                result = false;
+                break;
+            } else {
+                result = true;
             }
-
         }
 
-        return true;
+        return result;
     }
 }
