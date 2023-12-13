eb64cf49463193a4a44778f40c837aa6f0d365fb
Lolita <arameya2013@gmail.com>
2023-12-13 16:16:09 +0300

    Исправление ошибок

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -7,18 +7,19 @@ public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         int age;
+
         for (age = 0; age < 128; age++) {
             System.out.println(getAgeString(age));
         }
     }
 
     static String getAgeString(int age) {
-        int ageRecalc = age % 100;
-        if (ageRecalc > 4 && ageRecalc < 21) {
-            return "Вам " + age + " лет";
-        } else if (ageRecalc % 10 == 1) {
+        int ageMod = age % 100;
+
+        if (ageMod % 10 == 1 && ageMod != 11) {
             return "Вам " + age + " год";
-        } else if (ageRecalc % 10 == 2 || ageRecalc % 10 == 3 || ageRecalc % 10 == 4) {
+        }
+        if (ageMod % 10 > 1 && ageMod % 10 < 5 && (ageMod < 12 || ageMod > 14)) {
             return "Вам " + age + " года";
         } else {
             return "Вам " + age + " лет";
