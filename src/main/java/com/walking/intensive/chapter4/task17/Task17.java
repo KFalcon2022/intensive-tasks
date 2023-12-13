dda3d1729fec60a8668237bbdf2954f92e3d9a05
sauza7 <sauza777@yandex.ru>
2023-12-13 14:04:18 +0300

    Chapter 4. task 17 Improved Solution

--- src/main/java/com/walking/intensive/chapter4/task17/Task17.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Task17.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Task17 {
     public static void main(String[] args) {
 
-        int[] arr = {1, 90, 34, 12, 0, -1, 122, 45, 17};
+        int[] arr = {1, 90, 34, 12, 134, 0, -1, 122, 45, 17};
 
         System.out.println(Arrays.toString(sortByBubble(arr)));
     }
@@ -16,12 +16,13 @@ public class Task17 {
     private static int[] sortByBubble(int[] array) {
 
         boolean continueSorting = true;
+        int maxCheckedIndex = array.length -1;
 
         while (continueSorting) {
 
             continueSorting = false;
 
-            for (int i = 0; i < array.length - 1; i++) {
+            for (int i = 0; i < maxCheckedIndex; i++) {
 
                 if (array[i] > array[i + 1]) {
 
@@ -31,6 +32,7 @@ public class Task17 {
                     continueSorting = true;
                 }
             }
+            maxCheckedIndex--;
         }
         return array;
     }
