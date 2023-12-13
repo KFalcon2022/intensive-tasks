d0672fefda762a0e0864b1a7aa5411345b672839
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-12-13 18:23:07 +0300

    Chapter3Task14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,4 +1,6 @@
-package com.walking.intensive.chapter3.task14;
+package main.java.com.walking.intensive.chapter3.task14;
+
+import static java.lang.Math.*;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
@@ -8,15 +10,17 @@ public class Task14 {
 
     }
 
-    /**
-     * Расчет количества точек попавших в каждую окружность.
-     *
-     * @param points  массив с координатами точек
-     * @param queries массив с координатами центра и радиусом окружностей
-     * @return массив с количеством точек внутри окружностей
-     */
-    static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+    public static int[] getCountOfPoints(int[][] points, int[][] queries) {
+        int[] answer = new int[queries.length];
+
+        for (int j = 0; j < queries.length; j++) {
+            int countPoints = 0;
+            for (int[] point : points) {
+                if ((pow((point[0] - queries[j][0]), 2) + (pow((point[1] - queries[j][1]), 2))) <= pow(queries[j][2], 2)) {
+                    answer[j]++;
+                }
+            }
+        }
+        return answer;
     }
 }
