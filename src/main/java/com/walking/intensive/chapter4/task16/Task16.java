c656320807c1f8e1571ac5c096e849bcfcdd5557
SenyaDiamond <SenyaDiamond@gmail.com>
2023-12-13 13:46:37 +0300

    Chapter 4 Task 16 v 1.0

--- src/main/java/com/walking/intensive/chapter4/task16/Task16.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Task16.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter4.task16;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/%D0%A1hoicee-Sort.html">Сортировка выбором</a>
  */
@@ -10,6 +12,19 @@ public class Task16 {
 
     private static int[] sortBySelection(int[] array) {
 
-        return new int[]{};
+        for (int i = 0; i < array.length; i++) {
+            int currentMin = Integer.MAX_VALUE;
+            int minPosition = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] <= currentMin) {
+                    currentMin = array[j];
+                    minPosition = j;
+                }
+            }
+            array[minPosition] = array[i];
+            array[i] = currentMin;
+        }
+
+        return array;
     }
 }
