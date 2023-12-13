58ddb054167f7d748126e8ac0858882e49de1b0d
Lolita <arameya2013@gmail.com>
2023-12-13 15:10:05 +0300

    Решение задачи про возраст

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,14 +6,22 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
-        System.out.println(getAgeString(age));
+        int age;
+        for (age = 0; age < 128; age++) {
+            System.out.println(getAgeString(age));
+        }
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        int ageRecalc = age % 100;
+        if (ageRecalc > 4 && ageRecalc < 21) {
+            return "Вам " + age + " лет";
+        } else if (ageRecalc % 10 == 1) {
+            return "Вам " + age + " год";
+        } else if (ageRecalc % 10 == 2 || ageRecalc % 10 == 3 || ageRecalc % 10 == 4) {
+            return "Вам " + age + " года";
+        } else {
+            return "Вам " + age + " лет";
+        }
     }
 }
