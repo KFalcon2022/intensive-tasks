ff27cc3b8d62dd626fdd0431907dd321bae67139
mbishy <mbishy@yandex.ru>
2023-12-13 22:54:48 +0500

    2

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,7 +6,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 125;
+        int age = 12;
 
         System.out.println(getAgeString(age));
 
