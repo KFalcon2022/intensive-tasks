d6ed8860fa5c316031ca84f5676b90c39af449ec
Константин Синяков <k.siniakov@yandex.ru>
2023-12-13 22:14:15 +0300

    Chapter 3 Task 14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,11 +1,16 @@
 package com.walking.intensive.chapter3.task14;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
  */
 public class Task14 {
     public static void main(String[] args) {
+        int[][] points_1 = {{1, 3}, {3, 3}, {5, 3}, {2, 2}};
+        int[][] queries_1 = {{2, 3, 1}, {4, 3, 1}, {1, 1, 2}};
 
+        System.out.println(Arrays.toString(getCountOfPoints(points_1, queries_1)));
     }
 
     /**
@@ -16,7 +21,28 @@ public class Task14 {
      * @return массив с количеством точек внутри окружностей
      */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] answer = new int[queries.length];
+        int index = 0;
+        for (int[] query : queries
+        ) {
+            for (int[] point : points
+            ) {
+                if (isInsidePoint(query, point)) {
+                    answer[index] += 1;
+                }
+            }
+            index++;
+        }
+        return answer;
+    }
+
+    static boolean isInsidePoint(int[] query, int[] point) {
+        if ((query[0] - query[2] <= point[0] && point[0] <= query[0] + query[2]) && (query[1] - query[2] <= point[1] && point[1] <= query[1] + query[2])) {
+            int a = Math.abs(query[0] - point[0]);
+            int b = Math.abs(query[1] - point[1]);
+            return Math.sqrt(a * a + b * b) <= query[2];
+        }
+
+        return false;
     }
 }
