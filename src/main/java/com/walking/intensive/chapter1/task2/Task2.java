94c2d3608139cf66533041a6e01870149c00d68e
myLordShip <Volandemortp@bk.ru>
2023-12-13 22:10:43 +0400

    chapter1.task2 ver. 0.1

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -6,12 +6,15 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+        System.out.println(71 / 4);
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if ((flatNumber < 1) || (flatNumber > (floorAmount * entranceAmount * 4))) {
+            return ("Такой квартиры не существует");
+        }
+        int entranceFound = flatNumber / 4 + 1;
+        int floorFound = entranceFound / entranceAmount
+            return (flatNumber + " кв - " + (flatNumber / 4 + 1)/ entranceAmount); // Заглушка. При реализации - удалить
     }
 }
