9ee30d32ef4a6d19199f2e5f2702023f295a6406
ArtemiyDS <27071999@mail.ru>
2023-12-13 22:29:57 +0300

    Chapter 4 Task 17

--- src/main/java/com/walking/intensive/chapter4/task17/Task17.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Task17.java
@@ -5,11 +5,27 @@ package com.walking.intensive.chapter4.task17;
  */
 public class Task17 {
     public static void main(String[] args) {
+        int[] array = {1, 3, 4, 1, -1, 0, 18, -1};
 
+        int[] result = sortByBubble(array);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
     }
 
     private static int[] sortByBubble(int[] array) {
+        int tmp;
 
-        return new int[]{};
+        for (int i = array.length; i > 1; i--) {
+            for (int j = 1; j < i; j++) {
+                if (array[j] < array[j - 1]) {
+                    tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+            }
+        }
+
+        return array;
     }
 }
