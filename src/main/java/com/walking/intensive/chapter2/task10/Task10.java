b6518fef2b511e8542cdc0e3814825584a0e3c88
Rust <madishkin2@gmail.com>
2023-12-13 20:34:39 +0300

    Chapter 2 Task 10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,37 @@
 package com.walking.intensive.chapter2.task10;
 
+
+
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
 public class Task10 {
     public static void main(String[] args) {
+        //System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум."));
+        System.out.println(isPalindrome("А"));
+    }
 
+    public static StringBuilder getClearSentence(String dirtySentence) {
+        StringBuilder clearSentence = new StringBuilder();
+
+        for (int i = 0; i < dirtySentence.length(); i++) {
+            if (Character.isLetter(dirtySentence.charAt(i))) {
+                clearSentence.append(Character.toLowerCase(dirtySentence.charAt(i)));
+            }
+        }
+        return clearSentence;
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+
+        for (int i = 0; i <= getClearSentence(inputString).length() - 1; i++) {
+
+            if (getClearSentence(inputString).charAt(i) != getClearSentence(inputString)
+                    .charAt((getClearSentence(inputString).length() - 1) - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
