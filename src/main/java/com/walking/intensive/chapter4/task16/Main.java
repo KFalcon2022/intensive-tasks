5e3c81e029b6e94e33ce58a7b8f81aba6cd75eb4
вашÐSahaPWNZ <007saha@inbox.ru>
2023-12-13 11:20:59 +0300

    Chapter 4 Task 16

--- src/main/java/com/walking/intensive/chapter4/task16/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Main.java
@@ -1,7 +1,26 @@
 package com.walking.intensive.chapter4.task16;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {13, 8, 6, 1, 0, -8};
+        System.out.println(Arrays.toString(bubleSort(arr)));
+    }
 
+    static int[] bubleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+
+            for (int j = 0; j < array.length - 1; j++) {
+
+                if (array[j] > array[j + 1]) {
+                    int nextValue = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = nextValue;
+                }
+            }
+        }
+
+        return array;
     }
 }
