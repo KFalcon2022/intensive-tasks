cc69febe67bfb8368efe178a5b8513cedffc2d05
Pavlo <pavlokutsyn@gmail.com>
2023-12-13 12:17:30 +0100

    Chapter4_Task17

--- src/main/java/com/walking/intensive/chapter4/task17/Task17.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Task17.java
@@ -5,11 +5,30 @@ package com.walking.intensive.chapter4.task17;
  */
 public class Task17 {
     public static void main(String[] args) {
-
+        int[] array = {24, 17, 55, -19, 2008, 0, 2020, 67, 55};
+        array = sortByBubble(array);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
 
     private static int[] sortByBubble(int[] array) {
-
-        return new int[]{};
+        boolean swapped;
+        for (int i = 0; i < array.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // поменять местами элементы
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // если ни один из двух элементов не был заменен внутренним циклом, то прерываем
+            if (!swapped)
+                break;
+        }
+        return array;
     }
 }
