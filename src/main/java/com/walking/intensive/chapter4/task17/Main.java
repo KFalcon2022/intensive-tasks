8744de9ac3c6c19aa4f74185f921112c7851a344
KrisLu23 <krisbondarev@yandex.ru>
2023-12-13 11:18:21 +0300

    Task 17

--- src/main/java/com/walking/intensive/chapter4/task17/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Main.java
@@ -1,7 +1,36 @@
 package com.walking.intensive.chapter4.task17;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] array = {9, 8, 7, 6, 2, 0, 81};
+        System.out.println(Arrays.toString(getBubbleSortedArray(array)));
+    }
 
+    static int[] getBubbleSortedArray(int[] array) {
+
+        int count;
+        int tempArrayLength = array.length;
+
+        do {
+            count = 0;
+
+            for (int i = 0; i < tempArrayLength - 1; i++) {
+
+                int temp = array[i];
+
+                if (array[i + 1] < array[i]) {
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    count++;
+                }
+            }
+
+            tempArrayLength--;
+
+        } while (count != 0);
+
+        return array;
     }
 }
