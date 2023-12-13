de62b4877a7903cf83aab6d389efc096846017eb
Константин Синяков <k.siniakov@yandex.ru>
2023-12-13 21:26:55 +0300

    Chapter 3 Task 13

--- src/main/java/com/walking/intensive/chapter3/task13/Task13.java
+++ src/main/java/com/walking/intensive/chapter3/task13/Task13.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter3.task13;
  */
 public class Task13 {
     public static void main(String[] args) {
-
+        System.out.println(getCountSteps(new int[]{1, 1, 1, 4, 2, 3}, 2));
     }
 
     /**
@@ -16,7 +16,43 @@ public class Task13 {
      * @return количество шагов необходимое для полива всех растений
      */
     static int getCountSteps(int[] plants, int wateringCanVolume) {
-        // Ваш код
-        return 0;
+        checkInputData(plants, wateringCanVolume);
+
+        int steps = 0;
+        int wateredPlantsCount = 0;
+        int workingVolume;
+        int i = 1;
+
+        while (wateredPlantsCount < plants.length) {
+
+            workingVolume = wateringCanVolume;
+            steps += i - 1;
+
+
+            while (plants[i - 1] <= workingVolume) {
+                workingVolume -= plants[i - 1];
+                steps++;
+                wateredPlantsCount++;
+                i++;
+                if (wateredPlantsCount == plants.length) {
+                    return steps;
+                }
+            }
+
+            steps += i - 1;
+        }
+
+        return steps;
+    }
+
+    static void checkInputData(int[] plants, int wateringCanVolume) {
+        for (int plant : plants) {
+            if (plant < 0) {
+                throw new IllegalArgumentException("Ошибка входных параметров");
+            }
+            if (plant > wateringCanVolume) {
+                throw new IllegalArgumentException("Объема лейки должно быть достаточно для полива каждого цветка");
+            }
+        }
     }
 }
