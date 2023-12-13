991ce1c213e48201af6760af68cab4c499d31ba7
PavloKutsyn <pavlokutsyn@gmail.com>
2023-12-13 16:14:54 +0100

    Chapter4_Task18"сортировка проходит через дебагер пошагово, run даёт ошибку, причину понять не могу"

--- src/main/java/com/walking/intensive/chapter4/task18/Task18.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Task18.java
@@ -5,11 +5,50 @@ package com.walking.intensive.chapter4.task18;
  */
 public class Task18 {
     public static void main(String[] args) {
+        int[] array = {5, 3, 8, 4, 2};
+        array = sortByShaker(array);
 
+        // Вывод отсортированного массива
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
     }
 
     private static int[] sortByShaker(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        do {
+            swapped = false;
 
-        return new int[]{};
+            // Проход слева направо
+            for (int i = 0; i < n - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    // Обмен элементов
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // Если обменов не было, массив уже отсортирован
+            if (!swapped) break;
+
+            // Предполагаем, что обменов не будет
+            swapped = false;
+
+            // Проход справа налево
+            for (int i = n - 2; i >= 0; i--) {
+                if (array[i] > array[i + 1]) {
+                    // Обмен элементов
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+        } while (swapped); // Повторяем, пока есть обмены
+
+        return array;
     }
 }
