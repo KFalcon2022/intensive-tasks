b355bc3fe6e69816806493a4368a5c14172868eb
myLordShip <Volandemortp@bk.ru>
2023-12-13 12:23:41 +0400

    resolved to chapter1.task1 ver. 1.1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -6,20 +6,18 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+//        int age = 0;
 
         System.out.println(getAgeString(112));
     }
-
     static String getAgeString(int age) {
-
         if ((age < 0) || (age > 127)) {
             return ("указано некорректное значение возраста");
         }
-        if ((age != 11) && (age % 100 != 11) && (age % 10 == 1)) {
+        if ((age % 100 != 11) && (age % 10 == 1)) {
             return ("Вам " + age + " год");
         }
-        if (((age > 1) & (age < 5)) || (((age % 10 > 1) & (age % 10 < 5)) & (age > 20) & !((age > 111) & (age < 121)))){
+        if (((age > 1) && (age < 5)) || (((age % 10 > 1) && (age % 10 < 5)) && (age > 20) && !((age > 111) && (age < 121)))){
             return ("Вам " + age + " года");
         }
         return ("Вам " + age + " лет");
