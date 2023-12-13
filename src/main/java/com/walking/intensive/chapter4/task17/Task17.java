bc9ca428f840813ccb9fee5aa46b0f6249ec8fac
TitLukich <Nef-92@yandex.ru>
2023-12-13 23:14:09 +0300

    Chapter 4, Task 17

--- src/main/java/com/walking/intensive/chapter4/task17/Task17.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Task17.java
@@ -7,22 +7,24 @@ import java.util.Arrays;
  */
 public class Task17 {
     public static void main(String[] args) {
-        int [] array = {5, 8, 100, -50, -121, 0, 98, 58, 159, 65};
-        System.out.println(Arrays.toString(sortByBubble(array)));
+        int[] array = {160, 150, 10, -50, -121, -160};
 
+        System.out.println(Arrays.toString(sortByBubble(array)));
     }
 
     private static int[] sortByBubble(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             int temp;
-            for (int j = 0; j < array.length - i; j++) {
-                if (array [j] > array [j + 1]) {
-                   temp = array [j];
-                   array [j] = array [j + 1];
-                   array [j + 1] = temp;
+
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
+
         return array;
     }
 }
