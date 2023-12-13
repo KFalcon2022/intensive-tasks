e446a2615700562d9af34dc18c8e8c4dce572f4f
sauza7 <sauza777@yandex.ru>
2023-12-13 14:22:44 +0300

    Chapter 4. task 18

--- src/main/java/com/walking/intensive/chapter4/task18/Task18.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Task18.java
@@ -1,15 +1,52 @@
 package com.walking.intensive.chapter4.task18;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Shaker-Sort.html">Сортировка шейкером</a>
  */
 public class Task18 {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3, 4, 5, 122, 1112, 1, 2, 3, 4, 5, 122, 1112};
+        System.out.println(Arrays.toString(sortByShaker(arr)));
     }
 
     private static int[] sortByShaker(int[] array) {
 
-        return new int[]{};
+        boolean continueSorting = true;
+        int maxCheckedIndex = array.length - 1;
+        int minCheckedIndex = 0;
+        int temp;
+
+        while (continueSorting) {
+
+            continueSorting = false;
+
+            for (int i = minCheckedIndex; i < maxCheckedIndex; i++) {
+
+                if (array[i] > array[i + 1]) {
+
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    continueSorting = true;
+                }
+            }
+            maxCheckedIndex--;
+
+            for (int j = maxCheckedIndex; j > minCheckedIndex; j--) {
+
+                if (array[j] < array[j - 1]) {
+
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    continueSorting = true;
+                }
+            }
+            minCheckedIndex++;
+        }
+        return array;
     }
 }
+
