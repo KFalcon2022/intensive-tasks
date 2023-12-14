450458eaa1c7bd0a3cc1fe5920b52727e1bf4761
Lolita <arameya2013@gmail.com>
2023-12-14 17:28:30 +0300

    Решение задачи Дом

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -6,12 +6,34 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+        System.out.println(getFlatLocation(2, 5, 1));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int floorFlatAmount = 4;
+        double entranceFlatAmount = floorFlatAmount * floorAmount; // количество квартир в подъезде
+        int flatAmountBuild = floorFlatAmount * floorAmount * entranceAmount; // количество квартир в доме
+        int rightEntrance, rightFloor;
+        String direction;
+        int floorFlatAmountMod = flatNumber % floorFlatAmount;
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatAmountBuild <= 0 || flatNumber > flatAmountBuild || flatNumber <= 0) {
+            return "Такой квартиры не существует";
+        }
+
+        if (floorFlatAmountMod == 1) {
+            direction = "слева от лифта, влево";
+        } else if (floorFlatAmountMod == 2) {
+            direction = "слева от лифта, вправо";
+        } else if (floorFlatAmountMod == 3) {
+            direction = "справа от лифта, влево";
+        } else {
+            direction = "справа от лифта, вправо";
+        }
+        
+        rightEntrance = (int) Math.ceil(flatNumber / entranceFlatAmount);
+        rightFloor = (int) Math.ceil((flatNumber - ((rightEntrance - 1) * entranceFlatAmount)) / floorFlatAmount);
+
+        return flatNumber + " кв - " + rightEntrance + " подъезд, " + rightFloor + " этаж, " + direction;
     }
 }
