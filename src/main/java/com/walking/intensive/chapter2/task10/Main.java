bef3a0a11e78da1b62e7fc18271abde4f03633e7
Magpier <garson.ao@gmail.com>
2023-12-14 19:53:41 +0300

    Chapter2 Task 10 ver5

--- src/main/java/com/walking/intensive/chapter2/task10/Main.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Main.java
@@ -1,17 +1,32 @@
 package com.walking.intensive.chapter2.task10;
 
 public class Main {
+    static final String PANGRAMM = "thequickbrownfoxjumpsoverthelazydogñúåøüåù¸ıòèõìÿãêèõôğàíöóçñêèõáóëîêäàâûïåéæå÷àş";
+
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Was it a car or a cat I saw?"));
-        System.out.println(isPalindrome("Was it a car or a cat I saw?fd"));
-        System.out.println(isPalindrome("abcdcba"));
-        System.out.println(isPalindrome("abba"));
+
+        System.out.println(isPalindrome("wasitacaroracatisaw"));
+        System.out.println(isPalindrome("!!Was it a ca_r o_r a c!at I sa!w?!"));
+        System.out.println(isPalindrome("a_b!c@d$cba"));
+        System.out.println(isPalindrome("àáâáà!àáâáà"));
+        System.out.println(isPalindrome("cattac"));
+        System.out.println(isPalindrome("!#!!!daaad!@!!!"));
+
     }
 
     static boolean isPalindrome(String s) {
-        for (int i = 0; i < s.replaceAll("[^a-zA-Z]", "").toLowerCase().length() / 2; i++) {
-            if (s.replaceAll("[^a-zA-Z]", "").toLowerCase().charAt(i) != s.replaceAll("[^a-zA-Z]", "").toLowerCase().charAt(s.replaceAll("[^a-zA-Z]", "").toLowerCase().length() - i - 1)) {
-                return false;
+        int counterRight=0;
+        for (int i = 0; i < s.length(); i++) {
+            if (PANGRAMM.indexOf(s.toLowerCase().charAt(i)) > 0) {
+                for (int j = s.length() - counterRight - 1; j >= i; j--) {
+                    counterRight++;
+                    if (PANGRAMM.indexOf(s.toLowerCase().charAt(j)) > 0) {
+                        if (s.toLowerCase().charAt(i) != s.toLowerCase().charAt(j)) {
+                            return false;
+                        }
+                        break;
+                    }
+                }
             }
         }
         return true;
