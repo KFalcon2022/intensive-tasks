cdf9cb53e2fbc12288847f5ffae56d907ff87396
user <tasiqq@gmail.com>
2023-12-14 22:10:54 +0300

    Chapter 4 Task 18:
    - изменен выход из сортировки, условие выхода добавлено в условия цикла верхнего уровня и во второй цикл второго уровня.
    - первый цикл второго уровня не менялся, дополнительная проверка не нужна. Если бы isSorted был true, то до этого цикла не дойдет, потому что цикл верхнего уровня прекратился бы и не пустил дальше.

--- src/main/java/com/walking/intensive/chapter4/task18/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Main.java
@@ -5,49 +5,42 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 4, 3};
-
-        System.out.println(Arrays.toString(doCocktailSort(array)));
+        int[] array = {1, 2, 3, 4};
+        cocktailSort(array);
+        System.out.println(Arrays.toString(array));
     }
 
-    public static int[] doCocktailSort(int[] array) {
+    public static void cocktailSort(int[] array) {
 
+        boolean isSorted = false;
 
-        for (int i = 0; i < Math.ceil((double) array.length / 2); i++) {
-
-            int countSwap = 0;
+        for (int i = 0; i < Math.ceil((double) array.length / 2) && !isSorted; i++) {
+            int countMoves = 0;
 
             for (int j = i; j < array.length - 1 - i; j++) {
-
                 if (array[j] > array[j + 1]) {
                     int maxValue = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = maxValue;
-                    countSwap += 1;
+                    countMoves += 1;
                 }
             }
-
-            if (i != 0 && countSwap == 0) {
-                break;
+            if (countMoves == 0) {
+                isSorted = true;
             }
 
-            countSwap = 0;
-
-            for (int j = array.length - 2 - i; j > i; j--) {
-
+            countMoves = 0;
+            for (int j = array.length - 2 - i; j > i && !isSorted; j--) {
                 if (array[j] < array[j - 1]) {
                     int minValue = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = minValue;
-                    countSwap += 1;
+                    countMoves += 1;
                 }
             }
-
-            if (countSwap == 0) {
-                break;
+            if (countMoves == 0) {
+                isSorted = true;
             }
         }
-
-        return array;
     }
 }
