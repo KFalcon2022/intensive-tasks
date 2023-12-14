54ea46d30066d7c157e18418675c93df754834ae
SenyaDiamond <SenyaDiamond@gmail.com>
2023-12-14 12:24:26 +0300

    Chapter 4 Task 17 v 1.0

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -20,7 +20,6 @@ public class Task11 {
         for (int i = 1; i <= array.length; i += 2) {
             for (int j = 0; j + i <= array.length; j++) {
                 for (int k = j; k < i + j; k++) {
-                    System.out.print(array[k]);
                     sum += array[k];
                 }
             }
