4509c08237b48c2ff367ede77ad25554e46ee3a4
Greem4 <i@greem4.ru>
2023-12-14 14:12:52 +0300

    chapter 4, task17

--- src/main/java/com/walking/intensive/chapter4/task17/Task17.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Task17.java
@@ -1,15 +1,33 @@
 package com.walking.intensive.chapter4.task17;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Bubble-Sort.html">Сортировка пузырьком</a>
  */
 public class Task17 {
     public static void main(String[] args) {
+        int[] array = {10, 9, 8, 22, 33, 44, 534, 454, 23, 12, 5, 1};
 
+        System.out.println(Arrays.toString(sortByBubble(array)));
     }
 
     private static int[] sortByBubble(int[] array) {
+        boolean swapped;
+        do {
+            swapped = false;
 
-        return new int[]{};
+            for (int i = 0; i < array.length - 1; i++) {
+
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+
+        return array;
     }
 }
