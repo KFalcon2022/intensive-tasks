78ade71cfec3979d4423925e3ccef0b1791f9b87
Greem4 <i@greem4.ru>
2023-12-14 14:16:44 +0300

    chapter 4, task17.1

--- src/main/java/com/walking/intensive/chapter4/task16/Task16.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Task16.java
@@ -9,22 +9,25 @@ public class Task16 {
     public static void main(String[] args) {
         int[] array = {9, 44, 52345, 3, 56, 1, 54, 11, 43, 2, 4, 5, 99};
 
-        sortBySelection(array);
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(sortBySelection(array)));
     }
 
     private static int[] sortBySelection(int[] array) {
         for (int i = 0; i < array.length; i++) {
+
             int miniIndex = i;
+
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < array[miniIndex]) {
                     miniIndex = j;
                 }
             }
+
             int temp = array[miniIndex];
             array[miniIndex] = array[i];
             array[i] = temp;
         }
+
         return array;
     }
 }
