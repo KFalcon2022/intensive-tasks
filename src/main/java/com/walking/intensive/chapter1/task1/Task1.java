e9390d4b0e042bd52a3aeb568f2ed7d4920a4bf2
Dmitry <sendtoldi@gmail.com>
2023-12-14 23:12:45 +0300

    Add a solution for the task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -5,15 +5,18 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         int age = 0;
-
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age % 100 >= 11 && age % 100 <= 14) {
+            return "Вам " + age + " лет.";
+        } else if (age % 10 == 1) {
+            return "Вам " + age + " год.";
+        } else if (age % 10 >= 2 && age % 10 <= 4) {
+            return "Вам " + age + " года.";
+        }
+        return "Вам " + age + " лет.";
     }
 }
