a441dabbfe808b36a3a4ff1de87f7a4cf47b7fad
Lolita <arameya2013@gmail.com>
2023-12-14 20:16:18 +0300

    Замена на switch. Добавление валидации

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -6,33 +6,37 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        System.out.println(getFlatLocation(2, 5, 1));
+        System.out.println(getFlatLocation(2, -5, -1));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        int floorFlatAmount = 4;
+        final int floorFlatAmount = 4;
         double entranceFlatAmount = floorFlatAmount * floorAmount; // количество квартир в подъезде
         int flatAmountBuild = floorFlatAmount * floorAmount * entranceAmount; // количество квартир в доме
-        int rightEntrance, rightFloor;
-        String direction;
         int floorFlatAmountMod = flatNumber % floorFlatAmount;
 
-        if (flatAmountBuild <= 0 || flatNumber > flatAmountBuild || flatNumber <= 0) {
+        if (Math.min(Math.min(floorAmount, entranceAmount), flatNumber) <= 0 || flatNumber > flatAmountBuild) {
             return "Такой квартиры не существует";
         }
 
-        if (floorFlatAmountMod == 1) {
-            direction = "слева от лифта, влево";
-        } else if (floorFlatAmountMod == 2) {
-            direction = "слева от лифта, вправо";
-        } else if (floorFlatAmountMod == 3) {
-            direction = "справа от лифта, влево";
-        } else {
-            direction = "справа от лифта, вправо";
+        String direction;
+
+        switch(floorFlatAmountMod) {
+            case 1:
+                direction = "слева от лифта, влево";
+                break;
+            case 2:
+                direction = "слева от лифта, вправо";
+                break;
+            case 3:
+                direction = "справа от лифта, влево";
+                break;
+            default:
+                direction = "справа от лифта, вправо";
         }
-        
-        rightEntrance = (int) Math.ceil(flatNumber / entranceFlatAmount);
-        rightFloor = (int) Math.ceil((flatNumber - ((rightEntrance - 1) * entranceFlatAmount)) / floorFlatAmount);
+
+        int rightEntrance = (int) Math.ceil(flatNumber / entranceFlatAmount);
+        int rightFloor = (int) Math.ceil((flatNumber - ((rightEntrance - 1) * entranceFlatAmount)) / floorFlatAmount);
 
         return flatNumber + " кв - " + rightEntrance + " подъезд, " + rightFloor + " этаж, " + direction;
     }
