1fc2fa954f863211497b23c18ece59d199a5e25a
egor.djun <xedshotg@gmail.com>
2023-12-15 15:45:18 +0700

    Chapter 4, Task 17

--- src/main/java/com/walking/intensive/chapter4/task17/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Main.java
@@ -1,7 +1,26 @@
 package com.walking.intensive.chapter4.task17;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] initsArray = {1, 5, 4, -7, -1, 3, 2, 0};
 
+        System.out.println(Arrays.toString(initsArray));
+        bubbleSearch(initsArray);
+        System.out.println(Arrays.toString(initsArray));
+    }
+
+    static void bubbleSearch(int[] array) {
+        for (int i = array.length; i > 0; i--) {
+            for (int j = 1; j < i; j++) {
+                int temp = array[j - 1];
+
+                if (array[j - 1] > array[j]) {
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
 }
