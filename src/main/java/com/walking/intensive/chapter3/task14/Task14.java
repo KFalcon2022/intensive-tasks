ac295606b0003c3db03adc9ccf934b603f711f2b
Magpier <garson.ao@gmail.com>
2023-12-16 18:30:14 +0300

    Chapter3 Task 14 ver1

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,11 +1,16 @@
 package com.walking.intensive.chapter3.task14;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-Circle.html">КРУГИ</a>
  */
 public class Task14 {
     public static void main(String[] args) {
-
+        int[][] points_1 = {{1, 3}, {3, 3}, {5, 3}, {2, 2}};
+        int[][] queries_1 = {{2, 3, 1}, {4, 3, 1}, {1, 1, 2}};
+        int[] answer_1 = {3, 2, 2};
+        System.out.println(Arrays.toString(getCountOfPoints(points_1,queries_1)));
     }
 
     /**
@@ -16,7 +21,16 @@ public class Task14 {
      * @return массив с количеством точек внутри окружностей
      */
     static int[] getCountOfPoints(int[][] points, int[][] queries) {
-        // Ваш код
-        return new int[0];
+        int[] result = new int[queries.length];
+        Arrays.fill(result, 0);
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                // (x-x0)^2+(y-y0)^2=r^2
+                if ((queries[i][0] - points[j][0]) * (queries[i][0] - points[j][0]) + (queries[i][1] - points[j][1]) * (queries[i][1] - points[j][1]) <= (queries[i][2]) * (queries[i][2])) {
+                    result[i]++;
+                }
+            }
+        }
+        return result;
     }
 }
