0982437ab2eee856cf2894655a999e49696e20c5
Rust <madishkin2@gmail.com>
2023-12-16 14:06:15 +0300

    Chapter 2 Task 9

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -5,41 +5,62 @@ package com.walking.intensive.chapter2.task9;
  */
 public class Task9 {
     public static void main(String[] args) {
-        System.out.println(getPascalTriangle(14));
+        System.out.println(getPascalTriangle(5));
     }
 
     public static int[][] getTriangleArray(int n) {
-        int[][] triangleArray = new int[n + 1][(2 * n) + 1];
+        int[][] triangleArray = new int[n][(2 * n) - 1];
 
         for (int i = 0; i < n; i++) {
             if (i == 0) {
-                triangleArray[i][n - i] = triangleArray[i][n + i] = 1;
+                triangleArray[0][n - 1] = 1;
                 continue;
             }
+            triangleArray[i][(n - 1) - i] = triangleArray[i][(n - 1) + i] = 1;
 
-            for (int j = n - i; j <= n + i; j++) {
+            for (int j = ((n - 1) - i) + 2; j <= ((n - 1) + i) - 2; j++) {
                 triangleArray[i][j] = triangleArray[i - 1][j - 1] + triangleArray[i - 1][j + 1];
             }
         }
+
         return triangleArray;
     }
 
+    public static int getRowLength(int[][] triangleArray, int rowNumber) {
+        StringBuilder row = new StringBuilder();
+
+        for (int i = 0; i <= (triangleArray[rowNumber].length - 1); i++) {
+            row.append(triangleArray[rowNumber][i]);
+        }
+        return row.length();
+    }
+
+    public static StringBuilder getSpaceCompensation(int bottomRowLength, int currentRowLength) {
+        StringBuilder spaceCompensation = new StringBuilder();
+
+        for (int i = 1; i <= (bottomRowLength - currentRowLength) / 2; i++) {
+            spaceCompensation.append(" ");
+        }
+        return spaceCompensation;
+    }
 
     static String getPascalTriangle(int n) {
         String temp;
         StringBuilder pascalTriangle = new StringBuilder();
         int[][] trianglePascalArray = getTriangleArray(n);
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n - 1; i++) {
             pascalTriangle.append("\n");
 
-            for (int j = 0; j < n * 2; j++) {
+            pascalTriangle.append(getSpaceCompensation(getRowLength(trianglePascalArray, n - 1),
+                    getRowLength(trianglePascalArray, i)));
+
+            for (int j = 0; j < ((n * 2) - 1); j++) {
                 temp = String.valueOf(trianglePascalArray[i][j]);
 
                 if (temp.equals("0")) {
                     temp = " ";
                 }
-
                 pascalTriangle.append(temp);
             }
         }
