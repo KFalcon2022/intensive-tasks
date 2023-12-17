42605b259ee0be8ee435dc6957a154160a11ba13
Greem4 <i@greem4.ru>
2023-12-17 16:01:56 +0300

    chapter 5, task21

--- src/main/java/com/walking/intensive/chapter5/task21/Main.java
+++ src/main/java/com/walking/intensive/chapter5/task21/Main.java
@@ -2,6 +2,23 @@ package com.walking.intensive.chapter5.task21;
 
 public class Main {
     public static void main(String[] args) {
+        Sphere sphere = new Sphere(1, 1, 1, 1);
+        Parallelepiped parallelepiped = new Parallelepiped(5, 5, 5, 5, 5, 5);
 
+        System.out.println("Пересекаются ли сфера и параллелепипед? " + checkIntersection(sphere, parallelepiped));
+    }
+
+    public static boolean checkIntersection(Sphere sphere, Parallelepiped parallelepiped) {
+        double distance = Math.sqrt(
+                Math.pow(sphere.getX() - parallelepiped.getX1(), 2)
+                        + Math.pow(sphere.getY() - parallelepiped.getY1(), 2)
+                        + Math.pow(sphere.getZ() - parallelepiped.getZ1(), 2));
+
+        double parallelepipedDiagonal = Math.sqrt(
+                Math.pow(parallelepiped.getX2() - parallelepiped.getX1(), 2)
+                        + Math.pow(parallelepiped.getY2() - parallelepiped.getY1(), 2)
+                        + Math.pow(parallelepiped.getZ2() - parallelepiped.getZ1(), 2));
+
+        return distance <= (sphere.getRadius() + parallelepipedDiagonal);
     }
 }
---
+++ src/main/java/com/walking/intensive/chapter5/task21/Sphere.java
@@ -0,0 +1,47 @@
+package com.walking.intensive.chapter5.task21;
+
+public class Sphere {
+    private double x;
+    private double y;
+    private double z;
+    private double radius;
+
+    public Sphere(double x, double y, double z, double radius) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
