9647ad3452e96b75d44569e41e9d1808eba922cb
RVishez <oliohcmpn@gmail.com>
2023-12-17 13:11:10 +0100

    Chapter 3 task 11 solved

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -5,7 +5,8 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
-
+        int[] array = {1, 2, 12};
+        System.out.println(getSumSubarraysOddLength(array));
     }
 
     /**
@@ -15,7 +16,28 @@ public class Task11 {
      * @return сумма всех нечетныз подмассивов
      */
     static int getSumSubarraysOddLength(int[] array) {
-        // Ваш код
-        return 0;
+
+        int sumSubarraysOddLength = 0;
+
+        for (int i = 1; i <= array.length; i += 2) {
+
+            for (int j = 0; j < array.length; j++) {
+
+                if (j + i > array.length) {
+                    break;
+                }
+
+                for (int k = 0; k < i; k++) {
+
+                    if (j + k >= array.length) {
+                        break;
+                    }
+
+                    sumSubarraysOddLength += array[j + k];
+                    }
+                }
+            }
+
+        return sumSubarraysOddLength;
     }
 }
