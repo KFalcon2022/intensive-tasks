6b5f3b17938a4d5fbf96e9487ae42e5ae1aae78e
DevTorchez <torchez@yandex.ru>
2023-12-17 17:21:58 +0300

    Resolve Chapter4.Task18

--- src/main/java/com/walking/intensive/chapter4/task18/Task18.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Task18.java
@@ -1,15 +1,40 @@
 package com.walking.intensive.chapter4.task18;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Shaker-Sort.html">Сортировка шейкером</a>
  */
 public class Task18 {
     public static void main(String[] args) {
+        int[] inputArray = {5, 10, 12, 1, 3, 8, 25, 44, 4, 0};
+        System.out.println(Arrays.toString(sortByShaker(inputArray)));
 
     }
 
     private static int[] sortByShaker(int[] array) {
+        int temp;
+        int left = 0;
+        int right = array.length - 1;
 
-        return new int[]{};
+        while (left < right) {
+            for (int i = left; i < right; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+            right--;
+            for (int j = right; j > left; j--) {
+                if (array[j] < array[j - 1]) {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+            left++;
+        }
+        return array;
     }
 }
