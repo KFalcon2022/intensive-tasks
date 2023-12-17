fdd7733e1bd72f7a0ac6dc3b28c51d39034c172b
DevTorchez <torchez@yandex.ru>
2023-12-17 20:40:10 +0300

    re:Resolve Chapter4.Task18 | Исправление в соответствии с замечаниями. Надеюсь.

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -4,18 +4,16 @@ package com.walking.intensive.chapter1.task2;
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Task2 {
-    static String leftOrRight = "";
-    static int setEntrance, setFloor;
 
     public static void main(String[] args) {
 
         System.out.println(getFlatLocation(10, 3, 40));
+        System.out.println(getFlatLocation(-10, -3, 40));
         System.out.println(getFlatLocation(10, 3, 121));
-
     }
 
     static int getFloor(int floorAmount, int flatNumber) {
-        setFloor = floorAmount;
+        int setFloor = floorAmount;
         int maxFlat = getEntrance(floorAmount, flatNumber) * floorAmount * 4;
         while (maxFlat - 3 > flatNumber) {
             setFloor = setFloor - 1;
@@ -25,7 +23,8 @@ public class Task2 {
     }
 
     static int getEntrance(int floorAmount, int flatNumber) {
-        setEntrance = 1;
+        int setEntrance = 1;
+
         while ((setEntrance * floorAmount * 4) < flatNumber) {
             setEntrance++;
         }
@@ -33,22 +32,23 @@ public class Task2 {
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        setFloor = floorAmount;
+
+        if (floorAmount <= 0 || entranceAmount <= 0 || flatNumber <= 0) return "Такой квартиры не существует";
 
         int totalFlatQty = floorAmount * 4 * entranceAmount;
 
         if (flatNumber > totalFlatQty || flatNumber <= 0) {
             return "Такой квартиры не существует";
         }
-
+        String leftOrRight = "";
         switch (flatNumber % 4) {
-            case 1, 2 -> leftOrRight = ", слева от лифта, влево";
-            case 3, 0 -> leftOrRight = ", справа от лифта, вправо";
+            case 1 -> leftOrRight = ", слева от лифта, вправо";
+            case 2 -> leftOrRight = ", слева от лифта, влево";
+            case 3 -> leftOrRight = ", справа от лифта, вправо";
+            case 0 -> leftOrRight = ", справа от лифта, влево";
         }
 
-        String totalString = flatNumber + " кв - " + getEntrance(floorAmount, flatNumber) + " подъезд, "
+        return flatNumber + " кв - " + getEntrance(floorAmount, flatNumber) + " подъезд, "
                 + getFloor(floorAmount, flatNumber) + " этаж" + leftOrRight;
-
-        return totalString;
     }
 }
