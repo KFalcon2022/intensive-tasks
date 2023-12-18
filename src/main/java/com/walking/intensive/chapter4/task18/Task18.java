6ab4596116b0963b63a754bc4f3076a2a174030b
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-12-18 22:58:31 +0300

    Chapter4Task18

--- src/main/java/com/walking/intensive/chapter4/task18/Task18.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Task18.java
@@ -1,15 +1,37 @@
-package com.walking.intensive.chapter4.task18;
+package main.java.com.walking.intensive.chapter4.task18;
+
+import java.util.Arrays;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Shaker-Sort.html">Сортировка шейкером</a>
  */
 public class Task18 {
     public static void main(String[] args) {
-
+        int[] array = new int[]{5, 4, 3, 2, 1, 7, 0, 42, 42, 645, 73, 14, 0, 1000};
+        System.out.println(Arrays.toString(sortByShaker(array)));
     }
 
     private static int[] sortByShaker(int[] array) {
-
-        return new int[]{};
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            for (int i = 0; i < right; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                }
+            }
+            right--;
+            for (int j = right; left < j; j--) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+            left++;
+        }
+        return array;
     }
 }
