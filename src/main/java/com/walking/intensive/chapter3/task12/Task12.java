f0bd98edb6d7a1178055fab108010606ccdfa703
RVishez <oliohcmpn@gmail.com>
2023-12-18 11:49:14 +0100

    Chapter 3 task 12 solved

--- src/main/java/com/walking/intensive/chapter3/task12/Task12.java
+++ src/main/java/com/walking/intensive/chapter3/task12/Task12.java
@@ -1,11 +1,13 @@
 package com.walking.intensive.chapter3.task12;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-balls.html">МЯЧИКИ</a>
  */
 public class Task12 {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(getNumberOfMovements("110")));
     }
 
     /**
@@ -15,7 +17,44 @@ public class Task12 {
      * @return массив с минимальным количеством операций
      */
     static int[] getNumberOfMovements(String baskets) {
-        // Ваш код
-        return new int[]{};
+
+        int[] basketsArray = getBasketsArray(baskets);
+        int basketsAmount = baskets.length();
+        int[] ballMovementsInEachBasket = new int[basketsAmount];
+
+        for (int i = 0; i < basketsAmount; i++) {
+            int[] basketsArrayCopy = Arrays.copyOf(basketsArray, basketsAmount);
+
+            for (int k = 0; k < i; k++) {
+
+                while (!(basketsArrayCopy[k] == 0)) {
+                    basketsArrayCopy[k] -= 1;
+                    basketsArrayCopy[k + 1] += 1;
+                    ballMovementsInEachBasket[i] += 1;
+                }
+            }
+
+            for (int j = basketsAmount - 1; j > i; j--) {
+
+                while (!(basketsArrayCopy[j] == 0)) {
+                    basketsArrayCopy[j] -= 1;
+                    basketsArrayCopy[j - 1] += 1;
+                    ballMovementsInEachBasket[i] += 1;
+                }
+            }
+        }
+
+        return ballMovementsInEachBasket;
     }
+
+    static int[] getBasketsArray(String baskets) {
+        int[] basketsArray = new int[baskets.length()];
+
+        for (int i = 0; i < baskets.length(); i++) {
+            basketsArray[i] = Integer.parseInt(String.valueOf(baskets.charAt(i)));
+        }
+
+        return basketsArray;
+    }
+
 }
