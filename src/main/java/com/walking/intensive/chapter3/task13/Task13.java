481d772efba10a9ad4660ecfcb6becaa8ac00c48
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-12-18 21:13:22 +0300

    Chapter3Task13

--- src/main/java/com/walking/intensive/chapter3/task13/Task13.java
+++ src/main/java/com/walking/intensive/chapter3/task13/Task13.java
@@ -1,22 +1,23 @@
-package com.walking.intensive.chapter3.task13;
+package main.java.com.walking.intensive.chapter3.task13;
 
-/**
- * Условие: <a href="https://geometry-math.ru/Java-Watering-Plants.html">ПОЛИВ РАСТЕНИЙ</a>
- */
+
 public class Task13 {
     public static void main(String[] args) {
 
     }
 
-    /**
-     * Получить количество шагов необходимых для полива всех растений.
-     *
-     * @param plants            массив с количеством литров воды необходимому i-му растению
-     * @param wateringCanVolume объем лейки
-     * @return количество шагов необходимое для полива всех растений
-     */
-    static int getCountSteps(int[] plants, int wateringCanVolume) {
-        // Ваш код
-        return 0;
+    public static int getCountSteps(int[] plants, int wateringCanVolume) {
+        int currentWater = 0;
+        int steps = 0;
+
+        for (int i = 0; i < plants.length; i++) {
+            if (currentWater < plants[i]) {
+                steps += i * 2;
+                currentWater = wateringCanVolume;
+            }
+            currentWater -= plants[i];
+            steps++;
+        }
+        return steps;
     }
 }
