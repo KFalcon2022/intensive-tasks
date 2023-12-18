482ef44aaf1d5d5ce43993d619c0d40e92ac3be9
Pavlo <pavlokutsyn@gmail.com>
2023-12-19 00:52:36 +0100

    Chapter4_Task19

--- src/main/java/com/walking/intensive/chapter4/task19/Task19.java
+++ src/main/java/com/walking/intensive/chapter4/task19/Task19.java
@@ -4,9 +4,10 @@ package com.walking.intensive.chapter4.task19;
  * Условие: <a href="https://geometry-math.ru/homework/Quicksort.html">Быстрая сортировка - Quicksort</a>
  */
 public class Task19 {
-    // Основной метод, который инициирует процесс сортировки массива
     public static void main(String[] args) {
+        // Создание и инициализация массива
         int[] array = {10, 7, 8, 9, 1, 5, -100, 1000};
+        // Вызов метода быстрой сортировки
         sortByQuicksort(array, 0, array.length - 1);
         // Вывод отсортированного массива
         for (int value : array) {
@@ -14,41 +15,49 @@ public class Task19 {
         }
     }
 
-    // Метод, реализующий алгоритм быстрой сортировки
+    // Метод быстрой сортировки
     private static void sortByQuicksort(int[] array, int begin, int end) {
+        // Проверка условия для рекурсии
         if (begin < end) {
             // Вычисление индекса разделения
             int partitionIndex = partition(array, begin, end);
-
-            // Рекурсивный вызов сортировки для подмассивов
+            // Рекурсивный вызов для левой части массива
             sortByQuicksort(array, begin, partitionIndex - 1);
+            // Рекурсивный вызов для правой части массива
             sortByQuicksort(array, partitionIndex + 1, end);
         }
     }
 
     // Метод для разделения массива
     private static int partition(int[] array, int begin, int end) {
-        // Выбор опорного элемента
-        int pivot = array[end];
-        int i = (begin - 1);
-
-        // Перемещение элементов меньше опорного влево, больше - вправо
-        for (int j = begin; j < end; j++) {
-            if (array[j] < pivot) {
-                i++;
-                // Обмен элементов
-                int swapTemp = array[i];
-                array[i] = array[j];
-                array[j] = swapTemp;
+        // Нахождение минимального и максимального элементов в массиве
+        int min = array[begin], max = array[begin];
+        for (int i = begin + 1; i <= end; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            } else if (array[i] < min) {
+                min = array[i];
             }
         }
-
-        // Помещение опорного элемента на правильное место
-        int swapTemp = array[i + 1];
-        array[i + 1] = array[end];
-        array[end] = swapTemp;
-
-        // Возвращение индекса опорного элемента
-        return i + 1;
+        // Вычисление опорного элемента как среднее между max и min
+        int pivot = (max + min) / 2;
+        // Инициализация указателей
+        int i = begin, j = end;
+        // Цикл разделения
+        while (i <= j) {
+            // Перемещение указателей
+            while (array[i] < pivot) i++;
+            while (array[j] > pivot) j--;
+            // Обмен элементов
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        // Возвращение индекса разделения
+        return i;
     }
 }
