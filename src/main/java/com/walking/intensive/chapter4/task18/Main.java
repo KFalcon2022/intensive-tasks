b99fbf336a790a3e018a77b51feb3e4670609e21
user <tasiqq@gmail.com>
2023-12-18 22:41:11 +0300

    Chapter 4 Task 18: - условие досрочного выхода из перебора значений, если массив уже отсортирован, удалено из условий цикла for. Цикл прерывается через break, если за проход не было ни одной перестановки.

--- src/main/java/com/walking/intensive/chapter4/task18/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Main.java
@@ -5,41 +5,40 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 4};
+        int[] array = {5, 2, 3, 4, 1};
         cocktailSort(array);
         System.out.println(Arrays.toString(array));
     }
 
     public static void cocktailSort(int[] array) {
 
-        boolean isSorted = false;
+        for (int i = 0; i < Math.ceil((double) array.length / 2); i++) {
 
-        for (int i = 0; i < Math.ceil((double) array.length / 2) && !isSorted; i++) {
-            int countMoves = 0;
+            boolean isSorted = true;
 
             for (int j = i; j < array.length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
                     int maxValue = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = maxValue;
-                    countMoves += 1;
+                    isSorted = false;
                 }
             }
-            if (countMoves == 0) {
-                isSorted = true;
+            if (isSorted) {
+                break;
             }
 
-            countMoves = 0;
-            for (int j = array.length - 2 - i; j > i && !isSorted; j--) {
+            isSorted = true;
+            for (int j = array.length - 2 - i; j > i; j--) {
                 if (array[j] < array[j - 1]) {
                     int minValue = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = minValue;
-                    countMoves += 1;
+                    isSorted = false;
                 }
             }
-            if (countMoves == 0) {
-                isSorted = true;
+            if (isSorted) {
+                break;
             }
         }
     }
