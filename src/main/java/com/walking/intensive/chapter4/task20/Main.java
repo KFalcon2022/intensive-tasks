2a191954406b10ecf0bc5d17a56d2ad912b83edc
user <tasiqq@gmail.com>
2023-12-18 22:33:49 +0300

    Chapter 4 Task 20:
    - метод hepify переименован на restoreHeap;
    - в методе heapSort проверка на отсортированность переданного массива заменена с перебора циклом while на цикл for;
    - исправлена ошибка в методе buildHeap, теперь в нем метод restoreHeap вызывается с корректным значением правой границы массива array.length-1.
    
    nb: непонятно, почему оно вообще работало раньше, если постоянно пыталось обратиться к элементу массива array.length

--- src/main/java/com/walking/intensive/chapter4/task20/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task20/Main.java
@@ -7,15 +7,27 @@ public class Main {
 
         int[] array1 = {1, 2, 3, 4, 5};
         int[] array2 = {1, 5, 7, 6, 45, 4, 31, 9, 8, 3, 2};
+        int[] array3 = {54, 465, 12, 123, 88, 66, 44, 11};
+        int[] array4 = {5, 4, 3};
+        int[] array5 = {77, 88, 4, 11, 666, 444, 9999, 111, 1, 2, 3};
+        int[] array6 = {64, 87, 12, 64, 8788, 212, 449, 1231, 45488, 774, 1};
 
-        doHeapSort(array1);
-        doHeapSort(array2);
+        heapSort(array1);
+        heapSort(array2);
+        heapSort(array3);
+        heapSort(array4);
+        heapSort(array5);
+        heapSort(array6);
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
+        System.out.println(Arrays.toString(array3));
+        System.out.println(Arrays.toString(array4));
+        System.out.println(Arrays.toString(array5));
+        System.out.println(Arrays.toString(array6));
     }
 
-    public static void doHeapSort(int[] array) {
+    public static void heapSort(int[] array) {
 
         /*
         Если массив изначально не отсортирован возрастанию, то после приведения к куче и каждого дальнейшего
@@ -24,31 +36,36 @@ public class Main {
          */
         boolean isSorted = true;
 
-        int i = 0;
+        //int i = 0;
 
-        while (isSorted && i < array.length - 1) {
-            if (array[i] > array[i + 1])
+        for (int i = 0; i < array.length - 2 && isSorted; i++) {
+            if (array[i] > array[i + 1]) {
                 isSorted = false;
-            i++;
+            }
         }
+//        while (isSorted && i < array.length - 1) {
+//            if (array[i] > array[i + 1])
+//                isSorted = false;
+//            i++;
+//        }
 
         if (!isSorted) {
             buildHeap(array);
-            doHeapSort(array, 0, array.length - 1);
+            heapSort(array, 0, array.length - 1);
         }
     }
 
-    public static void doHeapSort(int[] array, int root, int rightBorder) {
+    public static void heapSort(int[] array, int root, int rightBorder) {
 
         if (rightBorder == 0) {
             return;
         }
         swapElements(array, root, rightBorder);
-        heapify(array, root, rightBorder - 1);
-        doHeapSort(array, root, rightBorder - 1);
+        restoreHeap(array, root, rightBorder - 1);
+        heapSort(array, root, rightBorder - 1);
     }
 
-    public static void heapify(int[] array, int root, int rightBorder) {
+    public static void restoreHeap(int[] array, int root, int rightBorder) {
 
         int maxValueIndex = root;
         int leftChildIndex = 2 * root + 1;
@@ -64,14 +81,14 @@ public class Main {
 
         if (maxValueIndex != root) {
             swapElements(array, root, maxValueIndex);
-            heapify(array, maxValueIndex, rightBorder);
+            restoreHeap(array, maxValueIndex, rightBorder);
         }
     }
 
     public static void buildHeap(int[] array) {
 
         for (int i = array.length / 2 - 1; i >= 0; i--) {
-            heapify(array, i, array.length);
+            restoreHeap(array, i, array.length - 1);
         }
     }
 
