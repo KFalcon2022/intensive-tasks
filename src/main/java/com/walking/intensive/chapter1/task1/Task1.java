4624244a2446f1a65a9db2f130d4ea294ff2a5ef
Magpier <garson.ao@gmail.com>
2023-12-18 17:46:43 +0300

    Chapter3 Task 20 ver1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -5,37 +5,6 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-        int age = 0;
-        for (int i = -1; i < 129; i++) {
-            System.out.println(getAgeString(i));
-        }
 
-    }
-
-    static String getAgeString(int age) {
-        String s = "";
-        if (age > 127 || age < 0) {
-            return "Проверьте правильность ввода возраста";
-        }
-
-        switch (age % 10) {
-            case 1:
-                if (age % 100 == 11) {
-                    s = "Вам " + age + " лет";
-                } else {
-                    s = "Вам " + age + " год";
-                }
-                break;
-            case 2, 3, 4:
-                if (age % 100 == 12 || age % 100 == 13 || age % 100 == 14) {
-                    s = "Вам " + age + " лет";
-                } else {
-                    s = "Вам " + age + " года";
-                }
-                break;
-            case 0, 5, 6, 7, 8, 9:
-                s = "Вам " + age + " лет";
-        }
-        return s;
     }
 }
--- src/main/java/com/walking/intensive/chapter4/task20/Task20.java
+++ src/main/java/com/walking/intensive/chapter4/task20/Task20.java
@@ -1,15 +1,49 @@
 package com.walking.intensive.chapter4.task20;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Pyramid-Sort.html">Пирамидальная сортировка</a>
  */
 public class Task20 {
     public static void main(String[] args) {
-
+        int[] array = {16, 12, 12, 28, 6, 28, -6, 1, 3, 38};
+        System.out.println(Arrays.toString(sortByPyramid(array)));
     }
 
     private static int[] sortByPyramid(int[] array) {
+        int length = array.length;
+        for (int layer = length / 2 - 1; layer >= 0; layer--) {
+            heapify(array, length, layer);
+        }
+        for (int layer = length - 1; layer >= 0; layer--) {
+            swap(array, 0, layer);
+            heapify(array, layer, 0);
+        }
 
-        return new int[]{};
+        return array;
+    }
+
+    static void heapify(int[] array, int length, int layer) {
+        int max = layer;
+        int left = 2 * layer + 1;
+        int right = 2 * layer + 2;
+
+        if (left < length && array[left] > array[max]) {
+            max = left;
+        }
+        if (right < length && array[right] > array[max]) {
+            max = right;
+        }
+        if (max != layer) {
+            swap(array, layer, max);
+            heapify(array, length, max);
+        }
+    }
+
+    private static void swap(int[] array, int ind1, int ind2) {
+        int temp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = temp;
     }
 }
