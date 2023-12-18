e8b820fe0e6b16dc45c7e678ebd1fb790608a12b
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2023-12-18 22:09:09 +0300

    Chapter4Task16

--- src/main/java/com/walking/intensive/chapter4/task16/Task16.java
+++ src/main/java/com/walking/intensive/chapter4/task16/Task16.java
@@ -1,15 +1,28 @@
-package com.walking.intensive.chapter4.task16;
+package main.java.com.walking.intensive.chapter4.task16;
+
+import java.util.Arrays;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/%D0%A1hoicee-Sort.html">Сортировка выбором</a>
  */
 public class Task16 {
     public static void main(String[] args) {
-
+        int[] array = new int[]{5, 4, 3, 2, 1, 7, 0,42,42,645,73,14,0,9};
+        System.out.println(Arrays.toString(sortBySelection(array)));
     }
 
     private static int[] sortBySelection(int[] array) {
-
-        return new int[]{};
+        for (int i = 0; i < array.length; i++) {
+            int indexMinElement = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[indexMinElement]) {
+                    indexMinElement = j;
+                }
+            }
+            int tempValue = array[indexMinElement];
+            array[indexMinElement] = array[i];
+            array[i] = tempValue;
+        }
+        return array;
     }
 }
