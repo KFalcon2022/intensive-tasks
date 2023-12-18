2a0c5dfc000c69d8b9f3bc389104c2847cb73e65
Makoto-2005 <makoto2005@yandex.ru>
2023-12-18 08:08:58 +0300

    solution of task "Palindrome"

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,15 +1,33 @@
 package com.walking.intensive.chapter2.task10;
 
+import java.util.Scanner;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
 public class Task10 {
     public static void main(String[] args) {
 
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите вашу строку: ");
+        String inputString = console.nextLine();
+
+        console.close();
+        System.out.println(isPalindrome(inputString));
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
+        char[] palindromeArray = inputString.toCharArray();
+        for (int i = 0; i < palindromeArray.length; ++i) {
+            for (int j = palindromeArray.length - 1; j >= 0; --j) {
+                if (Character.isAlphabetic(palindromeArray[i]) && Character.isAlphabetic(palindromeArray[j])) {
+                    palindromeArray[i] = Character.toLowerCase(palindromeArray[i]);
+                    return palindromeArray[i] == palindromeArray[j];
+                }
+            }
+        }
         return false;
     }
 }
+
+
