877511fc9a63625dc53a7389538185fffa6dd99d
PavloKutsyn <pavlokutsyn@gmail.com>
2023-12-19 16:34:50 +0100

    Chapter4_Task19

--- src/main/java/com/walking/intensive/chapter4/task19/Task19.java
+++ src/main/java/com/walking/intensive/chapter4/task19/Task19.java
@@ -25,17 +25,20 @@ public class Task19 {
      * @param end Конечный индекс подмассива для сортировки.
      */
     private static void sortByQuicksort(int[] array, int begin, int end) {
-        if (begin < end) {
-            if (end - begin == 1) {
-                if (array[begin] > array[end]) {
-                    swap(array, begin, end);
-                }
-            } else {
-                int partitionIndex = partition(array, begin, end);
-                sortByQuicksort(array, begin, partitionIndex - 1);
-                sortByQuicksort(array, partitionIndex, end); // Обновлено для включения partitionIndex
-            }
+        if (begin >= end) {
+            return;
         }
+
+        if (end - begin == 1) {
+            if (array[begin] > array[end]) {
+                swap(array, begin, end);
+            }
+            return;
+        }
+
+        int partitionIndex = getPartitionIndex(array, begin, end);
+        sortByQuicksort(array, begin, partitionIndex - 1);
+        sortByQuicksort(array, partitionIndex, end);
     }
 
     /**
@@ -49,41 +52,43 @@ public class Task19 {
      * @param end Конечный индекс подмассива.
      * @return Индекс, разделяющий массив на две части.
      */
-    private static int partition(int[] array, int begin, int end) {
+    private static int getPartitionIndex(int[] array, int begin, int end) {
         int min = array[begin], max = array[begin];
-        for (int i = begin + 1; i <= end; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
-            if (array[i] < min) {
-                min = array[i];
-            }
-        }
-        int pivot = (max + min) / 2;
 
-        int i = begin, j = end;
-        while (i <= j) {
-            while (array[i] < pivot) i++;
-            while (array[j] > pivot) j--;
-            if (i <= j) {
-                swap(array, i, j);
-                i++;
-                j--;
+        for (int k = begin + 1; k <= end; k++) {
+            if (array[k] > max) {
+                max = array[k];
+            } else if (array[k] < min) {
+                min = array[k];
             }
         }
-        return i;
+
+        int pivot = (max + min) / 2;
+        int left = begin, right = end;
+
+        while (left <= right) {
+            while (array[left] < pivot) left++;
+            while (array[right] > pivot) right--;
+            if (left <= right) {
+                swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
     }
 
     /**
      * Меняет местами два элемента в массиве.
      *
      * @param array Массив, в котором происходит обмен.
-     * @param i Индекс первого элемента для обмена.
-     * @param j Индекс второго элемента для обмена.
+     * @param first Индекс первого элемента для обмена.
+     * @param second Индекс второго элемента для обмена.
      */
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+    private static void swap(int[] array, int first, int second) {
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
     }
 }
