cd65b0d2c4dc523cad3345acdd40914886261ef0
Greem4 <i@greem4.ru>
2023-12-19 22:21:30 +0300

    chapter 5, task 21.8

--- src/main/java/com/walking/intensive/chapter5/task21/Main.java
+++ src/main/java/com/walking/intensive/chapter5/task21/Main.java
@@ -14,15 +14,20 @@ public class Main {
         Sphere sphere3 = new Sphere(5, 5, 5, 2);
         Parallelepiped parallelepiped3 = new Parallelepiped(3, 3, 3, 6, 6, 6);
 
+        Sphere sphere4 = new Sphere(-13, -13, -13, 15);
+        Parallelepiped parallelepiped4 = new Parallelepiped(0, 0, 0, -10, -10, -10);
+
         List<Sphere> spheres = new ArrayList<>();
         spheres.add(sphere1);
         spheres.add(sphere2);
         spheres.add(sphere3);
+        spheres.add(sphere4);
 
         List<Parallelepiped> parallelepipeds = new ArrayList<>();
         parallelepipeds.add(parallelepiped1);
         parallelepipeds.add(parallelepiped2);
         parallelepipeds.add(parallelepiped3);
+        parallelepipeds.add(parallelepiped4);
 
         for (int i = 0; i < spheres.size(); i++) {
             System.out.printf("Пересекаются ли сфера %d и параллелепипед %d ? \nответ: %s\n",
--- src/main/java/com/walking/intensive/chapter5/task21/Parallelepiped.java
+++ src/main/java/com/walking/intensive/chapter5/task21/Parallelepiped.java
@@ -9,12 +9,12 @@ public class Parallelepiped {
     private double z2;
 
     public Parallelepiped(double x1, double y1, double z1, double x2, double y2, double z2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.z1 = z1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.z2 = z2;
+        this.x1 = Math.min(x1, x2);
+        this.y1 = Math.min(y1, y2);
+        this.z1 = Math.min(z1, z2);
+        this.x2 = Math.max(x1, x2);
+        this.y2 = Math.max(y1, y2);
+        this.z2 = Math.max(z1, z2);
     }
 
     public double getX1() {
