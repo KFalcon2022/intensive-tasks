4448ce3be14b5a27a9a9395d62a6dee06863c7a4
lambo064 <tasha_lambo@mail.ru>
2023-12-19 16:02:51 +0300

    chapter 4 task 20

--- src/main/java/com/walking/intensive/chapter4/task20/Task20.java
+++ src/main/java/com/walking/intensive/chapter4/task20/Task20.java
@@ -5,11 +5,43 @@ package com.walking.intensive.chapter4.task20;
  */
 public class Task20 {
     public static void main(String[] args) {
-
+        int[] array = {12, 1, 9, 5, 6, 19, 8,4};
+        sortByPyramid(array);
     }
 
-    private static int[] sortByPyramid(int[] array) {
+    private static void sortByPyramid(int[] array) {
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, array.length, i);
+        }
 
-        return new int[]{};
+        for (int i = array.length - 1; i >= 0; i--) {
+            changePlace(array, 0, i);
+            heapify(array, i, 0);
+        }
     }
+
+    public static void heapify(int[] array, int n, int i) {
+        int maxElement = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[maxElement]) {
+            maxElement = left;
+        }
+
+        if (right < n && array[right] > array[maxElement]) {
+            maxElement = right;
+        }
+
+        if (maxElement != i) {
+            changePlace(array, i, maxElement);
+            heapify(array, n, maxElement);
+        }
+    }
+    public static void changePlace(int[] array, int firstPlace, int SecondPlace) {
+        int temp = array[firstPlace];
+        array[firstPlace] = array[SecondPlace];
+        array[SecondPlace] = temp;
+    }
+
 }
