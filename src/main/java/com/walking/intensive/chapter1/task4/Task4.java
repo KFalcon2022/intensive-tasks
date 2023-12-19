9a0160e61cad8ea1b130e3b994fd63e16e5803ed
DevTorchez <torchez@yandex.ru>
2023-12-19 13:45:55 +0300

    re:Resolve Chapter1.Task4 | Исправления с учетом замечаний по коду.

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -13,41 +13,42 @@ public class Task4 {
     }
 
     static String solveQuadraticEquation(double a, double b, double c) {
-        int setRoots;
-        int x1, x2 = 0;
-        String outputTail;
 
-        if (a == 0 && b == 0 && c != 0) return "Количество решений: 0.";
-        if (a == 0 && b == 0 && c == 0) return "Решений бесконечно";
+        if (a == 0 && b == 0 && c != 0) {
+            return "Количество решений: 0.";
+        }
 
-        if (a == 0 && b != 0) {
-            setRoots = 1;
-            x1 = (int) ((c / b) * -1);
-            return "Количество решений: " + setRoots + ". Корень: " + x1;
+        if (a == 0 && b == 0) {
+            return "Решений бесконечно";
+        }
+
+        double x1, x2;
+
+        if (a == 0) {
+            x1 = ((c / b) * -1);
+            return "Количество решений:  1. Корень: " + x1;
         }
 
         if (b == 0 && c == 0) {
-            setRoots = 1;
-            x1 = 0;
-            return "Количество решений: " + setRoots + ". Корень: " + x1;
+            return "Количество решений:  1. Корень: 0";
         }
 
         //Количество корней и решений
         double discriminant = b * b - 4 * a * c;
+
         if (discriminant == 0) {
-            setRoots = 1;
-            x1 = (int) ((-1 * b) / (2 * a));
-            outputTail = " Корень: " + x1;
-        } else if (discriminant > 0) {
-            setRoots = 2;
-            outputTail = " Корни: ";
-            x1 = (int) ((-1 * b - Math.sqrt(discriminant)) / (2 * a));
-            x2 = (int) ((-1 * b + Math.sqrt(discriminant)) / (2 * a));
+            x1 = ((-1 * b) / (2 * a));
+            return " Корень: " + x1;
+        }
 
-        } else return "Количество решений: 0.";
+        if (discriminant > 0) {
+            x1 = ((-1 * b - Math.sqrt(discriminant)) / (2 * a));
+            x2 = ((-1 * b + Math.sqrt(discriminant)) / (2 * a));
 
-        String result = "Количество решений: " + setRoots + "." + outputTail + x1 + ";" + x2;
+        } else {
+            return "Количество решений: 0.";
+        }
 
-        return result;
+        return "Количество решений: 2. Корни: " + x1 + ";" + x2;
     }
 }
