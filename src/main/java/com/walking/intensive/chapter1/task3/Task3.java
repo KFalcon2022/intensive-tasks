d419baf97f0b0d27761f0d379007ffe034c5c6f2
DevTorchez <torchez@yandex.ru>
2023-12-19 12:31:36 +0300

    re:Resolve Chapter1.Task3 | Исправления с учетом замечаний по коду.

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -11,9 +11,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-
-        if (year % 400 == 0) {
-            return true;
-        } else return year % 100 != 0 && year % 4 == 0;
+        return year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
     }
 }
