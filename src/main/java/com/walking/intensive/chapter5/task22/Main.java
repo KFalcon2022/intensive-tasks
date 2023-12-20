9d15def289758fbc4ecf53370e47dc16a8de779c
user <tasiqq@gmail.com>
2023-12-20 19:57:22 +0300

    Chapter 5 Task 22

--- src/main/java/com/walking/intensive/chapter5/task22/Main.java
+++ src/main/java/com/walking/intensive/chapter5/task22/Main.java
@@ -1,7 +1,45 @@
 package com.walking.intensive.chapter5.task22;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File input = new File("input.txt");
+        Scanner scanner = new Scanner(input);
+        ArrayList<String> wordsList = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            String string;
+            string = String.valueOf(scanner.nextLine()).replaceAll(("\\pP"), "").toLowerCase();
+            String[] words = string.split(" ");
+            Collections.addAll(wordsList, words);
+        }
+        scanner.close();
+
+        Map<String, Integer> wordFrequency = wordsList.stream().collect(Collectors.toMap(e -> e, e -> 1, Integer::sum));
+
+        int maxFreqeuncy = Collections.max(wordFrequency.values());
+        Collection<String> collection = wordFrequency.keySet();
+        String theWord = "";
+
+        for (String key : collection
+        ) {
+            int frequency = wordFrequency.get(key);
+            if (frequency == maxFreqeuncy) {
+                theWord = key;
+                break;
+            }
+        }
+
+        File output = new File("output.txt");
+        PrintWriter printWriter = new PrintWriter(output);
+
+        printWriter.println(theWord + " " + maxFreqeuncy);
+        printWriter.close();
     }
 }
