46d04919a62eebe4a609325819fe9a3e396facca
Rust <madishkin2@gmail.com>
2023-12-20 11:06:49 +0300

    Chapter 3 Task 13

--- src/main/java/com/walking/intensive/chapter3/task13/Task13.java
+++ src/main/java/com/walking/intensive/chapter3/task13/Task13.java
@@ -5,6 +5,7 @@ package com.walking.intensive.chapter3.task13;
  */
 public class Task13 {
     public static void main(String[] args) {
+        System.out.println(getCountSteps(new int[]{7, 7, 7, 7, 7, 7, 7}, 8));
 
     }
 
@@ -16,7 +17,23 @@ public class Task13 {
      * @return количество шагов необходимое для полива всех растений
      */
     static int getCountSteps(int[] plants, int wateringCanVolume) {
-        // Ваш код
-        return 0;
+        int counterSteps = 0;
+        int canWaterRemaining = wateringCanVolume;
+
+        for (int i = 0; i < plants.length; i++) {
+
+            canWaterRemaining = canWaterRemaining - plants[i];
+            counterSteps++;
+
+            if (i == (plants.length - 1)) {
+                break;
+            }
+
+            if (canWaterRemaining < plants[i + 1]) {
+                canWaterRemaining = wateringCanVolume;
+                counterSteps = counterSteps + ((i + 1) * 2);
+            }
+        }
+        return counterSteps;
     }
 }
