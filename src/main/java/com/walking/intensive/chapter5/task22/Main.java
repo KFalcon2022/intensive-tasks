e3c34d6fb2b924d6c2b6229cb2196e2db6c185cb
lambo064 <tasha_lambo@mail.ru>
2023-12-20 16:25:41 +0300

    chapter 4 task 22

--- src/main/java/com/walking/intensive/chapter5/task22/Main.java
+++ src/main/java/com/walking/intensive/chapter5/task22/Main.java
@@ -4,15 +4,26 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        String song = "";
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Патиссон\\intensive-tasks\\src\\main\\java\\com\\walking\\intensive\\chapter5\\task22\\input.txt"))) {
             String line = reader.readLine();
+
             while (line != null) {
-                System.out.println(line);
+                song += line + " ";
                 line = reader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        String longestWord= findLongestWord(splitSong(song));
+        String resultString = longestWord + " is written " + countWord(splitSong(song),longestWord)+ " times";
+        try {
+            FileWriter writer = new FileWriter("C:\\Users\\Патиссон\\intensive-tasks\\src\\main\\java\\com\\walking\\intensive\\chapter5\\task22\\output.txt");
+            writer.write(resultString);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
 
     }
