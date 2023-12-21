6e3df6eeb071c3d004deefe0dafb90fc49c2d87d
Boris <aksenov.boris8595@gmeil.com>
2023-12-21 21:51:02 +0500

    Version 2.0

--- src/main/java/com/walking/intensive/chapter5/task21/Parallelepiped.java
+++ src/main/java/com/walking/intensive/chapter5/task21/Parallelepiped.java
@@ -9,12 +9,12 @@ public class Parallelepiped {
     private final int z2;
 
     public Parallelepiped(int x1, int y1, int z1, int x2, int y2, int z2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.z1 = z1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.z2 = z2;
+        this.x1 = Math.min(x1, x2);
+        this.y1 = Math.min(y1, y2);
+        this.z1 = Math.min(z1, z2);
+        this.x2 = Math.max(x1, x2);
+        this.y2 = Math.max(y1, y2);
+        this.z2 = Math.max(z1, z2);
     }
 
     public int getX1() {
