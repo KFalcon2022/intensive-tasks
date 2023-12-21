16d8caf76d8f0933828e01e82fed534a647cd674
Boris <aksenov.boris8595@gmeil.com>
2023-12-21 23:41:38 +0500

    Version 2.0 Выход из сортировки, если за 1 проход ничего не переставили.

--- src/main/java/com/walking/intensive/chapter4/task17/Task17.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Task17.java
@@ -13,12 +13,17 @@ public class Task17 {
 
     private static int[] sortByBubble(int[] array) {
         for (int i = array.length - 1; i >= 0; i--) {
+            boolean isSort = false;
             for (int j = 0; j <= i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int bubble = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = bubble;
+                    isSort = true;
                 }
+            }
+            if(!isSort) {
+                return array;
             }
         }
         return array;
