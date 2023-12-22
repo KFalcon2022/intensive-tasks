37a7ddd52ff1d5cf426e477a1615c05416c4e352
DevTorchez <torchez@yandex.ru>
2023-12-22 17:04:41 +0300

    Resolve Chapter2.Task10 Исправленному верить.

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -8,21 +8,26 @@ import java.text.StringCharacterIterator;
  */
 public class Task10 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум"));
+        //System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум"));
+        System.out.println(isPalindrome("a!!!!..."));
     }
 
     static boolean isPalindrome(String inputString) {
 
-        if (inputString.length() < 2){
+        int charChecker = 0;
+        for (int i = 0; i < inputString.length() - 1; i++){
+            if (Character.isLetter(inputString.charAt(i))){
+                charChecker++;
+            }
+        }
+
+        if (inputString.length() < 2 || charChecker <= 1){
             return false;
         }
 
-        //Многоэтажное получилось решение. Ниже есть решение традиционным методом.
         CharacterIterator forward = new StringCharacterIterator(inputString);
         CharacterIterator backward = new StringCharacterIterator(inputString);
 
-        //Не уверен насчет верности выбранного способа инициализации, но (в отличие от присвоения 0) выглядит
-        //понятнее, хотя и «масло масляное».
         char c1 = forward.first();
         char c2 = backward.last();
 
