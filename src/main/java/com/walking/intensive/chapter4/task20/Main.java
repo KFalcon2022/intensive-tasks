e9eabcff8bb94497c36155d8f946169f36b72589
user <tasiqq@gmail.com>
2023-12-22 15:10:04 +0300

    Chapter 5 Task 21:
    - код перенесен из Task20 перенесен в Task21;
    - исправлена опечатка в методе isIntersectedAxisZ, где было обращение к MaxX параллелепипеда.
    
    Копипаст зло.

--- src/main/java/com/walking/intensive/chapter4/task20/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task20/Main.java
@@ -1,18 +1,7 @@
 package com.walking.intensive.chapter4.task20;
 
-import com.walking.intensive.chapter5.task21.Parallelepiped;
-import com.walking.intensive.chapter5.task21.Point;
-import com.walking.intensive.chapter5.task21.Sphere;
-
 public class Main {
     public static void main(String[] args) {
 
-        Point point1 = new Point(-4, -4, -1);
-        Point point2 = new Point(4, 4, 1);
-        Sphere sphere = new Sphere(0, 0, 2, 1);
-
-        Parallelepiped parallelepiped = new Parallelepiped(point1, point2);
-
-        System.out.println(sphere.isIntersected(parallelepiped));
     }
 }
--- src/main/java/com/walking/intensive/chapter5/task21/Main.java
+++ src/main/java/com/walking/intensive/chapter5/task21/Main.java
@@ -3,5 +3,12 @@ package com.walking.intensive.chapter5.task21;
 public class Main {
     public static void main(String[] args) {
 
+        Point point1 = new Point(-4, -4, -1);
+        Point point2 = new Point(4, 4, 1);
+        Sphere sphere = new Sphere(0, 0, 2, 1);
+
+        Parallelepiped parallelepiped = new Parallelepiped(point1, point2);
+
+        System.out.println(sphere.isIntersected(parallelepiped));
     }
 }
