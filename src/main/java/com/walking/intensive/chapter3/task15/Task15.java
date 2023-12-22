07190145d66b3a0a4904341db4ad2ba0a7415c4d
Rust <madishkin2@gmail.com>
2023-12-22 20:27:20 +0300

    Chapter3 Task15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -5,11 +5,39 @@ package com.walking.intensive.chapter3.task15;
  */
 public class Task15 {
     public static void main(String[] args) {
+        int[][] city = new int[][]{{3, 0, 8, 4},
+                                    {2, 4, 5, 7},
+                                    {9, 2, 6, 3},
+                                    {0, 3, 1, 0}};
+        System.out.println(getMaxFloors(city));
 
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+        int flatCounters = 0;
+
+        for (int[] ints : city) {
+            int rowMax = 0;
+            int limitHeight;
+
+            for (int anInt : ints) {
+                if (anInt > rowMax) {
+                    rowMax = anInt;
+                }
+            }
+
+            for (int j = 0; j < ints.length; j++) {
+                int columnMax = 0;
+
+                for (int[] value : city) {
+                    if (value[j] > columnMax) {
+                        columnMax = value[j];
+                    }
+                }
+                limitHeight = Math.min(rowMax, columnMax);
+                flatCounters = flatCounters + (limitHeight - ints[j]);
+            }
+        }
+        return flatCounters;
     }
 }
