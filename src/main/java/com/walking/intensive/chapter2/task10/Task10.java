d73721663e8cd89c79ea628cae89cd5ad2ce6b48
DevTorchez <torchez@yandex.ru>
2023-12-22 14:58:21 +0300

    Resolve Chapter2.Task10 Двумя способами

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,5 +1,8 @@
 package com.walking.intensive.chapter2.task10;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-palindrome.html">ссылка</a>
  */
@@ -10,28 +13,60 @@ public class Task10 {
 
     static boolean isPalindrome(String inputString) {
 
-        String temporaryString = "";
-        for (int i = 0; i < inputString.length(); i++) {
-
-            if (Character.isLetterOrDigit(inputString.charAt(i))) {
-                temporaryString += inputString.toLowerCase().charAt(i);
-            }
-        }
-
-        //Прошу прощения за скобки, задания отправлялись разом и многие ошибки тоже.
-        if (temporaryString.length() < 2) {
+        if (inputString.length() < 2){
             return false;
         }
-        int start = 0;
-        int end = temporaryString.length() - 1;
 
-        while (start < end) {
-            char stepFwd = temporaryString.charAt(start++);
-            char stepBack = temporaryString.charAt(end--);
+        //Многоэтажное получилось решение. Ниже есть решение традиционным методом.
+        CharacterIterator forward = new StringCharacterIterator(inputString);
+        CharacterIterator backward = new StringCharacterIterator(inputString);
 
-            return stepFwd == stepBack;
+        //Не уверен насчет верности выбранного способа инициализации, но (в отличие от присвоения 0) выглядит
+        //понятнее, хотя и «масло масляное».
+        char c1 = forward.first();
+        char c2 = backward.last();
+
+        while (forward.getIndex() < backward.getIndex()) {
+
+            while (!Character.isLetter(c1)) {
+                c1 = forward.next();
+            }
+
+            while (!Character.isLetter(c2)) {
+                c2 = backward.previous();
+            }
+
+            if (Character.toLowerCase(c1) != Character.toLowerCase(c2)) {
+                return false;
+            }
+            c1 = forward.next();
+            c2 = backward.previous();
         }
 
-        return false;
+        return true;
+
+/*        //Классический подход
+        int start = 0;
+        int end = inputString.length() - 1;
+
+        while (start < end) {
+
+            while (!Character.isLetter(inputString.charAt(start))) {
+                start++;
+            }
+
+            while (!Character.isLetter(inputString.charAt(end))) {
+                end--;
+            }
+
+            if (Character.toLowerCase(inputString.charAt(start)) != Character.toLowerCase(inputString.charAt(end))) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+        return true;
+        */
     }
 }
