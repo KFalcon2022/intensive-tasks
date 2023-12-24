4e021d2b358654b82c8eac7d2c44f0e698d55748
njmist <njmist@gmail.com>
2023-12-24 15:21:27 +0300

    chapter 5 task 22

--- src/main/java/com/walking/intensive/chapter5/task22/Main.java
+++ src/main/java/com/walking/intensive/chapter5/task22/Main.java
@@ -1,7 +1,68 @@
 package com.walking.intensive.chapter5.task22;
 
-public class Main {
-    public static void main(String[] args) {
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Metallica metallica = new Metallica(); // Здесь хранится песня
+        Path input = (Paths.get(".\\src\\main\\java\\com\\walking\\intensive\\chapter5\\task22\\input.txt")); // Путь, где будет лежать input.txt
+
+        if (Files.notExists(input)) { // Если файла нет, то создаем
+            Files.createFile(input);
+        }
+
+        try {
+            Files.write(input, metallica.getNothingElseMatters().getBytes()); // Записываем песню в файл
+        } catch (Exception e) {
+            System.out.println("Oops");
+        }
+
+        List<String> songWords = new ArrayList<>(); // Создаем массив для слов из песни
+
+        try (BufferedReader reader = Files.newBufferedReader(input)) { // Считываем слова по отдельности, сразу приводя к нижнему регистру и удаляя знаки препинания
+            while (reader.ready()) {
+                songWords.addAll(Arrays.asList(reader.readLine().toLowerCase().replaceAll("[,.]", "").split("\s+")));
+            }
+        }
+
+        HashMap<String, Integer> wordCount = new HashMap<>(); // Создадим ассоциативный массив, где слова будут уникальными ключами, а значения - сколько раз встретилось слово в песне
+
+        int maxValue = Integer.MIN_VALUE;
+        String key = null;
+
+        for (String word : songWords) {
+            if (!wordCount.containsKey(word)) { // Если такого слова нет
+                wordCount.put(word, 1); // то кладем его в мапу и записываем, что встретилось 1 раз
+            } else {
+                wordCount.put(word, 1 + wordCount.get(word)); // Если такое слово есть, то добавляем количество его встреч
+            }
+            if (wordCount.get(word) >= maxValue) { // По пути определяем победителя, если победителей несколько, то выпишем последнего
+                maxValue = wordCount.get(word);
+                key = word;
+            }
+        }
+
+        Path output = (Paths.get(".\\src\\main\\java\\com\\walking\\intensive\\chapter5\\task22\\output.txt")); // Путь, где будет лежать output.txt
+
+        if (Files.notExists(output)) { // Если файла нет, то создаем
+            Files.createFile(output);
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(output.toString()))) { // Записываем в файл результат
+            writer.write(key + " " + maxValue);
+        }
     }
 }
---
+++ src/main/java/com/walking/intensive/chapter5/task22/Metallica.java
@@ -0,0 +1,48 @@
+package com.walking.intensive.chapter5.task22;
+
+public class Metallica {
+    private String nothingElseMatters = "So close, no matter how far\n" +
+            "Couldn't be much more from the heart\n" +
+            "Forever trusting who we are\n" +
+            "And nothing else matters\n" +
+            "Never opened myself this way\n" +
+            "Life is ours, we live it our way\n" +
+            "All these words, I don't just say\n" +
+            "And nothing else matters\n" +
+            "Trust I seek and I find in you\n" +
+            "Every day for us something new\n" +
+            "Open mind for a different view\n" +
+            "And nothing else matters\n" +
+            "Never cared for what they do\n" +
+            "Never cared for what they know\n" +
+            "But I know\n" +
+            "So close, no matter how far\n" +
+            "It couldn't be much more from the heart\n" +
+            "Forever trusting who we are\n" +
+            "And nothing else matters\n" +
+            "Never cared for what they do\n" +
+            "Never cared for what they know\n" +
+            "But I know\n" +
+            "I never opened myself this way\n" +
+            "Life is ours, we live it our way\n" +
+            "All these words, I don't just say\n" +
+            "And nothing else matters\n" +
+            "Trust I seek and I find in you\n" +
+            "Every day for us something new\n" +
+            "Open mind for a different view\n" +
+            "And nothing else matters\n" +
+            "Never cared for what they say\n" +
+            "Never cared for games they play\n" +
+            "Never cared for what they do\n" +
+            "Never cared for what they know\n" +
+            "And I know, yeah, yeah\n" +
+            "So close, no matter how far\n" +
+            "Couldn't be much more from the heart\n" +
+            "Forever trusting who we are\n" +
+            "No, nothing else matters";
+
+    public String getNothingElseMatters() {
+        return nothingElseMatters;
+    }
+}
+
