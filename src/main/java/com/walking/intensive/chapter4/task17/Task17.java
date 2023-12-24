71a066dc0dca96e4c94bff10491f565e745393b8
ArtemiyDS <27071999@mail.ru>
2023-12-24 20:13:55 +0300

    Chapter 4 Task 17

--- src/main/java/com/walking/intensive/chapter4/task17/Task17.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Task17.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter4.task17;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Bubble-Sort.html">Сортировка пузырьком</a>
  */
@@ -7,22 +9,23 @@ public class Task17 {
     public static void main(String[] args) {
         int[] array = {1, 3, 4, 1, -1, 0, 18, -1};
 
-        int[] result = sortByBubble(array);
-        for (int num : result) {
-            System.out.print(num + " ");
-        }
+        System.out.println(Arrays.toString(sortByBubble(array)));
     }
 
     private static int[] sortByBubble(int[] array) {
-        int tmp;
 
         for (int i = array.length; i > 1; i--) {
+            boolean isSwitchNums = false;     // если хотя бы 1 раз произошла перестановка чисел, то true
             for (int j = 1; j < i; j++) {
                 if (array[j] < array[j - 1]) {
-                    tmp = array[j];
+                    int tmp = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = tmp;
+                    isSwitchNums = true;
                 }
+            }
+            if (!isSwitchNums){
+                break;
             }
         }
 
