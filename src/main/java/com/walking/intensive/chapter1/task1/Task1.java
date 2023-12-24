ac1015f2af1b048ab0ae43e11b7ce4c0c4bef92d
Nicholas <voskreshenie888@gmail.com>
2023-12-24 22:57:06 +0300

    Update Chapter1 Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -5,25 +5,23 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-        int age = 3;
+        int age = 15;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-
         int ageLastNumber = age % 10;
-        boolean exclusion = (age % 100 >= 11) && (age % 100 <= 14);
-        String old = "";
-        if (ageLastNumber == 1)
-            old = " год";
-        else if (ageLastNumber == 0 || ageLastNumber >= 5 && ageLastNumber <= 9)
-            old = " лет";
-        else if (ageLastNumber >= 2 && ageLastNumber <= 4)
-            old = " года";
-        if (exclusion)
-            old = " лет";
-        return ("Вам "+ age + old);
+        if ((age % 100 >= 11) && (age % 100 <= 14)) {
+            return "Вам " + age + " лет";
+        }
+        if (ageLastNumber == 1) {
+            return "Вам " + age + " год";
+        }
+        if (ageLastNumber == 0 || ageLastNumber >= 5) {
+            return "Вам " + age + " лет";
+        }
+        return "Вам " + age + " года";
     }
 }
 
