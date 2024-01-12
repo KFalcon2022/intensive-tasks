59b980e182faf931c250c789e8425f9c95ae004c
Rust <madishkin2@gmail.com>
2024-01-12 14:52:32 +0300

    Chapter4 Task19

--- src/main/java/com/walking/intensive/chapter4/task19/Task19.java
+++ src/main/java/com/walking/intensive/chapter4/task19/Task19.java
@@ -7,11 +7,17 @@ import java.util.Arrays;
  */
 public class Task19 {
     public static void main(String[] args) {
-        int[] array = {10, 1, -1, 3, 2, 5, 8, -45, 59, -56, 26, 73, 89};
-        System.out.println(Arrays.toString(sortByQuicksort(array)));
+        //int[] array = {10, 1, -1, 3, 2, 5, 8, -45, 59, -56, 26, 73, 89};
+        int[] array2 = {};
+        System.out.println(array2.length);
+        System.out.println(Arrays.toString(sortByQuicksort(array2)));
     }
 
     public static int[] sortByQuicksort(int[] unsortedArray) {
+        if (unsortedArray.length == 0) {
+            System.out.println("The Array is empty! Nothing to sort.");
+            return unsortedArray;
+        }
 
         quickSort(unsortedArray, 0, unsortedArray.length - 1);
 
@@ -27,25 +33,23 @@ public class Task19 {
         if ((right - left) == 1) {
             if (array[left] > array[right]) {
                 swap(array, left, right);
-                return;
             }
             return;
         }
 
-        int divideIndex = partition(array, left, right);
+        int divideIndex = getPartition(array, left, right);
 
         quickSort(array, left, divideIndex - 1);
         quickSort(array, divideIndex, right);
     }
 
 
-    public static int partition(int[] array, int from, int to) {
+    public static int getPartition(int[] array, int from, int to) {
         int leftIndex = from;
         int rightIndex = to;
         int pivot = getPivot(array, leftIndex, rightIndex);
 
         while (leftIndex < rightIndex) {
-
             while (array[leftIndex] < pivot) {
                 leftIndex++;
             }
@@ -60,6 +64,7 @@ public class Task19 {
                 rightIndex--;
             }
         }
+
         return leftIndex;
     }
 
@@ -77,6 +82,7 @@ public class Task19 {
             tmpMax = Math.max(tmpMax, array[i]);
             tmpMin = Math.min(tmpMin, array[i]);
         }
+
         return (tmpMax + tmpMin) / 2;
     }
 }
