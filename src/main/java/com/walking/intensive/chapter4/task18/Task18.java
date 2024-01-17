34ed221066003a29de841920569cc1546eb6cc44
RVishez <oliohcmpn@gmail.com>
2024-01-17 11:33:27 +0100

    Chapter 4 task 18 complete

--- src/main/java/com/walking/intensive/chapter4/task18/Task18.java
+++ src/main/java/com/walking/intensive/chapter4/task18/Task18.java
@@ -1,15 +1,50 @@
 package com.walking.intensive.chapter4.task18;
 
+import java.util.Arrays;
+
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Shaker-Sort.html">Сортировка шейкером</a>
  */
 public class Task18 {
     public static void main(String[] args) {
+        int[] array = {-1, 4, 56, 3, 56, 87, 3, 0, 0, 3, -56, Integer.MAX_VALUE, 2};
+        int[] array2 = {-1, 4, 56, 3};
+        int[] array3 = {-1, 56, 4};
 
+        System.out.println(Arrays.toString(sortByShaker(array)));
+        System.out.println(Arrays.toString(sortByShaker(array2)));
+        System.out.println(Arrays.toString(sortByShaker(array3)));
     }
 
     private static int[] sortByShaker(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            boolean isArrayChanged = false;
 
-        return new int[]{};
+            for (int j = i; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int buffer = array[j];
+
+                    array[j] = array[j + 1];
+                    array[j + 1] = buffer;
+                    isArrayChanged = true;
+                }
+            }
+
+            for (int k = array.length - i - 2; k > i; k--) {
+                if (array[k] < array[k - 1]) {
+                    int buffer = array[k];
+
+                    array[k] = array[k - 1];
+                    array[k - 1] = buffer;
+                    isArrayChanged = true;
+                }
+            }
+
+            if (!isArrayChanged) {
+                return array;
+            }
+        }
+
+        return array;
     }
 }
