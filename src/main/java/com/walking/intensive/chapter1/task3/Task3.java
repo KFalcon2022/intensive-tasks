92af15382edd8add3b08613ea53363a638a75335
Максим <gmax7911@gmail.com>
2024-02-06 21:12:41 +0300

    Решена 3 задача

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -5,7 +5,7 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println(isLeap(2090));
+        System.out.println(isLeap(2096));
     }
 
     static boolean isLeap(int year) {
