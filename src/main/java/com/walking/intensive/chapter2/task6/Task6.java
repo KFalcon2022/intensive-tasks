bd8ad918befb83489c0ac712618a5806ad09ed66
lion <li_on1@mail.ru>
2024-03-03 20:41:23 +0300

    Task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -5,22 +5,52 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
+        final int a = 36,
+                b = 28;
 
+        System.out.println("НОД: " + getNod(a, b) +
+                "\nНОК: " + getNoc(a, b) +
+                "\nНОД по алгоритму Евклида: " + getNodByEuclideanAlgorithm(a, b));
     }
 
     static int getNoc(int m, int n) {
-        // Ваш код
+        int noc;
+
+        for (int i = 1; i <= n; i++) {
+            noc = i * m;
+
+            if (noc % n == 0) {
+                return noc;
+            }
+        }
+
         return 0;
     }
 
     static int getNod(int m, int n) {
-        // Ваш код
-        return 0;
+        int ceiling = Math.min(m, n),
+                nod = 0;
+
+        for (int i = 1; i <= ceiling; i++) {
+            if (m % i == 0 && n % i == 0) {
+                nod = i;
+            }
+        }
+
+        return nod;
     }
 
-    static int getNodByEuclideanAlgorithm(int m, int n){
-        // Ваш код
-        return 0;
-    }
+    static int getNodByEuclideanAlgorithm(int m, int n) {
+        int ceiling = Math.max(m, n),
+                floor = Math.min(m, n),
+                remainder = ceiling % floor;
 
+        if (remainder == 0) {
+            return floor;
+        } else {
+            ceiling = remainder;
+        }
+
+        return getNodByEuclideanAlgorithm(floor, ceiling); //рекурсия
+    }
 }
