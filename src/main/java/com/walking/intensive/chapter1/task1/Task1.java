24a5211bf3215f5f5148e32fecdb85cb444849d6
lion <li_on1@mail.ru>
2024-03-03 12:37:12 +0300

    Task1 by Lev

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,19 +1,31 @@
 package com.walking.intensive.chapter1.task1;
+import java.util.*;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
+        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Введите число лет (только целые значения): ");
+        int age = sc.nextInt();
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        String postfix = "";
+        if (age%10 == 1) {
+            postfix = " год";
+        }
+        else if(age%10 == 0 || age%10 >= 5 && age%10 <= 9) {
+            postfix = " лет";
+        }
+        else if(age%10 >= 2 && age%10 <= 4) {
+            postfix = " года";
+        }
+        else {
+            return "Ошибка обработки";
+        }
+        return "Вам " + age + postfix;
     }
 }
