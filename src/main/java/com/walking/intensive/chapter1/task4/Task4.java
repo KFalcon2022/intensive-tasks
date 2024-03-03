733652ee03dc677ffa7e4c451d6d9b9d099ea7da
lion <li_on1@mail.ru>
2024-03-03 17:16:02 +0300

    Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -15,24 +15,28 @@ public class Task4 {
         if (a == 0 && b == 0 && c == 0) {
             return "Решений бесконечно";
         }
+
         if (a == 0 && b == 0) {
             return "Количество решений: 0.";
         }
+
         if (a == 0) {
             var x = -c / b;
             return "Количество решений: 1. Корень: " + (int)x;
         }
+
         var discriminant = Math.pow(b, 2) - 4 * a * c;
         if (discriminant < 0) {
             return "Количество решений: 0.";
         }
+
         if (discriminant > 0) {
             var x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
             var x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
             return "Количество решений: 2. Корни: " + (int)Math.min(x1, x2) + ";" + (int)Math.max(x1, x2);
-        } else {
-            var x = -b / (2 * a);
-            return "Количество решений: 1. Корень: " + (int)x;
         }
+
+        var x = -b / (2 * a);
+        return "Количество решений: 1. Корень: " + (int)x;
     }
 }
