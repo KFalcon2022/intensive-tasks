d1480069306cfd053d9f34c922132d1a6882fa78
lion <li_on1@mail.ru>
2024-03-03 14:37:51 +0300

    Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -1,17 +1,20 @@
 package com.walking.intensive.chapter1.task3;
+import java.util.*;
 
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-leap-year.html">ссылка</a>
  */
 public class Task3 {
     public static void main(String[] args) {
-        //Для собственных проверок можете делать любые изменения в этом методе
-
+        Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Введите год (только целые значения): ");
+        int year = sc.nextInt();
+        System.out.println((isLeap(year) ? "Год " + year + " - високосный" : "Год " + year + " - не високосный"));
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        return cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365;
     }
 }
