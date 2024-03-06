884a813ed7d2b2db58bd82e2ade5843011b870e2
lion <li_on1@mail.ru>
2024-03-06 19:37:57 +0300

    Task7

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -5,11 +5,46 @@ package com.walking.intensive.chapter2.task7;
  */
 public class Task7 {
     public static void main(String[] args) {
-
+        System.out.println(getFriendlyPair(10000));
     }
 
-    static int getFriendlyPair(int m){
-        // Ваш код
-        return 0;
+    static int getFriendlyPair(int m) {
+        int numOne = 0,
+                numTwo = -1;
+
+        for (int i = 200; i <= m; i++) {
+            if (getSum(getSum(i)) == i && getSum(i) != i) {
+                if (i + getSum(i) > 1000000) {
+                    break;
+                } else {
+                    numOne = i;
+                    numTwo = getSum(i);
+                }
+            }
+        }
+
+        if (numOne == 0 && numTwo == -1) {
+            System.out.println("Дружественных чисел на интервале от 1 до " + m + " нет.");
+
+            return 0;
+        }
+
+        return Math.max(numOne, numTwo);
+    }
+    static int getSum(int num) {
+        int counter = 1;
+
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                counter += i;
+                counter += num / i;
+            }
+        }
+
+        if (Math.sqrt(num) % 1 == 0) {
+            counter += (int)Math.round(Math.sqrt(num));
+        }
+
+        return counter;
     }
 }
