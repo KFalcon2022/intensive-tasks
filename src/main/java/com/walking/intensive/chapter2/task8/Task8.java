47a8f4e594ac581d5b571021ea2ceeecd5a3c04e
lion <li_on1@mail.ru>
2024-03-06 21:44:06 +0300

    Task8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -5,11 +5,35 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-
+        System.out.println("Вероятность поесть: " + getHappyTicketChance());
     }
 
     static double getHappyTicketChance() {
-        // Ваш код
-        return 0.0;
+        String ticket;
+        int sum = 0;
+        final int ticketsNumber = 999999;
+
+        for (int i = 1001; i < ticketsNumber; i++) {
+            ticket = String.valueOf(i);
+
+            int len = ticket.length();
+
+            if (len < 6) {
+                String zeroes = "00";
+                ticket = zeroes.substring(0, 6 - len) + ticket;
+            }
+
+            if (happy(ticket)) {
+                sum++;
+                System.out.println(ticket);
+            }
+        }
+
+        return (double) sum / ticketsNumber;
+    }
+
+    static boolean happy(String ticket) {
+        char[] ticketCharArray = ticket.toCharArray();
+        return (ticketCharArray[0] + ticketCharArray[1] + ticketCharArray[2]) == (ticketCharArray[3] + ticketCharArray[4] + ticketCharArray[5]);
     }
 }
